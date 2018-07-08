e933b1e1bac59060b4fa83474bb3485ea99153c7
Oshan Mudannayake <oshan.ivantha@gmail.com>
2018-07-08 11:59:46 +0530

    Register a service worker

---
+++ sw.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef,no-console */
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.3.1/workbox-sw.js');
+
+// workbox.routing.registerRoute(
+// 	new RegExp('http://localhost:4000/.*'),
+// 	workbox.strategies.cacheFirst()
+// );
