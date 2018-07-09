9a88281a6307c2693ee68043ad3e88a1b11f39de
Oshan Mudannayake <oshan.ivantha@gmail.com>
2018-07-09 23:41:14 +0530

    Enable service worker with CacheFirst strategy

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.3.1/workbox-sw.js');
 
-// workbox.routing.registerRoute(
-// 	new RegExp('http://localhost:4000/.*'),
-// 	workbox.strategies.cacheFirst()
-// );
+workbox.routing.registerRoute(
+	new RegExp('http://localhost:4000/.*'),
+	workbox.strategies.cacheFirst()
+);
