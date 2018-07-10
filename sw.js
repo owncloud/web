4a1c1e9c975c4cf7f905f973d489e00f28a1190d
Oshan Mudannayake <oshan.ivantha@gmail.com>
2018-07-10 12:21:32 +0530

    Disable Workbox logs

--- sw.js
+++ sw.js
@@ -2,6 +2,8 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.3.1/workbox-sw.js');
 
+workbox.setConfig({debug: false});
+
 workbox.routing.registerRoute(
 	new RegExp('http://localhost:4000/.*'),
 	workbox.strategies.cacheFirst()
