6daf444f10ef24e61fef4b8e74834bdf148476b4
Thomas Müller <thomas.mueller@tmit.eu>
2018-10-11 17:24:05 +0200

    Switch to implicit grant

--- src/services/auth.js
+++ src/services/auth.js
@@ -5,17 +5,15 @@ const vueAuthInstance = new VueAuthenticate(Axios, {
   providers: {
     oauth2: {
       name: 'ownCloud',
-      clientId: 'oYrkjUIfkDv8kZsz5P5GSvRnn0Stk7U7lUL5cfKJd0re9gMp29qz7DHqvPfq1KBb',
-      clientSecret: 'G7oVHQZrMjCADrKMJtn0U2JXHx1NdMbPiWvEyV8ocRbNyafvoa5c4OKC7hDPJytR',
+      clientId: 'Cxfj9F9ZZWQbQZps1E1M0BszMz6OOFq3lxjSuc8Uh4HLEYb9KIfyRMmgY5ibXXrU',
       url: 'http://deepdiver:8080/index.php/apps/oauth2/api/v1/token',
       authorizationEndpoint: 'http://deepdiver:8080/index.php/apps/oauth2/authorize',
+      responseType: 'token',
       responseParams: {
         code: 'code',
         clientId: 'client_id',
-        redirectUri: 'redirect_uri',
-        grantType: 'grant_type'
+        redirectUri: 'redirect_uri'
       }
-
     }
   }
 })
