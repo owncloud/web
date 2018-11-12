7d509db27d7182cc965c1ebcece1203eacd3bbd3
Carsten Schildknecht <carsten.schildknecht@serv3r.de>
2018-11-12 16:47:18 +0100

    fixed material-icon path for production builds

--- webpack.common.js
+++ webpack.common.js
@@ -49,7 +49,7 @@ module.exports = {
 							loader: 'file-loader',
 							options: {
 									name: '[name].[ext]',
-									outputPath: '../fonts'
+									outputPath: './fonts'
 							}
 					}]
 			},
