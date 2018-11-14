a8e89d386ea264b9e4c1cbc7662d76d6fcc77516
Carsten Schildknecht <carsten.schildknecht@serv3r.de>
2018-11-14 15:10:17 +0100

    set default value vor v-btn, h2 and flex

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <v-layout class="background primary">
-      <v-flex xs12>
+      <v-flex class="pa-0" xs12>
         <v-img
         v-if="configuration.theme.logo.big"
         :src="configuration.theme.logo.big"
         :aspect-ratio="1"
         height="100vh">
-        <v-flex class="theme--light grey lighten-2 pa-4 ma-3 center-dialog" md4 xs8>
-          <h2 class="pa-2">
+        <v-flex class="grey lighten-2 pa-4 ma-auto elevation-5 center-dialog" md4 xs8>
+          <h2>
             <span v-translate>Welcome to</span> {{ configuration.theme.general.name }}
           </h2>
-          <v-flex class="pa-2" v-translate>
+          <v-flex v-translate>
             Please click the button below to authenticate with {{ configuration.theme.general.name }} and get access to your data.
           </v-flex>
-          <v-btn class="pa-2" color="primary" id="authenticate" @click="authenticate"><span v-translate>Authenticate</span></v-btn>
+          <v-btn color="primary" id="authenticate" @click="authenticate"><span v-translate>Authenticate</span></v-btn>
         </v-flex>
       </v-img>
     </v-flex>
@@ -52,13 +52,11 @@ export default {
 </script>
 
 <style scoped="true">
-  .background{
-    height: "100%";
+  .v-btn, h2, .flex{
+    padding: 8px 8px;
   }
   .center-dialog {
-    /* FIXME General center class */
-    margin-left: auto!important;
-    margin-right: auto!important;
+    /* FIXME: use justify-center and align-center */
     margin-top: 32vh!important;
   }
 </style>
