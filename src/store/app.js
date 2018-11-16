97360973406b51a3f85a39c00929ebf3a8c86b80
Carsten Schildknecht <carsten.schildknecht@serv3r.de>
2018-11-16 10:43:33 +0100

    added app-store, added getter for user

---
+++ src/store/app.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const state = {
+  sidebarVisible: false
+}
+
+const actions = {
+  toggleSidebar (context, visible) {
+    context.commit('TOGGLE_SIDEBAR' , visible)
+  }
+}
+
+const mutations = {
+  TOGGLE_SIDEBAR (state, visible) {
+    state.sidebarVisible = visible
+  }
+}
+
+const getters = {
+  isSidebarVisible: state => {
+    return state.sidebarVisible
+  }
+}
+
+export default {
+  state,
+  actions,
+  mutations,
+  getters
+}
