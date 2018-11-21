51a5fdc59ee869cdc577c26db581b5f2839bb3a5
Carsten Schildknecht <carsten.schildknecht@serv3r.de>
2018-11-21 13:48:11 +0100

    start working on file-details sidebar - build again & add fileactons-tab

--- apps/files/src/components/File-Details.vue
+++ apps/files/src/components/File-Details.vue
@@ -1,65 +1,39 @@
-<template lang="pug">
-	// material icon mixin
-	mixin icon(a)
-		i.material-icons(class=a.class)= a.icon
+<template>
+	<v-navigation-drawer
+      v-model="drawer"
+    >
+      <v-toolbar flat class="transparent">
+        <v-list class="pa-0">
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <img src="https://randomuser.me/api/portraits/men/85.jpg">
+            </v-list-tile-avatar>
 
-	#file-details.uk-position-relative
-		button(type="button", uk-close, @click="close").uk-position-top-right
-		section(v-if="file")
-			.uk-grid-small.uk-flex-middle(uk-grid)
-				.uk-width-auto
-					i.material-icons.-x2 {{ file.type }}
+            <v-list-tile-content>
+              <v-list-tile-title>John Leider</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
 
-				.uk-width-expand
-					h3.uk-card-title.uk-margin-remove-bottom.uk-text-truncate {{ file.name }}
-						span(uk-icon="icon: link", uk-tooltip, title="Copy local link", style="cursor:pointer").uk-margin-small-left
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
 
-					p.uk-margin-remove-top
-						span.uk-text-meta Size:&nbsp;
-						span.uk-text-small {{ file.size | fileSize }}
+        <v-list-tile
+          v-for="item in items"
+          :key="item.title"
+          @click=""
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
 
-			Buttonrow(:files="file")
-
-			ul(:uk-switcher="'connect:#'+switcherId", data-exp="file-details-panel-tabs").uk-child-width-expand.uk-tab
-				li.uk-active
-					a(href="") Info
-				li(v-for="(plugin, pid) in extendButtonRow", :key="pid")
-					a(href="") {{ plugin.title }}
-
-			ul.uk-switcher(data-exp="file-details-panel-contents", :id="switcherId")
-				li
-					.uk-overflow-hidden
-						table.uk-table.uk-table-small.uk-text-meta
-							tbody
-								tr
-									td Modified:
-									td.uk-table-expand {{ file.mdate | formDateTime }}
-								tr.uk-padding-bottom-small
-									td Created:
-									td.uk-table-expand {{ file.cdate | formDateTime }}
-								tr(v-for="(meta, key) in file.meta")
-									td {{ key | ucFirst }}:
-									td.uk-table-expand {{ meta }}
-				component(v-for="(plugin, pid) in extendButtonRow", :is="plugin.component", :key="pid", v-if="extendButtonRow.length > 0")
-
-		section(v-if="files")
-			.uk-grid-small.uk-flex-middle(uk-grid)
-				.uk-width-auto
-					i.material-icons.-x2 filter_{{ files.length }}
-
-				.uk-width-expand
-					h3.uk-card-title.uk-margin-remove-bottom.uk-text-truncate Multiple files
-						span(uk-icon="icon: link", uk-tooltip, title="Copy local link", style="cursor:pointer").uk-margin-small-left
-
-					p.uk-margin-remove-top
-						span.uk-text-meta Size:&nbsp;
-						span.uk-text-small {{ accumulatedFilesSize | fileSize }}
-
-			Buttonrow(:files="file")
-
-			hr.uk-hr
-			ul.uk-list.uk-list-bullet
-				li(v-for="item in files") {{ item.name }}
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
 </template>
 
 <script>
@@ -69,12 +43,18 @@ const _filter = require('lodash/filter');
 const _unique = require('lodash/uniqueId');
 
 import Mixins from '../mixins';
-import Buttonrow from './File-Details-Buttonrow.vue';
+//import Buttonrow from './File-Details-Buttonrow.vue';
 
 export default {
 	mixins: [Mixins],
+	data (){
+		return {
+			drawer: true,
+			items: []
+		}
+	},
 	components: {
-		Buttonrow
+		//Buttonrow
 	},
 	methods: {
 		close() {
@@ -114,16 +94,3 @@ export default {
 	}
 }
 </script>
-<style lang="less">
-	.material-icons {
-		user-select: none;
-
-		&.-x2 {
-			font-size: 48px;
-		}
-	}
-
-	.uk-button-small .material-icons {
-		font-size: 16px;
-	}
-</style>
