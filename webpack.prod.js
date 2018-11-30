7106cc9662675ddf3d87d67cc3861573d23f9c2c
Carsten Schildknecht <carsten.schildknecht@serv3r.de>
2018-11-30 15:37:28 +0100

    adjusted makefile release

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,26 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const WebpackCopyPlugin = require('copy-webpack-plugin')
 
 module.exports = merge(common, {
+  plugins: [
+    WebpackCopyPlugin([{
+                        from: 'README.md',
+                        to: './dist/'
+                      },{
+                        from: 'sw.js',
+                        to: './dist/'
+                      },{
+                        from: 'LICENSE',
+                        to: './dist/'
+                      },{
+                        from: 'CHANGELOG.md',
+                        to: './dist/'
+                      },{
+                        from: 'manifest.json',
+                        to: './dist/'
+                      }])
+  ],
 	mode: 'production',
 	devtool: 'none',
 	resolve: {
