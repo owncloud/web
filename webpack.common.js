a86921d7870362c4e66e0d2cd03f38af8b13db72
Thomas Müller <thomas.mueller@tmit.eu>
2018-12-07 09:01:15 +0100

    Adjust eslint + cleanup package.json

--- webpack.common.js
+++ webpack.common.js
@@ -50,6 +50,11 @@ module.exports = {
 								outputPath: 'fonts'
 						}
 				}]
+      }, {
+        enforce: 'pre',
+        test: /\.(js|vue)$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
 			},{
 				test: /\.vue$/,
 				loader: 'vue-loader',
@@ -65,11 +70,5 @@ module.exports = {
 				]
 			}
 		]
-	},
-	// some weird bug when loading js-owncloud-client, no idea... see https://github.com/webpack-contrib/css-loader/issues/447
-	node: {
-		fs: 'empty',
-		net: 'empty',
-		tls: 'empty'
 	}
 };
