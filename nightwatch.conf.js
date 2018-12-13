2080cfd20a58da8ec36f22069e30140f247a8777
Marlin Baumgart <marlin.baumgart@serv3r.de>
2018-12-13 01:47:32 +0100

    respect SERVER_HOST env var in acceptance-test

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,39 +1,40 @@
-const chromedriver = require('chromedriver');
+const chromedriver = require('chromedriver')
+const localLaunchUrl = process.env.SERVER_HOST || 'http://localhost:8300'
 
 module.exports = {
-	page_objects_path : './tests/acceptance/pageObjects',
-	test_settings: {
-		local: {
-			launch_url : 'http://localhost:8300',
-			webdriver: {
-				start_process: true,
-				server_path: chromedriver.path,
-				port: 4445,
-				cli_args: ['--port=4445']
-			},
-			desiredCapabilities: {
-				browserName: 'chrome',
-				javascriptEnabled: true,
-				acceptSslCerts: true,
-				chromeOptions: {
-					args: ['disable-gpu']
-				}
-			}
-		},
-		drone: {
-			launch_url : 'http://phoenix:8300',
-			selenium_host: 'selenium',
-			webdriver: {
-				start_process: false
-			},
-			desiredCapabilities: {
-				browserName: 'chrome',
-				javascriptEnabled: true,
-				acceptSslCerts: true,
-				chromeOptions: {
-					args: ['disable-gpu']
-				}
-			}
-		},
-	}
-};
+  page_objects_path: './tests/acceptance/pageObjects',
+  test_settings: {
+    local: {
+      launch_url: localLaunchUrl,
+      webdriver: {
+        start_process: true,
+        server_path: chromedriver.path,
+        port: 4445,
+        cli_args: ['--port=4445']
+      },
+      desiredCapabilities: {
+        browserName: 'chrome',
+        javascriptEnabled: true,
+        acceptSslCerts: true,
+        chromeOptions: {
+          args: ['disable-gpu']
+        }
+      }
+    },
+    drone: {
+      launch_url: 'http://phoenix:8300',
+      selenium_host: 'selenium',
+      webdriver: {
+        start_process: false
+      },
+      desiredCapabilities: {
+        browserName: 'chrome',
+        javascriptEnabled: true,
+        acceptSslCerts: true,
+        chromeOptions: {
+          args: ['disable-gpu']
+        }
+      }
+    }
+  }
+}
