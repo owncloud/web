d95a790d86880b7913412e510d1f1b6d04e3b6e0
Carsten Schildknecht <carsten.schildknecht@serv3r.de>
2019-01-24 16:24:17 +0100

    bugfix/missing-topbar

--- apps/markdown-editor/src/MarkdownEditor.vue
+++ apps/markdown-editor/src/MarkdownEditor.vue
@@ -1,29 +1,31 @@
 <template lang="html">
-<v-layout column>
-<v-layout row align-self-center>
-<h1 class="mt-2">
-{{ filePath }}
-</h1>
-<v-btn flat @click="saveContent(text)" :disabled="!isTouched">
-<v-icon large>save</v-icon>
-</v-btn>
-<v-btn flat @click="closeApp">
-<v-icon large>home</v-icon>
-</v-btn>
-</v-layout>
-<v-container align-content-center row>
-<v-layout row>
-<v-textarea
-name="input" box
-full-width :value="text"
-@input="onType" height="30vh">
-</v-textarea>
-<v-flex xs6>
-<div v-html="renderedMarkdown"></div>
-</v-flex>
-</v-layout>
-</v-container>
-</v-layout>
+  <v-content>
+    <v-layout column>
+      <v-layout row align-self-center>
+        <h1 class="mt-2">
+          {{ filePath }}
+        </h1>
+        <v-btn flat @click="saveContent(text)" :disabled="!isTouched">
+          <v-icon large>save</v-icon>
+        </v-btn>
+        <v-btn flat @click="closeApp">
+          <v-icon large>home</v-icon>
+        </v-btn>
+      </v-layout>
+      <v-container align-content-center row>
+        <v-layout row>
+          <v-textarea
+          name="input" box
+          full-width :value="text"
+          @input="onType" height="30vh">
+          </v-textarea>
+          <v-flex xs6>
+            <div v-html="renderedMarkdown"></div>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-layout>
+  </v-content>
 </template>
 <script>
 import marked from 'marked'
