655ac5b98dd5b321267c2f6baa1762e8001760f6
Artur Neumann <artur@jankaritech.com>
2019-03-13 14:32:13 +0545

    better debug output

--- tests/acceptance/setup-drone.js
+++ tests/acceptance/setup-drone.js
@@ -5,10 +5,10 @@ setDefaultTimeout(60000)
 
 Before(async () => {
   await createSession({ env: 'drone' })
-  console.log("Before")
+  console.log("Before scenario")
 })
 
 After(async () => {
   await closeSession()
-  console.log("After")
+  console.log("After scenario")
 })
--- tests/acceptance/setup-local.js
+++ tests/acceptance/setup-local.js
@@ -6,11 +6,11 @@ setDefaultTimeout(60000)
 Before(async () => {
   await startWebDriver({ env: 'local' })
   await createSession({ env: 'local' })
-  console.log("Before")
+  console.log("Before scenario")
 })
 
 After(async () => {
   await closeSession()
   await stopWebDriver()
-  console.log("After")
+  console.log("After scenario")
 })
