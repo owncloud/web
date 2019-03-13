97393fbc3194eafe69853053bda8a8ee34b1fc1b
Artur Neumann <artur@jankaritech.com>
2019-03-13 13:40:45 +0545

    close session after each step

--- tests/acceptance/setup-local.js
+++ tests/acceptance/setup-local.js
@@ -1,14 +1,14 @@
-import { setDefaultTimeout, AfterAll, BeforeAll } from 'cucumber'
+import { setDefaultTimeout, After, Before } from 'cucumber'
 import { createSession, closeSession, startWebDriver, stopWebDriver } from 'nightwatch-api'
 
 setDefaultTimeout(60000)
 
-BeforeAll(async () => {
+Before(async () => {
   await startWebDriver({ env: 'local' })
   await createSession({ env: 'local' })
 })
 
-AfterAll(async () => {
+After(async () => {
   await closeSession()
   await stopWebDriver()
 })
