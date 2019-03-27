781be6680be2128553099b5e3e4336ff9500faac
Tom Needham <tom@owncloud.com>
2019-03-27 20:52:11 +0000

    Rename to Avatar

--- src/components/Top-Bar.vue
+++ src/components/Top-Bar.vue
@@ -5,7 +5,7 @@
       <oc-img :src="configuration.theme.logo.big" style="height: 60px" />
     </oc-topbar-item>
     <oc-topbar-item slot="right">
-      <ocAvatar class="uk-margin-small-right" :userid="user.displayname" />
+      <Avatar class="uk-margin-small-right" :userid="user.displayname" />
       <span>{{ user.displayname }}</span>
     </oc-topbar-item>
   </oc-topbar>
@@ -14,7 +14,7 @@
 <script>
 import { mapGetters, mapState, mapActions } from 'vuex'
 import pluginHelper from '../mixins/pluginHelper.js'
-import OcAvatar from './OcAvatar.vue'
+import Avatar from './Avatar.vue'
 
 export default {
   data () {
@@ -29,7 +29,7 @@ export default {
   mixins: [
     pluginHelper
   ],
-  components: {OcAvatar},
+  components: { Avatar },
   methods: {
     ...mapActions(['toggleSidebar']),
     parseApp (app) {
