602996bb31481d964afd6bf33b5ce51242bda42d
Jörn Friedrich Dreyer <jfd@butonic.de>
2019-04-09 16:04:26 +0200

    add collaboration status
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- apps/files/src/components/FileSharingSidebar.vue
+++ apps/files/src/components/FileSharingSidebar.vue
@@ -1,11 +1,51 @@
 <template>
-    <div>
-      <p>file sharing sidebar</p>
+  <div>
+    <oc-collaboration-status :owner="owner" :collaborators="collaborators" :roles="roles" />
   </div>
 </template>
 
 <script>
+import OcCollaborationStatus from './OcCollaborationStatus.vue'
 export default {
-  name: 'FileSharingSidebar'
+  components: {
+    OcCollaborationStatus
+  },
+  name: 'FileSharingSidebar',
+  data () {
+    return {
+      owner: {
+        avatar: 'https://picsum.photos/64/64?image=1074',
+        name: 'simba1',
+        displayName: 'Simba',
+        email: 'simba@djungle.com'
+      },
+      collaborators: [
+        {
+          avatar: 'https://picsum.photos/64/64?image=1075',
+          name: 'foo',
+          displayName: 'Foo',
+          email: 'foo@djungle.com',
+          role: 'editor',
+          expires: 1554391618281
+        }, {
+          avatar: 'https://picsum.photos/64/64?image=1076',
+          name: 'bar',
+          displayName: 'Bar',
+          email: 'bar@djungle.com',
+          role: 'viewer',
+          expires: 1554491618281
+        }
+      ],
+      roles: {
+        viewer: { name: 'Viewer', description: 'Download and preview' },
+        editor: { name: 'Editor', description: 'Upload, edit, delete, download and preview' },
+        coowner: {
+          name: 'Co-owner',
+          description: 'Share, upload, edit, delete, download and preview'
+        },
+        hammer: { name: 'MC Hammer', description: 'U Can\'t touch this' }
+      }
+    }
+  }
 }
 </script>
