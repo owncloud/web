084a98ab093a80f8c07cd0ac65a2c2cc6f004a6c
Artur Neumann <artur@jankaritech.com>
2019-04-16 16:45:38 +0545

    init user when creating it

--- tests/acceptance/stepDefinitions/provisioningContext.js
+++ tests/acceptance/stepDefinitions/provisioningContext.js
@@ -2,26 +2,29 @@ const { client } = require('nightwatch-api')
 const { Given } = require('cucumber')
 const fetch = require('node-fetch')
 require('url-search-params-polyfill')
+const httpHelper = require('../helpers/httpHelper')
 
 function createUser (userId) {
   const body = new URLSearchParams()
   body.append('userid', userId)
   body.append('password', userId)
 
-  const headers = {
-    'Authorization': 'Basic ' + Buffer.from(client.globals.backend_admin_username + ':' + client.globals.backend_admin_password).toString('base64')
-  }
+  const headers = httpHelper.createAuthHeader(client.globals.backend_admin_username)
   return fetch(client.globals.backend_url + '/ocs/v2.php/cloud/users?format=json', { method: 'POST', body: body, headers: headers })
 }
 
 function deleteUser (userId) {
-  const headers = {
-    'Authorization': 'Basic ' + Buffer.from(client.globals.backend_admin_username + ':' + client.globals.backend_admin_password).toString('base64')
-  }
+  const headers = httpHelper.createAuthHeader(client.globals.backend_admin_username)
   return fetch(client.globals.backend_url + '/ocs/v2.php/cloud/users/' + userId, { method: 'DELETE', headers: headers })
+}
+
+function initUser (userId) {
+  const headers = httpHelper.createAuthHeader(userId)
+  return fetch(client.globals.backend_url + '/ocs/v2.php/cloud/users/' + userId, { method: 'GET', headers: headers })
 }
 
 Given('user {string} has been created with default attributes', function (userId) {
   return deleteUser(userId)
     .then(() => createUser(userId))
+    .then(() => initUser(userId))
 })
