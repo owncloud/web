220b128660f1be07a15498894344519fbba97156
Artur Neumann <artur@jankaritech.com>
2019-04-16 16:47:24 +0545

    search tests

--- tests/acceptance/pageObjects/phoenixPage.js
+++ tests/acceptance/pageObjects/phoenixPage.js
@@ -2,10 +2,25 @@ module.exports = {
   url: function () {
     return this.api.launchUrl + '/#/'
   },
+  commands: {
+    search: function (searchTerm) {
+      return this.waitForElementVisible('@searchInputField')
+        .initAjaxCounters()
+        .setValue('@searchInputField', [searchTerm, this.api.Keys.ENTER])
+        .waitForElementNotVisible('@searchLoadingIndicator')
+        .waitForOutstandingAjaxCalls()
+    }
+  },
   elements: {
     message: {
       selector: '//*[contains(@class, "uk-notification-message-primary")]/div',
       locateStrategy: 'xpath'
+    },
+    searchInputField: {
+      selector: '.oc-search-input'
+    },
+    searchLoadingIndicator: {
+      selector: '.appTopbar .uk-spinner'
     }
   }
 }
