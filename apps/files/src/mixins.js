9fb3dfb565f11210b8b6cbba830ac82a6d9836ab
Lukas Hirt <lhirt@owncloud.com>
2019-07-31 13:22:53 +0200

    Added getter for token into mixins

--- apps/files/src/mixins.js
+++ apps/files/src/mixins.js
@@ -23,6 +23,7 @@ export default {
   }),
   computed: {
     ...mapGetters('Files', ['searchTerm', 'inProgress', 'files']),
+    ...mapGetters(['getToken']),
 
     _renameDialogTitle () {
       let translated
