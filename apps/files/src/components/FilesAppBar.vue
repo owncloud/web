4d33f78d2dee24e3a63e8d025baf98424e274d5d
Thomas Müller <thomas.mueller@tmit.eu>
2019-08-05 10:55:32 +0200

    Current folder shall not be clickable in breadcrumbs

--- apps/files/src/components/FilesAppBar.vue
+++ apps/files/src/components/FilesAppBar.vue
@@ -170,10 +170,14 @@ export default {
 
       if (this.$route.params.item) {
         for (let i = startIndex; i < pathSplit.length; i++) {
+          let to = baseUrl + encodeURIComponent(pathSplit.slice(0, i + 1).join('/'))
+          if (i === pathSplit.length - 1) {
+            to = null
+          }
           breadcrumbs.push({
             index: i,
             text: pathSplit.slice(0, i + 1)[i],
-            to: baseUrl + encodeURIComponent(pathSplit.slice(0, i + 1).join('/'))
+            to: to
           })
         }
       }
