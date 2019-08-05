764b50be28626ebbed24dd7f9dee82353893309f
Lukas Hirt <lhirt@owncloud.com>
2019-08-05 15:59:24 +0200

    Added icons as placeholders
    Added spinner

--- apps/files/src/components/Collaborators/AutocompleteItem.vue
+++ apps/files/src/components/Collaborators/AutocompleteItem.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="uk-flex uk-flex-middle">
-    <oc-avatar :src="avatar" class="uk-margin-small-right" width=50 height=50 />
+  <div class="uk-flex uk-flex-middle uk-light">
+    <oc-spinner v-if="loading" uk-spinner="ratio:1.6" class="uk-margin-small-right" />
+    <template v-else>
+      <oc-avatar v-if="avatar" :src="avatar" class="uk-margin-small-right" width=50 height=50 />
+      <template v-else>
+        <oc-icon v-if="item.value.shareType === 1" class="uk-margin-small-right" name="group" size="large" />
+        <oc-icon v-else class="uk-margin-small-right" name="person" size="large" />
+      </template>
+    </template>
     <div>
       <div class="uk-text-bold files-collaborators-autocomplete-username">{{ item.label }}</div>
       <span class="uk-text-meta">{{ $_ocCollaborators_collaboratorType(item.value.shareType) }}</span>
@@ -20,6 +27,7 @@ export default {
   },
   data () {
     return {
+      loading: false,
       avatar: ''
     }
   }
