2dcccadecd844685960beae8a0c41e5a2a41f386
Felix Heidecke <felix@heidecke.me>
2019-08-06 19:05:01 +0200

    Add name and password capabilities

--- apps/files/src/store/actions.js
+++ apps/files/src/store/actions.js
@@ -125,6 +125,8 @@ function _buildLink (l) {
     permissions: link.permissions,
     description,
     stime: link.stime,
+    name: link.name,
+    password: !!(link.share_with && link.share_with_displayname),
     expiration: moment(link.expiration).format('YYYY-MM-DD'),
     itemSource: link.item_source,
     file: {
@@ -572,6 +574,7 @@ export default {
 
       client.shares.shareFileWithLink(path, params)
         .then(data => {
+          console.log(data)
           const link = _buildLink(data)
           context.commit('LINKS_ADD', link)
           context.commit('LINKS_LOADING', false)
