d22248d5bbeb3d94b1c1d2c4833990eac783957c
Lukas Hirt <lhirt@owncloud.com>
2019-08-09 14:19:03 +0200

    Hide versions tab for folders

--- apps/files/src/components/FileDetails.vue
+++ apps/files/src/components/FileDetails.vue
@@ -60,7 +60,7 @@ export default {
     ...mapGetters(['getToken', 'fileSideBars', 'capabilities']),
     ...mapGetters('Files', ['highlightedFile']),
     fileSideBarsEnabled () {
-      return this.fileSideBars.filter(b => b.enabled === undefined || b.enabled(this.capabilities))
+      return this.fileSideBars.filter(b => b.enabled === undefined || b.enabled(this.capabilities, this.highlightedFile))
     },
     activeTabComponent () {
       return this.fileSideBarsEnabled[this.activeTab]
