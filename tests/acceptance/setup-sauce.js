ebcb4f77a4c5879a62f4beba8dae1bea58dd0fd1
Artur Neumann <artur@jankaritech.com>
2019-09-30 14:11:17 +0545

    show URL to saucelabs job

--- tests/acceptance/setup-sauce.js
+++ tests/acceptance/setup-sauce.js
@@ -1,5 +1,5 @@
 import { setDefaultTimeout, After, Before } from 'cucumber'
-import { createSession, closeSession, startWebDriver, stopWebDriver } from 'nightwatch-api'
+import { createSession, closeSession, startWebDriver, stopWebDriver, client } from 'nightwatch-api'
 // import { SauceLabs } from 'saucelabs'
 
 setDefaultTimeout(600000)
@@ -7,6 +7,10 @@ setDefaultTimeout(600000)
 Before(async () => {
   await startWebDriver({ env: 'sauce' })
   await createSession({ env: 'sauce' })
+  await client
+    .session(function (session) {
+      console.log('  Link to saucelabs job: https://app.saucelabs.com/tests/' + session.sessionId)
+    })
 })
 
 After(async () => {
