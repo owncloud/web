jasson99/phoenix

Add acceptance tests for deleting shared files and deleting shares

tests/acceptance/stepDefinitions/filesContext.js
@@ -501,3 +501,8 @@ Then('the clipboard content should match permalink of resource {string}', async 
 Then('the thumbnail should be visible in the app-sidebar', function () {
   return client.page.FilesPageElement.appSideBar().isThumbnailVisible()
 })
+
+When('the user uploads overwriting file {string} using the webUI', function (file) {
+  return client.page.filesPage().uploadFile(file)
+    .then(() => client.page.filesPage().confirmFileOverwrite())
+})

owncloud/web

tests/acceptance/stepDefinitions/filesContext.js
@@ -531,3 +531,8 @@ Then('it should not be possible to delete file/folder {string} using the webUI',
     .filesList()
     .assertActionDisabled('delete', resource)
 })
+
+When('the user uploads overwriting file {string} using the webUI', function (file) {
+  return client.page.filesPage().selectFileForUpload(file)
+    .then(() => client.page.filesPage().confirmFileOverwrite())
+})