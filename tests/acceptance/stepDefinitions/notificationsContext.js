b4468b221da1b6a690b339e75b24bf64dbf932bc
jasson99 <jasmine.baral@hotmail.com>
2019-10-22 16:14:30 +0545

    Add acceptance test to share files and folders with internal groups

--- tests/acceptance/stepDefinitions/notificationsContext.js
+++ tests/acceptance/stepDefinitions/notificationsContext.js
@@ -1,5 +1,5 @@
 const { client } = require('nightwatch-api')
-const { When, Then } = require('cucumber')
+const { Given, When, Then } = require('cucumber')
 const httpHelper = require('../helpers/httpHelper')
 const fetch = require('node-fetch')
 
@@ -18,6 +18,22 @@ When('the user marks the notification as read', function () {
   return client.page.phoenixPage().markNotificationAsRead()
 })
 
+Given('app {string} has been enabled', function (app) {
+  const headers = httpHelper.createAuthHeader('admin')
+  return fetch(client.globals.backend_url + '/ocs/v2.php/cloud/apps/' + app + '?format=json', {
+    headers,
+    body: {},
+    method: 'POST'
+  }).then(res => {
+    httpHelper.checkStatus(res, 'Failed while trying to enable the app')
+    return res.json()
+  })
+})
+
+Then('the user should see the notification bell on the webUI', function () {
+  return client.page.phoenixPage().waitForElementVisible('@notificationBell')
+})
+
 Then('the user should see the notification bell on the webUI after a page reload', function () {
   client.refresh()
   return client.page.phoenixPage().waitForElementVisible('@notificationBell')
@@ -25,4 +41,9 @@ Then('the user should see the notification bell on the webUI after a page reload
 
 Then('the notification bell should disappear on the webUI', function () {
   return client.page.phoenixPage().waitForElementNotPresent('@notificationBell')
+})
+
+Then('the user should see {int} notifications on the webUI with these details', function (numberOfNotifications, dataTable) {
+  const dataTableHashed = dataTable.hashes()
+  return client.page.phoenixPage().assertNotificationIsPresent(numberOfNotifications, dataTableHashed)
 })
