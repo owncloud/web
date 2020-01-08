2b73fde08c53931b25ca53bee02fc1bd9ef3d595
Vincent Petry <pvince81@owncloud.com>
2020-01-08 10:31:05 +0100

    Another try

--- tests/acceptance/helpers/browserConsole.js
+++ tests/acceptance/helpers/browserConsole.js
@@ -1,38 +1,9 @@
 import { client } from 'nightwatch-api'
 
-function cleanupLogMessage (message) {
-  return message
-    .replace(/\\u003C/gi, '')
-    .replace(/\\n/g, '\n') // revive newlines
-}
-
-function formatLog (log) {
-  return new Date(log.timestamp).toLocaleTimeString() + ' - ' +
-    log.level + ' - ' +
-    cleanupLogMessage(log.message)
-}
-
-async function getAllLogs () {
-  let logs = []
-  await client.getLog('browser', entries => {
-    logs = entries
+export async function getAllLogsWithDateTime (level = null) {
+  let logs
+  await client.elementIdText('debugArray', text => {
+    logs = 'from debugArray: ' + text.value
   })
   return logs
-}
-
-export async function getAllLogsWithDateTime (level = null) {
-  let logs = await getAllLogs()
-  if (!logs) {
-    await client.elementIdText('debugArray', text => {
-      logs = 'from debugArray: ' + text.value
-    })
-    return logs
-  }
-  if (level) {
-    logs = logs.filter(entry => entry.level === level)
-  }
-
-  return logs
-    .filter(e => !e.message.includes('favicon.ico'))
-    .map(formatLog)
 }
