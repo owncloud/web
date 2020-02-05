f219142b10499c8a053fa27206251e940fcd5697
Vincent Petry <pvince81@owncloud.com>
2020-02-05 22:26:35 +0100

    Test click authorize button directly
    Don't wait for the element to be visible, as the click method will wait
    on its own.
    
    Hoping that this can fix the issues that sometimes clicking doesn't do
    anything, maybe because the layout shifts during page load
    after the element was detected.

--- tests/acceptance/pageObjects/ownCloudAuthorizePage.js
+++ tests/acceptance/pageObjects/ownCloudAuthorizePage.js
@@ -12,7 +12,6 @@ module.exports = {
     {
       authorize: function () {
         return this
-          .waitForElementVisible('@authorizeButton')
           .click('@authorizeButton')
           .waitForElementNotPresent({
             selector: '@authorizeButton',
