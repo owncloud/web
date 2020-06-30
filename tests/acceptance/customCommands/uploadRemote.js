f746b433adb4bf3c9b0ba054fa1840529af74361
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-06-30 11:42:03 +0545

    WIP ...

--- tests/acceptance/customCommands/uploadRemote.js
+++ tests/acceptance/customCommands/uploadRemote.js
@@ -1,8 +1,6 @@
-const path = require('path')
 const events = require('events')
-const archiver = require('archiver')
-const _ = require('lodash')
 const fs = require('fs')
+const yazl = require('yazl')
 
 class UploadRemote extends events.EventEmitter {
   /**
@@ -10,57 +8,19 @@ class UploadRemote extends events.EventEmitter {
    * file to a remote Selenium server for use in testing uploads.
    *
    * @param {string} filePath Local path to the file used for uploading
+   * @param mtime
    * @param {function (string): void} [callback] - called when file gets uploaded with path to uploaded file
    */
   command(filePath, mtime, callback) {
-    const buffers = []
-    const zip = archiver('zip')
-    /*
-    We add all the buffered data to  a list, and then,
-    we concat all the buffer instances, base64encode it and send to the remote
-    selenium browser. On completion, we get the result (containing the destination path to where the file
-    was uploaded) which we can retrieve from the callback
-     */
-    zip
-      .on('data', data => {
-        buffers.push(data)
-      })
-      .on('error', err => {
-        throw err
-      })
-      .on('warning', console.log)
-      .on('finish', () => {
-        const file = Buffer.concat(buffers).toString('base64')
-        this.api.session(session => {
-          const opt = {
-            path: `/session/${session.sessionId}/file`,
-            method: 'POST',
-            data: { file }
-          }
-          this.client.transport
-            .runProtocolAction(opt) // private api
-            .then(result => {
-              if (result.status !== 0) {
-                throw new Error(result.value.message)
-              }
-              return result.value
-            })
-            .then(pathToFile => _.isFunction(callback) && callback(pathToFile))
-            .then(() => this.emit('complete'))
-        })
-      })
-
-    const name = path.basename(filePath)
-    const date = new Date()
-    const mtimeInMs = new Date(date.setMonth(date.getMonth() - 8))
-    console.log(mtimeInMs)
-    zip.file(filePath, { name })
-    const source = fs.createReadStream(filePath)
-    zip.append(source, {
-      name: name,
-      date: new Date()
+    const zip = new yazl.ZipFile()
+    console.log(filePath)
+    zip.addFile(filePath, '~/www/testZip', {
+      mtime: new Date(mtime)
     })
-    zip.finalize() // `finish` event fires after calling this
+    zip.outputStream.pipe(fs.createWriteStream('output.zip')).on('close', function() {
+      console.log('done')
+    })
+    zip.end()
   }
 }
 
