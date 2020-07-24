f1e1d233e882aa06c2dfcd6662cf8fa68e8882d9
Lukas Hirt <lukashirt@mail.com>
2020-07-24 11:55:50 +0200

    Pass null instead of undefined headers to mediaSource

--- apps/media-viewer/src/Mediaviewer.vue
+++ apps/media-viewer/src/Mediaviewer.vue
@@ -242,7 +242,7 @@ export default {
       const url = this.$client.helpers._webdavUrl + this.activeMediaFile.path
       const promise = this.isUrlSigningEnabled
         ? this.$client.signUrl(url, 7200)
-        : this.mediaSource(this.thumbPath, 'url', this.headers)
+        : this.mediaSource(this.thumbPath, 'url', null)
 
       promise
         .then(mediaUrl => {
