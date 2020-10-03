63d48d5ea6d302aa3de8c43e42d41659c092be14
Benedikt Kulmann <benedikt@kulmann.biz>
2020-10-03 05:55:30 +0200

    Fix trashbin page layout

--- apps/files/src/components/Trashbin.vue
+++ apps/files/src/components/Trashbin.vue
@@ -1,72 +1,68 @@
 <template>
-  <div id="files-list-container" class="uk-height-1-1">
-    <div class="uk-overflow-auto uk-height-1-1">
-      <file-list
-        id="files-list"
-        :file-data="fileData"
-        :loading="loadingFolder"
-        :actions="actions"
-        :is-action-enabled="isActionEnabled"
-        :selectable-row="false"
+  <file-list
+    id="files-list"
+    :file-data="fileData"
+    :loading="loadingFolder"
+    :actions="actions"
+    :is-action-enabled="isActionEnabled"
+    :selectable-row="false"
+  >
+    <template #headerColumns>
+      <div class="uk-text-truncate uk-text-meta uk-width-expand">
+        <sortable-column-header
+          :aria-label="$gettext('Sort files by name')"
+          :is-active="fileSortField === 'name'"
+          :is-desc="fileSortDirectionDesc"
+          @click="toggleSort('name')"
+        >
+          <translate translate-context="Name column in files table">Name</translate>
+        </sortable-column-header>
+      </div>
+      <div
+        :class="{ 'uk-visible@s': !_sidebarOpen, 'uk-hidden': _sidebarOpen }"
+        class="uk-text-nowrap uk-text-meta uk-width-small"
       >
-        <template #headerColumns>
-          <div class="uk-text-truncate uk-text-meta uk-width-expand">
-            <sortable-column-header
-              :aria-label="$gettext('Sort files by name')"
-              :is-active="fileSortField === 'name'"
-              :is-desc="fileSortDirectionDesc"
-              @click="toggleSort('name')"
-            >
-              <translate translate-context="Name column in files table">Name</translate>
-            </sortable-column-header>
-          </div>
-          <div
-            :class="{ 'uk-visible@s': !_sidebarOpen, 'uk-hidden': _sidebarOpen }"
-            class="uk-text-nowrap uk-text-meta uk-width-small"
-          >
-            <sortable-column-header
-              class="uk-align-right"
-              :aria-label="$gettext('Sort files by deletion time')"
-              :is-active="fileSortField === 'deleteTimestampMoment'"
-              :is-desc="fileSortDirectionDesc"
-              @click="toggleSort('deleteTimestampMoment')"
-            >
-              <translate translate-context="Deletion time column in 'deleted files' table">
-                Deletion Time
-              </translate>
-            </sortable-column-header>
-          </div>
-          <div class="oc-icon" />
+        <sortable-column-header
+          class="uk-align-right"
+          :aria-label="$gettext('Sort files by deletion time')"
+          :is-active="fileSortField === 'deleteTimestampMoment'"
+          :is-desc="fileSortDirectionDesc"
+          @click="toggleSort('deleteTimestampMoment')"
+        >
+          <translate translate-context="Deletion time column in 'deleted files' table">
+            Deletion Time
+          </translate>
+        </sortable-column-header>
+      </div>
+      <div class="oc-icon" />
+    </template>
+    <template #rowColumns="{ item }">
+      <div class="uk-width-expand uk-flex uk-flex-middle">
+        <file-item
+          :key="item.viewId"
+          :item="item"
+          :name="$_ocTrashbin_fileName(item)"
+          :display-preview="false"
+        />
+      </div>
+      <div
+        class="uk-text-meta uk-text-nowrap uk-width-small uk-text-right"
+        :class="{ 'uk-visible@s': !_sidebarOpen, 'uk-hidden': _sidebarOpen }"
+      >
+        {{ formDateFromNow(item.deleteTimestamp) }}
+      </div>
+    </template>
+    <template #loadingMessage>
+      <translate>Loading deleted files</translate>
+    </template>
+    <template #noContentMessage>
+      <no-content-message icon="delete">
+        <template #message>
+          <span v-translate>You have no deleted files.</span>
         </template>
-        <template #rowColumns="{ item }">
-          <div class="uk-width-expand uk-flex uk-flex-middle">
-            <file-item
-              :key="item.viewId"
-              :item="item"
-              :name="$_ocTrashbin_fileName(item)"
-              :display-preview="false"
-            />
-          </div>
-          <div
-            class="uk-text-meta uk-text-nowrap uk-width-small uk-text-right"
-            :class="{ 'uk-visible@s': !_sidebarOpen, 'uk-hidden': _sidebarOpen }"
-          >
-            {{ formDateFromNow(item.deleteTimestamp) }}
-          </div>
-        </template>
-        <template #loadingMessage>
-          <translate>Loading deleted files</translate>
-        </template>
-        <template #noContentMessage>
-          <no-content-message icon="delete">
-            <template #message>
-              <span v-translate>You have no deleted files.</span>
-            </template>
-          </no-content-message>
-        </template>
-      </file-list>
-    </div>
-  </div>
+      </no-content-message>
+    </template>
+  </file-list>
 </template>
 <script>
 import { mapGetters, mapActions } from 'vuex'
