3671e2ea45e3cbbb57840b3a8b4c13a211fb2212
Esteban Puentes <virtualevan@gmail.com>
2020-11-18 11:17:27 +0100

    Do not load moment locales to reduce the bundle size
    As no locales are being used, is not necessary to load them
    
    This and the moment.js usage can be removed if the creation
    of the new .drawio file is managed with a modal instead
    of adding a timestamp to the filename

--- apps/draw-io/webpack.common.js
+++ apps/draw-io/webpack.common.js
@@ -1,15 +1,10 @@
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const webpack = require('webpack')
 
 module.exports = {
-  plugins: [
-    new VueLoaderPlugin()
-  ],
+  plugins: [new VueLoaderPlugin(), new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)],
   entry: {
-    'draw-io': [
-      'core-js/modules/es6.promise',
-      'core-js/modules/es6.array.iterator',
-      './src/app.js'
-    ]
+    'draw-io': ['core-js/modules/es6.promise', 'core-js/modules/es6.array.iterator', './src/app.js']
   },
   output: {
     publicPath: 'apps/draw-io/',
@@ -17,42 +12,42 @@ module.exports = {
     filename: 'draw-io.bundle.js'
   },
   module: {
-    rules: [{
-      test: /\.js?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      options: {
-        rootMode: 'upward'
-      }
-    }, {
-      test: /\.vue$/,
-      loader: 'vue-loader'
-    },
-    {
-      test: /\.jsx?$/,
-      include: /node_modules\/(?=(query-string|split-on-first|strict-uri-encode)\/).*/,
-      use: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                targets: {
-                  ie: '11'
-                }
-              }
-            ]
-          ]
+          rootMode: 'upward'
         }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.jsx?$/,
+        include: /node_modules\/(?=(query-string|split-on-first|strict-uri-encode)\/).*/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: {
+                    ie: '11'
+                  }
+                }
+              ]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader']
       }
-    },
-    {
-      test: /\.css$/,
-      use: [
-        'vue-style-loader',
-        'css-loader'
-      ]
-    }]
+    ]
   }
 }
