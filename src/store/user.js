a9bbf956da327fe7008a2af9a5f48b63519e0865
Florian Schade <f.schade@icloud.com>
2020-12-11 18:22:28 +0100

    fix logoutFinalizier typo

--- src/store/user.js
+++ src/store/user.js
@@ -31,7 +31,7 @@ const actions = {
     vueAuthInstance.clearLoginState()
   },
   async logout({ dispatch }) {
-    const logoutFinalizier = (forceRedirect = false) => {
+    const logoutFinalizer = (forceRedirect = false) => {
       // Remove signed in user
       dispatch('cleanUpLoginState')
       dispatch('hideModal')
@@ -48,7 +48,7 @@ const actions = {
       vueAuthInstance
         .createSignoutRequest({ id_token_hint: u.id_token })
         .then(signoutRequestUrl => {
-          logoutFinalizier()
+          logoutFinalizer()
 
           // Navigate to signout URL
           window.open(signoutRequestUrl, '_self')
@@ -58,7 +58,7 @@ const actions = {
         })
     } else {
       // Oauth2 logout
-      logoutFinalizier(true)
+      logoutFinalizer(true)
     }
   },
   async initAuth(context, payload = { autoRedirect: false }) {
