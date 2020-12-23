4cd334e938560fabb15dfb849cec9f0c247f72b9
Swikriti Tripathi <swikriti808@gmail.com>
2020-12-23 13:32:30 +0545

    Added test for performing cancel action while copying and moving a files/folders

--- tests/acceptance/pageObjects/filesPage.js
+++ tests/acceptance/pageObjects/filesPage.js
@@ -291,6 +291,13 @@ module.exports = {
       return client.page.locationPicker().selectFolderAndConfirm(target)
     },
 
+    attemptToMoveMultipleResources: async function(target) {
+      // Trigger move
+      await this.click('@moveSelectedBtn')
+
+      return client.page.locationPicker().selectFolder(target)
+    },
+
     copyMultipleResources: function(target) {
       // Trigger copy
       this.click('@copySelectedBtn')
@@ -299,6 +306,12 @@ module.exports = {
       return client.page.locationPicker().selectFolderAndConfirm(target)
     },
 
+    attemptToCopyMultipleResources: async function(target) {
+      // Trigger copy
+      await this.click('@copySelectedBtn')
+
+      return client.page.locationPicker().selectFolder(target)
+    },
     /**
      * Create a md file with the given name
      *
@@ -457,7 +470,11 @@ module.exports = {
     editorCloseBtn: {
       selector: '#markdown-editor-app-bar .uk-text-right .oc-button'
     },
-    cancelMoveBtn: {
+    clearSelectionBtn: {
+      selector: '//span[contains(text(),"Clear selection")]',
+      locateStrategy: 'xpath'
+    },
+    cancelMoveCopyBtn: {
       selector: '//button[.="Cancel"]',
       locateStrategy: 'xpath'
     }
--- tests/acceptance/stepDefinitions/filesContext.js
+++ tests/acceptance/stepDefinitions/filesContext.js
@@ -596,6 +596,21 @@ Then(
   }
 )
 
+Then(
+  'file/folder/resource {string} should not be listed in the folder {string} on the webUI',
+  async function(file, target) {
+    await client.page.filesPage().navigateAndWaitTillLoaded(target)
+    return client.page.FilesPageElement.filesList()
+      .isElementListed(file, 'file', client.globals.waitForNegativeConditionTimeout)
+      .then(state => {
+        const message = state
+          ? `Error: Folder '${file}' is listed on the '${target}'`
+          : `File '${file}' is not listed on the '${target}'`
+        return client.assert.ok(!state, message)
+      })
+  }
+)
+
 /**
  * currently this only works with the files page, on other pages the user cannot navigate into subfolders
  *
@@ -1152,11 +1167,12 @@ When('the user tries to move file/folder {string} into folder {string} using the
   return client.page.FilesPageElement.filesList().attemptToMoveResource(resource, target)
 })
 
-When('the user cancels an attempt to move file/folder {string} using the webUI', function(
-  resource
-) {
-  return client.page.FilesPageElement.filesList().cancelMoveResource(resource)
-})
+When(
+  'the user cancels the attempt to move/copy file/folder into folder {string} using the webUI',
+  function(target) {
+    return client.page.FilesPageElement.filesList().cancelResourceMoveOrCopyProgress(target)
+  }
+)
 
 Then('it should not be possible to copy/move into folder {string} using the webUI', function(
   target
@@ -1172,6 +1188,17 @@ When(
     }
 
     return client.page.filesPage().moveMultipleResources(target)
+  }
+)
+
+When(
+  'the user tries to batch move these files/folders into folder {string} using the webUI',
+  async function(target, resources) {
+    for (const item of resources.rows()) {
+      await client.page.FilesPageElement.filesList().toggleFileOrFolderCheckbox('enable', item[0])
+    }
+
+    return client.page.filesPage().attemptToMoveMultipleResources(target)
   }
 )
 
@@ -1197,6 +1224,17 @@ When(
     }
 
     return client.page.filesPage().copyMultipleResources(target)
+  }
+)
+
+When(
+  'the user tries to batch copy these files/folders into folder {string} using the webUI',
+  async function(target, resources) {
+    for (const item of resources.rows()) {
+      await client.page.FilesPageElement.filesList().toggleFileOrFolderCheckbox('enable', item[0])
+    }
+
+    return client.page.filesPage().attemptToCopyMultipleResources(target)
   }
 )
 
