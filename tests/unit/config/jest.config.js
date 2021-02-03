ea5719d2ef67c6071152104cd0fa8d0aeb831acb
Florian Schade <f.schade@icloud.com>
2021-02-03 16:45:25 +0100

    fix lint

--- tests/unit/config/jest.config.js
+++ tests/unit/config/jest.config.js
@@ -2,20 +2,17 @@ const path = require('path')
 const rootDir = path.resolve(__dirname, '../../../')
 
 module.exports = {
-    rootDir,
-    modulePaths: ['<rootDir>'],
-    moduleFileExtensions: ['js', 'json', 'vue'],
-    transform: {
-        '^.+\\.js$': ['babel-jest', { configFile: path.join(rootDir, '.babelrc') }],
-        '.*\\.(vue)$': 'vue-jest',
-        '^.+\\.svg$': 'jest-svg-transformer'
-    },
-    setupFiles: ['<rootDir>/tests/unit/config/jest.init.js'],
-    snapshotSerializers: ['jest-serializer-vue'],
-    coverageDirectory: '<rootDir>/coverage',
-    coverageReporters: ['lcov'],
-    collectCoverageFrom: [
-        '<rootDir>/packages/**/src/**/*.{js,vue}',
-        '!<rootDir>/**/node_modules/**'
-    ]
+  rootDir,
+  modulePaths: ['<rootDir>'],
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  transform: {
+    '^.+\\.js$': ['babel-jest', { configFile: path.join(rootDir, '.babelrc') }],
+    '.*\\.(vue)$': 'vue-jest',
+    '^.+\\.svg$': 'jest-svg-transformer'
+  },
+  setupFiles: ['<rootDir>/tests/unit/config/jest.init.js'],
+  snapshotSerializers: ['jest-serializer-vue'],
+  coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: ['lcov'],
+  collectCoverageFrom: ['<rootDir>/packages/**/src/**/*.{js,vue}', '!<rootDir>/**/node_modules/**']
 }
