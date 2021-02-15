51143750ed62fe4d6bda1e903cb89ec08cf607e7
Lukas Hirt <lukashirt@mail.com>
2021-02-15 16:05:56 +0100

    Add no content message and loader

--- packages/web-app-files/src/views/PublicFiles.vue
+++ packages/web-app-files/src/views/PublicFiles.vue
@@ -1,14 +1,36 @@
 <template>
   <div>
-    <oc-table-files :resources="activeFiles" />
+    <list-loader v-if="state === 'loading'" />
+    <oc-table-files v-else-if="state === 'loaded'" :resources="activeFiles" />
+    <no-content-message v-else-if="state === 'empty'" icon="folder">
+      <template v-slot:message>
+        <span v-translate>There are no resources in this folder</span>
+      </template>
+      <template v-if="currentFolder.canCreate()" v-slot:callToAction>
+        <span v-translate>Drag files and folders here or use the "+ New" button to upload</span>
+      </template>
+    </no-content-message>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
+
+import ListLoader from '../components/ListLoader.vue'
+import NoContentMessage from '../components/NoContentMessage.vue'
+
 export default {
+  components: {
+    ListLoader,
+    NoContentMessage
+  },
+
+  data: () => ({
+    state: 'loading'
+  }),
+
   computed: {
-    ...mapGetters('Files', ['publicLinkPassword', 'activeFiles', 'davProperties'])
+    ...mapGetters('Files', ['publicLinkPassword', 'activeFiles', 'davProperties', 'currentFolder'])
   },
 
   created() {
@@ -26,6 +48,8 @@ export default {
         this.publicLinkPassword,
         this.davProperties
       )
+
+      console.log(resources)
 
       this.loadFiles({ currentFolder: resources[0], files: resources.slice(1) })
 
