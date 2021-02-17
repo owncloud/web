fa33b6f985e2abd2d647ab63503a69457c0abae6
Lukas Hirt <lukashirt@mail.com>
2021-02-17 14:53:43 +0100

    Fix move action

--- packages/web-app-files/src/mixins/actions/move.js
+++ packages/web-app-files/src/mixins/actions/move.js
@@ -16,7 +16,12 @@ export default {
               'Move'
             ),
           isEnabled: ({ resource }) => {
-            if (!checkRoute(['files-list', 'public-files', 'files-favorites'], this.$route.name)) {
+            if (
+              !checkRoute(
+                ['files-all-files', 'files-public-list', 'files-favorites'],
+                this.$route.name
+              )
+            ) {
               return false
             }
 
@@ -35,7 +40,7 @@ export default {
       // Parent of the resource selected for copy used as a default target location
       const parent = dirname(resource.path)
       this.$router.push({
-        name: 'location-picker',
+        name: 'files-location-picker',
         query: { action: 'move', target: parent, resource: resource.path }
       })
     }
