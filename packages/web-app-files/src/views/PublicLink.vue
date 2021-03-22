08bb72904ebc3ad3fd4a2bd1117de4b53c53606b
Lukas Hirt <lukashirt@mail.com>
2021-03-22 11:45:24 +0100

    Theme public link page

--- packages/web-app-files/src/views/PublicLink.vue
+++ packages/web-app-files/src/views/PublicLink.vue
@@ -71,6 +71,14 @@ export default {
     },
     $_loadingPublicLinkTitle() {
       return this.$gettext('Loading public link…')
+    },
+
+    backgroundImg() {
+      return this.configuration.theme.loginPage.backgroundImg
+    },
+
+    logoImg() {
+      return this.configuration.theme.logo.login
     }
   },
   mounted() {
