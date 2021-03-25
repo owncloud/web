e3e3632b8c69e6cae58cea39e4d70d9c724471fe
Lukas Hirt <lukashirt@mail.com>
2021-03-25 16:55:14 +0100

    Create shared by link page

---
+++ packages/web-app-files/src/views/SharedByLink.vue
@@ -0,0 +1,194 @@
+<template>
+  <div>
+    <list-loader v-if="loading" />
+    <template v-else>
+      <no-content-message
+        v-if="isEmpty"
+        id="files-shared-with-others-empty"
+        class="files-empty"
+        icon="group"
+      >
+        <template #message>
+          <span v-translate>There are no resources with a public link at the moment</span>
+        </template>
+      </no-content-message>
+      <oc-table-files
+        v-else
+        id="files-shared-with-others-table"
+        v-model="selected"
+        class="files-table"
+        :class="{ 'files-table-squashed': isSidebarOpen }"
+        :are-previews-displayed="displayPreviews"
+        :resources="activeFiles"
+        :target-route="targetRoute"
+        :highlighted="highlightedFile ? highlightedFile.id : null"
+        :header-position="headerPosition"
+        @showDetails="setHighlightedFile"
+        @fileClick="$_fileActions_triggerDefaultAction"
+      >
+        <template #footer>
+          <div
+            v-if="activeFilesCount.folders > 0 || activeFilesCount.files > 0"
+            class="uk-text-nowrap uk-text-meta uk-text-center uk-width-1-1"
+          >
+            <span id="files-list-count-folders" v-text="activeFilesCount.folders" />
+            <translate :translate-n="activeFilesCount.folders" translate-plural="folders">
+              folder
+            </translate>
+            <translate>and</translate>
+            <span id="files-list-count-files" v-text="activeFilesCount.files" />
+            <translate :translate-n="activeFilesCount.files" translate-plural="files">
+              file
+            </translate>
+          </div>
+        </template>
+      </oc-table-files>
+    </template>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapState, mapActions, mapMutations } from 'vuex'
+
+import { aggregateResourceShares, buildResource } from '../helpers/resources'
+import FileActions from '../mixins/fileActions'
+import MixinFilesListPositioning from '../mixins/filesListPositioning'
+import MixinResources from '../mixins/resources'
+
+import ListLoader from '../components/ListLoader.vue'
+import NoContentMessage from '../components/NoContentMessage.vue'
+
+export default {
+  components: { ListLoader, NoContentMessage },
+
+  mixins: [FileActions, MixinFilesListPositioning, MixinResources],
+
+  data: () => ({
+    loading: true
+  }),
+
+  computed: {
+    ...mapState(['app']),
+    ...mapGetters('Files', [
+      'davProperties',
+      'highlightedFile',
+      'activeFiles',
+      'selectedFiles',
+      'inProgress',
+      'activeFilesCount'
+    ]),
+    ...mapGetters(['isOcis', 'configuration', 'getToken', 'user']),
+
+    selected: {
+      get() {
+        return this.selectedFiles
+      },
+      set(resources) {
+        this.SELECT_RESOURCES(resources)
+      }
+    },
+
+    isEmpty() {
+      return this.activeFiles.length < 1
+    },
+
+    isSidebarOpen() {
+      return this.highlightedFile !== null
+    },
+
+    uploadProgressVisible() {
+      return this.inProgress.length > 0
+    },
+
+    targetRoute() {
+      return { name: 'files-personal' }
+    },
+
+    displayPreviews() {
+      return !this.configuration.options.disablePreviews
+    }
+  },
+
+  watch: {
+    uploadProgressVisible() {
+      this.adjustTableHeaderPosition()
+    }
+  },
+
+  created() {
+    this.loadResources()
+    window.onresize = this.adjustTableHeaderPosition
+  },
+
+  mounted() {
+    this.adjustTableHeaderPosition()
+  },
+
+  methods: {
+    ...mapActions('Files', ['setHighlightedFile', 'loadIndicators', 'loadPreviews']),
+    ...mapMutations('Files', ['LOAD_FILES', 'SELECT_RESOURCES', 'CLEAR_CURRENT_FILES_LIST']),
+    ...mapMutations(['SET_QUOTA']),
+
+    async loadResources() {
+      this.loading = true
+      this.CLEAR_CURRENT_FILES_LIST()
+
+      let resources = await this.$client.requests.ocs({
+        service: 'apps/files_sharing',
+        action: '/api/v1/shares?format=json&share_types=3&include_tags=false',
+        method: 'GET'
+      })
+      let rootFolder = await this.$client.files.fileInfo('/', this.davProperties)
+
+      resources = await resources.json()
+      resources = resources.ocs.data
+      rootFolder = buildResource(rootFolder)
+
+      if (resources.length < 1) {
+        this.LOAD_FILES({ currentFolder: rootFolder, files: [] })
+        this.loading = false
+
+        return
+      }
+
+      resources = await aggregateResourceShares(
+        resources,
+        false,
+        !this.isOcis,
+        this.configuration.server,
+        this.getToken
+      )
+
+      this.LOAD_FILES({ currentFolder: rootFolder, files: resources })
+
+      if (this.displayPreviews) {
+        await this.loadPreviews({
+          resources,
+          isPublic: false,
+          mediaSource: this.mediaSource,
+          encodePath: this.encodePath,
+          headers: this.requestHeaders
+        })
+      }
+
+      // Load quota
+      const user = await this.$client.users.getUser(this.user.id)
+
+      this.SET_QUOTA(user.quota)
+      this.loading = false
+    },
+
+    shareStatus(status) {
+      if (status === 0) {
+        return this.$gettext('Accepted')
+      }
+      if (status === 1) {
+        return this.$gettext('Pending')
+      }
+      if (status === 2) {
+        return this.$gettext('Declined')
+      }
+    }
+  }
+}
+</script>
