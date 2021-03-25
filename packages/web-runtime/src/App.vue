08b01c9c21082df6b67a4001043e7d584f7069c2
Lukas Hirt <lukashirt@mail.com>
2021-03-25 09:51:13 +0100

    Hide navigation on route change

--- packages/web-runtime/src/App.vue
+++ packages/web-runtime/src/App.vue
@@ -220,6 +220,7 @@ export default {
     $route: {
       immediate: true,
       handler: function(to) {
+        this.appNavigationVisible = false
         document.title = this.extractPageTitleFromRoute(to)
       }
     },
