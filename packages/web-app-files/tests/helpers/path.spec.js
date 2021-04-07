73f9e059d1d1ba80193118beae820928c7c8c014
Benedikt Kulmann <benedikt@kulmann.biz>
2021-04-07 12:36:57 +0200

    More unit test cases

--- packages/web-app-files/tests/helpers/path.spec.js
+++ packages/web-app-files/tests/helpers/path.spec.js
@@ -16,10 +16,22 @@ describe('build an array of parent paths from a provided path', () => {
     expect(paths).toEqual(['/a/b', '/a', ''])
   })
 
-  it('should make no difference between "a/b/c" and "/a/b/c"', () => {
+  it('should make no difference between "a/b/c" and "/a/b/c" with includeCurrent=false', () => {
     const paths1 = getParentPaths('a/b/c', false)
     const paths2 = getParentPaths('/a/b/c', false)
     expect(paths1).toEqual(paths2)
+  })
+
+  it('should make no difference between "a/b/c" and "/a/b/c" with includeCurrent=true', () => {
+    const paths1 = getParentPaths('a/b/c', true)
+    const paths2 = getParentPaths('/a/b/c', true)
+    expect(paths1).toEqual(paths2)
+  })
+
+  it('should have different results for different values of includeCurrent', () => {
+    const paths1 = getParentPaths('a/b/c', true)
+    const paths2 = getParentPaths('a/b/c', false)
+    expect(paths1).not.toEqual(paths2)
   })
 
   it('should not interpret a trailing slash as yet another path segment', () => {
