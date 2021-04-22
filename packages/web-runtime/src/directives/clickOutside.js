0ba758b6d16a661b2743d6baf520086248fea5d2
JanAckermann <jackermann@owncloud.com>
2021-04-22 16:53:10 +0200

    close right side bar on outside click

---
+++ packages/web-runtime/src/directives/clickOutside.js
@@ -0,0 +1,13 @@
+export default {
+  bind: function(el, binding, vnode) {
+    window.event = function(event) {
+      if (!(el === event.target || el.contains(event.target))) {
+        vnode.context[binding.expression](event)
+      }
+    }
+    document.body.addEventListener('click', window.event)
+  },
+  unbind: function(el) {
+    document.body.removeEventListener('click', window.event)
+  }
+}
