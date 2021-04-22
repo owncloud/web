0bf6fc28327969a3b2b94f19a2bac2cccf33106b
Jannik Stehle <jannik.stehle@gmail.com>
2021-04-22 17:56:39 +0200

    Remove aria label on file sidebar actions, fix folder href

--- packages/web-app-files/src/mixins/actions/copy.js
+++ packages/web-app-files/src/mixins/actions/copy.js
@@ -11,7 +11,7 @@ export default {
         {
           icon: 'file_copy',
           handler: this.$_copy_trigger,
-          ariaLabel: () => this.$gettext('Copy'),
+          label: () => this.$gettext('Copy'),
           isEnabled: () => {
             if (
               !checkRoute(
@@ -27,7 +27,8 @@ export default {
             }
 
             return true
-          }
+          },
+          componentType: 'oc-button'
         }
       ]
     }
