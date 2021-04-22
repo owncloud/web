7241fc952f80c7f0f4e8a6a1415039a4e4e2bf66
Jannik Stehle <jannik.stehle@gmail.com>
2021-04-22 17:56:39 +0200

    Remove aria label on file sidebar actions, fix folder href

--- packages/web-app-files/src/components/Sidebar/ActionsAccordion.vue
+++ packages/web-app-files/src/components/Sidebar/ActionsAccordion.vue
@@ -1,16 +1,15 @@
 <template>
   <ul id="oc-files-actions-sidebar" class="uk-list oc-mt-s">
-    <li v-for="action in actions" :key="action.ariaLabel(highlightedFile)" class="oc-py-xs">
+    <li v-for="action in actions" :key="action.label(highlightedFile)" class="oc-py-xs">
       <component
-        :is="getComponentType(action)"
-        v-bind="getComponentProps(action)"
-        :aria-Label="action.ariaLabel(highlightedFile)"
+        :is="action.componentType"
+        v-bind="getComponentProps(action, highlightedFile)"
         appearance="raw"
         class="oc-text-bold"
         @click.stop="action.handler(highlightedFile, action.handlerData)"
       >
         <oc-icon :name="action.icon" size="medium" />
-        {{ action.ariaLabel(highlightedFile) }}
+        {{ action.label(highlightedFile) }}
       </component>
     </li>
   </ul>
@@ -42,18 +41,15 @@ export default {
     }
   },
   methods: {
-    getComponentType(action) {
-      if (action.componentType) {
-        return action.componentType
-      }
-
-      return 'oc-button'
-    },
-    getComponentProps(action) {
-      const route = action.getRoute()
-      if (action.componentType === 'router-link' && route) {
+    getComponentProps(action, highlightedFile) {
+      if (action.componentType === 'router-link' && action.route) {
         return {
-          to: route
+          to: {
+            name: action.route,
+            params: {
+              item: highlightedFile.path
+            }
+          }
         }
       }
 
