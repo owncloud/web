cc2ba26a0fe9fde013770c3c65c8157271a995f3
Lukas Hirt <lukashirt@mail.com>
2021-04-23 14:00:21 +0200

    feat: move location picker hint under breadcrumbs

--- packages/web-runtime/src/store/sidebar.js
+++ packages/web-runtime/src/store/sidebar.js
@@ -1,18 +1,8 @@
 const state = {
-  navigationHidden: false,
-  mainContentComponent: null,
   sidebarFooterContentComponent: null
 }
 
 const mutations = {
-  SET_NAVIGATION_HIDDEN(state, hidden) {
-    state.navigationHidden = hidden
-  },
-
-  SET_MAIN_CONTENT_COMPONENT(state, component) {
-    state.mainContentComponent = component
-  },
-
   SET_SIDEBAR_FOOTER_CONTENT_COMPONENT(state, component) {
     state.sidebarFooterContentComponent = component
   }
