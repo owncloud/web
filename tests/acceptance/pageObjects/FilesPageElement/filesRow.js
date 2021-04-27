a435f45569f16409703d8318f644e964dd9d1282
Pascal Wengerter <pascal@wengerter.info>
2021-04-27 10:56:57 +0100

    Change from IDs to classes in quick action buttons

--- tests/acceptance/pageObjects/FilesPageElement/filesRow.js
+++ tests/acceptance/pageObjects/FilesPageElement/filesRow.js
@@ -50,7 +50,7 @@ module.exports = {
       locateStrategy: 'xpath'
     },
     quickAction: {
-      selector: '//button[@id="files-quick-action-%s"]',
+      selector: '//button[@class="files-quick-action-%s"]',
       locateStrategy: 'xpath'
     }
   }
