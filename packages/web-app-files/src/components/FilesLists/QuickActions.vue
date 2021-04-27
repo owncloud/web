11298514dd66dc347170115ec9eddd704d5e36c5
Pascal Wengerter <pascal@wengerter.info>
2021-04-27 10:56:57 +0100

    Change from IDs to classes in quick action buttons

--- packages/web-app-files/src/components/FilesLists/QuickActions.vue
+++ packages/web-app-files/src/components/FilesLists/QuickActions.vue
@@ -2,12 +2,12 @@
   <div class="uk-flex">
     <oc-button
       v-for="action in filteredActions"
-      :id="`files-quick-action-${action.id}`"
       :key="action.label"
       :aria-label="action.label"
       :uk-tooltip="action.label"
       appearance="raw"
       class="oc-mr-xs"
+      :class="`files-quick-action-${action.id}`"
       @click.stop="action.handler({ item, client: $client, store: $store })"
     >
       <oc-icon :name="action.icon" class="uk-flex" />
