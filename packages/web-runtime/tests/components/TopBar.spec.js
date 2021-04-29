a941dec1a4c0c69aa54559b181367d7a3b12654d
Robert Pocklington <rpocklin@gmail.com>
2021-04-29 19:06:56 +1000

    add unit tests

--- packages/web-runtime/tests/components/TopBar.spec.js
+++ packages/web-runtime/tests/components/TopBar.spec.js
@@ -3,21 +3,28 @@ import Vuex from 'vuex'
 import TopBar from 'web-runtime/src/components/TopBar.vue'
 import stubs from '../../../../tests/unit/config/stubs'
 
+import GetTextPlugin from 'vue-gettext'
+import translations from '../../l10n/translations.json'
+
 const localVue = createLocalVue()
 localVue.use(Vuex)
+localVue.use(GetTextPlugin, {translations })
 
 const globalSearch = enabled => ({
   options: { hideSearchBar: !enabled }
 })
 
 const routeWithHiddenSearchBar = () => ({ meta: { hideSearchBar: true } })
+const routeWithHiddenSideBar = () => ({ meta: { hideSideBar: true }})
 const defaultRoute = () => ({
   meta: {
     /* default is empty */
   }
 })
 
+
 describe('Top Bar component', () => {
+  describe('search bar', () => {
   describe('when search bar visible globally', () => {
     it('Displays search bar if not disabled for specified route', () => {
       const wrapper = shallowMount(TopBar, {
@@ -150,5 +157,52 @@ describe('Top Bar component', () => {
     wrapper.find('.oc-app-navigation-toggle').vm.$emit('click')
     await wrapper.vm.$nextTick()
     expect(wrapper.emitted().toggleAppNavigationVisibility).toBeTruthy()
+  })
+})
+  describe('side bar', () => {
+    describe('when the side bar is visible', () => {
+      it('Does not show the back button navigation', () => {
+        const wrapper = shallowMount(TopBar, {
+          store: new Vuex.Store({
+            getters: {
+              configuration: () => globalSearch(true)
+            }
+          }),
+          localVue,
+          stubs,
+          propsData: {
+            userId: 'einstein',
+            userDisplayName: 'Albert Einstein'
+          },
+          mocks: {
+            $route: defaultRoute()
+          }
+        })
+        expect(wrapper.find('#back-btn').exists()).toBe(false)
+        expect(wrapper).toMatchSnapshot()
+      })
+    })
+    describe('when the side bar is hidden', () => {
+      it('Show the back button navigation', () => {
+        const wrapper = shallowMount(TopBar, {
+          store: new Vuex.Store({
+            getters: {
+              configuration: () => globalSearch(true)
+            }
+          }),
+          localVue,
+          stubs,
+          propsData: {
+            userId: 'einstein',
+            userDisplayName: 'Albert Einstein'
+          },
+          mocks: {
+            $route: routeWithHiddenSideBar()
+          }
+        })
+        expect(wrapper.find('#back-btn').exists()).toBe(true)
+        expect(wrapper).toMatchSnapshot()
+      })
+    })
   })
 })
