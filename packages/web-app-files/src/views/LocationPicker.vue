b6d2d563a4933083da8426ee5acebe90054ebe0c
Lukas Hirt <lukashirt@mail.com>
2021-05-07 14:08:11 +0200

    feat: remove italic font style

--- packages/web-app-files/src/views/LocationPicker.vue
+++ packages/web-app-files/src/views/LocationPicker.vue
@@ -3,7 +3,7 @@
     <div tabindex="-1" class="files-list-wrapper uk-width-expand">
       <div id="files-app-bar" class="oc-p-s">
         <h1 class="location-picker-selection-info oc-mb" v-text="title" />
-        <p class="oc-text-muted uk-text-meta uk-text-italic" v-text="currentHint" />
+        <p class="oc-text-muted uk-text-meta" v-text="currentHint" />
         <hr class="oc-mt-rm" />
         <oc-breadcrumb :items="breadcrumbs" class="oc-mb-s" />
         <oc-grid gutter="small" flex class="uk-flex-middle">
