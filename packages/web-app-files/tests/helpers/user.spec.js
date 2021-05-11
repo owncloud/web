1c6ebbb3df41dd49e8ca76c50934dd82c431da08
Pascal Wengerter <pascal@wengerter.info>
2021-05-11 17:11:02 +0100

    Add caching case to user helper / lazy avatar loading

--- packages/web-app-files/tests/helpers/user.spec.js
+++ packages/web-app-files/tests/helpers/user.spec.js
@@ -6,16 +6,23 @@ describe('getAvatarSrc', () => {
     await expect(getAvatarSrc('userId', 'server', 'token')).rejects.toBeTruthy()
   })
 
+  it('returns a signed url', async () => {
+    fetch.mockResponse('', { status: 200 })
+    const client = { signUrl: jest.fn() }
+    await getAvatarSrc('userId', 'server', 'token', client)
+    expect(client.signUrl.mock.calls.length).toBe(1)
+  })
+
   it('returns the url if no valid client is present', async () => {
     fetch.mockResponse('', { status: 200 })
     const url = await getAvatarSrc('userId', 'server', 'token')
     expect(url).toBe('serverremote.php/dav/avatars/userId/128.png')
   })
 
-  it('returns a signed url', async () => {
+  it('fetches avatarSrc from cache if url exists there', async () => {
     fetch.mockResponse('', { status: 200 })
     const client = { signUrl: jest.fn() }
     await getAvatarSrc('userId', 'server', 'token', client)
-    expect(client.signUrl.mock.calls.length).toBe(1)
+    expect(client.signUrl.mock.calls.length).toBe(0)
   })
 })
