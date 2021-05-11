b2c7ffdd8604be1ef96851383acb2ae6fcbbc388
Florian Schade <f.schade@icloud.com>
2021-05-11 16:39:28 +0200

    refactor buildSharedResource tests into 2 steps

--- packages/web-app-files/tests/helpers/resources.spec.js
+++ packages/web-app-files/tests/helpers/resources.spec.js
@@ -1,44 +1,44 @@
 import { buildSharedResource } from '../../src/helpers/resources'
 
 describe('buildSharedResource', () => {
-  it('fetches avatars in the background', async () => {
-    const resourceGen = (incomingShares = false) => {
-      let doResolve
-      let pending = true
-      let fulfilled = false
+  const resourceFactory = (incomingShares = false) => {
+    let doResolve
+    let pending = true
+    let fulfilled = false
 
-      const resource = buildSharedResource(
-        {
-          file_target: '/path.pdf',
-          path: '/path.pdf',
-          displayname_owner: 'displayname_owner',
-          sharedWith: [
-            {
-              username: 'sharedWith',
-              avatar: undefined
-            }
-          ]
-        },
-        incomingShares,
-        '',
-        '',
-        '',
-        '',
-        r => {
-          fulfilled = true
-          pending = false
-          doResolve(r)
-        }
-      )
+    const resourceUpdater = new Promise(resolve => (doResolve = resolve))
+    resourceUpdater.pending = () => pending
+    resourceUpdater.fulfilled = () => fulfilled
 
-      const resourceUpdater = new Promise(resolve => (doResolve = resolve))
-      resourceUpdater.pending = () => pending
-      resourceUpdater.fulfilled = () => fulfilled
+    const resource = buildSharedResource(
+      {
+        file_target: '/path.pdf',
+        path: '/path.pdf',
+        displayname_owner: 'displayname_owner',
+        sharedWith: [
+          {
+            username: 'sharedWith',
+            avatar: undefined
+          }
+        ]
+      },
+      incomingShares,
+      '',
+      '',
+      '',
+      '',
+      r => {
+        fulfilled = true
+        pending = false
+        doResolve(r)
+      }
+    )
 
-      return { resource, resourceUpdater }
-    }
+    return { resource, resourceUpdater }
+  }
 
-    const { resource, resourceUpdater } = resourceGen()
+  it('fetches avatars for outgoing shares in the background', async () => {
+    const { resource, resourceUpdater } = resourceFactory()
     expect(resource.sharedWith[0].avatar).toBeFalsy()
     expect(resourceUpdater.pending()).toBeTruthy()
     expect(resourceUpdater.fulfilled()).toBeFalsy()
@@ -47,15 +47,17 @@ describe('buildSharedResource', () => {
     expect(updatedResource.sharedWith[0].avatar).toBeTruthy()
     expect(resourceUpdater.pending()).toBeFalsy()
     expect(resourceUpdater.fulfilled()).toBeTruthy()
+  })
 
-    const { resource: isResource, resourceUpdater: isResourceUpdater } = resourceGen(true)
-    expect(isResource.owner[0].avatar).toBeFalsy()
-    expect(isResourceUpdater.pending()).toBeTruthy()
-    expect(isResourceUpdater.fulfilled()).toBeFalsy()
+  it('fetches avatars for incoming shares in the background', async () => {
+    const { resource, resourceUpdater } = resourceFactory(true)
+    expect(resource.owner[0].avatar).toBeFalsy()
+    expect(resourceUpdater.pending()).toBeTruthy()
+    expect(resourceUpdater.fulfilled()).toBeFalsy()
 
-    const updatedIsResource = await isResourceUpdater
-    expect(updatedIsResource.owner[0].avatar).toBeTruthy()
-    expect(isResourceUpdater.pending()).toBeFalsy()
-    expect(isResourceUpdater.fulfilled()).toBeTruthy()
+    const updatedResource = await resourceUpdater
+    expect(updatedResource.owner[0].avatar).toBeTruthy()
+    expect(resourceUpdater.pending()).toBeFalsy()
+    expect(resourceUpdater.fulfilled()).toBeTruthy()
   })
 })
