852d6470ae042b21fa48a04cbdb08d63089f7486
Benedikt Kulmann <benedikt@kulmann.biz>
2021-06-02 23:39:50 +0200

    Fix selectors

--- tests/acceptance/pageObjects/FilesPageElement/fileActionsMenu.js
+++ tests/acceptance/pageObjects/FilesPageElement/fileActionsMenu.js
@@ -191,7 +191,7 @@ module.exports = {
       selector: '.oc-modal-body-actions-confirm'
     },
     dialogInput: {
-      selector: '.oc-modal-body-input > input'
+      selector: '.oc-modal-body-input .oc-text-input'
     },
     moveButtonInAccordion: {
       selector: '//button[normalize-space()="Move"]',
