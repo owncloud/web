b8f2a3613ab96187708ce2b7386aa38495c69c85
Florian Schade <f.schade@icloud.com>
2021-06-03 12:36:20 +0200

    basic cache implementation

---
+++ packages/web-pkg/src/utils/cache.spec.ts
@@ -0,0 +1,110 @@
+import { Cache } from './cache'
+
+const newCache = <T>(vs: T[], ttl?: number, capacity?: number): Cache<number, T> => {
+  const cache = new Cache<number, T>({ ttl, capacity })
+  vs.forEach((v, i) => cache.set(i, v))
+  return cache
+}
+
+describe('cache', () => {
+  it('can set and get entries', () => {
+    const cacheValues: number[] = [1, 2, 3, 4]
+    const cache = newCache(cacheValues)
+
+    cacheValues.forEach((v, i) => {
+      expect(cache.get(i)).toBe(v)
+    })
+
+    cache.set(4, 5)
+    expect(cache.get(4)).toBe(5)
+  })
+
+  it('return all keys', async () => {
+    const cache = newCache([1, 2, 3, 4])
+    expect(cache.keys()).toMatchObject([0, 1, 2, 3])
+  })
+
+  it('return all values', async () => {
+    const cacheValues: number[] = [1, 2, 3, 4]
+    const cache = newCache(cacheValues)
+    expect(cache.values()).toMatchObject(cacheValues)
+  })
+
+  it('return all entries', async () => {
+    const cache = newCache([1, 2, 3, 4])
+    expect(cache.entries()).toMatchObject([
+      [0, 1],
+      [1, 2],
+      [2, 3],
+      [3, 4]
+    ])
+  })
+
+  it('can handle ttl', async () => {
+    jest.useFakeTimers('modern')
+    const cacheValues: number[] = []
+    const cache = newCache(cacheValues, 50)
+
+    cache.set(1, 1)
+    jest.setSystemTime(new Date().getTime() + 10)
+    cache.set(2, 2)
+
+    expect(cache.get(1)).toBe(1)
+    expect(cache.get(2)).toBe(2)
+    expect(cache.values().length).toBe(2)
+    expect(cache.keys().length).toBe(2)
+    expect(cache.entries().length).toBe(2)
+
+    jest.setSystemTime(new Date().getTime() + 41)
+
+    expect(cache.get(1)).toBeFalsy()
+    expect(cache.get(2)).toBe(2)
+    expect(cache.values().length).toBe(1)
+    expect(cache.keys().length).toBe(1)
+    expect(cache.entries().length).toBe(1)
+
+    jest.setSystemTime(new Date().getTime() + 10)
+
+    expect(cache.get(2)).toBeFalsy()
+    expect(cache.values().length).toBe(0)
+    expect(cache.keys().length).toBe(0)
+    expect(cache.entries().length).toBe(0)
+
+    cache.set(3, 3, 10)
+    cache.set(4, 4, 20)
+
+    expect(cache.get(3)).toBe(3)
+    expect(cache.get(4)).toBe(4)
+    expect(cache.values().length).toBe(2)
+    expect(cache.keys().length).toBe(2)
+    expect(cache.entries().length).toBe(2)
+
+    jest.setSystemTime(new Date().getTime() + 11)
+
+    expect(cache.get(3)).toBeFalsy()
+    expect(cache.get(4)).toBe(4)
+    expect(cache.values().length).toBe(1)
+    expect(cache.keys().length).toBe(1)
+    expect(cache.entries().length).toBe(1)
+
+    jest.setSystemTime(new Date().getTime() + 10)
+
+    expect(cache.get(4)).toBeFalsy()
+    expect(cache.values().length).toBe(0)
+    expect(cache.keys().length).toBe(0)
+    expect(cache.entries().length).toBe(0)
+  })
+
+  it('can handle capacity', async () => {
+    const initialValues: number[] = [1, 2, 3, 4, 5]
+    const newValues: number[] = [6, 7, 8, 9, 10]
+    const capacity = 5
+    const cache = newCache(initialValues, 0, capacity)
+
+    newValues.forEach(v => {
+      cache.set(v, v)
+      expect(cache.get(v)).toBe(v)
+      expect(cache.entries().length).toBe(capacity)
+    })
+  })
+})
---
+++ packages/web-pkg/src/utils/cache.ts
@@ -0,0 +1,90 @@
+class Entry<T> {
+  public value: T
+  public expires: number
+
+  constructor(value: T, ttl: number) {
+    this.value = value
+    this.expires = ttl === 0 ? 0 : new Date().getTime() + ttl
+  }
+
+  get expired(): boolean {
+    return this.expires > 0 && this.expires < new Date().getTime()
+  }
+}
+
+export class Cache<K, V> {
+  private map: Map<K, Entry<V>>
+  private readonly ttl: number
+  private readonly capacity: number
+
+  constructor({ ttl, capacity }: { ttl?: number; capacity?: number } = {}) {
+    this.ttl = ttl || 0
+    this.capacity = capacity || 0
+
+    this.map = new Map()
+  }
+
+  public set(key: K, value: V, ttl?: number): V {
+    this.map.set(key, new Entry<V>(value, ttl || this.ttl))
+
+    if (this.capacity && this.map.size > this.capacity) {
+      const itemsToDelete = this.map.size - this.capacity
+      let i = 1
+      for (const [mk] of this.map.entries()) {
+        if (i > itemsToDelete) {
+          break
+        }
+        this.delete(mk)
+        i++
+      }
+    }
+
+    return value
+  }
+
+  public get(key: K): V {
+    const entry = this.map.get(key)
+    if (!entry || entry.expired) {
+      this.delete(key)
+      return undefined
+    }
+
+    return entry.value
+  }
+
+  public delete(key: K): boolean {
+    return this.map.delete(key)
+  }
+
+  public clear(): void {
+    return this.map.clear()
+  }
+
+  public entries(): [K, V][] {
+    const kv: [K, V][] = []
+
+    this.map.forEach((mv, mk) => {
+      if (mv.expired) {
+        this.delete(mk)
+        return
+      }
+      kv.push([mk, mv.value])
+    })
+
+    return kv
+  }
+
+  public keys(): K[] {
+    const k: K[] = []
+    this.entries().forEach(e => k.push(e[0]))
+
+    return k
+  }
+
+  public values(): V[] {
+    const v: V[] = []
+    this.entries().forEach(e => v.push(e[1]))
+
+    return v
+  }
+}
