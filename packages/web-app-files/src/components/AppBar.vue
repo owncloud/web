abhishekshiju/owncloud

Add mode prop for opening file editors

packages/web-app-files/src/components/AppBar.vue
@@ -108,7 +108,7 @@ import pathUtil from 'path'
 import isEmpty from 'lodash-es/isEmpty'
 
 import Mixins from '../mixins'
-import MixinFileActions from '../mixins/fileActions'
+import MixinFileActions, { editorModeCreate } from '../mixins/fileActions'
 import MixinRoutes from '../mixins/routes'
 import MixinScrollToResource from '../mixins/filesListScrolling'
 import { buildResource } from '../helpers/resources'
@@ -419,7 +419,7 @@ export default {
         if (this.newFileAction) {
           const fileId = resource.fileInfo['{http://owncloud.org/ns}fileid']
 
-          this.$_fileActions_openEditor(this.newFileAction, path, fileId)
+          this.$_fileActions_openEditor(this.newFileAction, path, fileId, editorModeCreate)
           this.hideModal()
 
           return

packages/web-app-files/src/mixins/fileActions.js
@@ -24,6 +24,9 @@ const actionsMixins = [
   'delete'
 ]
 
+export const editorModeEdit = 'edit'
+export const editorModeCreate = 'create'
+
 export default {
   mixins: [Copy, Delete, Download, Favorite, Fetch, Move, Navigate, Rename, Restore],
   computed: {
@@ -53,7 +56,8 @@ export default {
             )
           },
           icon: this.apps.meta[editor.app].icon,
-          handler: item => this.$_fileActions_openEditor(editor, item.path, item.id),
+          handler: item =>
+            this.$_fileActions_openEditor(editor, item.path, item.id, editorModeEdit),
           isEnabled: ({ resource }) => {
             if (editor.routes?.length > 0 && !checkRoute(editor.routes, this.$route.name)) {
               return false
@@ -74,25 +78,26 @@ export default {
   methods: {
     ...mapActions(['openFile']),
 
-    $_fileActions_openEditor(editor, filePath, fileId) {
+    $_fileActions_openEditor(editor, filePath, fileId, mode) {
       if (editor.handler) {
         return editor.handler({
           config: this.configuration,
           extensionConfig: editor.config,
           filePath,
-          fileId
+          fileId,
+          mode
         })
       }
 
       // TODO: Refactor in the store
       this.openFile({
-        filePath: filePath
+        filePath
       })
 
       if (editor.newTab) {
         const path = this.$router.resolve({
           name: editor.routeName,
-          params: { filePath: filePath }
+          params: { filePath, fileId, mode }
         }).href
         const target = `${editor.routeName}-${filePath}`
         const win = window.open(path, target)
@@ -103,15 +108,14 @@ export default {
         return
       }
 
-      const routeName = editor.routeName || editor.app
-      const params = {
-        filePath,
-        contextRouteName: this.$route.name
-      }
-
       this.$router.push({
-        name: routeName,
-        params
+        name: editor.routeName || editor.app,
+        params: {
+          filePath,
+          fileId,
+          mode,
+          contextRouteName: this.$route.name
+        }
       })
     },
 

owncloud/web

packages/web-app-files/src/components/AppBar.vue
@@ -108,7 +108,7 @@ import pathUtil from 'path'
 import isEmpty from 'lodash-es/isEmpty'
 
 import Mixins from '../mixins'
-import MixinFileActions from '../mixins/fileActions'
+import MixinFileActions, { EDITOR_MODE_CREATE } from '../mixins/fileActions'
 import MixinRoutes from '../mixins/routes'
 import MixinScrollToResource from '../mixins/filesListScrolling'
 import { buildResource } from '../helpers/resources'
@@ -419,7 +419,7 @@ export default {
         if (this.newFileAction) {
           const fileId = resource.fileInfo['{http://owncloud.org/ns}fileid']
 
-          this.$_fileActions_openEditor(this.newFileAction, path, fileId)
+          this.$_fileActions_openEditor(this.newFileAction, path, fileId, EDITOR_MODE_CREATE)
           this.hideModal()
 
           return

packages/web-app-files/src/mixins/fileActions.js
@@ -24,6 +24,9 @@ const actionsMixins = [
   'delete'
 ]
 
+export const EDITOR_MODE_EDIT = 'edit'
+export const EDITOR_MODE_CREATE = 'create'
+
 export default {
   mixins: [Copy, Delete, Download, Favorite, Fetch, Move, Navigate, Rename, Restore],
   computed: {
@@ -53,7 +56,8 @@ export default {
             )
           },
           icon: this.apps.meta[editor.app].icon,
-          handler: item => this.$_fileActions_openEditor(editor, item.path, item.id),
+          handler: item =>
+            this.$_fileActions_openEditor(editor, item.path, item.id, EDITOR_MODE_EDIT),
           isEnabled: ({ resource }) => {
             if (editor.routes?.length > 0 && !checkRoute(editor.routes, this.$route.name)) {
               return false
@@ -74,25 +78,26 @@ export default {
   methods: {
     ...mapActions(['openFile']),
 
-    $_fileActions_openEditor(editor, filePath, fileId) {
+    $_fileActions_openEditor(editor, filePath, fileId, mode) {
       if (editor.handler) {
         return editor.handler({
           config: this.configuration,
           extensionConfig: editor.config,
           filePath,
-          fileId
+          fileId,
+          mode
         })
       }
 
       // TODO: Refactor in the store
       this.openFile({
-        filePath: filePath
+        filePath
       })
 
       if (editor.newTab) {
         const path = this.$router.resolve({
           name: editor.routeName,
-          params: { filePath: filePath }
+          params: { filePath, fileId, mode }
         }).href
         const target = `${editor.routeName}-${filePath}`
         const win = window.open(path, target)
@@ -103,15 +108,14 @@ export default {
         return
       }
 
-      const routeName = editor.routeName || editor.app
-      const params = {
-        filePath,
-        contextRouteName: this.$route.name
-      }
-
       this.$router.push({
-        name: routeName,
-        params
+        name: editor.routeName || editor.app,
+        params: {
+          filePath,
+          fileId,
+          mode,
+          contextRouteName: this.$route.name
+        }
       })
     },
 