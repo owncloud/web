3a19aa6281828da7a81be46aa921f40da7edbdc3
Florian Schade <f.schade@icloud.com>
2021-06-17 14:32:33 +0200

    add tests for publicPreviewUrl

---
+++ packages/web-app-files/tests/helpers/resource/publicPreviewUrl.spec.ts
@@ -0,0 +1,31 @@
+import { publicPreviewUrl } from '../../../src/helpers/resource'
+import mockAxios from 'jest-mock-axios'
+import merge from 'lodash-es/merge'
+
+beforeEach(mockAxios.reset)
+
+const defaultOptions = {
+  resource: {
+    etag: 'etag',
+    downloadURL: 'downloadURL'
+  }
+}
+
+describe('publicPreviewUrl', () => {
+  test('publicPreviewUrl', async () => {
+    window.URL.createObjectURL = jest.fn()
+    let publicPreviewUrlPromise = publicPreviewUrl(defaultOptions)
+    mockAxios.mockResponse({ data: undefined })
+
+    let url = await publicPreviewUrlPromise
+    await expect(url).toBe('downloadURL?a=1&c=etag&preview=1&scalingup=0')
+    await expect(mockAxios.head).toBeCalledTimes(1)
+
+    publicPreviewUrlPromise = publicPreviewUrl(defaultOptions)
+    mockAxios.mockResponse({ data: undefined, status: 404 })
+
+    url = await publicPreviewUrlPromise
+    await expect(url).toBeFalsy()
+    await expect(mockAxios.head).toBeCalledTimes(2)
+  })
+})
