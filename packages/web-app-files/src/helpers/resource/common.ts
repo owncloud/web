65b8932a1e3bc01d0037cf482ab36bca9352a27c
Florian Schade <f.schade@icloud.com>
2021-06-17 12:20:47 +0200

    fix 0 falsy eval in buildQueryString add buildQueryString tests

--- packages/web-app-files/src/helpers/resource/common.ts
+++ packages/web-app-files/src/helpers/resource/common.ts
@@ -11,10 +11,10 @@ interface buildQueryStringOptions {
 export const buildQueryString = (options: buildQueryStringOptions): string => {
   return queryString.stringify({
     scalingup: options.scalingup || 0,
-    preview: options.preview || 1,
-    a: options.a || 1,
+    preview: Object.hasOwnProperty.call(options, 'preview') ? options.preview : 1,
+    a: Object.hasOwnProperty.call(options, 'a') ? options.a : 1,
     ...(options.etag && { c: options.etag.replaceAll('"', '') }),
-    ...(options.dimensions[0] && { x: options.dimensions[0] }),
-    ...(options.dimensions[1] && { y: options.dimensions[1] })
+    ...(options.dimensions && options.dimensions[0] && { x: options.dimensions[0] }),
+    ...(options.dimensions && options.dimensions[1] && { y: options.dimensions[1] })
   })
 }
