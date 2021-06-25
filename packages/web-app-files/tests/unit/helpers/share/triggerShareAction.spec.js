3ff7e9d7e6c0991a3f0719c9fe7c269e3b03366f
Benedikt Kulmann <benedikt@kulmann.biz>
2021-06-25 13:07:30 +0200

    Move unit test from SharedWithMe view to helper

---
+++ packages/web-app-files/tests/unit/helpers/share/triggerShareAction.spec.js
@@ -0,0 +1,50 @@
+import { triggerShareAction } from '../../../../src/helpers/share/triggerShareAction'
+import { shareStatus } from '../../../../src/helpers/shareStatus'
+
+import OwnCloud from 'owncloud-sdk'
+
+jest.unmock('axios')
+const $client = new OwnCloud()
+
+jest.mock('../../../../src/helpers/resources', () => ({
+  buildSharedResource: jest.fn(share => share)
+}))
+
+describe('method triggerShareAction', () => {
+  beforeEach(() => {
+    fetch.resetMocks()
+  })
+
+  it('throws error if invalid share status given', async () => {
+    const statusText = 'invalid new share status'
+    await expect(triggerShareAction(null, shareStatus.pending, true, null)).rejects.toThrow(
+      statusText
+    )
+  })
+
+  it('throws error if share action response status is not 200', async () => {
+    const statusText = 'status is not 200'
+    fetch.mockResponse(new Error(), { status: 404, statusText })
+    await expect(
+      triggerShareAction({ share: { id: 1 } }, shareStatus.accepted, true, $client)
+    ).rejects.toThrow(statusText)
+  })
+
+  it('silently fails when the response has a content-length header value of 0', async () => {
+    fetch.mockResponse('', {
+      headers: { 'content-length': 0 }
+    })
+    await expect(
+      triggerShareAction({ share: { id: 1 } }, shareStatus.accepted, true, $client)
+    ).resolves.toBeNull()
+  })
+
+  it('returns a resource of type share if content-length header is present and valid', async () => {
+    fetch.mockResponse(JSON.stringify({ ocs: { data: [{ id: 1 }] } }), {
+      headers: { 'content-length': 1 }
+    })
+    await expect(
+      triggerShareAction({ share: { id: 1 } }, shareStatus.accepted, true, $client)
+    ).resolves.toMatchObject({ id: 1 })
+  })
+})
