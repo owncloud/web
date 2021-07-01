fee2fb57e81f3e1c74fc33e1a2443ec866357759
Benedikt Kulmann <benedikt@kulmann.biz>
2021-07-01 16:10:43 +0200

    Revert share loading when sidebar opens as it's not needed, yet

--- packages/web-app-files/src/mixins/sidebar/mountSideBar.js
+++ packages/web-app-files/src/mixins/sidebar/mountSideBar.js
@@ -1,31 +1,9 @@
-import { mapActions, mapGetters, mapMutations } from 'vuex'
-import { buildCollaboratorShare } from '../../helpers/resources'
-import { cloneDeep } from 'lodash-es'
+import { mapActions } from 'vuex'
 
 export default {
-  computed: {
-    ...mapGetters(['isOcis'])
-  },
   methods: {
-    ...mapMutations('Files', ['UPDATE_RESOURCE']),
     ...mapActions('Files', ['setHighlightedFile']),
-    async $_mountSideBar_showDetails(resource) {
-      // FIXME: to be improved when switching to graph api
-      if (resource.isReceivedShare() && !resource.share) {
-        // there are some pages where the share information is not pre-loaded. fetch on demand (upon mounting the sidebar)
-        const response = await this.$client.shares.getShares(resource.path, {
-          shared_with_me: true
-        })
-        const shares = response.map(element => {
-          return buildCollaboratorShare(element.shareInfo, resource, !this.isOcis)
-        })
-        if (shares?.length === 1) {
-          const r = cloneDeep(resource)
-          r.share = shares[0]
-          this.UPDATE_RESOURCE(r)
-        }
-      }
-
+    $_mountSideBar_showDetails(resource) {
       this.setHighlightedFile(resource)
     }
   }
