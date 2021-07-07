8162137e61c17487ead5566f53c3b10d7ca3b908
Benedikt Kulmann <benedikt@kulmann.biz>
2021-07-07 11:11:02 +0200

    Add feedback link to topbar

---
+++ packages/web-runtime/src/components/FeedbackLink.vue
@@ -0,0 +1,28 @@
+<template>
+  <oc-button
+    v-oc-tooltip="label"
+    type="a"
+    :href="href"
+    target="_blank"
+    appearance="raw"
+    :aria-label="label"
+  >
+    <oc-icon name="comment" />
+  </oc-button>
+</template>
+
+<script>
+export default {
+  name: 'FeedbackLink',
+  computed: {
+    href() {
+      return 'https://owncloud.com/web-design-feedback'
+    },
+    label() {
+      return this.$gettext(
+        "We'd like to improve the web design and would be happy to hear your feedback. Thank you! Your ownCloud team."
+      )
+    }
+  }
+}
+</script>
