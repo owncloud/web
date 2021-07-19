3eaa1d93af242b1384524b4850c7251f1abe29f4
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-07-19 14:56:40 +0545

    Use router link stub to assert link to prop

--- packages/web-app-files/tests/unit/components/FilesList/Pagination.spec.js
+++ packages/web-app-files/tests/unit/components/FilesList/Pagination.spec.js
@@ -1,13 +1,60 @@
-import Pagination from '../../../../src/components/FilesList/Pagination.vue'
-import { createLocalVue, mount, shallowMount } from '@vue/test-utils'
 import Vuex from 'vuex'
+import GetTextPlugin from 'vue-gettext'
 import DesignSystem from 'owncloud-design-system'
+import Pagination from '../../../../src/components/FilesList/Pagination.vue'
+import { createLocalVue, mount, RouterLinkStub, shallowMount } from '@vue/test-utils'
 
 const localVue = createLocalVue()
-localVue.use(DesignSystem)
 localVue.use(Vuex)
+localVue.use(DesignSystem)
+localVue.use(GetTextPlugin, {
+  translations: 'does-not-matter.json',
+  silent: true
+})
 
 describe('Pagination', () => {
+  describe('pages', () => {
+    describe('when value is less than or equals one', () => {
+      it.each([-1, 0, 1])('should not show wrapper', pages => {
+        const store = createStore(0, pages)
+        const wrapper = getWrapper(store)
+
+        expect(wrapper.find(selectors.filesPagination).exists()).toBeFalsy()
+      })
+    })
+
+    describe('when value is greater than one', () => {
+      const wrapper = getWrapper(createStore(1, 2))
+
+      it('should show wrapper', () => {
+        const paginationEl = wrapper.find('.files-pagination')
+
+        expect(paginationEl.exists()).toBeTruthy()
+        expect(paginationEl.attributes().pages).toBe('2')
+      })
+
+      it('should set provided current page', () => {
+        const paginationEl = wrapper.find(selectors.filesPagination)
+
+        expect(paginationEl.attributes().currentpage).toBe('1')
+      })
+    })
+  })
+
+  describe('current route', () => {
+    it('should use provided route to render pages', () => {
+      const wrapper = getMountedWrapper(createStore())
+      const currentRoute = wrapper.vm.$route
+      const links = wrapper.findAllComponents(RouterLinkStub)
+
+      // three links (route to prev, next and last page)
+      expect(links.length).toBe(3)
+      expect(links.at(0).props().to.name).toBe(currentRoute.name)
+      expect(links.at(1).props().to.name).toBe(currentRoute.name)
+      expect(links.at(2).props().to.name).toBe(currentRoute.name)
+    })
+  })
+
   const filesPersonalRoute = { name: 'files-personal', path: '/files/home' }
 
   const selectors = {
@@ -40,10 +87,7 @@ describe('Pagination', () => {
         'oc-pagination': true
       },
       mocks: {
-        $route: {
-          name: 'some-path',
-          path: '/some/path'
-        }
+        $route: filesPersonalRoute
       }
     })
   }
@@ -53,49 +97,12 @@ describe('Pagination', () => {
       localVue,
       store: store,
       stubs: {
-        'oc-pagination': true
+        'oc-pagination': false,
+        RouterLink: RouterLinkStub
       },
       mocks: {
         $route: filesPersonalRoute
       }
     })
   }
-
-  describe('pages', () => {
-    describe('when value is less than or equals one', () => {
-      it.each([-1, 0, 1])('should not show wrapper', pages => {
-        const store = createStore(0, pages)
-        const wrapper = getWrapper(store)
-
-        expect(wrapper.find(selectors.filesPagination).exists()).toBeFalsy()
-      })
-    })
-
-    describe('when value is greater than one', () => {
-      const store = createStore(1, 2)
-      const wrapper = getWrapper(store)
-
-      it('should show wrapper', () => {
-        const paginationEl = wrapper.find('.files-pagination')
-
-        expect(paginationEl.exists()).toBeTruthy()
-        expect(paginationEl.attributes().pages).toBe('2')
-      })
-
-      it('should set provided current page', () => {
-        const paginationEl = wrapper.find(selectors.filesPagination)
-
-        expect(paginationEl.attributes().currentpage).toBe('1')
-      })
-    })
-  })
-
-  describe('current route', () => {
-    it('should set current route as pagination current route', async () => {
-      const wrapper = await getMountedWrapper(createStore(1, 2))
-      const pagination = await wrapper.find(selectors.filesPagination)
-      // should be current route path but getting undefined 😪
-      expect(pagination.props().currentroute).toBe(undefined)
-    })
-  })
 })
