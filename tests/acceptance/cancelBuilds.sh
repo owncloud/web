8fd6e50ae857f981397b358801087cbdc501dfba
Jasmine Baral <jasmine.baral@hotmail.com>
2021-07-27 16:35:53 +0545

    use grep

--- tests/acceptance/cancelBuilds.sh
+++ tests/acceptance/cancelBuilds.sh
@@ -2,20 +2,22 @@ SCRIPT_PATH=$(dirname "$0")
 SCRIPT_PATH=$( cd "${SCRIPT_PATH}" && pwd )  # normalized and made absolute
 
 awk "/Build #|Ref: refs\/pull/" ${SCRIPT_PATH}/../../recentBuilds.txt > ${SCRIPT_PATH}/filteredDescriptions.txt
-cat ${SCRIPT_PATH}/filteredDescriptions.txt
 awk "/Ref: refs\/pull/" ${SCRIPT_PATH}/../../thisBuildInfo.txt > ${SCRIPT_PATH}/thisBuildFiltered.txt
-echo "2"
 cat ${SCRIPT_PATH}/thisBuildFiltered.txt
-NUMBER=$(awk -F"\/head" "{print $(1)}" ${SCRIPT_PATH}/thisBuildFiltered.txt | awk -F"pull\/" "{print $(2)}")
-echo "3"
-echo $NUMBER
-echo "4"
-awk -v ref="Ref: refs\/pull\/" $NUMBER "\/head" "index($0,ref){print p} {p=$0}" ${SCRIPT_PATH}/filteredDescriptions.txt > ${SCRIPT_PATH}/buildsToStop.txt
+NUMBER=$(grep -o -E "[0-9]+" ${SCRIPT_PATH}/thisBuildFiltered.txt)
+referenceNum="Ref: refs/pull/"$NUMBER"/head"
+awk -v ref="$referenceNum" 'index($0,ref){print p} {p=$0}' ${SCRIPT_PATH}/filteredDescriptions.txt > ${SCRIPT_PATH}/buildsToStop.txt
 cat ${SCRIPT_PATH}/buildsToStop.txt
 
 while IFS="" read -r p || [ -n "$p" ]
 do
   printf '%s\n' "$p"
- echo "$p" | awk -F'#' '{print $(2)}'
+ buildNumber=$(echo "$p" | awk -F'#' '{print $(2)}')
+ echo $DRONE_BUILD_NUMBER
+ echo $buildNumber
+ if [[ $DRONE_BUILD_NUMBER > $buildNumber ]]; then
+     echo "THIS BUILD SHOULD BE CANCELLED: " $buildNumber
+     drone build stop owncloud/web $buildNumber
+ fi
  echo "done"
 done <${SCRIPT_PATH}/buildsToStop.txt
