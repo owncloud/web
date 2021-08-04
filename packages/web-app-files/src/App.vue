c9e326209a80a25eb5173227f86f0dbf57d48bf8
Paul Neubauer <paulneubauer@live.de>
2021-08-04 13:18:41 +0200

    fixed interfering with file upload drop

--- packages/web-app-files/src/App.vue
+++ packages/web-app-files/src/App.vue
@@ -100,8 +100,18 @@ export default {
         revert: event === 'beforeDestroy'
       })
     },
-
-    $_ocApp_dragOver() {
+    containsFiles(event) {
+      if (event.dataTransfer.types) {
+        for (let i = 0; i < event.dataTransfer.types.length; i++) {
+          if (event.dataTransfer.types[i] === 'Files') {
+            return true
+          }
+        }
+      }
+      return false
+    },
+    $_ocApp_dragOver(e) {
+      if (!this.containsFiles(e)) return
       this.dragOver(true)
     }
   }
