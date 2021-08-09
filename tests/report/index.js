90f6217fa7d9f922b82b273f9573063b7fb61091
ScharfViktor <scharf.vi@gmail.com>
2021-08-09 12:19:07 +0200

    fix after lint-test

--- tests/report/index.js
+++ tests/report/index.js
@@ -1,22 +1,21 @@
-var reporter = require('cucumber-html-reporter');
+const reporter = require('cucumber-html-reporter')
 
-var options = {
-        theme: 'bootstrap',
-        jsonFile: 'tests/report/cucumber_report.json',
-        output: 'tests/report/cucumber_report.html',
-        scenarioTimestamp: true,
-        reportSuiteAsScenarios: true,
-        scenarioTimestamp: true,
-        launchReport: true,
-        metadata: {
-            "Ocis Version":"1.9.0",
-            "Web Version":"4.0.0",
-            "Test Environment": "docker",
-            "Browser": "Chrome  54.0.2840.98",
-            "Platform": "MacOs Big Sure v11.4",
-            "Parallel": "Scenarios",
-            "Executed": "Remote"
-        }
-    };
+const options = {
+  theme: 'bootstrap',
+  jsonFile: 'tests/report/cucumber_report.json',
+  output: 'tests/report/cucumber_report.html',
+  reportSuiteAsScenarios: true,
+  scenarioTimestamp: true,
+  launchReport: true,
+  metadata: {
+    'Ocis Version': '1.9.0',
+    'Web Version': '4.0.0',
+    'Test Environment': 'docker',
+    Browser: 'Chrome  54.0.2840.98',
+    Platform: 'MacOs Big Sure v11.4',
+    Parallel: 'Scenarios',
+    Executed: 'Remote'
+  }
+}
 
-    reporter.generate(options);
+reporter.generate(options)
