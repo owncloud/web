c9a2e58dcf8d22aa96736e0f2e5d11b90633b12e
Paul Neubauer <paulneubauer@live.de>
2021-08-17 17:11:33 +0200

    added no selection panel

---
+++ packages/web-app-files/src/components/SideBar/NoSelection.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="oc-no-selection">
+    <div class="files-preview oc-mb">
+      <div class="files-preview-body">
+        <oc-icon class="preview-icon" size="xxlarge" variation="passive" name="file_copy" />
+        <span class="preview-text" data-testid="selectedFilesText" v-text="selectedFilesString" />
+      </div>
+    </div>
+    <div>
+    </div>
+  </div>
+</template>
+<script>
+import Mixins from '../../mixins'
+import MixinResources from '../../mixins/resources'
+import MixinRoutes from '../../mixins/routes'
+
+export default {
+  mixins: [Mixins, MixinResources, MixinRoutes],
+  title: $gettext => {
+    return $gettext('Details')
+  },
+  computed: {
+    selectedFilesString() {
+      return this.$gettext('Select a file or folder to view details.')
+    },
+  }
+}
+</script>
+<style lang="scss" scoped>
+.files-preview {
+  position: relative;
+  border: 10px solid transparent;
+  height: 230px;
+  text-align: center;
+  color: var(--oc-color-swatch-passive-muted);
+
+  &-body {
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+
+    .preview-icon {
+      display: inline-block;
+    }
+    .preview-text {
+      display: block;
+    }
+  }
+}
+.details-table {
+  text-align: left;
+
+  tr {
+    height: 1.5rem;
+  }
+
+  th {
+    font-weight: 600;
+  }
+}
+</style>
