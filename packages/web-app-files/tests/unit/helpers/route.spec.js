f990d9d611a727988032fab41913f9b1fa2f4fe8
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-09-10 10:36:12 +0545

    Implemented todo unit tests for route helper

--- packages/web-app-files/tests/unit/helpers/route.spec.js
+++ packages/web-app-files/tests/unit/helpers/route.spec.js
@@ -1,48 +1,100 @@
+import {
+  checkRoute,
+  isAnySharedWithRoute,
+  isFavoritesRoute,
+  isPersonalRoute,
+  isPublicFilesRoute,
+  isPublicPage,
+  isSharedWithMeRoute,
+  isSharedWithOthersRoute,
+  isTrashbinRoute
+} from '@files/src/helpers/route.js'
+
 describe('route', () => {
   describe('checkRoute', () => {
-    it.todo('should return "true" if given "routes" array contains given "currentRoute"')
-    it.todo('should return "false" if given "routes" array does not contains given "currentRoute"')
+    it('should return "true" if given "routes" array contains given "currentRoute"', () => {
+      expect(checkRoute(['some-route'], 'some-route')).toBe(true)
+    })
+    it('should return "false" if given "routes" array does not contains given "currentRoute"', () => {
+      expect(checkRoute(['some-route'], 'another-route')).toBe(false)
+    })
   })
 
   describe('isPersonalRoute', () => {
-    it.todo('should return "true" if given route name is "files-personal"')
-    it.todo('should return "false" if given route name is not "files-personal"')
+    it('should return "true" if given route name is "files-personal"', () => {
+      expect(isPersonalRoute({ name: 'files-personal' })).toBe(true)
+    })
+    it('should return "false" if given route name is not "files-personal"', () => {
+      expect(isPersonalRoute({ name: 'files-favorites' })).toBe(false)
+    })
   })
 
   describe('isFavoritesRoute', () => {
-    it.todo('should return "true" if given route name is "files-favorites"')
-    it.todo('should return "false" if given route name is not "files-favorites"')
+    it('should return "true" if given route name is "files-favorites"', () => {
+      expect(isFavoritesRoute({ name: 'files-favorites' })).toBe(true)
+    })
+    it('should return "false" if given route name is not "files-favorites"', () => {
+      expect(isFavoritesRoute({ name: 'files-personal' })).toBe(false)
+    })
   })
 
   describe('isTrashbinRoute', () => {
-    it.todo('should return "true" if given route name is "files-trashbin"')
-    it.todo('should return "false" if given route name is not "files-trashbin"')
+    it('should return "true" if given route name is "files-trashbin"', () => {
+      expect(isTrashbinRoute({ name: 'files-trashbin' })).toBe(true)
+    })
+    it('should return "false" if given route name is not "files-trashbin"', () => {
+      expect(isTrashbinRoute({ name: 'files-personal' })).toBe(false)
+    })
   })
 
   describe('isSharedWithMeRoute', () => {
-    it.todo('should return "true" if given route name is "files-shared-with-me"')
-    it.todo('should return "false" if given route name is not "files-shared-with-me"')
+    it('should return "true" if given route name is "files-shared-with-me"', () => {
+      expect(isSharedWithMeRoute({ name: 'files-shared-with-me' })).toBe(true)
+    })
+    it('should return "false" if given route name is not "files-shared-with-me"', () => {
+      expect(isSharedWithMeRoute({ name: 'files-personal' })).toBe(false)
+    })
   })
 
   describe('isSharedWithOthersRoute', () => {
-    it.todo('should return "true" if given route name is "files-shared-with-others"')
-    it.todo('should return "false" if given route name is not "files-shared-with-others"')
+    it('should return "true" if given route name is "files-shared-with-others"', () => {
+      expect(isSharedWithOthersRoute({ name: 'files-shared-with-others' })).toBe(true)
+    })
+    it('should return "false" if given route name is not "files-shared-with-others"', () => {
+      expect(isSharedWithOthersRoute({ name: 'files-shared-with-me' })).toBe(false)
+    })
   })
 
   describe('isAnySharedWithRoute', () => {
-    it.todo('should return "false" if given route is not "SharedWithMe" and "SharedWithOthers"')
-    it.todo('should return "true" if given route is "SharedWithMe"')
-    it.todo('should return "true" if given route is "SharedWithOthers"')
+    it('should return "false" if given route is not "SharedWithMe" and "SharedWithOthers"', () => {
+      expect(isAnySharedWithRoute({ name: 'files-personal' })).toBe(false)
+    })
+    it('should return "true" if given route is "SharedWithMe"', () => {
+      expect(isAnySharedWithRoute({ name: 'files-shared-with-me' })).toBe(true)
+    })
+    it('should return "true" if given route is "SharedWithOthers"', () => {
+      expect(isAnySharedWithRoute({ name: 'files-shared-with-others' })).toBe(true)
+    })
   })
 
   describe('isPublicFilesRoute', () => {
-    it.todo('should return "true" if given route name is "files-public-list"')
-    it.todo('should return "false" if given route name is not "files-public-list"')
+    it('should return "true" if given route name is "files-public-list"', () => {
+      expect(isPublicFilesRoute({ name: 'files-public-list' })).toBe(true)
+    })
+    it('should return "false" if given route name is not "files-public-list"', () => {
+      expect(isPublicFilesRoute({ name: 'files-shared-with-others' })).toBe(false)
+    })
   })
 
   describe('isPublicPage', () => {
-    it.todo('should return "false" if given route has no meta information')
-    it.todo('should return "false" if given route meta auth is "true"')
-    it.todo('should return "true" if given route meta auth is "false"')
+    it('should return "false" if given route has no meta information', () => {
+      expect(isPublicPage({ name: 'files-public-list' })).toBe(false)
+    })
+    it('should return "false" if given route meta auth is "true"', () => {
+      expect(isPublicPage({ meta: { auth: true } })).toBe(false)
+    })
+    it('should return "true" if given route meta auth is "false"', () => {
+      expect(isPublicPage({ meta: { auth: false } })).toBe(true)
+    })
   })
 })
