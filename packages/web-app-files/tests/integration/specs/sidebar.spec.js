d5487a32d796b30b296ef09e5bd61ff3695cb717
Benedikt Kulmann <benedikt@kulmann.biz>
2021-09-20 11:13:30 +0200

    Fix integration test about sidebar

--- packages/web-app-files/tests/integration/specs/sidebar.spec.js
+++ packages/web-app-files/tests/integration/specs/sidebar.spec.js
@@ -52,6 +52,7 @@ const store = {
 describe('Files sidebar', () => {
   test('Links panel displays private link for a shared file', async () => {
     config.mocks.publicPage = () => false
+    config.mocks.$client.shares.getShares = () => new Promise(resolve => resolve([]))
 
     const { getByTestId, findByTestId, findByText } = render(
       Sidebar,
