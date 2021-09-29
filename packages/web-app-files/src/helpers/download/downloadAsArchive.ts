332a051e6cc82321ef72830c1a037a24172f69f9
Benedikt Kulmann <benedikt@kulmann.biz>
2021-09-29 15:32:07 +0200

    Fix code smell

--- packages/web-app-files/src/helpers/download/downloadAsArchive.ts
+++ packages/web-app-files/src/helpers/download/downloadAsArchive.ts
@@ -26,35 +26,30 @@ export const triggerDownloadAsArchive = async (
     throw new RuntimeError('requested archive with empty list of resources')
   }
   const majorVersion = major(archiverService.capability.version)
-  switch (majorVersion) {
-    case 2: {
-      // trigger the download into memory
-      const queryParams = [...options.fileIds.map(id => `id=${id}`)]
-      const archiverUrl = archiverService.url + '?' + queryParams.join('&')
-      const response = await clientService.httpAuthenticated(options.token).get(archiverUrl)
+  if (majorVersion === 2) {
+    // trigger the download into memory
+    const queryParams = [...options.fileIds.map(id => `id=${id}`)]
+    const archiverUrl = archiverService.url + '?' + queryParams.join('&')
+    const response = await clientService.httpAuthenticated(options.token).get(archiverUrl)
 
-      // check response status
-      if (response.status !== 200) {
-        throw new RuntimeError('download failed')
-      }
-      const fileName = extractFileNameFromContentDisposition(
-        response.headers['content-disposition']
-      )
-      if (!fileName) {
-        throw new RuntimeError('received archive has no file name')
-      }
-
-      // download the file with a data url
-      const link = document.createElement('a')
-      link.href = window.URL.createObjectURL(new Blob([response.data]))
-      link.setAttribute('download', fileName)
-      document.body.appendChild(link)
-      link.click()
-      document.body.removeChild(link)
-      return new Promise(resolve => resolve())
+    // check response status
+    if (response.status !== 200) {
+      throw new RuntimeError('download failed')
     }
-    default:
-    // do nothing for unknown versions
+    const fileName = extractFileNameFromContentDisposition(
+      response.headers['content-disposition']
+    )
+    if (!fileName) {
+      throw new RuntimeError('received archive has no file name')
+    }
+
+    // download the file with a data url
+    const link = document.createElement('a')
+    link.href = window.URL.createObjectURL(new Blob([response.data]))
+    link.setAttribute('download', fileName)
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
   }
 }
 
