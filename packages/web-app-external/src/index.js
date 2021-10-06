a05242f8cb48e92203432efd93480103e27996f9
Benedikt Kulmann <benedikt@kulmann.biz>
2021-10-07 00:22:03 +0200

    Set a more friendly looking title for the external app route

--- packages/web-app-external/src/index.js
+++ packages/web-app-external/src/index.js
@@ -2,6 +2,11 @@ import translations from '../l10n/translations'
 import App from './App.vue'
 import store from './store'
 
+// just a dummy function to trick gettext tools
+function $gettext(msg) {
+  return msg
+}
+
 const appInfo = {
   name: 'External',
   id: 'external'
@@ -13,6 +18,9 @@ const routes = [
     path: '/:app/:file_id',
     components: {
       app: App
+    },
+    meta: {
+      title: $gettext('External app')
     }
   }
 ]
