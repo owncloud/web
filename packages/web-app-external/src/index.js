a625dad1bd733ee8bb094d7711d704fae51657fe
Benedikt Kulmann <benedikt@kulmann.biz>
2021-10-06 23:54:16 +0200

    Move fetching mime types into ready hook and fully into store
    The ready hook is available now in the runtime. This enables us to fetch
    the available mime types from the app provider backend before the app
    gets mounted. Additionally fetching the mime types was fully moved into
    the store, since it only relies on store internals.

--- packages/web-app-external/src/index.js
+++ packages/web-app-external/src/index.js
@@ -18,23 +18,12 @@ const routes = [
   }
 ]
 
-async function fetchAvailableMimeTypes() {
-  const vueStore = window.Vue.$store
-  if (!vueStore.getters.capabilities.files.app_providers[0]?.enabled) {
-    return
-  }
-  const serverUrl = vueStore.getters.configuration.server
-  const appList = vueStore.getters.capabilities.files.app_providers[0].apps_url
-  const url = serverUrl + appList.replace('/app', 'app')
-  await vueStore.dispatch('External/fetchMimeTypes', url)
-}
-
 export default {
   appInfo,
   routes,
   store,
   translations,
-  async mounted() {
-    await fetchAvailableMimeTypes()
+  async ready({ store: runtimeStore }) {
+    await runtimeStore.dispatch('External/fetchMimeTypes')
   }
 }
