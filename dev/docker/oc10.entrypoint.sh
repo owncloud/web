e2c804fd5969e89857a46d99b1c9d0dbf568b05f
Florian Schade <f.schade@icloud.com>
2021-10-06 15:42:49 +0200

    support sidecar deployment in local dev server

--- dev/docker/oc10.entrypoint.sh
+++ dev/docker/oc10.entrypoint.sh
@@ -24,6 +24,13 @@ then
       M8W5mo3wQV3VHWYsaYpWhkr8dwa949i4GljCkedHhl7GWqmHMkxSeJgK2PcS0jt5 \
       sqvPYXK94tMsEEVOYORxg8Ufesi2kC4WpJJSYb0Kj1DSAYl6u2XvJZjc3VcitjDv \
       http://host.docker.internal:8080/index.php/apps/web/oidc-callback.html
+    occ oauth2:add-client \
+      web-sidecar \
+      UmCVsEIxdWmssxa6uVRRPC3txYBVN4qqxooJbsPhuuoPmHk9Pt9Oy68N4ZaKXUYy \
+      HW1fo6lbtgEERBQufBouJ4HID2QaDfngvIdc2vjDUE46qKB4JRG1YDir41LliReC \
+      http://localhost:9100/oidc-callback.html
+    occ config:system:set trusted_domains 0 --value="localhost"
+    occ config:system:set cors.allowed-domains 0 --value="http://localhost:9100"
 fi
 
 if [ -d /var/www/owncloud/apps/web/ ]
