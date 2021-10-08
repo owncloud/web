44535f103703fafda1ba7de0951d39cf7f7d0d35
Swikriti Tripathi <swikriti808@gmail.com>
2021-10-08 13:34:34 +0545

    test if increasing pause only before statusResponse works

--- tests/acceptance/helpers/webdavHelper.js
+++ tests/acceptance/helpers/webdavHelper.js
@@ -199,7 +199,7 @@ exports.createFile = async function(user, fileName, contents = '', waitMaxIfExis
    */
   uploadTimeStamps[user] = uploadTimeStamps[user] || {}
 
-  const pollCheck = async (retries = 0, waitFor = 1000, waitMax = waitMaxIfExisting) => {
+  const pollCheck = async (retries = 0, waitFor = 100, waitMax = waitMaxIfExisting) => {
     if (!uploadTimeStamps[user][fileName] || waitMax <= waitFor) {
       return
     } else {
@@ -216,7 +216,6 @@ exports.createFile = async function(user, fileName, contents = '', waitMaxIfExis
   await pollCheck()
 
   const davPath = exports.createDavPath(user, fileName)
-  await client.pause(1000)
   const putResponse = await httpHelper.put(davPath, user, contents)
 
   delete uploadTimeStamps[user][fileName]
