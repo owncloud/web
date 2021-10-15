d1d3de45b6f5558759418d8ee776ec49f6b82935
elizavetaRa <svillyfly@mail.ru>
2021-10-15 10:09:39 +0200

    linting

--- packages/web-app-files/src/helpers/resources.js
+++ packages/web-app-files/src/helpers/resources.js
@@ -174,7 +174,10 @@ export function buildSharedResource(share, incomingShares = false, allowSharePer
     id: share.id,
     fileId: share.item_source,
     type: share.item_type,
-    mimeType: share.state === 0? share.mimetype: ''
+    mimeType: share.state === 0 ? share.mimetype : '',
+    isFolder,
+    sdate: share.stime * 1000,
+    indicators: []
   }
 
   if (incomingShares) {
