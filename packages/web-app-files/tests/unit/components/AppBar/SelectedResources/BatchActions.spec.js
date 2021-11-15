1807908489969e2c35e403382df67521eb712b8b
Benedikt Kulmann <benedikt@kulmann.biz>
2021-11-15 17:13:43 +0100

    Kick out unit test code

--- packages/web-app-files/tests/unit/components/AppBar/SelectedResources/BatchActions.spec.js
+++ packages/web-app-files/tests/unit/components/AppBar/SelectedResources/BatchActions.spec.js
@@ -1,8 +1,6 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils'
+import { createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import DesignSystem from 'owncloud-design-system'
-import stubs from 'tests/unit/stubs'
-import BatchActions from '../../../../../src/components/AppBar/SelectedResources/BatchActions.vue'
 const permissionsHelper = '../../../../../src/helpers/permissions'
 
 jest.mock(permissionsHelper, () => ({
@@ -14,19 +12,6 @@ const localVue = createLocalVue()
 localVue.use(Vuex)
 localVue.use(DesignSystem)
 
-const files = [
-  {
-    path: '/Folder'
-  },
-  {
-    path: '/lorem.txt'
-  }
-]
-const componentStubs = { ...stubs, translate: true }
-const elSelector = {
-  ocButtonStub: 'oc-button-stub'
-}
-
 describe('Batch Actions component', () => {
   afterEach(() => {
     jest.clearAllMocks()
@@ -36,30 +21,3 @@ describe('Batch Actions component', () => {
   it.todo('renders an empty list if there are no batch actions available')
   it.todo('renders a button for each available batch action')
 })
-
-function createShallowMountWrapper(options = {}) {
-  return shallowMount(BatchActions, {
-    localVue,
-    stubs: componentStubs,
-    ...options
-  })
-}
-
-function createStore(state = { selected: [], currentFolder: {} }) {
-  return new Vuex.Store({
-    modules: {
-      Files: {
-        state: {
-          currentFolder: { path: '' },
-          ...state
-        },
-        namespaced: true,
-        getters: {
-          selectedFiles: () => state.selected,
-          currentFolder: () => state.currentFolder,
-          activeFilesCurrentPage: () => files
-        }
-      }
-    }
-  })
-}
