3b786efdcc09335b2d6184894fc305649b2b6eaa
Florian Schade <f.schade@icloud.com>
2021-12-15 15:44:59 +0100

    remove user from spaces url and replace it with home till loading of users in initial state is possible

--- packages/web-app-files/src/router/spaces.ts
+++ packages/web-app-files/src/router/spaces.ts
@@ -1,24 +1,13 @@
 import { Location, RouteConfig } from 'vue-router'
 import { RouteComponents } from './router'
 import { createLocation, isLocationActiveDirector, $gettext } from './utils'
-import get from 'lodash-es/get'
-import Vue from 'vue'
-import { Store } from 'vuex'
-
-/**
- * retrieve vue store, this is just a transitional solution till we have refactored
- * application loading and can inject it.
- */
-const store = (Vue as any).$store as Store<any>
 
 export const createLocationSpaces = (location = {}): Location =>
   createLocation(
     'files-spaces-storage',
     {
       params: {
-        get storage() {
-          return get(store, 'getters.user.id') || 'public'
-        },
+        storage: 'home',
         namespace: 'personal'
       }
     },
