8377453add997f0345a39e6e08efd6572869ba0c
Jannik Stehle <jannik.stehle@gmail.com>
2022-01-04 13:12:39 +0100

    Minor fix

--- packages/web-app-files/src/index.js
+++ packages/web-app-files/src/index.js
@@ -85,7 +85,7 @@ const navItems = [
       path: `/${appInfo.id}/spaces/projects`
     },
     enabled(capabilities) {
-      return capabilities.spaces.enabled === true
+      return capabilities.spaces && capabilities.spaces.enabled === true
     }
   },
   {
