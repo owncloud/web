93a82dfe6048eb342b4fd70ba896ad01bd529ad2
Benedikt Kulmann <benedikt@kulmann.biz>
2022-01-14 14:06:18 +0100

    Rename composables to a consistent structure

--- packages/web-app-files/src/composables/index.ts
+++ packages/web-app-files/src/composables/index.ts
@@ -1,9 +1,9 @@
 export * from './router'
 export * from './store'
-export * from './useDefaults'
-export * from './useFileListHeaderPosition'
-export * from './usePagination'
-export * from './useSort'
+export * from './defaults'
+export * from './fileListHeaderPosition'
+export * from './pagination'
+export * from './sort'
 
 declare module 'vue/types/vue' {
   interface Vue {
