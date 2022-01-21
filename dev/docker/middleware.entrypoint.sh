10135e8b4bfdf6e0d00f2c2d722d58509a1e767e
Parajuli Kiran <kiranparajuli589@gmail.com>
2022-01-21 11:01:10 +0545

    Updated selenium start information

--- dev/docker/middleware.entrypoint.sh
+++ dev/docker/middleware.entrypoint.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 # cleanup filesForUpload dir before start
-ls filesForUpload
 rm -rf /uploads/*
-# populate shared volume with the files for upload
+# populate shared volume with the files
 cp -r filesForUpload/* /uploads/
 # start the middleware service
 yarn start
