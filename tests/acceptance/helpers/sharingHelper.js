dfbb1a07dd56f054d59a5d541ce845d6e0387e39
Swikriti Tripathi <swikriti808@gmail.com>
2022-01-25 12:10:52 +0545

    Restore fetchLastPublicLinkShare function

--- tests/acceptance/helpers/sharingHelper.js
+++ tests/acceptance/helpers/sharingHelper.js
@@ -1,4 +1,11 @@
+const httpHelper = require('./httpHelper')
 module.exports = {
+  SHARE_TYPES: Object.freeze({
+    user: 0,
+    group: 1,
+    public_link: 3,
+    federated_cloud_share: 6
+  }),
   PERMISSION_TYPES: Object.freeze({
     read: 1,
     update: 2,
@@ -59,5 +66,43 @@ module.exports = {
     const expectedDate = new Date(Date.parse(date))
     const expectedMonth = months[expectedDate.getMonth()]
     return `${expectedMonth} ${expectedDate.getDate().toString()}, ${expectedDate.getFullYear()}`
+  },
+
+  /**
+   * Asynchronously fetches the last public link created by the given link creator
+   *
+   * @async
+   * @param {string} linkCreator link creator
+   * @return {Promise<Object>} last share token
+   */
+  fetchLastPublicLinkShare: async function (linkCreator) {
+    const self = this
+    const apiURL = 'apps/files_sharing/api/v1/shares'
+    let lastShareToken
+    let lastShare
+    await httpHelper
+      .getOCS(apiURL, linkCreator)
+      .then((res) => res.json())
+      .then(function (sharesResult) {
+        httpHelper.checkOCSStatus(
+          sharesResult,
+          'Could not get shares. Message: ' + sharesResult.ocs.meta.message
+        )
+        const shares = sharesResult.ocs.data
+        let lastShareSTime = 0
+        for (const share of shares) {
+          if (share.share_type === self.SHARE_TYPES.public_link && share.stime > lastShareSTime) {
+            lastShareSTime = share.stime
+            lastShareToken = share.token
+            lastShare = share
+          }
+        }
+        if (lastShareToken === null) {
+          throw Error(
+            'Could not find public shares. All shares: ' + JSON.stringify(shares, null, 2)
+          )
+        }
+      })
+    return lastShare
   }
 }
