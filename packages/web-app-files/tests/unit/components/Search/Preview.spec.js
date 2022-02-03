a756cbc4ac3cbd7ee2216ed2014043f917b6f082
Jannik Stehle <jannik.stehle@gmail.com>
2022-02-03 17:43:58 +0100

    Add some more unit tests

--- packages/web-app-files/tests/unit/components/Search/Preview.spec.js
+++ packages/web-app-files/tests/unit/components/Search/Preview.spec.js
@@ -24,36 +24,43 @@ const spyTriggerDefaultAction = jest
   .spyOn(Preview.mixins[0].methods, '$_fileActions_triggerDefaultAction')
   .mockImplementation()
 
-const wrapper = shallowMount(Preview, {
-  localVue,
-  mocks: {
-    $route: {
-      query: {},
-      params: {}
-    }
-  },
-  propsData: {
-    searchResult
-  },
-  stubs: {
-    'oc-progress': true,
-    'oc-resource': true
-  },
-  setup: () => {
-    return {
-      resourceTargetLocation: {}
-    }
+function getWrapper({
+  resourceTargetLocation = {},
+  route = {
+    query: {},
+    params: {}
   }
-})
+} = {}) {
+  return shallowMount(Preview, {
+    localVue,
+    mocks: {
+      $route: route
+    },
+    propsData: {
+      searchResult
+    },
+    stubs: {
+      'oc-progress': true,
+      'oc-resource': true
+    },
+    setup: () => {
+      return {
+        resourceTargetLocation
+      }
+    }
+  })
+}
 
 describe('Preview component', () => {
   it('should set correct props on oc-resource component', () => {
+    const wrapper = getWrapper()
     const ocResource = wrapper.find('oc-resource-stub')
 
     expect(ocResource.exists()).toBeTruthy()
     expect(ocResource.props().resource).toMatchObject(searchResult.data)
   })
   it('should trigger the default action when search preview button is clicked', async () => {
+    const wrapper = getWrapper()
     const searchPreview = wrapper.find(selectors.searchPreview)
 
     expect(spyTriggerDefaultAction).toHaveBeenCalledTimes(0)
@@ -61,5 +68,20 @@ describe('Preview component', () => {
     await searchPreview.trigger('click')
 
     expect(spyTriggerDefaultAction).toHaveBeenCalledTimes(1)
+  })
+  it('should have an empty (parent) folder link without a resource target location', () => {
+    const wrapper = getWrapper({ resourceTargetLocation: null })
+    expect(wrapper.vm.folderLink(searchResult.data)).toEqual({})
+    expect(wrapper.vm.parentFolderLink(searchResult.data)).toEqual({})
+  })
+  it('should set the space id as route param if given', () => {
+    const wrapper = getWrapper({
+      route: {
+        query: {},
+        params: { spaceId: 1 }
+      }
+    })
+    expect(wrapper.vm.folderLink(searchResult.data).params.spaceId).toEqual(1)
+    expect(wrapper.vm.parentFolderLink(searchResult.data).params.spaceId).toEqual(1)
   })
 })
