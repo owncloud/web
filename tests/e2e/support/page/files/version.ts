d0787b8417ab809c26edaead370cfde48a4d67dc
Prarup Gurung <grgprarup@gmail.com>
2022-02-07 15:33:21 +0545

    Added implementation for file old version restore, check file is restored

--- tests/e2e/support/page/files/version.ts
+++ tests/e2e/support/page/files/version.ts
@@ -1,15 +1,26 @@
-import { Actor } from '../../types'
+import { Actor, File } from '../../types'
 import { filesCta } from '../../cta'
-import { Locator } from '@playwright/test'
+import { expect, Locator } from '@playwright/test'
+import path from 'path'
 
 export class VersionPage {
   private readonly actor: Actor
   private readonly fileVersionSidebarXPath: Locator
+  private readonly fileVersionSizeXPath: Locator
+  private readonly fileVersionRevertButtonXPath: Locator
+  private fileSizeXPath: Locator
+  private static olderVersionSize: string
 
   constructor({ actor }: { actor: Actor }) {
     this.actor = actor
     this.fileVersionSidebarXPath = this.actor.page.locator(
       '//*[@id="oc-file-versions-sidebar"]/table/tbody/tr'
+    )
+    this.fileVersionSizeXPath = this.actor.page.locator(
+      '//*[@data-testid="file-versions-file-size"]'
+    )
+    this.fileVersionRevertButtonXPath = this.actor.page.locator(
+      '//*[@data-testid="file-versions-revert-button"]'
     )
   }
 
@@ -27,5 +38,34 @@ export class VersionPage {
 
     const elements = this.fileVersionSidebarXPath
     return await elements.count()
+  }
+
+  async restoreOlderVersion({ files, folder }: { files: File[]; folder?: string }) {
+    const { page } = this.actor
+    const fileName = files.map((file) => path.basename(file.name))
+    await filesCta.navigateToFolder({ page: page, path: folder })
+    await filesCta.sidebar.open({ page: page, resource: fileName[0] })
+    await filesCta.sidebar.openPanel({ page: page, name: 'versions' })
+    const fileSize = await this.fileVersionSizeXPath.textContent()
+    VersionPage.olderVersionSize = fileSize.trim()
+    await Promise.all([
+      page.waitForResponse(
+        (resp) =>
+          resp.url().includes('/v/') && resp.status() === 204 && resp.request().method() === 'COPY'
+      ),
+      await this.fileVersionRevertButtonXPath.click()
+    ])
+    // Files details page does not update after restore button is clicked
+    // This is the issue: https://github.com/owncloud/web/issues/6361
+    await page.reload()
+  }
+
+  async checkOlderVersionRestored({ resource }: { resource: string }) {
+    const { page } = this.actor
+    this.fileSizeXPath = page.locator(
+      `//*[@data-test-resource-name="${resource}"]/ancestor::tr//*[@class="oc-resource-size"]`
+    )
+    const fileSize = await this.fileSizeXPath.textContent()
+    expect(fileSize.length).toBe(VersionPage.olderVersionSize.length)
   }
 }
