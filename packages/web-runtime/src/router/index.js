00e7c6943445216bf7ccf57b6d460215bb882d4d
Benedikt Kulmann <benedikt@kulmann.biz>
2022-02-16 13:15:18 +0100

    Improve readability of buildUrl function

--- packages/web-runtime/src/router/index.js
+++ packages/web-runtime/src/router/index.js
@@ -108,20 +108,29 @@ export const router = patchRouter(
 )
 
 export const buildUrl = (pathname) => {
+  const isHistoryMode = !!base
   const baseUrl = new URL(window.location.href.split('#')[0])
-  if (baseUrl.pathname.endsWith('/index.html')) {
-    baseUrl.pathname = baseUrl.pathname.split('/').slice(0, -1).filter(Boolean).join('/')
+  if (isHistoryMode) {
+    // in history mode we can't determine the base path, it must be provided by the document
+    baseUrl.pathname = new URL(base.href).pathname
+  } else {
+    // in hash mode, auto-determine the base path by removing `/index.html`
+    if (baseUrl.pathname.endsWith('/index.html')) {
+      baseUrl.pathname = baseUrl.pathname.split('/').slice(0, -1).filter(Boolean).join('/')
+    }
   }
 
+  /**
+   * build full url by either
+   * - concatenating baseUrl and pathname (for unknown/non-router urls, e.g. `oidc-callback.html`) or
+   * - resolving via router (for known routes)
+   */
   if (/\.(html?)$/i.test(pathname)) {
-    baseUrl.pathname = [
-      ...(base ? new URL(base.href) : baseUrl).pathname.split('/'),
-      ...pathname.split('/')
-    ]
+    baseUrl.pathname = [...baseUrl.pathname.split('/'), ...pathname.split('/')]
       .filter(Boolean)
       .join('/')
   } else {
-    baseUrl[base ? 'pathname' : 'hash'] = router.resolve(pathname).href
+    baseUrl[isHistoryMode ? 'pathname' : 'hash'] = router.resolve(pathname).href
   }
 
   return baseUrl.href
