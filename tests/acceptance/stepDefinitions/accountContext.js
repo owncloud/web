ad3a0125d76c53ba61638487eeeda8c43d1e0e55
Parajuli Kiran <kiranparajuli589@gmail.com>
2022-02-23 20:39:26 +0545

    Fix account information elements selector

--- tests/acceptance/stepDefinitions/accountContext.js
+++ tests/acceptance/stepDefinitions/accountContext.js
@@ -13,6 +13,37 @@ Then(
     const expectedAccInfo = dataTable.rowsHash()
     const actualAccInfo = await client.page.accountPage().getAccountInformation()
     const actualEntries = Object.entries(actualAccInfo)
+
+    const expectedKeys = Object.keys(expectedAccInfo)
+    const actualKeys = Object.keys(actualAccInfo)
+
+    if (actualKeys.length === 0) {
+      throw new Error('Sorry, no any account information was found.')
+    }
+
+    const notFoundArray = []
+
+    // every expected key should be inside the actual keys array
+    expectedKeys.forEach((key) => {
+      if (!actualKeys.includes(key)) {
+        notFoundArray.push(key)
+      }
+    })
+
+    if (notFoundArray.length > 0) {
+      throw new Error(
+        'Error: Missing account information' +
+          '\n' +
+          'Expected: ' +
+          expectedKeys.join(', ') +
+          '\n' +
+          'Found: ' +
+          actualKeys.join(', ') +
+          '\n' +
+          'Missing: ' +
+          notFoundArray.join(', ')
+      )
+    }
     for (const [key, value] of actualEntries) {
       if (key === 'Group memberships') {
         const actualGroupMembership = value.split(',').map((grp) => grp.trim())
@@ -21,10 +52,11 @@ Then(
         assert.strictEqual(
           differenceInGroups.length,
           0,
-          'Actual group membership for the user was ' +
+          'Actual group membership for the user was "' +
             actualGroupMembership +
-            ' but was expected to be ' +
-            expectedGroupMembership
+            '" but was expected to be "' +
+            expectedGroupMembership +
+            '"'
         )
       } else {
         assert.strictEqual(value, expectedAccInfo[key])
