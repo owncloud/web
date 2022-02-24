9d4b812f4bd6c36738a8d8fcf99393d0da03b25b
Florian Schade <f.schade@icloud.com>
2022-02-24 12:12:13 +0100

    revert all files breadcrumb link to / (#6467)
    * revert all files breadcrumb link to /

--- packages/web-app-files/src/components/AppBar/AppBar.vue
+++ packages/web-app-files/src/components/AppBar/AppBar.vue
@@ -290,6 +290,7 @@ export default {
             if (acc.length) {
               if (this.isPersonalLocation) {
                 acc[0].text = this.$gettext('All files')
+                acc[0].to = acc[0].to + '/'
               } else if (this.isSpacesProjectLocation || this.isSpacesProjectsLocation) {
                 acc[0] = {
                   text: this.$gettext('Spaces'),
