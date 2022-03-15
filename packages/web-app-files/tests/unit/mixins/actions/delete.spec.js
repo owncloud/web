c9b894ec0bef4ec0ce7722cc6c9301d7ce5e14c4
JanAckermann <jackermann@owncloud.com>
2022-03-15 15:21:31 +0100

    Add route check

--- packages/web-app-files/tests/unit/mixins/actions/delete.spec.js
+++ packages/web-app-files/tests/unit/mixins/actions/delete.spec.js
@@ -1,7 +1,7 @@
 import Vuex from 'vuex'
 import { mount, createLocalVue } from '@vue/test-utils'
 import Delete from '@files/src/mixins/actions/delete.js'
-import { createLocationSpaces } from '../../../../src/router'
+import { createLocationShares, createLocationSpaces } from '../../../../src/router'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
@@ -15,10 +15,15 @@ describe('delete', () => {
   describe('computed property "$_delete_items"', () => {
     describe('isEnabled property of returned element', () => {
       it.each([
-        { resources: [{ canBeDeleted: () => true }], expectedStatus: true },
-        { resources: [{ canBeDeleted: () => false }], expectedStatus: false }
+        { resources: [{ canBeDeleted: () => true }], invalidLocation: false, expectedStatus: true },
+        { resources: [{ canBeDeleted: () => true }], invalidLocation: true, expectedStatus: false },
+        {
+          resources: [{ canBeDeleted: () => false }],
+          invalidLocation: false,
+          expectedStatus: false
+        }
       ])('should be set correctly', (inputData) => {
-        const wrapper = getWrapper()
+        const wrapper = getWrapper({ invalidLocation: inputData.invalidLocation })
         const resources = inputData.resources
         expect(wrapper.vm.$_delete_items[0].isEnabled({ resources })).toBe(inputData.expectedStatus)
       })
@@ -26,16 +31,16 @@ describe('delete', () => {
   })
 })
 
-function getWrapper() {
+function getWrapper({ invalidLocation = false }) {
   return mount(Component, {
     localVue,
     mocks: {
-      $route: createLocationSpaces('files-spaces-personal-home'),
       $router: {
-        resolve: () => {
-          return {
-            href: 'href'
-          }
+        currentRoute: invalidLocation
+          ? createLocationShares('files-shares-via-link')
+          : createLocationSpaces('files-spaces-personal-home'),
+        resolve: (r) => {
+          return { href: r.name }
         }
       }
     }
