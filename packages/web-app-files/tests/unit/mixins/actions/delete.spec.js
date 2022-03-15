78991abca6891022e6f1dd9b3e34a7c07ffe82be
JanAckermann <jackermann@owncloud.com>
2022-03-15 15:58:59 +0100

    Add tests for delete permanent action

--- packages/web-app-files/tests/unit/mixins/actions/delete.spec.js
+++ packages/web-app-files/tests/unit/mixins/actions/delete.spec.js
@@ -1,7 +1,11 @@
 import Vuex from 'vuex'
 import { mount, createLocalVue } from '@vue/test-utils'
 import Delete from '@files/src/mixins/actions/delete.js'
-import { createLocationShares, createLocationSpaces } from '../../../../src/router'
+import {
+  createLocationShares,
+  createLocationSpaces,
+  createLocationTrash
+} from '../../../../src/router'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
@@ -13,7 +17,7 @@ const Component = {
 
 describe('delete', () => {
   describe('computed property "$_delete_items"', () => {
-    describe('isEnabled property of returned element', () => {
+    describe('delete isEnabled property of returned element', () => {
       it.each([
         { resources: [{ canBeDeleted: () => true }], invalidLocation: false, expectedStatus: true },
         { resources: [{ canBeDeleted: () => true }], invalidLocation: true, expectedStatus: false },
@@ -28,16 +32,37 @@ describe('delete', () => {
         expect(wrapper.vm.$_delete_items[0].isEnabled({ resources })).toBe(inputData.expectedStatus)
       })
     })
+    describe('delete-permanent isEnabled property of returned element', () => {
+      it.each([
+        { resources: [{}], deletePermanent: true, invalidLocation: false, expectedStatus: true },
+        { resources: [{}], deletePermanent: true, invalidLocation: true, expectedStatus: false },
+        {
+          resources: [],
+          deletePermanent: true,
+          invalidLocation: false,
+          expectedStatus: false
+        }
+      ])('should be set correctly', (inputData) => {
+        const wrapper = getWrapper({
+          deletePermanent: true,
+          invalidLocation: inputData.invalidLocation
+        })
+        const resources = inputData.resources
+        expect(wrapper.vm.$_delete_items[1].isEnabled({ resources })).toBe(inputData.expectedStatus)
+      })
+    })
   })
 })
 
-function getWrapper({ invalidLocation = false }) {
+function getWrapper({ deletePermanent = false, invalidLocation = false } = {}) {
   return mount(Component, {
     localVue,
     mocks: {
       $router: {
         currentRoute: invalidLocation
           ? createLocationShares('files-shares-via-link')
+          : deletePermanent
+          ? createLocationTrash('files-trash-personal')
           : createLocationSpaces('files-spaces-personal-home'),
         resolve: (r) => {
           return { href: r.name }
