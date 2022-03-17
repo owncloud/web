f0dd02b5f1f6485e49af3865a9a632b6a20f6224
JanAckermann <jackermann@owncloud.com>
2022-03-17 10:00:41 +0100

    Fix single space page title, add trash space title

--- packages/web-app-files/src/views/spaces/Trashbin.vue
+++ packages/web-app-files/src/views/spaces/Trashbin.vue
@@ -2,8 +2,38 @@
 
 <script>
 import Trashbin from '../../components/Trashbin.vue'
+import { useStore } from 'web-pkg/src/composables'
+import { ref } from '@vue/composition-api'
+import { clientService } from 'web-pkg/src/services'
+import { useTask } from 'vue-concurrency'
+import { buildSpace } from '../../helpers/resources'
 
 export default {
-  components: { Trashbin }
+  components: { Trashbin },
+
+  setup() {
+    const store = useStore()
+    const space = ref({})
+    const graphClient = clientService.graphAuthenticated(
+      store.getters.configuration.server,
+      store.getters.getToken
+    )
+
+    const loadResourcesTask = useTask(function* (signal, ref) {
+      const response = yield graphClient.drives.getDrive(ref.$router.currentRoute.params.storageId)
+      const loadedSpace = response.data || {}
+      space.value = buildSpace(loadedSpace)
+    })
+
+    return {
+      space,
+      loadResourcesTask
+    }
+  },
+
+  async mounted() {
+    await this.loadResourcesTask.perform(this)
+    document.title = `${this.$route.meta.title} - ${this.space.name} - ${this.configuration.currentTheme.general.name}`
+  }
 }
 </script>
