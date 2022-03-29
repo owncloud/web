8ddfbea210eacf1372124ccc4f9d3d80b5018282
Pascal Wengerter <pascal@wengerter.info>
2022-03-29 16:02:34 +0200

    Fix&refactor AppBar unit tests

--- packages/web-app-files/tests/unit/components/AppBar/AppBar.spec.js
+++ packages/web-app-files/tests/unit/components/AppBar/AppBar.spec.js
@@ -14,41 +14,113 @@ localVue.use(GetTextPlugin, {
   silent: true
 })
 
-const selectedFile = Files['/'][1]
 const selectedFiles = [Files['/'][1], Files['/'][4]]
 
+const actionSlot = "<button class='action-slot'>Click</button>"
+const contextMenuSlot = "<button class='context-menu-slot'>Click</button>"
+const contentSlot = "<div class='content-slot'>Foo</div>"
+
+const breadcrumbItems = [
+  { text: 'Example1', to: '/' },
+  { text: 'Example2', to: '/foo' }
+]
+const breadCrumbItemWithContextActionAllowed = {
+  text: 'Example Special',
+  to: '/bar',
+  allowContextActions: true
+}
+
 describe('AppBar component', () => {
-  describe('with no selected files', () => {
-    const store = createStore({ selected: [] })
-    const wrapper = getShallowWrapper(store)
-
-    it('renders basic appBar with content slot', () => {
-      const slotEl = wrapper.find('ruby')
-      expect(slotEl.exists()).toBeTruthy()
-
-      expect(wrapper).toMatchSnapshot()
-    })
-  })
-  describe('with one selected file', () => {
-    it('tbd', () => {
-      const store = createStore({ selected: selectedFile })
+  describe('renders', () => {
+    it('by default no breadcrumbs, no bulkactions, no sharesnavigation but viewoptions and sidebartoggle', () => {
+      const store = createStore()
       const wrapper = getShallowWrapper(store)
       expect(wrapper).toMatchSnapshot()
     })
-  })
-  describe('with multiple selected files', () => {
-    it('tbd', () => {
-      const store = createStore({ selected: selectedFiles })
-      const wrapper = getShallowWrapper(store)
+    describe('breadcrumbs', () => {
+      it('if given, by default without breadcrumbsContextActionsItems', () => {
+        const store = createStore()
+        const wrapper = getShallowWrapper(store, {}, { breadcrumbs: breadcrumbItems })
+        expect(wrapper).toMatchSnapshot()
+      })
+      it('if given, with breadcrumbsContextActionsItems if allowed on last breadcrumb item', () => {
+        const store = createStore()
+        const wrapper = getShallowWrapper(
+          store,
+          {},
+          { breadcrumbs: [...breadcrumbItems, breadCrumbItemWithContextActionAllowed] }
+        )
+        expect(wrapper).toMatchSnapshot()
+      })
+      it('if given, with content in the contextMenu slot', () => {
+        const store = createStore()
+        const wrapper = getShallowWrapper(
+          store,
+          { contextMenu: contextMenuSlot },
+          { breadcrumbs: [...breadcrumbItems, breadCrumbItemWithContextActionAllowed] }
+        )
+        expect(wrapper).toMatchSnapshot()
+      })
+    })
+    describe('bulkActions', () => {
+      it('if enabled', () => {
+        const store = createStore({ selected: selectedFiles })
+        const wrapper = getShallowWrapper(store, {}, { hasBulkActions: true })
+        expect(wrapper).toMatchSnapshot()
+      })
+    })
+    describe('sharesNavigation', () => {
+      it('if enabled', () => {
+        const store = createStore()
+        const wrapper = getShallowWrapper(store, {}, { hasSharesNavigation: true })
+        expect(wrapper).toMatchSnapshot()
+      })
+    })
+    describe('viewoptions and sidebartoggle', () => {
+      it('only viewoptions if sidebartoggle is disabled', () => {
+        const store = createStore()
+        const wrapper = getShallowWrapper(store, {}, { hasSidebarToggle: false })
+        expect(wrapper).toMatchSnapshot()
+      })
+      it('only sidebartoggle if viewoptions is disabled', () => {
+        const store = createStore()
+        const wrapper = getShallowWrapper(store, {}, { hasViewOptions: false })
+        expect(wrapper).toMatchSnapshot()
+      })
+      it('neither if both are disabled', () => {
+        const store = createStore()
+        const wrapper = getShallowWrapper(
+          store,
+          {},
+          { hasSidebarToggle: false, hasViewOptions: false }
+        )
+        expect(wrapper).toMatchSnapshot()
+      })
+    })
+    it('if given, with content in the actions slot', () => {
+      const store = createStore()
+      const wrapper = getShallowWrapper(store, { actions: actionSlot })
+      expect(wrapper).toMatchSnapshot()
+    })
+    it('if given, with content in the content slot', () => {
+      const store = createStore()
+      const wrapper = getShallowWrapper(store, { content: contentSlot })
       expect(wrapper).toMatchSnapshot()
     })
   })
-
-  it.todo('renders breadcrumbs')
-  it.todo('last breadcrumb item emits event upon click')
 })
 
-function getShallowWrapper(store = {}) {
+function getShallowWrapper(
+  store = {},
+  slots = {},
+  props = {
+    breadcrumbs: [],
+    hasBulkActions: false,
+    hasSharesNavigation: false,
+    hasSidebarToggle: true,
+    hasViewOptions: true
+  }
+) {
   return shallowMount(AppBar, {
     localVue,
     mocks: {
@@ -58,11 +130,9 @@ function getShallowWrapper(store = {}) {
         }
       }
     },
+    slots,
     propsData: {
-      slots: {
-        actions: '<button>Click me</button>',
-        content: '<ruby>Static content</ruby>'
-      }
+      ...props
     },
     store
   })
