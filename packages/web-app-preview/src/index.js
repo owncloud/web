dadc77c608611703f0cf5695a8efaf286a00e4f0
Benedikt Kulmann <benedikt@kulmann.biz>
2022-04-04 18:07:57 +0200

    Revert back to a handpicked list of mimetypes
    Using mimetype categories turned out to be a bad idea as of now because
    we would need to put a lot of thought into usability edge cases (like
    falling back to raw images if they are small enough, ignoring files that
    are unsupported in browsers, ...)

--- packages/web-app-preview/src/index.js
+++ packages/web-app-preview/src/index.js
@@ -20,7 +20,18 @@ const routes = [
 ]
 
 const routeName = 'preview-media'
-const mimeTypes = ['image', 'video', 'audio']
+const mimeTypes = [
+  'audio/flac',
+  'audio/mpeg',
+  'audio/ogg',
+  'audio/wav',
+  'audio/x-flac',
+  'audio/x-wav',
+  'image/gif',
+  'image/jpeg',
+  'image/png',
+  'video/mp4'
+]
 
 const appInfo = {
   name: 'Preview',
