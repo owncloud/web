0d5ba87cc2b361ed0104c8db74338e53bb79e19b
Tobias Speicher <rootcommander@gmail.com>
2022-04-04 04:49:33 +0200

    refactor: replace deprecated String.prototype.substr()
    .substr() is deprecated so we replace it with functions which work similarily but aren't deprecated
    
    Signed-off-by: Tobias Speicher <rootcommander@gmail.com>

--- packages/web-app-media-viewer/src/App.vue
+++ packages/web-app-media-viewer/src/App.vue
@@ -186,7 +186,7 @@ export default {
         y: this.thumbDimensions,
         // strip double quotes from etag
         // we have no etag, e.g. on shared with others page
-        c: this.activeMediaFile.etag?.substr(1, this.activeMediaFile.etag.length - 2),
+        c: this.activeMediaFile.etag?.slice(1, -1),
         scalingup: 0,
         preview: 1,
         a: 1
