a259a6086237f19697c625e1ed849e1bbe543b37
JanAckermann <jackermann@owncloud.com>
2022-04-12 15:47:51 +0200

    Add tests

--- packages/web-app-user-management/tests/unit/components/Users/UsersList.spec.js
+++ packages/web-app-user-management/tests/unit/components/Users/UsersList.spec.js
@@ -48,6 +48,44 @@ describe('UsersList', () => {
       expect(wrapper.vm.getUserRole({ id: 1 })).toEqual('')
     })
   })
+
+  describe('method "orderBy"', () => {
+    it('should return an ascending ordered list while desc is set to false', () => {
+      const wrapper = getWrapper()
+
+      expect(
+        wrapper.vm.orderBy(
+          [{ displayName: 'user' }, { displayName: 'admin' }],
+          'displayName',
+          false
+        )
+      ).toEqual([{ displayName: 'admin' }, { displayName: 'user' }])
+    })
+    it('should return an descending ordered list while desc is set to true', () => {
+      const wrapper = getWrapper()
+
+      expect(
+        wrapper.vm.orderBy([{ displayName: 'admin' }, { displayName: 'user' }], 'displayName', true)
+      ).toEqual([{ displayName: 'user' }, { displayName: 'admin' }])
+    })
+  })
+
+  describe('method "filter"', () => {
+    it('should return a list containing record admin if search term is "ad"', () => {
+      const wrapper = getWrapper()
+
+      expect(wrapper.vm.filter([{ displayName: 'user' }, { displayName: 'admin' }], 'ad')).toEqual([
+        { displayName: 'admin' }
+      ])
+    })
+    it('should return an an empty list if search term does not match any entry', () => {
+      const wrapper = getWrapper()
+
+      expect(
+        wrapper.vm.filter([{ displayName: 'admin' }, { displayName: 'user' }], 'jana')
+      ).toEqual([])
+    })
+  })
 })
 
 function getWrapper({ propsData = {} } = {}) {
