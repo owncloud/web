06d8d6e85eaf95bbbb80a1740e34340ef1a4c8d6
Benedikt Kulmann <benedikt@kulmann.biz>
2022-06-23 10:42:56 +0200

    Allow changing primary file extensions for text editor

--- packages/web-app-text-editor/src/index.js
+++ packages/web-app-text-editor/src/index.js
@@ -6,6 +6,8 @@ function $gettext(msg) {
   return msg
 }
 
+const appId = 'text-editor'
+
 const routes = [
   {
     path: '/:filePath*',
@@ -19,41 +21,76 @@ const routes = [
   }
 ]
 
-const fileExtensionConfig = {
-  canBeDefault: true
+const fileExtensions = () => {
+  const extensions = [
+    {
+      extension: 'txt',
+      label: $gettext('Plain text file')
+    },
+    {
+      extension: 'md',
+      label: $gettext('Markdown file')
+    },
+    {
+      extension: 'js',
+      label: $gettext('JavaScript file')
+    },
+    {
+      extension: 'json',
+      label: $gettext('JSON file')
+    },
+    {
+      extension: 'xml',
+      label: $gettext('XML file')
+    },
+    {
+      extension: 'py',
+      label: $gettext('Python file')
+    },
+    {
+      extension: 'php',
+      label: $gettext('PHP file')
+    },
+    {
+      extension: 'yaml',
+      label: $gettext('YAML file')
+    }
+  ]
+
+  let primaryExtensions = window.Vue.$store.getters.extensionConfigByAppId(appId)
+    .primaryExtensions || ['txt', 'md']
+  if (typeof primaryExtensions === 'string') {
+    primaryExtensions = [primaryExtensions]
+  }
+  return extensions.reduce((acc, extensionItem) => {
+    if (primaryExtensions.includes(extensionItem.extension)) {
+      extensionItem.canBeDefault = true
+      extensionItem.newFileMenu = {
+        menuTitle($gettext) {
+          return $gettext(extensionItem.label)
+        }
+      }
+    }
+    acc.push(extensionItem)
+    return acc
+  }, [])
 }
 
 const appInfo = {
   name: $gettext('Text Editor'),
-  id: 'text-editor',
+  id: appId,
   icon: 'file-text',
   isFileEditor: true,
-  extensions: [
-    {
-      extension: 'txt',
-      newFileMenu: {
-        menuTitle($gettext) {
-          return $gettext('Plain text file')
-        }
-      },
-      ...fileExtensionConfig
-    },
-    {
-      extension: 'md',
-      newFileMenu: {
-        menuTitle($gettext) {
-          return $gettext('Markdown file')
-        }
-      },
-      ...fileExtensionConfig
+  extensions: fileExtensions().map((extensionItem) => {
+    return {
+      extension: extensionItem.extension,
+      ...(Object.prototype.hasOwnProperty.call(extensionItem, 'newFileMenu') && {
+        newFileMenu: extensionItem.newFileMenu
+      }),
+      ...(Object.prototype.hasOwnProperty.call(extensionItem, 'canBeDefault') && {
+        canBeDefault: extensionItem.canBeDefault
+      })
     }
-  ]
-}
-
-for (const ext of ['js', 'json', 'xml', 'py', 'php', 'yaml']) {
-  appInfo.extensions.push({
-    extension: ext,
-    ...fileExtensionConfig
   })
 }
 
