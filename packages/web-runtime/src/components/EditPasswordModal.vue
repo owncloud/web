0262b54766dce33b31538582cdb50117819d9e29
JanAckermann <jackermann@owncloud.com>
2022-07-01 13:39:00 +0200

    Add more tests

--- packages/web-runtime/src/components/EditPasswordModal.vue
+++ packages/web-runtime/src/components/EditPasswordModal.vue
@@ -1,38 +1,36 @@
 <template>
-  <portal to="app.runtime.modal">
-    <oc-modal
-      :title="$gettext('Change password')"
-      :button-cancel-text="$gettext('Cancel')"
-      :button-confirm-text="$gettext('Confirm')"
-      :button-confirm-disabled="confirmButtonDisabled"
-      @confirm="editPassword"
-      @cancel="$emit('cancel')"
-    >
-      <template #content>
-        <oc-text-input
-          v-model="currentPassword"
-          :label="$gettext('Current password')"
-          type="password"
-          :fix-message-line="true"
-        />
-        <oc-text-input
-          v-model="newPassword"
-          :label="$gettext('New password')"
-          type="password"
-          :fix-message-line="true"
-          @change="validatePasswordConfirm"
-        />
-        <oc-text-input
-          v-model="newPasswordConfirm"
-          :label="$gettext('Repeat new password')"
-          type="password"
-          :fix-message-line="true"
-          :error-message="passwordConfirmErrorMessage"
-          @change="validatePasswordConfirm"
-        />
-      </template>
-    </oc-modal>
-  </portal>
+  <oc-modal
+    :title="$gettext('Change password')"
+    :button-cancel-text="$gettext('Cancel')"
+    :button-confirm-text="$gettext('Confirm')"
+    :button-confirm-disabled="confirmButtonDisabled"
+    @confirm="editPassword"
+    @cancel="$emit('cancel')"
+  >
+    <template #content>
+      <oc-text-input
+        v-model="currentPassword"
+        :label="$gettext('Current password')"
+        type="password"
+        :fix-message-line="true"
+      />
+      <oc-text-input
+        v-model="newPassword"
+        :label="$gettext('New password')"
+        type="password"
+        :fix-message-line="true"
+        @change="validatePasswordConfirm"
+      />
+      <oc-text-input
+        v-model="newPasswordConfirm"
+        :label="$gettext('Repeat new password')"
+        type="password"
+        :fix-message-line="true"
+        :error-message="passwordConfirmErrorMessage"
+        @change="validatePasswordConfirm"
+      />
+    </template>
+  </oc-modal>
 </template>
 
 <script>
@@ -70,7 +68,10 @@ export default {
         this.passwordConfirmErrorMessage = this.$gettext(
           'Password and password confirmation must be identical'
         )
+        return false
       }
+
+      return true
     }
   }
 }
