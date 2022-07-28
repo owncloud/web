7e6633b42442f1ef9a29052ba8b9d5fde75a451f
JanAckermann <jackermann@owncloud.com>
2022-07-28 23:25:56 +0200

    Make contextual helpers work again

--- packages/web-app-files/src/helpers/contextualHelpers.js
+++ packages/web-app-files/src/helpers/contextualHelpers.js
@@ -9,41 +9,51 @@ export const empty = {
 export const shareInviteCollaboratorHelp = {
   text: $gettext('Invite persons or groups to access this file or folder.'),
   list: [
-    $gettext('Enter a name or group to share this item'),
-    $gettext(
-      'If you share a folder,  all of its contents and subfolders will be shared with the entered persons or groups'
-    ),
-    $gettext('Invited persons or groups will be notified via e-mail or in-app notification'),
-    $gettext('Invited persons can not see who else has access')
+    { text: $gettext('Enter a name or group to share this item') },
+    {
+      text: $gettext(
+        'If you share a folder,  all of its contents and subfolders will be shared with the entered persons or groups'
+      )
+    },
+    {
+      text: $gettext('Invited persons or groups will be notified via e-mail or in-app notification')
+    },
+    { text: $gettext('Invited persons can not see who else has access') }
   ]
 }
 export const shareInviteCollaboratorHelpCern = {
   list: [
-    $gettext(
-      'To search for service or secondary accounts prefix the username with "a:" (like "a:doe") and for guest accounts prefix the username with "l:" (like "l:doe")'
-    )
+    {
+      text: $gettext(
+        'To search for service or secondary accounts prefix the username with "a:" (like "a:doe") and for guest accounts prefix the username with "l:" (like "l:doe")'
+      )
+    }
   ]
 }
 export const shareSpaceAddMemberHelp = {
   text: $gettext('Add persons or groups as members to this Space. Members are allowed to:'),
   list: [
-    $gettext('see who else is member of this space'),
-    $gettext('view all files in this space'),
-    $gettext('(if permitted) edit or delete files in this Space'),
-    $gettext('(if permitted) share folders of this Space with non-Members'),
-    $gettext('see with whom a folder is shared')
+    { text: $gettext('see who else is member of this space') },
+    { text: $gettext('view all files in this space') },
+    { text: $gettext('(if permitted) edit or delete files in this Space') },
+    { text: $gettext('(if permitted) share folders of this Space with non-Members') },
+    { text: $gettext('see with whom a folder is shared') }
   ],
   endText: 'Members can only be added or removed by a Space Manager.'
 }
 export const shareViaLinkHelp = {
   text: $gettext('Share a file or folder by link. Choose how access is granted:'),
   list: [
-    $gettext(
-      'Only invited people can access: Only people from the list "Invited people" can access. If there is no list, no people are invited yet.'
-    ),
-    $gettext(
-      'Everyone with the link: Everyone with the link can access. Note: If you share this link with people from the list "Invited people", they need to login-in so that their individual assigned permissions can take effect. If they are not logged-in, the permissions of the link take effect.'
-    )
+    {
+      text: $gettext(
+        'Only invited people can access: Only people from the list "Invited people" can access. If there is no list, no people are invited yet.'
+      )
+    },
+    {
+      text: $gettext(
+        'Everyone with the link: Everyone with the link can access. Note: If you share this link with people from the list "Invited people", they need to login-in so that their individual assigned permissions can take effect. If they are not logged-in, the permissions of the link take effect.'
+      )
+    }
   ]
 }
 export const shareViaIndirectLinkHelp = {
@@ -52,8 +62,8 @@ export const shareViaIndirectLinkHelp = {
 export const shareQuickLinkHelp = {
   text: $gettext('The Quick link is the link that is always copied if you'),
   list: [
-    $gettext('right click on a file and choose "Get link" or'),
-    $gettext('click on the quickaction "Get link"')
+    { text: $gettext('right click on a file and choose "Get link" or') },
+    { text: $gettext('click on the quickaction "Get link"') }
   ]
 }
 export const shareAddLinkHelp = {
