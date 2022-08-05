52c0a3e8d0c0a2a8a342ed2656254afe03725260
PKiran <39373750+kiranparajuli589@users.noreply.github.com>
2022-08-05 13:26:49 +0545

    [ci] Update build ocis script (#7408)
    * [ci] Update build ocis script
    
    Signed-off-by: Kiran Parajuli <kiranparajuli589@gmail.com>
    
    * Check for existing oCIS cache in seperate step
    
    Signed-off-by: Kiran Parajuli <kiranparajuli589@gmail.com>
    
    * Remove skipIfUnchanged check
    
    Signed-off-by: Kiran Parajuli <kiranparajuli589@gmail.com>

--- tests/drone/build-ocis.sh
+++ tests/drone/build-ocis.sh
@@ -1,42 +1,32 @@
 #!/bin/bash
 
-STEP=$1 # which step to run
+# {string} which step to run
+# options: nodejs, golang
+STEP=$1
 
-if test -f runUnitTestsOnly
-then echo 'skipping build-ocis'
-else
-	source .drone.env
-	echo "checking ocis version - $OCIS_COMMITID in cache"
+REPO_URL=https://github.com/owncloud/ocis.git
+BASE_PATH=/var/www/owncloud/ocis-build/
 
-	url="https://cache.owncloud.com/owncloud/web/ocis-build/$OCIS_COMMITID/ocis"
+source .drone.env
 
-	echo "downloading ocis from $url"
-
-	if curl --output /dev/null --silent --head --fail "$url"
+if [ "$STEP" == "nodejs" ]
+then
+	mkdir -p "$GOPATH"/src/github.com/owncloud/
+	cd "$GOPATH"/src/github.com/owncloud/ || exit
+	git clone -b "$OCIS_BRANCH" --single-branch --no-tags $REPO_URL
+	cd ocis || exit
+	git checkout "$OCIS_COMMITID"
+	make ci-node-generate
+else # golang
+	cd "$GOPATH"/src/github.com/owncloud/ocis/ocis || exit
+	if make build
 	then
-		echo "ocis binary for $OCIS_COMMITID already available in cache"
-		exit 0
+		echo "oCIS build successful."
 	else
-		if [ "$STEP" == "nodejs" ]
-		then
-			mkdir -p "$GOPATH"/src/github.com/owncloud/
-			cd "$GOPATH"/src/github.com/owncloud/ || exit
-			git clone -b "$OCIS_BRANCH" --single-branch --no-tags https://github.com/owncloud/ocis
-			cd ocis || exit
-			git checkout "$OCIS_COMMITID"
-			make ci-node-generate
-		else # golang
-			cd "$GOPATH"/src/github.com/owncloud/ocis/ocis || exit
-			if make build
-			then
-				echo "oCIS build successful."
-			else
-				echo "oCIS build failed."
-				exit 1
-			fi
-			mkdir -p /var/www/owncloud/ocis-build/"$OCIS_COMMITID"
-			cp bin/ocis /var/www/owncloud/ocis-build/"$OCIS_COMMITID"/
-			ls -la /var/www/owncloud/ocis-build/"$OCIS_COMMITID"/
-		fi
+		echo "oCIS build failed."
+		exit 1
 	fi
+	mkdir -p "$BASE_PATH""$OCIS_COMMITID"
+	cp bin/ocis "$BASE_PATH""$OCIS_COMMITID"/
+	ls -la "$BASE_PATH""$OCIS_COMMITID"/
 fi
