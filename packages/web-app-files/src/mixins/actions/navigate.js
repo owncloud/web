4f5607feea25c26feefc957dc7c25dd38d230020
JanAckermann <jackermann@owncloud.com>
2022-08-10 13:29:47 +0200

    StorageId missing

--- packages/web-app-files/src/mixins/actions/navigate.js
+++ packages/web-app-files/src/mixins/actions/navigate.js
@@ -55,10 +55,12 @@ export default {
           route: ({ resources }) => {
             const shareId = this.getShareId(resources[0])
             const shareName = this.getShareName(resources[0])
+            const storageId = this.$route.params?.storageId
             return merge({}, this.routeName, {
               params: {
                 item: resources[0].path,
-                ...(shareName && { shareName })
+                ...(shareName && { shareName }),
+                ...(storageId && { storageId })
               },
               query: {
                 ...(shareId && { shareId })
