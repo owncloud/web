ea27a0fda9718bcdb0db6e2cf0e5b540dda90929
gitstart <gitstart@users.noreply.github.com>
2022-08-25 06:15:19 +0000

    resolved comments

--- packages/web-runtime/src/pages/logout.vue
+++ packages/web-runtime/src/pages/logout.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <p>Please wait while you're being logged out.</p>
+    <p v-translate>Please wait while you're being logged out.</p>
   </div>
 </template>
 <script>
 import { authService } from '../services/auth'
 export default {
   name: 'LogoutPage',
-  created() {
-    authService.logoutUser()
+  async created() {
+    await authService.logoutUser()
     this.router.push({ name: 'login' })
   }
 }
