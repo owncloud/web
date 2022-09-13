efcd70314940d822cfcf68e3825ad67085d4c2a4
Paul Neubauer <paulneubauer@live.de>
2022-09-13 17:19:07 +0200

    Linting, remove dev leftover

--- packages/web-app-files/src/mixins/actions/restore.ts
+++ packages/web-app-files/src/mixins/actions/restore.ts
@@ -99,7 +99,7 @@ export default {
         const hasConflict = parentResources.some((e) => e.name === resource.name)
         if (!hasConflict) {
           resolvedResources.push(resource)
-        }else {
+        } else {
           conflicts.push(resource)
         }
       }
@@ -140,13 +140,16 @@ export default {
       }
 
       //! 3
-      const filesToOverwrite = resolvedConflicts.filter(e => e.strategy === ResolveStrategy.REPLACE).map(e => e.resource)
+      const filesToOverwrite = resolvedConflicts
+        .filter((e) => e.strategy === ResolveStrategy.REPLACE)
+        .map((e) => e.resource)
       resolvedResources.push(...filesToOverwrite)
       const filesToKeepBoth = resolvedConflicts
-        .filter(e => e.strategy === ResolveStrategy.KEEP_BOTH).map(e => e.resource)
-      
+        .filter((e) => e.strategy === ResolveStrategy.KEEP_BOTH)
+        .map((e) => e.resource)
+
       for (let resource of filesToKeepBoth) {
-        resource = {...resource}
+        resource = { ...resource }
         const parentPath = getParentFolderFromResource(resource)
         const webDavParentPath = getWebdavParentFolderFromResource(resource)
         const parentResources = parentFolders[webDavParentPath]
@@ -224,84 +227,6 @@ export default {
         const driveId = isLocationTrashActive(this.$router, 'files-trash-spaces-project')
           ? this.$route.params.storageId
           : this.spaces.find((s) => s.driveType === 'personal').id
-        const driveResponse = await graphClient.drives.getDrive(driveId)
-        this.UPDATE_SPACE_FIELD({
-          id: driveResponse.data.id,
-          field: 'spaceQuota',
-          value: driveResponse.data.quota
-        })
-      } else {
-        const user = await this.$client.users.getUser(this.user.id)
-        this.SET_QUOTA(user.quota)
-      }
-      return
-
-      /*const restoredResources = []
-      const failedResources = []
-      const restorePromises = []
-      const restoreQueue = new PQueue({ concurrency: 4 })*/
-      resources.forEach((resource) => {
-        const hasShareJail = this.capabilities?.spaces?.share_jail === true
-        const path = hasShareJail
-          ? buildWebDavSpacesTrashPath(this.space.id)
-          : buildWebDavFilesTrashPath(this.user.id)
-        const restorePath = hasShareJail
-          ? buildWebDavSpacesPath(this.space.id, resource.path)
-          : buildWebDavFilesPath(this.user.id, resource.path)
-
-        restorePromises.push(
-          restoreQueue.add(async () => {
-            try {
-              await this.$client.fileTrash.restore(path, resource.id, restorePath)
-              restoredResources.push(resource)
-            } catch (e) {
-              console.error(e)
-              failedResources.push(resource)
-            }
-          })
-        )
-      })
-      await Promise.all(restorePromises)
-
-      // success handler (for partial and full success)
-      if (restoredResources.length > 0) {
-        this.removeFilesFromTrashbin(restoredResources)
-        let translated
-        const translateParams: any = {}
-        if (restoredResources.length === 1) {
-          translated = this.$gettext('%{resource} was restored successfully')
-          translateParams.resource = restoredResources[0].name
-        } else {
-          translated = this.$gettext('%{resourceCount} files restored successfully')
-          translateParams.resourceCount = restoredResources.length
-        }
-        this.showMessage({
-          title: this.$gettextInterpolate(translated, translateParams, true)
-        })
-      }
-
-      // failure handler (for partial and full failure)
-      if (failedResources.length > 0) {
-        let translated
-        const translateParams: any = {}
-        if (failedResources.length === 1) {
-          translated = this.$gettext('Failed to restore "%{resource}"')
-          translateParams.resource = failedResources[0].name
-        } else {
-          translated = this.$gettext('Failed to restore %{resourceCount} files')
-          translateParams.resourceCount = failedResources.length
-        }
-        this.showMessage({
-          title: this.$gettextInterpolate(translated, translateParams, true),
-          status: 'danger'
-        })
-      }
-
-      // Load quota
-      if (this.capabilities?.spaces?.enabled) {
-        const accessToken = this.$store.getters['runtime/auth/accessToken']
-        const graphClient = clientService.graphAuthenticated(this.configuration.server, accessToken)
-        const driveId = this.space.id
         const driveResponse = await graphClient.drives.getDrive(driveId)
         this.UPDATE_SPACE_FIELD({
           id: driveResponse.data.id,
