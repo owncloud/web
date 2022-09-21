3ad57555a693b1cc9968b48018f3bfa53ffad34b
Dominik Schmidt <dev@dominik-schmidt.de>
2022-09-21 20:54:34 +0200

    Fix type narrowing

--- packages/web-client/src/webdav/listFiles.ts
+++ packages/web-client/src/webdav/listFiles.ts
@@ -21,8 +21,6 @@ export const GetListFilesFactory = (sdk: OwnCloudSdk) => {
           resource.name = resource.name.split('/').slice(2).join('/')
         })
       } else {
-        // FIXME: why does typescript assign type never here? Not every Resource is a PublicSpaceResource
-        space = space as SpaceResource
         resources = await sdk.files.list(`${space.webDavPath}/${path}`, 1, DavProperties.Default)
       }
 
