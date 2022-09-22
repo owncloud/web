041e0786fb2290636a7c389c0532e391ab61e3f8
Dominik Schmidt <dev@dominik-schmidt.de>
2022-09-22 16:25:18 +0200

    add createFolder.ts

---
+++ packages/web-client/src/webdav/createFolder.ts
@@ -0,0 +1,28 @@
+import { buildResource } from 'files/src/helpers/resources'
+import { getFileInfo } from 'prettier'
+import { DavProperties } from 'web-pkg/src/constants'
+import { OwnCloudSdk } from 'web-pkg/src/services'
+import { FolderResource, isPublicSpaceResource, Resource, SpaceResource } from '../helpers'
+import { GetFileInfoFactory } from './getFileInfo'
+
+export const CreateFolderFactory = (getFileInfoFactory: ReturnType<typeof GetFileInfoFactory>, sdk: OwnCloudSdk) => {
+  return {
+    async createFolder(space: SpaceResource, { path }: { path?: string }): Promise<FolderResource> {
+      let resource: Resource[]
+      if (isPublicSpaceResource(space)) {
+        await sdk.publicFiles.createFolder(
+            `${space.webDavPath.replace(/^\/public-files/, '')}/${path || ''}`,
+            null,
+            space.publicLinkPassword
+        )
+      } else {
+        await sdk.files.createFolder(
+            `${space.webDavPath}/${path || ''}`,
+            DavProperties.Default
+        )
+      }
+
+      return getFileInfoFactory.getFileInfo(space, { path }) as Promise<FolderResource>
+    }
+  }
+}
