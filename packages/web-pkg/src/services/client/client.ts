03d2f5e1bc4de58758d8349efd2dc3f1f8e756c7
Dominik Schmidt <dev@dominik-schmidt.de>
2022-09-22 21:01:20 +0200

    Add loose types for OwncloudSdk

--- packages/web-pkg/src/services/client/client.ts
+++ packages/web-pkg/src/services/client/client.ts
@@ -5,7 +5,44 @@ import axios, { AxiosInstance } from 'axios'
 import { v4 as uuidV4 } from 'uuid'
 import { WebDAV } from 'web-client/src/webdav'
 
-export type OwnCloudSdk = any
+export type OwnCloudSdk = {
+  files: {
+    createFolder: (...args) => any
+    fileInfo: (...args) => any
+    getFileUrl: (...args) => any
+    list: (...args) => any
+    getFileContents: (...args) => any
+    putFileContents: (...args) => any
+    getFavoriteFiles: (...args) => any
+    search: (...args) => any
+  }
+  fileTrash: {
+    list: (...args) => any
+  }
+  publicFiles: {
+    createFolder: (...args) => any
+    download: (...args) => any
+    list: (...args) => any
+    getFileContents: (...args) => any
+    getFileInfo: (...args) => any
+    getFileUrl: (...args) => any
+    putFileContents: (...args) => any
+  }
+  settings: {
+    getSettingsValues: (...args) => any
+  }
+  shares: {
+    getShare: (...args) => any
+    getShares: (...args) => any
+  }
+  users: {
+    getUser: (...args) => any
+    getUserGroups: (...args) => any
+  }
+  getCurrentUser: (...args) => any
+  init: (...args) => any
+  signUrl: (...args) => any
+}
 interface OcClient {
   token: string
   graph: Graph
