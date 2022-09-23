26fd5b76e343c9cfb9ece9bdc6a42ec7e9745edd
Dominik Schmidt <dev@dominik-schmidt.de>
2022-09-23 09:15:10 +0200

    fixup! Move OwncloudSdk type to web-client to make the package standalone again

---
+++ packages/web-client/src/types.ts
@@ -0,0 +1,38 @@
+export type OwnCloudSdk = {
+  files: {
+    createFolder(...args): any
+    fileInfo(...args): any
+    getFileUrl(...args): any
+    list(...args): any
+    getFileContents(...args): any
+    putFileContents(...args): any
+    getFavoriteFiles(...args): any
+    search(...args): any
+  }
+  fileTrash: {
+    list(...args): any
+  }
+  publicFiles: {
+    createFolder(...args): any
+    download(...args): any
+    list(...args): any
+    getFileContents(...args): any
+    getFileInfo(...args): any
+    getFileUrl(...args): any
+    putFileContents(...args): any
+  }
+  settings: {
+    getSettingsValues(...args): any
+  }
+  shares: {
+    getShare(...args): any
+    getShares(...args): any
+  }
+  users: {
+    getUser(...args): any
+    getUserGroups(...args): any
+  }
+  getCurrentUser(...args): any
+  init(...args): any
+  signUrl(...args): any
+}
