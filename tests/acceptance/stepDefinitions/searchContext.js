8cacccca4f0299337d617a441e44679eb84c6906
amrita <anukanxi05@gmail.com>
2022-09-27 09:53:17 +0545

    Add pause before serach

--- tests/acceptance/stepDefinitions/searchContext.js
+++ tests/acceptance/stepDefinitions/searchContext.js
@@ -1,6 +1,7 @@
 const { client } = require('nightwatch-api')
 const { When } = require('@cucumber/cucumber')
 
-When('the user searches for {string} using the webUI', function (searchTerm) {
-  return client.page.webPage().search(searchTerm)
+When('the user searches for {string} using the webUI', async function (searchTerm) {
+  await client.pause(500)
+  await client.page.webPage().search(searchTerm)
 })
