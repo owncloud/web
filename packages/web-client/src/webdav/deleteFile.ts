fbb1d66ce3e06c6eda4218465f649846a6e13c9a
Dominik Schmidt <dev@dominik-schmidt.de>
2022-09-27 16:54:18 +0200

    Add missing deleteFile.ts

---
+++ packages/web-client/src/webdav/deleteFile.ts
@@ -0,0 +1,22 @@
+import urlJoin from 'proper-url-join'
+import { FileResource, isPublicSpaceResource, SpaceResource } from '../helpers'
+import { WebDavOptions } from './types'
+
+export const DeleteFileFactory = ({ sdk }: WebDavOptions) => {
+  return {
+    async deleteFile(
+      space: SpaceResource,
+      { path, ...options }: { path?: string; content?: string; previousEntityTag?: string }
+    ): Promise<FileResource> {
+      if (isPublicSpaceResource(space)) {
+        return sdk.publicFiles.delete(
+          urlJoin(space.webDavPath.replace(/^\/public-files/, ''), path),
+          null,
+          space.publicLinkPassword
+        )
+      }
+
+      return sdk.files.delete(urlJoin(space.webDavPath, path))
+    }
+  }
+}
