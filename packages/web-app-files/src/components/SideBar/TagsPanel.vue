2e6b24e5763d4ae80589278485707d7966f0cc52
Florian Schade <f.schade@icloud.com>
2022-10-18 14:18:57 +0200

    revert resource tags on resource switch

--- packages/web-app-files/src/components/SideBar/TagsPanel.vue
+++ packages/web-app-files/src/components/SideBar/TagsPanel.vue
@@ -96,6 +96,11 @@ export default defineComponent({
       return this.displayedItem.value
     }
   },
+  watch: {
+    resource() {
+      this.revertChanges()
+    }
+  },
   mounted() {
     this.editAssignedTags = [...this.resource.tags]
     this.loadAllTagsTask.perform(this)
