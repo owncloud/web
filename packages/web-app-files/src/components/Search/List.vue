4c178d98d9644d7b26c536ef9ffe43394af2f758
Florian Schade <f.schade@icloud.com>
2022-10-19 14:57:34 +0200

    fix oc10 search tests

--- packages/web-app-files/src/components/Search/List.vue
+++ packages/web-app-files/src/components/Search/List.vue
@@ -173,7 +173,7 @@ export default defineComponent({
   watch: {
     searchResult: {
       handler: function () {
-        if (!this.searchResult || this.searchResult.totalResults === null) {
+        if (!this.searchResult) {
           return
         }
 
@@ -184,8 +184,7 @@ export default defineComponent({
             ? this.searchResult.values.map((searchResult) => searchResult.data)
             : []
         })
-      },
-      immediate: true
+      }
     }
   },
   beforeDestroy() {
