5e2bfaf842c04e4b5356a0888e816dd35fc20a01
Jan <jackermann@owncloud.com>
2022-11-03 12:36:38 +0100

    Add space member filter (#7901)

--- packages/web-app-files/src/components/SideBar/Shares/SpaceMembers.vue
+++ packages/web-app-files/src/components/SideBar/Shares/SpaceMembers.vue
@@ -1,8 +1,37 @@
 <template>
   <div id="oc-files-sharing-sidebar" class="oc-position-relative">
-    <div class="oc-flex">
-      <h3 v-translate class="oc-text-bold oc-text-medium oc-m-rm">Members</h3>
-      <oc-contextual-helper v-if="helpersEnabled" class="oc-pl-xs" v-bind="spaceAddMemberHelp" />
+    <div class="oc-flex oc-flex-between oc-flex-center oc-flex-middle">
+      <div class="oc-flex oc-py-s">
+        <h3 v-translate class="oc-text-bold oc-text-medium oc-m-rm">Members</h3>
+        <oc-contextual-helper v-if="helpersEnabled" class="oc-pl-xs" v-bind="spaceAddMemberHelp" />
+      </div>
+      <div class="oc-flex">
+        <div v-if="isFilterOpen" class="oc-flex">
+          <oc-text-input
+            v-model="filterTerm"
+            class="oc-text-truncate space-members-filter oc-mr-s"
+            label=""
+            :placeholder="$gettext('Filter members')"
+          />
+          <oc-button
+            v-oc-tooltip="$gettext('Close filter')"
+            :aria-label="$gettext('Close filter')"
+            appearance="raw"
+            @click="toggleFilter"
+          >
+            <oc-icon name="close" fill-type="line" size="small" />
+          </oc-button>
+        </div>
+        <oc-button
+          v-else
+          v-oc-tooltip="$gettext('Filter members')"
+          :aria-label="$gettext('Filter members')"
+          appearance="raw"
+          @click="toggleFilter"
+        >
+          <oc-icon name="search" fill-type="line" size="small" />
+        </oc-button>
+      </div>
     </div>
     <invite-collaborator-form
       v-if="currentUserCanShare"
@@ -14,10 +43,11 @@
     <template v-if="hasCollaborators">
       <ul
         id="files-collaborators-list"
+        ref="collaboratorList"
         class="oc-list oc-list-divider oc-overflow-hidden oc-m-rm"
         :aria-label="$gettext('Space members')"
       >
-        <li v-for="collaborator in spaceMembers" :key="collaborator.key">
+        <li v-for="collaborator in filteredSpaceMembers" :key="collaborator.key">
           <collaborator-list-item
             :share="collaborator"
             :modifiable="isModifiable(collaborator)"
@@ -39,6 +69,8 @@ import { defineComponent, PropType } from '@vue/composition-api'
 import { shareSpaceAddMemberHelp } from '../../../helpers/contextualHelpers'
 import { SpaceResource } from 'web-client/src/helpers'
 import { useGraphClient } from 'web-pkg/src/composables'
+import Fuse from 'fuse.js'
+import Mark from 'mark.js'
 
 export default defineComponent({
   name: 'SpaceMembers',
@@ -58,11 +90,21 @@ export default defineComponent({
       ...useGraphClient()
     }
   },
+  data: () => {
+    return {
+      filterTerm: '',
+      isFilterOpen: false,
+      markInstance: null
+    }
+  },
   computed: {
     ...mapGetters(['configuration']),
     ...mapGetters('runtime/spaces', ['spaceMembers']),
     ...mapState(['user']),
 
+    filteredSpaceMembers() {
+      return this.filter(this.spaceMembers, this.filterTerm)
+    },
     helpersEnabled() {
       return this.configuration?.options?.contextHelpers
     },
@@ -83,10 +125,43 @@ export default defineComponent({
       return currentUserCollaborator?.role?.name === spaceRoleManager.name
     }
   },
+  watch: {
+    isFilterOpen() {
+      this.filterTerm = ''
+    },
+    filterTerm() {
+      this.$nextTick(() => {
+        if (this.$refs.collaboratorList) {
+          this.markInstance = new Mark(this.$refs.collaboratorList)
+          this.markInstance.unmark()
+          this.markInstance.mark(this.filterTerm, {
+            element: 'span',
+            className: 'highlight-mark'
+          })
+        }
+      })
+    }
+  },
   methods: {
     ...mapActions('runtime/spaces', ['deleteSpaceMember']),
     ...mapActions(['createModal', 'hideModal', 'showMessage']),
 
+    filter(collection, term) {
+      if (!(term || '').trim()) {
+        return collection
+      }
+      const searchEngine = new Fuse(collection, {
+        includeScore: true,
+        useExtendedSearch: true,
+        threshold: 0.3,
+        keys: ['collaborator.displayName', 'collaborator.name']
+      })
+
+      return searchEngine.search(term).map((r) => r.item)
+    },
+    toggleFilter() {
+      this.isFilterOpen = !this.isFilterOpen
+    },
     isModifiable(share) {
       if (!this.currentUserIsManager) {
         return false
@@ -153,6 +228,12 @@ export default defineComponent({
 </script>
 
 <style>
+.highlight-mark {
+  font-weight: 600;
+}
+.space-members-filter {
+  max-width: 160px;
+}
 .avatars-wrapper {
   height: 40px;
 }
