cf21a758cc26fd14f00914ecd3eef56aaec16180
Dominik Schmidt <dev@dominik-schmidt.de>
2022-11-12 11:00:24 +0100

    Upgrade rollup-plugin-node-resolve to @rollup/plugin-node-resolve

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,5 @@
 import vue from 'rollup-plugin-vue'
-// import resolve from '@rollup/plugin-node-resolve'
-import resolve from 'rollup-plugin-node-resolve'
-
+import resolve from '@rollup/plugin-node-resolve'
 import json from '@rollup/plugin-json'
 import commonjs from '@rollup/plugin-commonjs'
 import babel from 'rollup-plugin-babel'
