2703cd9202b93f6fa4fd1c27182b339dcd25fc63
Jan <j.ackermann91@gmail.com>
2022-11-15 09:12:45 +0100

    Enter key submits form

--- packages/web-app-user-management/src/components/Groups/CreateGroupModal.vue
+++ packages/web-app-user-management/src/components/Groups/CreateGroupModal.vue
@@ -3,13 +3,13 @@
     :title="$gettext('Create group')"
     :button-cancel-text="$gettext('Cancel')"
     :button-confirm-text="$gettext('Create')"
-    :button-confirm-disabled="buttonConfirmDisabled"
+    :button-confirm-disabled="isFormInvalid"
     focus-trap-initial="#create-group-input-display-name"
     @cancel="$emit('cancel')"
     @confirm="$emit('confirm', group)"
   >
     <template #content>
-      <form autocomplete="off">
+      <form autocomplete="off" @submit.prevent="onFormSubmit">
         <oc-text-input
           id="create-group-input-display-name"
           v-model="group.displayName"
@@ -50,7 +50,7 @@ export default {
     }
   },
   computed: {
-    buttonConfirmDisabled() {
+    isFormInvalid() {
       return Object.keys(this.formData)
         .map((k) => !!this.formData[k].valid)
         .includes(false)
@@ -80,6 +80,12 @@ export default {
       this.formData.displayName.errorMessage = ''
       this.formData.displayName.valid = true
       return true
+    },
+    onFormSubmit() {
+      if (this.isFormInvalid) {
+        return
+      }
+      this.$emit('confirm', this.group)
     }
   }
 }
