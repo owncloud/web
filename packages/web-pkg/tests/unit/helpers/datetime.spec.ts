7cd20686619bb13c7fa8fb4c044e578070973db5
Paul Neubauer <paulneubauer@live.de>
2022-11-29 12:33:14 +0100

    adjust step descriptions

--- packages/web-pkg/tests/unit/helpers/datetime.spec.ts
+++ packages/web-pkg/tests/unit/helpers/datetime.spec.ts
@@ -32,7 +32,7 @@ describe('datetime helper', () => {
       [DateTime.fromISO('2010-10-22T21:38:00'), 'fr', DateTime.DATETIME_MED, '22 oct. 2010, 21:38'],
       [DateTime.fromISO('2010-10-22T21:38:00'), 'it', DateTime.DATETIME_MED, '22 ott 2010, 21:38']
     ])(
-      'invalid input "%s"',
+      'correct output for %s (lang: %s)',
       (date: DateTime, currentLanguage: string, format: any, expected: string) => {
         expect(formatDateFromDateTime(date, currentLanguage, format)).toBe(expected)
       }
@@ -42,7 +42,7 @@ describe('datetime helper', () => {
     it.each([
       [new Date('2010-10-22T21:38:00'), 'de', DateTime.DATETIME_MED, '22. Okt. 2010, 21:38']
     ])(
-      'invalid input "%s"',
+      'correct output for %s (lang: %s)',
       (date: Date, currentLanguage: string, format: any, expected: string) => {
         expect(formatDateFromJSDate(date, currentLanguage, format)).toBe(expected)
       }
@@ -52,7 +52,7 @@ describe('datetime helper', () => {
     it.each([
       ['Tue, 15 Nov 1994 12:45:26 GMT', 'de', DateTime.DATETIME_MED, '15. Nov. 1994, 12:45']
     ])(
-      'invalid input "%s"',
+      'correct output for %s (lang: %s)',
       (date: string, currentLanguage: string, format: any, expected: string) => {
         expect(formatDateFromHTTP(date, currentLanguage, format)).toBe(expected)
       }
@@ -60,7 +60,7 @@ describe('datetime helper', () => {
   })
   describe('formatDateFromISO', () => {
     it.each([['2010-10-22T21:38:00', 'de', DateTime.DATETIME_MED, '22. Okt. 2010, 21:38']])(
-      'invalid input "%s"',
+      'correct output for %s (lang: %s)',
       (date: string, currentLanguage: string, format: any, expected: string) => {
         expect(formatDateFromISO(date, currentLanguage, format)).toBe(expected)
       }
@@ -68,7 +68,7 @@ describe('datetime helper', () => {
   })
   describe('formatDateFromRFC', () => {
     it.each([['01 Jun 2016 14:31:46 -0700', 'de', DateTime.DATETIME_MED, '1. Juni 2016, 21:31']])(
-      'invalid input "%s"',
+      'correct output for %s (lang: %s)',
       (date: string, currentLanguage: string, format: any, expected: string) => {
         expect(formatDateFromRFC(date, currentLanguage, format)).toBe(expected)
       }
@@ -79,13 +79,16 @@ describe('datetime helper', () => {
       [DateTime.now().minus({ years: 12 }), 'de', 'vor 12 Jahren'],
       [DateTime.now().minus({ years: 12 }), 'en', '12 years ago'],
       [DateTime.now().minus({ years: 12 }), 'it', '12 anni fa']
-    ])('invalid input "%s"', (date: DateTime, currentLanguage: string, expected) => {
-      expect(formatRelativeDateFromDateTime(date, currentLanguage)).toBe(expected)
-    })
+    ])(
+      'correct relative time for %s (lang: %s)',
+      (date: DateTime, currentLanguage: string, expected) => {
+        expect(formatRelativeDateFromDateTime(date, currentLanguage)).toBe(expected)
+      }
+    )
   })
   describe('formatRelativeDateFromJSDate', () => {
     it.each([[DateTime.now().minus({ years: 12 }).toJSDate(), 'de', 'vor 12 Jahren']])(
-      'invalid input "%s"',
+      'correct relative time for %s (lang: %s)',
       (date: Date, currentLanguage: string, expected) => {
         expect(formatRelativeDateFromJSDate(date, currentLanguage)).toBe(expected)
       }
@@ -93,7 +96,7 @@ describe('datetime helper', () => {
   })
   describe('formatRelativeDateFromISO', () => {
     it.each([[DateTime.now().minus({ years: 12 }).toISO(), 'de', 'vor 12 Jahren']])(
-      'invalid input "%s"',
+      'correct relative time for %s (lang: %s)',
       (date: string, currentLanguage: string, expected) => {
         expect(formatRelativeDateFromISO(date, currentLanguage)).toBe(expected)
       }
@@ -101,7 +104,7 @@ describe('datetime helper', () => {
   })
   describe('formatRelativeDateFromRFC', () => {
     it.each([[DateTime.now().minus({ years: 12 }).toRFC2822(), 'de', 'vor 12 Jahren']])(
-      'invalid input "%s"',
+      'correct relative time for %s (lang: %s)',
       (date: string, currentLanguage: string, expected) => {
         expect(formatRelativeDateFromRFC(date, currentLanguage)).toBe(expected)
       }
