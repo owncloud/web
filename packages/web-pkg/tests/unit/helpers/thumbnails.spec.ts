3a2bfc4c7ff211cfd060a5bcc6373395d5497433
Paul Neubauer <paulneubauer@live.de>
2022-12-09 16:54:40 +0100

    Fix linting

--- packages/web-pkg/tests/unit/helpers/thumbnails.spec.ts
+++ packages/web-pkg/tests/unit/helpers/thumbnails.spec.ts
@@ -4,16 +4,24 @@ import { isResourceTxtFileAlmostEmpty } from '../../../src/helpers'
 describe('thumbnails', () => {
   describe('isResourceTxtFileAlmostEmpty', () => {
     it('return true for resources smaller 30 bytes', () => {
-      expect(isResourceTxtFileAlmostEmpty({ mimeType: 'text/plain', size: 20 } as Resource)).toBe(true)
+      expect(isResourceTxtFileAlmostEmpty({ mimeType: 'text/plain', size: 20 } as Resource)).toBe(
+        true
+      )
     })
-		it('return false for resources larger 30 bytes', () => {
-      expect(isResourceTxtFileAlmostEmpty({ mimeType: 'text/plain', size: 35 } as Resource)).toBe(false)
+    it('return false for resources larger 30 bytes', () => {
+      expect(isResourceTxtFileAlmostEmpty({ mimeType: 'text/plain', size: 35 } as Resource)).toBe(
+        false
+      )
     })
-		it('return false for resources that are not text', () => {
-      expect(isResourceTxtFileAlmostEmpty({ mimeType: 'application/json', size: 35 } as Resource)).toBe(false)
+    it('return false for resources that are not text', () => {
+      expect(
+        isResourceTxtFileAlmostEmpty({ mimeType: 'application/json', size: 35 } as Resource)
+      ).toBe(false)
     })
-		it('return false for resources that have undefined mimeType', () => {
-      expect(isResourceTxtFileAlmostEmpty({ mimeType: undefined, size: 35 } as Resource)).toBe(false)
+    it('return false for resources that have undefined mimeType', () => {
+      expect(isResourceTxtFileAlmostEmpty({ mimeType: undefined, size: 35 } as Resource)).toBe(
+        false
+      )
     })
   })
 })
