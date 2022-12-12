15f20a09ebe7298fd5da16c7dabe092564d36842
Jannik Stehle <jannik.stehle@gmail.com>
2022-12-12 19:53:00 +0100

    Use new test syntax for SharesPanel component

---
+++ packages/web-app-files/tests/unit/components/SideBar/Shares/SharesPanel.spec.ts
@@ -0,0 +1,44 @@
+import SharesPanel from 'web-app-files/src/components/SideBar/Shares/SharesPanel.vue'
+import { createStore, defaultPlugins, shallowMount } from 'web-test-helpers'
+import { defaultStoreMockOptions } from 'web-test-helpers/src/mocks/store/defaultStoreMockOptions'
+
+const ocLoaderStubSelector = 'oc-loader-stub'
+
+jest.mock('web-pkg/src/composables/reactivity')
+describe('SharesPanel', () => {
+  describe('when loading is set to true', () => {
+    it('should show the oc loader', () => {
+      const { wrapper } = getWrapper({ sharesLoading: true })
+
+      expect(wrapper.find(ocLoaderStubSelector).exists()).toBeTruthy()
+      expect(wrapper.find(ocLoaderStubSelector).attributes().arialabel).toBe(
+        'Loading list of shares'
+      )
+    })
+  })
+  describe('when sharesLoading is set to false', () => {
+    it('should not show the oc loader', () => {
+      const { wrapper } = getWrapper()
+      expect(wrapper.find('oc-loader-stub').exists()).toBeFalsy()
+    })
+  })
+
+  function getWrapper({ sharesLoading = false } = {}) {
+    const storeOptions = defaultStoreMockOptions
+    storeOptions.modules.Files.getters.sharesTreeLoading.mockImplementation(() => sharesLoading)
+    const store = createStore(storeOptions)
+    return {
+      wrapper: shallowMount(SharesPanel, {
+        global: {
+          plugins: [...defaultPlugins(), store],
+          provide: {
+            activePanel: null,
+            displayedItem: {},
+            displayedSpace: {},
+            spaceMembers: { value: [] }
+          }
+        }
+      })
+    }
+  }
+})
