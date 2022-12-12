06ddeaecff67330c0949c6e66ddf306ec427eebf
Jannik Stehle <jannik.stehle@gmail.com>
2022-12-12 14:34:47 +0100

    Use new test syntax for CreateSpace component

--- packages/web-app-files/tests/unit/components/AppBar/CreateSpace.spec.ts
+++ packages/web-app-files/tests/unit/components/AppBar/CreateSpace.spec.ts
@@ -1,14 +1,11 @@
-import { mount } from '@vue/test-utils'
 import CreateSpace from '../../../../src/components/AppBar/CreateSpace.vue'
-import Vuex from 'vuex'
-import { defaultLocalVue } from 'web-test-helpers/src/localVue/defaultLocalVue'
 import { defaultStoreMockOptions } from 'web-test-helpers/src/mocks/store/defaultStoreMockOptions'
 import { defaultComponentMocks } from 'web-test-helpers/src/mocks/defaultComponentMocks'
 import { mockDeep } from 'jest-mock-extended'
 import { Graph, Resource } from 'web-client'
 import { Drive } from 'web-client/src/generated'
+import { createStore, defaultPlugins, mount } from 'web-test-helpers'
 
-const localVue = defaultLocalVue()
 const selectors = {
   newSpaceBtn: '#new-space-menu-btn'
 }
@@ -79,15 +76,16 @@ describe('CreateSpace component', () => {
 
 function getWrapper() {
   const storeOptions = { ...defaultStoreMockOptions }
-  const store = new Vuex.Store(storeOptions)
+  const store = createStore(storeOptions)
   const mocks = defaultComponentMocks()
   return {
     storeOptions,
     mocks,
     wrapper: mount(CreateSpace, {
-      localVue,
-      mocks,
-      store
+      global: {
+        mocks,
+        plugins: [...defaultPlugins(), store]
+      }
     })
   }
 }
