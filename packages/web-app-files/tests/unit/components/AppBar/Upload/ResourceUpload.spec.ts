416ecfcc7f3e557f31287dc055ab3a658b4085f2
Jannik Stehle <jannik.stehle@gmail.com>
2022-12-12 14:51:22 +0100

    Use new test syntax for ResourceUpload component

--- packages/web-app-files/tests/unit/components/AppBar/Upload/ResourceUpload.spec.ts
+++ packages/web-app-files/tests/unit/components/AppBar/Upload/ResourceUpload.spec.ts
@@ -1,13 +1,19 @@
-import { mount, createLocalVue } from '@vue/test-utils'
 import ResourceUpload from '../../../../../src/components/AppBar/Upload/ResourceUpload.vue'
-import DesignSystem from 'owncloud-design-system'
+import { defaultStoreMockOptions } from 'web-test-helpers/src/mocks/store/defaultStoreMockOptions'
+import {
+  createStore,
+  defaultComponentMocks,
+  defaultPlugins,
+  defaultStubs,
+  mount
+} from 'web-test-helpers'
 
 const Translate = jest.fn()
 
 describe('Resource Upload Component', () => {
   describe('file upload', () => {
     it('should render component', () => {
-      const wrapper = mount(ResourceUpload, getOptions())
+      const { wrapper } = getWrapper()
       expect(wrapper.exists()).toBeTruthy()
       expect(wrapper).toMatchSnapshot()
     })
@@ -15,7 +21,7 @@ describe('Resource Upload Component', () => {
 
   describe('folder upload', () => {
     it('should render component', () => {
-      const wrapper = mount(ResourceUpload, getOptions({ isFolder: true }))
+      const { wrapper } = getWrapper({ isFolder: true })
       expect(wrapper.exists()).toBeTruthy()
       expect(wrapper).toMatchSnapshot()
     })
@@ -23,40 +29,38 @@ describe('Resource Upload Component', () => {
 
   describe('when upload file button is clicked', () => {
     it('should call "triggerUpload"', async () => {
-      const wrapper = mount(ResourceUpload, {
-        ...getOptions(),
-        stubs: {
-          'oc-icon': true,
-          'oc-button': false
-        }
-      })
+      const { wrapper } = getWrapper()
 
       const spyTriggerUpload = jest.spyOn(wrapper.vm, 'triggerUpload')
       const uploadButton = wrapper.find('button')
       const fileUploadInput = wrapper.find('#files-file-upload-input')
-      fileUploadInput.element.click = jest.fn()
+
+      ;(fileUploadInput.element as HTMLElement).click = jest.fn()
       await wrapper.vm.$forceUpdate()
 
       await uploadButton.trigger('click')
 
       expect(spyTriggerUpload).toHaveBeenCalledTimes(1)
-      expect(fileUploadInput.element.click).toHaveBeenCalledTimes(1)
+      expect((fileUploadInput.element as HTMLElement).click).toHaveBeenCalledTimes(1)
     })
   })
 })
 
-function getOptions(propsData = {}) {
-  const localVue = createLocalVue()
-  localVue.use(DesignSystem)
+function getWrapper(props = {}) {
+  const storeOptions = { ...defaultStoreMockOptions }
+  const store = createStore(storeOptions)
+  const mocks = defaultComponentMocks()
   return {
-    propsData,
-    localVue,
-    directives: { Translate },
-    mocks: {
-      $uppyService: {
-        registerUploadInput: jest.fn(),
-        removeUploadInput: jest.fn()
+    storeOptions,
+    mocks,
+    wrapper: mount(ResourceUpload, {
+      props,
+      global: {
+        mocks,
+        stubs: defaultStubs,
+        plugins: [...defaultPlugins(), store],
+        directives: { Translate }
       }
-    }
+    })
   }
 }
