89dc86810a951cf6c9df22f5ad4481b9def035cb
Jannik Stehle <jannik.stehle@gmail.com>
2022-12-12 18:23:37 +0100

    Use new test syntax for FIleDetailsMultiple component

--- packages/web-app-files/tests/unit/components/SideBar/Details/FileDetailsMultiple.spec.ts
+++ packages/web-app-files/tests/unit/components/SideBar/Details/FileDetailsMultiple.spec.ts
@@ -1,17 +1,6 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils'
-import Vuex from 'vuex'
 import FileDetailsMultiple from '../../../../../src/components/SideBar/Details/FileDetailsMultiple.vue'
-import stubs from '../../../../../../../tests/unit/stubs'
-import GetTextPlugin from 'vue-gettext'
-import AsyncComputed from 'vue-async-computed'
-
-const localVue = createLocalVue()
-localVue.use(Vuex)
-localVue.use(AsyncComputed)
-localVue.use(GetTextPlugin, {
-  translations: 'does-not-matter.json',
-  silent: true
-})
+import { createStore, defaultPlugins, shallowMount } from 'web-test-helpers'
+import { defaultStoreMockOptions } from 'web-test-helpers/src/mocks/store/defaultStoreMockOptions'
 
 const selectors = {
   selectedFilesText: '[data-testid="selectedFilesText"]',
@@ -51,21 +40,21 @@ const fileB = {
 
 describe('Details Multiple Selection SideBar Item', () => {
   it('should display information for two selected folders', () => {
-    const wrapper = createWrapper([folderA, folderB])
+    const { wrapper } = createWrapper([folderA, folderB])
     expect(wrapper.find(selectors.selectedFilesText).text()).toBe('2 items selected')
     expect(wrapper.find(selectors.filesCount).text()).toBe('Files 0')
     expect(wrapper.find(selectors.foldersCount).text()).toBe('Folders 2')
     expect(wrapper.find(selectors.size).text()).toBe('Size 1 kB')
   })
   it('should display information for two selected files', () => {
-    const wrapper = createWrapper([fileA, fileB])
+    const { wrapper } = createWrapper([fileA, fileB])
     expect(wrapper.find(selectors.selectedFilesText).text()).toBe('2 items selected')
     expect(wrapper.find(selectors.filesCount).text()).toBe('Files 2')
     expect(wrapper.find(selectors.foldersCount).text()).toBe('Folders 0')
     expect(wrapper.find(selectors.size).text()).toBe('Size 1 kB')
   })
   it('should display information for one selected file, one selected folder', () => {
-    const wrapper = createWrapper([fileA, folderA])
+    const { wrapper } = createWrapper([fileA, folderA])
     expect(wrapper.find(selectors.selectedFilesText).text()).toBe('2 items selected')
     expect(wrapper.find(selectors.filesCount).text()).toBe('Files 1')
     expect(wrapper.find(selectors.foldersCount).text()).toBe('Folders 1')
@@ -73,21 +62,15 @@ describe('Details Multiple Selection SideBar Item', () => {
   })
 })
 
-function createWrapper(testResource) {
-  return shallowMount(FileDetailsMultiple, {
-    store: new Vuex.Store({
-      modules: {
-        Files: {
-          namespaced: true,
-          getters: {
-            selectedFiles: function () {
-              return testResource
-            }
-          }
-        }
+function createWrapper(testResources) {
+  const storeOptions = defaultStoreMockOptions
+  storeOptions.modules.Files.getters.selectedFiles.mockImplementation(() => testResources)
+  const store = createStore(storeOptions)
+  return {
+    wrapper: shallowMount(FileDetailsMultiple, {
+      global: {
+        plugins: [...defaultPlugins(), store]
       }
-    }),
-    localVue,
-    stubs: stubs
-  })
+    })
+  }
 }
