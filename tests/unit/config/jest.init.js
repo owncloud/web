569e1fbd4c0921a405360bfd35065777cc5c23e7
Dominik Schmidt <dev@dominik-schmidt.de>
2022-12-15 17:36:39 +0100

    Remove gettext mocks from jest.init.js

--- tests/unit/config/jest.init.js
+++ tests/unit/config/jest.init.js
@@ -14,15 +14,6 @@ window.IntersectionObserver = jest.fn(() => ({
 fetchMock.enableMocks()
 
 config.mocks = {
-  $gettext: (str) => str,
-  $pgettext: (context, msgid) => msgid,
-  $ngettext: (msgid, plural, n) => {
-    if (n > 1 || n === 0 || n < 0 || !Number.isInteger(n)) {
-      return plural
-    } else {
-      return msgid
-    }
-  },
   $language: {
     current: 'en'
   }
