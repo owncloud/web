b9ba3d8894bf635b9dda1cb70edd827ca44e1521
Jannik Stehle <jannik.stehle@gmail.com>
2023-01-04 15:26:06 +0100

    Refactor admin settings index unit tests

---
+++ packages/web-app-admin-settings/tests/unit/index.spec.ts
@@ -0,0 +1,53 @@
+import index from '../../src/index'
+
+describe('admin settings index', () => {
+  beforeEach(() => {
+    ;(window as any).Vue = {}
+  })
+  describe('navItems', () => {
+    describe('user management', () => {
+      it.each([true, false])('should be enabled according to the permissions', (enabled) => {
+        ;(window as any).Vue.$permissionManager = {
+          hasUserManagement: () => enabled
+        }
+        expect(index.navItems.find((n) => n.name === 'Users').enabled()).toBe(enabled)
+      })
+    })
+    describe('group management', () => {
+      it.each([true, false])('should be enabled according to the permissions', (enabled) => {
+        ;(window as any).Vue.$permissionManager = {
+          hasUserManagement: () => enabled
+        }
+        expect(index.navItems.find((n) => n.name === 'Groups').enabled()).toBe(enabled)
+      })
+    })
+    describe('space management', () => {
+      it.each([true, false])('should be enabled according to the permissions', (enabled) => {
+        ;(window as any).Vue.$permissionManager = {
+          hasSpaceManagement: () => enabled
+        }
+        expect(index.navItems.find((n) => n.name === 'Spaces').enabled()).toBe(enabled)
+      })
+    })
+  })
+  describe('routes', () => {
+    describe('default-route "/"', () => {
+      it('should redirect to user management if permission given', () => {
+        ;(window as any).Vue.$permissionManager = {
+          hasUserManagement: () => true
+        }
+        expect(index.routes.find((n) => n.path === '/').redirect().name).toEqual(
+          'admin-settings-users'
+        )
+      })
+      it('should redirect to space management if no user management permission given', () => {
+        ;(window as any).Vue.$permissionManager = {
+          hasUserManagement: () => false
+        }
+        expect(index.routes.find((n) => n.path === '/').redirect().name).toEqual(
+          'admin-settings-spaces'
+        )
+      })
+    })
+  })
+})
