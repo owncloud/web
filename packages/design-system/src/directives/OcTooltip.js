a99cce64f8ee2b1b698f8265513efd7646c91a8b
Dominik Schmidt <dev@dominik-schmidt.de>
2023-01-09 22:26:08 +0100

    Vue 3: Use new directive hooks

--- packages/design-system/src/directives/OcTooltip.js
+++ packages/design-system/src/directives/OcTooltip.js
@@ -82,7 +82,7 @@ const initOrUpdate = (el, { value = {} }, { elm }) => {
 
 export default {
   name: 'OcTooltip',
-  bind: initOrUpdate,
-  componentUpdated: initOrUpdate,
-  unbind: (el, binding, { elm }) => destroy(elm.tooltip)
+  beforeMount: initOrUpdate,
+  updated: initOrUpdate,
+  unmounted: (el, binding, { elm }) => destroy(elm.tooltip)
 }
