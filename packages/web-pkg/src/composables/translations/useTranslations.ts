9221d6656878493f235f9c61ad64b14edbe741a5
Jannik Stehle <jannik.stehle@gmail.com>
2023-01-12 12:04:22 +0100

    Add types for the useTranslations composable

--- packages/web-pkg/src/composables/translations/useTranslations.ts
+++ packages/web-pkg/src/composables/translations/useTranslations.ts
@@ -1,6 +1,11 @@
 import { useService } from '../service'
 
-export const useTranslations = () => {
+export const useTranslations = (): {
+  $gettext: (msgid: string) => string
+  $pgettext: (context: string, msgid: string) => string
+  $ngettext: (msgid: string, plural: string, n: number) => string
+  $gettextInterpolate: (msgid: string, context: object, disableHtmlEscaping?: boolean) => string
+} => {
   return {
     $gettext: useService('$gettext'),
     $pgettext: useService('$pgettext'),
