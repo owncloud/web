b9a308bc142135582a0fd111a5950ad5e6b047ad
Prarup Gurung <grgprarup@gmail.com>
2023-01-13 10:29:14 +0545

    Implemented api steps for accepting share using API

--- tests/e2e/support/api/share/share.ts
+++ tests/e2e/support/api/share/share.ts
@@ -1,6 +1,8 @@
 import { checkResponseStatus, request } from '../http'
 import { User } from '../../types'
 import join from 'join-path'
+import convert from 'xml-js'
+import _ from 'lodash/object'
 
 export const shareTypes: Readonly<{
   user: string
@@ -40,4 +42,49 @@ export const createShare = async ({
     user: user
   })
   checkResponseStatus(response, 'Failed while creating share')
+}
+
+export const acceptShare = async ({ user, path }: { user: User; path: string }): Promise<void> => {
+  const shareId = await getShareIdFromShareWithMePage({ user, path })
+  console.log(shareId)
+  const response = await request({
+    method: 'POST',
+    path: join('ocs', 'v2.php', 'apps', 'files_sharing', 'api', 'v1', 'shares', 'pending', shareId),
+    user: user
+  })
+  checkResponseStatus(response, 'Failed while accepting share')
+}
+
+export const getShareIdFromShareWithMePage = async ({
+  user,
+  path
+}: {
+  user: User
+  path: string
+}): Promise<string> => {
+  const response = await request({
+    method: 'GET',
+    path: join(
+      'ocs',
+      'v2.php',
+      'apps',
+      'files_sharing',
+      'api',
+      'v1',
+      'shares',
+      '?include_tags=false&state=all&shared_with_me=true'
+    ),
+    user: user,
+    formatJson: false
+  })
+  checkResponseStatus(response, 'Failed while geting share id')
+  const fileData = JSON.parse(convert.xml2json(await response.text(), { compact: true }))
+  let elements = _.get(fileData, 'ocs.data.element')
+  elements = elements instanceof Array ? elements : [elements]
+  for (const element of elements) {
+    const elementPath = element.path._text.replace(/^\/|\/$/g, '')
+    if (elementPath === path) {
+      return element.id._text
+    }
+  }
 }
