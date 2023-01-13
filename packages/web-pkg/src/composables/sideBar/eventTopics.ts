b8cfdb1b715f99442bce2fcf1ebc269333f24279
Jannik Stehle <jannik.stehle@gmail.com>
2023-01-13 11:11:18 +0100

    Move useSidebar composable to web-pkg

---
+++ packages/web-pkg/src/composables/sideBar/eventTopics.ts
@@ -0,0 +1,7 @@
+export abstract class SideBarEventTopics {
+  public static readonly open = 'sidebar.open'
+  public static readonly close = 'sidebar.close'
+  public static readonly toggle = 'sidebar.toggle'
+  public static readonly openWithPanel = 'sidebar.openWithPanel'
+  public static readonly setActivePanel = 'sidebar.setActivePanel'
+}
