d7b6fb10f57197e9523fe6299c7721cdba6f687a
Paul Neubauer <paulneubauer@live.de>
2023-01-18 13:12:21 +0100

    [full-ci] Squash spacelist on sidebar open (#8262)
    * Squash spacelist on sidebar open
    
    * Add to changelog
    
    * Remove availableQuota
    
    * Hide more cells on squashed
    
    * Add squashed media query

--- packages/web-app-admin-settings/src/components/Spaces/SpacesList.vue
+++ packages/web-app-admin-settings/src/components/Spaces/SpacesList.vue
@@ -469,14 +469,31 @@ export default defineComponent({
   }
   .oc-table-header-cell-totalQuota,
   .oc-table-data-cell-totalQuota,
-  .oc-table-header-cell-availableQuota,
-  .oc-table-data-cell-availableQuota,
   .oc-table-header-cell-usedQuota,
   .oc-table-data-cell-usedQuota {
     display: none;
 
-    @media only screen and (min-width: 1700px) {
+    @media only screen and (min-width: 1600px) {
       display: table-cell;
+    }
+  }
+  &-squashed {
+    .oc-table-header-cell-manager,
+    .oc-table-data-cell-manager,
+    .oc-table-header-cell-totalQuota,
+    .oc-table-data-cell-totalQuota,
+    .oc-table-header-cell-usedQuota,
+    .oc-table-data-cell-usedQuota {
+      display: none;
+    }
+    .oc-table-header-cell-remainingQuota,
+    .oc-table-data-cell-remainingQuota,
+    .oc-table-header-cell-mdate,
+    .oc-table-data-cell-mdate {
+      display: none;
+      @media only screen and (min-width: 1400px) {
+        display: table-cell;
+      }
     }
   }
 }
