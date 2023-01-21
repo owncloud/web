be1cd98a4d78d11eb04d3bd648f79254f1596ba4
Dominik Schmidt <dev@dominik-schmidt.de>
2023-01-21 12:40:26 +0100

    Fix breadcrumbs unit test

--- packages/web-app-files/tests/unit/helpers/breadcrumbs.spec.ts
+++ packages/web-app-files/tests/unit/helpers/breadcrumbs.spec.ts
@@ -1,8 +1,12 @@
+import { RouteLocation } from 'web-test-helpers/src'
 import { breadcrumbsFromPath, concatBreadcrumbs } from '../../../src/helpers/breadcrumbs'
 
 describe('builds an array of breadcrumbitems', () => {
   it('from a path', () => {
-    const breadCrumbs = breadcrumbsFromPath({ path: '/files/spaces/personal/home/test' }, '/test')
+    const breadCrumbs = breadcrumbsFromPath(
+      { path: '/files/spaces/personal/home/test' } as RouteLocation,
+      '/test'
+    )
     expect(breadCrumbs).toEqual([
       {
         allowContextActions: true,
@@ -14,15 +18,18 @@ describe('builds an array of breadcrumbitems', () => {
 
   it('from an array of breadcrumbitems', () => {
     const initialBreadCrumbs = [{ text: 'Foo' }, { text: 'Bar' }]
-    const breadCrumbsFromPath = breadcrumbsFromPath({ path: '/app/foo/bar?all=500' }, '/bar')
-    const result = concatBreadcrumbs(...initialBreadCrumbs, breadCrumbsFromPath)
+    const breadCrumbsFromPath = breadcrumbsFromPath(
+      { path: '/app/foo/bar?all=500' } as RouteLocation,
+      '/bar'
+    )
+    const result = concatBreadcrumbs(...initialBreadCrumbs, ...breadCrumbsFromPath)
     expect(result[0]).toMatchObject({ text: 'Foo' })
     expect(result[1]).toMatchObject({ text: 'Bar' })
     expect(JSON.stringify(result[2])).toEqual(
       JSON.stringify({
-        allowContextActions: undefined,
-        onClick: () => jest.fn(() => {}),
-        text: undefined
+        allowContextActions: true,
+        text: 'bar',
+        onClick: () => undefined
       })
     )
   })
