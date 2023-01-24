0ecc32b8c4465c8905e3fa0d4024505cb9db4492
Jannik Stehle <50302941+JammingBen@users.noreply.github.com>
2023-01-24 12:02:50 +0100

    [full-ci] Move user group select to edit panel (#8279)
    * Move user group select to edit panel
    
    * Fix group listing and user saving

---
+++ packages/web-app-admin-settings/src/components/Users/SideBar/GroupSelect.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="user-group-select-form">
+    <oc-select
+      :model-value="selectedOption"
+      class="oc-mb-s"
+      multiple
+      :options="availableGroups"
+      option-label="displayName"
+      :label="$gettext('Groups')"
+      :fix-message-line="true"
+      @update:modelValue="onUpdate"
+    >
+      <template #selected-option="{ displayName, id }">
+        <span class="oc-flex oc-flex-center">
+          <avatar-image
+            class="oc-flex oc-align-self-center oc-mr-s"
+            :width="16.8"
+            :userid="id"
+            :user-name="displayName"
+          />
+          <span>{{ displayName }}</span>
+        </span>
+      </template>
+      <template #option="{ displayName, id }">
+        <div class="oc-flex">
+          <span class="oc-flex oc-flex-center">
+            <avatar-image
+              class="oc-flex oc-align-self-center oc-mr-s"
+              :width="16.8"
+              :userid="id"
+              :user-name="displayName"
+            />
+            <span>{{ displayName }}</span>
+          </span>
+        </div>
+      </template>
+    </oc-select>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType, ref, unref, watch } from 'vue'
+import { computed } from 'vue'
+import { Group } from 'web-client/src/generated'
+
+export default defineComponent({
+  name: 'GroupSelect',
+  props: {
+    selectedGroups: {
+      type: Array as PropType<Group[]>,
+      required: true
+    },
+    availableGroups: {
+      type: Array as PropType<Group[]>,
+      required: true
+    }
+  },
+  setup(props, { emit }) {
+    const selectedOption = ref(props.selectedGroups)
+    const onUpdate = (event) => {
+      selectedOption.value = event
+      emit('selectedOptionChange', unref(selectedOption))
+    }
+
+    const currentGroups = computed(() => props.selectedGroups)
+    watch(currentGroups, () => {
+      selectedOption.value = props.selectedGroups
+    })
+
+    return { selectedOption, onUpdate }
+  }
+})
+</script>
---
+++ packages/web-app-admin-settings/tests/unit/components/Users/SideBar/GroupSelect.spec.ts
@@ -0,0 +1,34 @@
+import GroupSelect from '../../../../../src/components/Users/SideBar/GroupSelect.vue'
+import { defaultPlugins, shallowMount } from 'web-test-helpers'
+import { mock } from 'jest-mock-extended'
+import { Group } from 'web-client/src/generated'
+
+const groupMock = mock<Group>({ id: '1' })
+
+describe('GroupSelect', () => {
+  it('renders the select input', () => {
+    const { wrapper } = getWrapper()
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+  it('emits "selectedOptionChange" on update', () => {
+    const group = mock<Group>({ id: '2' })
+    const { wrapper } = getWrapper()
+    wrapper.vm.onUpdate(group)
+    expect(wrapper.emitted().selectedOptionChange).toBeTruthy()
+    expect(wrapper.vm.selectedOption).toEqual(group)
+  })
+})
+
+function getWrapper() {
+  return {
+    wrapper: shallowMount(GroupSelect, {
+      props: {
+        selectedGroups: [groupMock],
+        availableGroups: [groupMock]
+      },
+      global: {
+        plugins: [...defaultPlugins()]
+      }
+    })
+  }
+}
