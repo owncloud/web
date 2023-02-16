0f2a28fc2d52eb44690738b9e1be67151955378e
Jannik Stehle <jannik.stehle@gmail.com>
2023-02-16 09:35:51 +0100

    Refactor getAbilities method to improve readability

--- packages/web-runtime/src/services/auth/abilities.ts
+++ packages/web-runtime/src/services/auth/abilities.ts
@@ -4,67 +4,44 @@ import { AbilityActions, AbilitySubjects } from 'web-pkg'
 export const getAbilities = (
   permissions: string[]
 ): SubjectRawRule<AbilityActions, AbilitySubjects, any>[] => {
-  const abilities: SubjectRawRule<AbilityActions, AbilitySubjects, any>[] = []
-
-  // account
-  if (permissions.includes('account-management')) {
-    abilities.push(
+  const abilities = {
+    'account-management': [
       { action: 'create', subject: 'Account' },
       { action: 'delete', subject: 'Account' },
       { action: 'read', subject: 'Account' },
       { action: 'update', subject: 'Account' }
-    )
-  }
-
-  // groups
-  if (permissions.includes('group-management')) {
-    abilities.push(
+    ],
+    'group-management': [
       { action: 'create', subject: 'Group' },
       { action: 'delete', subject: 'Group' },
       { action: 'read', subject: 'Group' },
       { action: 'update', subject: 'Group' }
-    )
-  }
-
-  // language
-  if (permissions.includes('language-readwrite')) {
-    abilities.push({ action: 'read', subject: 'Language' })
-    abilities.push({ action: 'update', subject: 'Language' })
-  }
-
-  // logo
-  if (permissions.includes('change-logo')) {
-    abilities.push({ action: 'update', subject: 'Logo' })
-  }
-
-  // roles
-  if (permissions.includes('role-management')) {
-    abilities.push(
+    ],
+    'language-readwrite': [
+      { action: 'read', subject: 'Language' },
+      { action: 'update', subject: 'Language' }
+    ],
+    'change-logo': [{ action: 'update', subject: 'Logo' }],
+    'role-management': [
       { action: 'create', subject: 'Role' },
       { action: 'delete', subject: 'Role' },
       { action: 'read', subject: 'Role' },
       { action: 'update', subject: 'Role' }
-    )
+    ],
+    'settings-management': [
+      { action: 'read', subject: 'Setting' },
+      { action: 'update', subject: 'Setting' }
+    ],
+    'create-space': [{ action: 'create', subject: 'Space' }],
+    'delete-all-spaces': [{ action: 'delete', subject: 'Space' }],
+    'list-all-spaces': [{ action: 'read', subject: 'Space' }],
+    'set-space-quota': [{ action: 'set-quota', subject: 'Space' }]
   }
 
-  // settings
-  if (permissions.includes('settings-management')) {
-    abilities.push({ action: 'read', subject: 'Setting' }, { action: 'update', subject: 'Setting' })
-  }
-
-  // spaces
-  if (permissions.includes('create-space')) {
-    abilities.push({ action: 'create', subject: 'Space' })
-  }
-  if (permissions.includes('delete-all-spaces')) {
-    abilities.push({ action: 'delete', subject: 'Space' })
-  }
-  if (permissions.includes('list-all-spaces')) {
-    abilities.push({ action: 'read', subject: 'Space' })
-  }
-  if (permissions.includes('set-space-quota')) {
-    abilities.push({ action: 'set-quota', subject: 'Space' })
-  }
-
-  return abilities
+  return Object.keys(abilities).reduce((acc, permission) => {
+    if (permissions.includes(permission)) {
+      acc.push(...abilities[permission])
+    }
+    return acc
+  }, [])
 }
