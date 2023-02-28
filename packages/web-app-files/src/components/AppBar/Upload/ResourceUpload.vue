87d7dc42ae2aa27bb1551e869112599da802aa6d
hurradieweltgehtunter <hi@florianlenz.com>
2023-02-28 15:32:19 +0100

    Make component more customizable

--- packages/web-app-files/src/components/AppBar/Upload/ResourceUpload.vue
+++ packages/web-app-files/src/components/AppBar/Upload/ResourceUpload.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
-    <oc-button :class="btnClass" justify-content="left" appearance="raw" @click="triggerUpload">
-      <oc-resource-icon :resource="{ extension: '', isFolder }" size="medium" />
-      <span :id="uploadLabelId">{{ buttonLabel }}</span>
-    </oc-button>
+    <div class="oc-text-xlarge">
+      <h1 class="oc-text-normal">
+        <oc-button :class="btnClass" justify-content="left" appearance="raw" @click="triggerUpload" class="oc-display-inline-block oc-text-bold oc-position-relative">
+          <span :id="uploadLabelId">{{ buttonLabel }}</span>
+        </oc-button>
+        <span v-text="$gettext('or drag it here')" /></h1>
+    </div>
     <input
       :id="inputId"
       ref="input"
@@ -84,9 +87,38 @@ export default defineComponent({
 })
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .upload-input-target {
   position: absolute;
   left: -99999px;
 }
+
+button {
+  color: var(--oc-color-swatch-primary-default);
+  font-weight: 600;
+
+  &::after {
+    content: '';
+    position: absolute;
+    width: 100%;
+    transform: scaleX(0);
+    height: 2px;
+    bottom: -2px;
+    left: 0;
+    background-color: #0087ca;
+    transform-origin: bottom right;
+    transition: transform 0.25s ease-out;
+  }
+
+  &:hover {
+    color: var(--oc-color-swatch-primary-default);
+    text-decoration: none;
+
+    &::after {
+      transform: scaleX(1);
+      transform-origin: bottom left;
+    }
+  }
+}
+
 </style>
