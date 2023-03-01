e9ab07b75f6fdd41c1316095a963d707c5793315
Jan <j.ackermann91@gmail.com>
2023-03-01 10:42:26 +0100

    Init trash overview

--- packages/web-app-files/src/router/trash.ts
+++ packages/web-app-files/src/router/trash.ts
@@ -1,15 +1,20 @@
 import { RouteComponents } from './router'
 import { RouteLocationNamedRaw, RouteRecordRaw } from 'vue-router'
-import { createLocation, isLocationActiveDirector } from './utils'
+import { $gettext, createLocation, isLocationActiveDirector } from './utils'
 
-type trashTypes = 'files-trash-generic'
+type trashTypes = 'files-trash-generic' | 'files-trash-overview'
 
 export const createLocationTrash = (name: trashTypes, location = {}): RouteLocationNamedRaw =>
   createLocation(name, location)
 
 export const locationTrashGeneric = createLocationTrash('files-trash-generic')
 
-export const isLocationTrashActive = isLocationActiveDirector<trashTypes>(locationTrashGeneric)
+export const locationTrashOverview = createLocationTrash('files-trash-overview')
+
+export const isLocationTrashActive = isLocationActiveDirector<trashTypes>(
+  locationTrashGeneric,
+  locationTrashOverview
+)
 
 export const buildRoutes = (components: RouteComponents): RouteRecordRaw[] => [
   {
@@ -17,8 +22,17 @@ export const buildRoutes = (components: RouteComponents): RouteRecordRaw[] => [
     component: components.App,
     children: [
       {
+        path: '',
+        name: locationTrashOverview.name,
+        component: components.Trash.Overview,
+        meta: {
+          authContext: 'user',
+          title: $gettext('Trash overview')
+        }
+      },
+      {
         name: locationTrashGeneric.name,
-        path: ':driveAliasAndItem(.*)?',
+        path: 'trash/:driveAliasAndItem(.*)?',
         component: components.Spaces.DriveResolver,
         meta: {
           authContext: 'user',
