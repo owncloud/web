84d9406bb62e22ae48aaecbd8a7c99ae1d27b262
Paul Neubauer <paulneubauer@live.de>
2023-03-03 14:29:24 +0100

    Linting

--- tests/e2e/support/objects/app-admin-settings/general/actions.ts
+++ tests/e2e/support/objects/app-admin-settings/general/actions.ts
@@ -1,4 +1,4 @@
-import { basename } from "path"
+import { basename } from 'path'
 import { expect } from '@playwright/test'
 
 export const uploadLogo = async (path, page): Promise<void> => {
@@ -8,24 +8,23 @@ export const uploadLogo = async (path, page): Promise<void> => {
   await logoInput.setInputFiles(path)
 
   await page.waitForSelector('.oc-notification-message')
-	await page.reload()
-	const logoImg = await page.locator('.logo-wrapper img');
-	console.log(logoImg)
-	const logoSrc = await logoImg.getAttribute('src');
-	expect(logoSrc).toContain(basename(path))
+  await page.reload()
+  const logoImg = await page.locator('.logo-wrapper img')
+  const logoSrc = await logoImg.getAttribute('src')
+  expect(logoSrc).toContain(basename(path))
 }
 
 export const resetLogo = async (page): Promise<void> => {
-	const imgBefore = await page.locator('.logo-wrapper img');
-	const srcBefore = await imgBefore.getAttribute('src');
+  const imgBefore = await page.locator('.logo-wrapper img')
+  const srcBefore = await imgBefore.getAttribute('src')
   await page.click('#logo-context-btn')
 
   await page.click('.oc-general-actions-reset-logo-trigger')
 
   await page.waitForSelector('.oc-notification-message')
-	await page.reload()
+  await page.reload()
 
-	const imgAfter = await page.locator('.logo-wrapper img');
-	const srcAfter = await imgAfter.getAttribute('src');
-	expect(srcAfter).not.toEqual(srcBefore)
+  const imgAfter = await page.locator('.logo-wrapper img')
+  const srcAfter = await imgAfter.getAttribute('src')
+  expect(srcAfter).not.toEqual(srcBefore)
 }
