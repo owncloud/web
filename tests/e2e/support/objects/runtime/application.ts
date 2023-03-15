bb7d6900a5f0d8e2f7b45d9936d452b4097ef5ac
Viktor Scharf <scharf.vi@gmail.com>
2023-03-15 09:22:15 +0100

    add wait response (#8610)

--- tests/e2e/support/objects/runtime/application.ts
+++ tests/e2e/support/objects/runtime/application.ts
@@ -26,6 +26,12 @@ export class Application {
   }
 
   async getNotificationMessages(): Promise<string[]> {
+    await this.#page.waitForResponse(
+      (resp) =>
+        resp.url().endsWith('notifications?format=json') &&
+        resp.status() === 200 &&
+        resp.request().method() === 'GET'
+    )
     const dropIsOpen = await this.#page.locator(notificationsDrop).isVisible()
     if (!dropIsOpen) {
       await this.#page.locator(notificationsBell).click()
