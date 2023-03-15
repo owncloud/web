8248ae4444b26a1089f821379dbeff05df60caae
hurradieweltgehtunter <hi@florianlenz.com>
2023-03-15 10:39:32 +0100

    Move OcAlert to own PR, remove here

--- packages/design-system/src/components/index.ts
+++ packages/design-system/src/components/index.ts
@@ -1,4 +1,3 @@
-export { default as OcAlert } from './OcAlert/OcAlert.vue'
 export { default as OcAvatar } from './OcAvatar/OcAvatar.vue'
 export { default as OcAvatarCount } from './OcAvatarCount/OcAvatarCount.vue'
 export { default as OcAvatarFederated } from './OcAvatarFederated/OcAvatarFederated.vue'
