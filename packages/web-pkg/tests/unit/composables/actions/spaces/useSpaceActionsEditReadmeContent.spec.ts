17753dbfb648434cd2a1f6a0237c4953d10ea392
Dominik Schmidt <dev@dominik-schmidt.de>
2023-03-21 14:35:31 +0100

    Fix FIXMEs

--- packages/web-pkg/tests/unit/composables/actions/spaces/useSpaceActionsEditReadmeContent.spec.ts
+++ packages/web-pkg/tests/unit/composables/actions/spaces/useSpaceActionsEditReadmeContent.spec.ts
@@ -14,12 +14,11 @@ describe('editReadmeContent', () => {
 
       const { wrapper } = getWrapper({
         setup: ({ actions }) => {
-          // FIXME
-          // expect(
-          //   unref(actions)[0].isEnabled({
-          //     resources: [buildSpace(spaceMock)]
-          //   })
-          // ).toBe(true)
+          expect(
+            unref(actions)[0].isEnabled({
+              resources: [buildSpace(spaceMock)]
+            })
+          ).toBe(true)
         }
       })
     })
@@ -78,9 +77,10 @@ function getWrapper({
   ) => void
 }) {
   const storeOptions = {
-    ...defaultStoreMockOptions,
-    modules: { ...defaultStoreMockOptions.modules, user: { state: { id: 'alice', uuid: 1 } } }
+    ...defaultStoreMockOptions
   }
+  storeOptions.getters.user.mockReturnValue({ id: 'alice', uuid: 1 })
+
   const store = createStore(storeOptions)
   return {
     wrapper: getComposableWrapper(
