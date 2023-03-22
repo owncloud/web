ec4a3f6fa94c94f7771e574376003eeec5b27779
Dominik Schmidt <dev@dominik-schmidt.de>
2023-03-22 23:41:03 +0100

    Fix logo upload in vite

--- vite.config.ts
+++ vite.config.ts
@@ -74,13 +74,15 @@ export default defineConfig(({ mode, command }) => {
           },
           // workaround: https://github.com/owncloud/ocis/issues/5108
           proxy: Object.fromEntries(
-            ['api', 'ocs', 'graph', 'remote.php', 'app', 'archiver', 'settings.js'].map((p) => [
-              `/${p}`,
-              {
-                target: 'https://host.docker.internal:9200',
-                secure: false
-              }
-            ])
+            ['api', 'ocs', 'graph', 'remote.php', 'app', 'archiver', 'branding', 'settings.js'].map(
+              (p) => [
+                `/${p}`,
+                {
+                  target: 'https://host.docker.internal:9200',
+                  secure: false
+                }
+              ]
+            )
           )
         }
       })
