a11abb857dd6c132b151f92876df55dc8ef9ac0b
Prarup Gurung <grgprarup@gmail.com>
2023-03-24 14:39:09 +0545

    Hot-fix for flaky e2e test, oc10

--- tests/e2e/support/objects/app-files/utils/sidebar.ts
+++ tests/e2e/support/objects/app-files/utils/sidebar.ts
@@ -53,7 +53,7 @@ export const open = async ({
 }
 
 export const close = async ({ page }: { page: Page }): Promise<void> => {
-  await page.locator('.sidebar-panel .header__close:visible').click()
+  await page.locator('.sidebar-panel .header__close:visible').first().click()
 }
 
 export const openPanel = async ({ page, name }: { page: Page; name: string }): Promise<void> => {
