8aded94c4a9ef5a01f2ac66884f73d9970bdca75
hurradieweltgehtunter <hi@florianlenz.com>
2023-03-28 12:27:15 +0200

    lint

--- packages/web-pkg/src/components/ItemFilter.vue
+++ packages/web-pkg/src/components/ItemFilter.vue
@@ -13,7 +13,9 @@
           v-model="filterTerm"
           class="item-filter-input oc-mb-m oc-mt-s"
           autocomplete="off"
-          :placeholder="optionFilterPlaceholder === '' ? $gettext('Enter term') : optionFilterPlaceholder"
+          :placeholder="
+            optionFilterPlaceholder === '' ? $gettext('Enter term') : optionFilterPlaceholder
+          "
           :label="optionFilterLabel === '' ? $gettext('Filter list') : optionFilterLabel"
         />
         <div ref="itemFilterListRef">
