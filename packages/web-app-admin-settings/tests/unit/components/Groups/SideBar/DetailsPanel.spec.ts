95124b7f57b2772aa2de8fcc63553554ee12c130
Jan <j.ackermann91@gmail.com>
2023-03-30 16:46:16 +0200

    Fix unit tests

--- packages/web-app-admin-settings/tests/unit/components/Groups/SideBar/DetailsPanel.spec.ts
+++ packages/web-app-admin-settings/tests/unit/components/Groups/SideBar/DetailsPanel.spec.ts
@@ -4,8 +4,10 @@ import { defaultPlugins, mount } from 'web-test-helpers'
 describe('DetailsPanel', () => {
   describe('computed method "group"', () => {
     it('should be set if only one group is given', () => {
-      const { wrapper } = getWrapper({ propsData: { groups: [{ displayName: 'group' }] } })
-      expect(wrapper.vm.group).toEqual({ displayName: 'group' })
+      const { wrapper } = getWrapper({
+        propsData: { groups: [{ displayName: 'group', members: [] }] }
+      })
+      expect(wrapper.vm.group).toEqual({ displayName: 'group', members: [] })
     })
     it('should not be set if no groups are given', () => {
       const { wrapper } = getWrapper({
@@ -15,7 +17,12 @@ describe('DetailsPanel', () => {
     })
     it('should not be set if multiple groups are given', () => {
       const { wrapper } = getWrapper({
-        propsData: { groups: [{ displayName: 'group1' }, { displayName: 'group2' }] }
+        propsData: {
+          groups: [
+            { displayName: 'group1', members: [] },
+            { displayName: 'group2', members: [] }
+          ]
+        }
       })
       expect(wrapper.vm.group).toEqual(null)
     })
@@ -29,7 +36,9 @@ describe('DetailsPanel', () => {
       expect(wrapper.vm.noGroups).toBeTruthy()
     })
     it('should be false if groups are given', () => {
-      const { wrapper } = getWrapper({ propsData: { groups: [{ displayName: 'group' }] } })
+      const { wrapper } = getWrapper({
+        propsData: { groups: [{ displayName: 'group', members: [] }] }
+      })
       expect(wrapper.vm.noGroups).toBeFalsy()
     })
   })
@@ -40,12 +49,14 @@ describe('DetailsPanel', () => {
       expect(wrapper.vm.multipleGroups).toBeFalsy()
     })
     it('should be false if one group is given', () => {
-      const { wrapper } = getWrapper({ propsData: { groups: [{ displayName: 'group' }] } })
+      const { wrapper } = getWrapper({
+        propsData: { groups: [{ displayName: 'group', members: [] }] }
+      })
       expect(wrapper.vm.multipleGroups).toBeFalsy()
     })
     it('should be true if multiple groups are given', () => {
       const { wrapper } = getWrapper({
-        propsData: { groups: [{ displayName: 'group1' }, { displayName: 'group2' }] }
+        propsData: { groups: [{ displayName: 'group1' }, { displayName: 'group2', members: [] }] }
       })
       expect(wrapper.vm.multipleGroups).toBeTruthy()
     })
