249304417f51f5a3197f354f49b4b41a95ec1b68
Jan <j.ackermann91@gmail.com>
2023-04-11 15:41:34 +0200

    don't throw

--- packages/web-runtime/src/index.ts
+++ packages/web-runtime/src/index.ts
@@ -65,19 +65,20 @@ export const bootstrapApp = async (configurationPath: string): Promise<void> => 
   const customTranslations = {}
 
   for (const customTranslation of configurationManager.customTranslations) {
-    const customTranslationResponse = await fetch(customTranslation.url, {
-      headers: { 'X-Request-ID': uuidV4() }
-    })
-    if (customTranslationResponse.status !== 200) {
-      throw new Error(
-        `translation file ${customTranslation} could not be loaded. HTTP status-code ${customTranslationResponse.status}`
-      )
-    }
     try {
+      const customTranslationResponse = await fetch(customTranslation.url, {
+        headers: { 'X-Request-ID': uuidV4() }
+      })
+      if (customTranslationResponse.status !== 200) {
+        console.error(
+          `translation file ${customTranslation} could not be loaded. HTTP status-code ${customTranslationResponse.status}`
+        )
+        break
+      }
       const customTranslationJSON = await customTranslationResponse.json()
       merge(customTranslations, customTranslationJSON)
     } catch (e) {
-      throw new Error(`translation file ${customTranslation} could not be parsed. ${e}`)
+      console.error(`translation file ${customTranslation} could not be parsed. ${e}`)
     }
   }
 
