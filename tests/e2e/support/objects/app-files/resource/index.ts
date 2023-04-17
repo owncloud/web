3d59e211c721e86ac41a56fd9cff58ca929b6fc0
Viktor Scharf <scharf.vi@gmail.com>
2023-04-17 16:08:31 +0200

    fix flaky while uploading

--- tests/e2e/support/objects/app-files/resource/index.ts
+++ tests/e2e/support/objects/app-files/resource/index.ts
@@ -70,8 +70,6 @@ export class Resource {
     const startUrl = this.#page.url()
     await uploadResource({ ...args, page: this.#page })
     await this.#page.goto(startUrl)
-    // why? o_O
-    await this.#page.locator('body').click()
   }
 
   async download(args: Omit<downloadResourcesArgs, 'page'>): Promise<Download[]> {
