f50668f50bcde6170b21c6020a5966d692bb1bc9
Jannik Stehle <jannik.stehle@gmail.com>
2023-04-17 13:33:47 +0200

    Add unit tests for the vertical context menu button placement

--- packages/web-pkg/tests/unit/helpers/contextMenuDropdown.spec.ts
+++ packages/web-pkg/tests/unit/helpers/contextMenuDropdown.spec.ts
@@ -4,7 +4,7 @@ describe('displayPositionedDropdown', () => {
   it('shows the dropdown', () => {
     const dropdown = { setProps: jest.fn(), show: jest.fn() }
     const ctxMenuBtn = { $el: { getBoundingClientRect: jest.fn() } }
-    displayPositionedDropdown(dropdown, undefined, ctxMenuBtn)
+    displayPositionedDropdown(dropdown, {}, ctxMenuBtn)
     expect(dropdown.show).toHaveBeenCalled()
   })
   it('horizontally positions the drop at the cursor for "contextmenu"-event', () => {
@@ -37,5 +37,36 @@ describe('displayPositionedDropdown', () => {
     const data = dropdownProps.getReferenceClientRect()
     expect(data.left).toEqual(ctxMenuBtnX)
     expect(data.right).toEqual(ctxMenuBtnX)
+  })
+  it('vertically positions the drop via "clientY" if given', () => {
+    const event = { clientY: 100 }
+    let dropdownProps
+    const dropdown = {
+      setProps: jest.fn((props) => {
+        dropdownProps = props
+      }),
+      show: jest.fn()
+    }
+    const ctxMenuBtn = { $el: { getBoundingClientRect: jest.fn(() => ({ x: 200 })) } }
+    displayPositionedDropdown(dropdown, event, ctxMenuBtn)
+    const { top, bottom } = dropdownProps.getReferenceClientRect()
+    expect(top).toEqual(event.clientY)
+    expect(bottom).toEqual(event.clientY)
+  })
+  it('vertically positions the drop via the context button position if "clientY" is 0', () => {
+    const yPos = 200
+    const event = { clientY: 0, srcElement: { getBoundingClientRect: () => ({ top: yPos }) } }
+    let dropdownProps
+    const dropdown = {
+      setProps: jest.fn((props) => {
+        dropdownProps = props
+      }),
+      show: jest.fn()
+    }
+    const ctxMenuBtn = { $el: { getBoundingClientRect: jest.fn(() => ({ x: 200 })) } }
+    displayPositionedDropdown(dropdown, event, ctxMenuBtn)
+    const { top, bottom } = dropdownProps.getReferenceClientRect()
+    expect(top).toEqual(yPos)
+    expect(bottom).toEqual(yPos)
   })
 })
