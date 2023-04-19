d666587d148a67653c5d8f9123c3f6294e0efd9e
Jan <j.ackermann91@gmail.com>
2023-04-19 10:18:04 +0200

    Add examples

--- packages/design-system/src/components/OcTextInput/OcTextInput.vue
+++ packages/design-system/src/components/OcTextInput/OcTextInput.vue
@@ -201,7 +201,15 @@ export default defineComponent({
       default: null
     },
     /**
-     * Determines if the select field is read only
+     * Determines if the input field is read only.
+     *
+     * Read only field will be visualized by a lock item and additionally behaves like a disabled field.
+     * Read only takes effect if the server won't allow to change the value at all,
+     * disabled should be used if the value can't be changed in a specific context.
+     *
+     * For example: If the backend doesn't allow to set the login states for users in general, use read only.
+     * If it's not allowed to change for the current logged-in User, use disabled.
+     *
      */
     readOnly: {
       type: Boolean,
