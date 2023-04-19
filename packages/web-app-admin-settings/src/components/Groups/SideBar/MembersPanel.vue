c966194452427b2781e781c9f5f890dc9878494c
Paul Neubauer <paulneubauer@live.de>
2023-04-19 12:11:37 +0200

    [full-ci] Admin settings add group members panel (#8826)
    * Fix quicklink icon alignment
    
    * Admin-settings groups add members panel
    
    * Update snapshot, add to changelog
    
    * remove wrong style
    
    * Fix type error
    
    * Fix typescript error
    
    * Fix 'kind' error
    
    * Fix membersrolesection array type
    
    * (Please) fix membersrolesection type errors
    
    * remove wrong changelog, fix snapshots
    
    * Refactor code
    
    * Fix refactoring errors, add unittests
    
    * Address PR issues
    
    * Fix weird filter output
    
    * Update snapshots
    
    * Fix filter span missing, remove RegEx

---
+++ packages/web-app-admin-settings/src/components/Groups/SideBar/MembersPanel.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="oc-ml-s">
+    <oc-text-input
+      v-model="filterTerm"
+      class="oc-text-truncate oc-mr-s oc-mt-m"
+      :label="$gettext('Filter members')"
+    />
+    <div ref="membersListRef" data-testid="space-members">
+      <div v-if="!filteredGroupMembers.length">
+        <h3 class="oc-text-bold oc-text-medium" v-text="$gettext('No members found')" />
+      </div>
+      <div v-if="filteredGroupMembers.length" class="oc-mb-m" data-testid="group-members">
+        <h3 class="oc-text-bold oc-text-medium" v-text="$gettext('Members')" />
+        <members-role-section :group-members="filteredGroupMembers" />
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { computed, defineComponent, inject, ref, watch, unref, Ref } from 'vue'
+import MembersRoleSection from '../../Groups/SideBar/MembersRoleSection.vue'
+import Fuse from 'fuse.js'
+import Mark from 'mark.js'
+import { Group } from 'web-client/src/generated'
+
+export default defineComponent({
+  name: 'GroupsMembersPanel',
+  components: { MembersRoleSection },
+  setup() {
+    const group = inject<Ref<Group>>('group')
+    const filterTerm = ref('')
+    const markInstance = ref(null)
+    const membersListRef = ref(null)
+
+    const filterMembers = (collection, term) => {
+      if (!(term || '').trim()) {
+        return collection
+      }
+
+      const searchEngine = new Fuse(collection, {
+        includeScore: false,
+        useExtendedSearch: true,
+        threshold: 0.1,
+        keys: ['displayName']
+      })
+
+      return searchEngine.search(term).map((r) => r.item)
+    }
+
+    const members = computed(() => {
+      if (group) {
+        return unref(group).members.sort((a, b) => a.displayName.localeCompare(b.displayName))
+      }
+      return []
+    })
+
+    const filteredGroupMembers = computed(() => {
+      return filterMembers(unref(members), unref(filterTerm))
+    })
+
+    watch(filterTerm, () => {
+      if (unref(membersListRef)) {
+        markInstance.value = new Mark(unref(membersListRef))
+        unref(markInstance).unmark()
+        const searchTermRegex = unref(filterTerm)
+        unref(markInstance).mark(searchTermRegex, {
+          element: 'span',
+          className: 'highlight-mark'
+        })
+      }
+    })
+
+    return {
+      filterTerm,
+      membersListRef,
+      members,
+      filteredGroupMembers
+    }
+  }
+})
+</script>
---
+++ packages/web-app-admin-settings/src/components/Groups/SideBar/MembersRoleSection.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="oc-list">
+    <li
+      v-for="(member, index) in groupMembers"
+      :key="index"
+      class="oc-flex oc-flex-middle oc-mb-s"
+      data-testid="group-members-list"
+    >
+      <oc-avatar :user-name="member.displayName" :width="36" class="oc-mr-s" />
+      <span>{{ member.displayName }}</span>
+    </li>
+  </ul>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { User } from 'web-client/src/helpers'
+
+export default defineComponent({
+  name: 'MembersRoleSection',
+  props: {
+    groupMembers: {
+      type: Array as PropType<User[]>,
+      required: false
+    }
+  }
+})
+</script>
