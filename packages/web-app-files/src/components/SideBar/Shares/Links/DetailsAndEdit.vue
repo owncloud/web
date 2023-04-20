c889c2c415a3a7aecaa6b69d7dbf01286eaa1ccd
hurradieweltgehtunter <hi@florianlenz.com>
2023-04-20 21:48:48 +0200

    adapt layout of link permission dropdown, too

--- packages/web-app-files/src/components/SideBar/Shares/Links/DetailsAndEdit.vue
+++ packages/web-app-files/src/components/SideBar/Shares/Links/DetailsAndEdit.vue
@@ -4,7 +4,7 @@
       <oc-button
         :id="`edit-public-link-role-dropdown-toggle-${link.id}`"
         appearance="raw"
-        class="edit-public-link-role-dropdown-toggle oc-text-left"
+        class="edit-public-link-role-dropdown-toggle oc-text-left oc-pill oc-background-default oc-px-s"
         gap-size="none"
       >
         <span class="link-current-role" v-text="$gettext(currentLinkRoleLabel)" />
