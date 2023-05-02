c92da28fc8dfdff7efd939b897405801a0ac4f76
Paul Neubauer <paulneubauer@live.de>
2023-05-02 13:59:35 +0200

    Implement working unittests for useFileActionsCreateNewFolder.spec.ts

--- packages/web-app-files/src/composables/actions/files/useFileActionsCreateNewFolder.ts
+++ packages/web-app-files/src/composables/actions/files/useFileActionsCreateNewFolder.ts
@@ -137,6 +137,7 @@ export const useFileActionsCreateNewFolder = ({ store }: { store?: Store<any> } 
 
   return {
     actions,
-    checkNewFolderName
+    checkNewFolderName,
+    addNewFolder
   }
 }
--- packages/web-app-files/tests/unit/composables/actions/files/useFileActionsCreateNewFolder.spec.ts
+++ packages/web-app-files/tests/unit/composables/actions/files/useFileActionsCreateNewFolder.spec.ts
@@ -1,45 +1,134 @@
+import { mock } from 'jest-mock-extended'
+import { nextTick, unref } from 'vue'
 import { useFileActionsCreateNewFolder } from 'web-app-files/src/composables'
+import { SpaceResource } from 'web-client/src'
+import { FolderResource } from 'web-client/src/helpers'
+import {
+  RouteLocation,
+  createStore,
+  defaultComponentMocks,
+  defaultStoreMockOptions,
+  getComposableWrapper
+} from 'web-test-helpers/src'
 
 describe('useFileActionsCreateNewFolder', () => {
-  describe('checkNewFileName', () => {
-    it('returns error message if file name is empty', () => {
-      const { checkNewFolderName } = useFileActionsCreateNewFolder()
-
-      const result = checkNewFolderName('')
-
-      expect(result).toBe('File name cannot be empty')
+  describe('checkFolderName', () => {
+    it.each([
+      { input: '', output: 'Folder name cannot be empty' },
+      { input: '/', output: 'Folder name cannot contain "/"' },
+      { input: '.', output: 'Folder name cannot be equal to "."' },
+      { input: '..', output: 'Folder name cannot be equal to ".."' },
+      { input: 'myfolder', output: null }
+    ])('should validate folder name %s', async (data) => {
+      const space = mock<SpaceResource>({ id: '1' })
+      getWrapper({
+        space,
+        setup: async ({ checkNewFolderName }) => {
+          const result = checkNewFolderName(data.input)
+          expect(result).toBe(data.output)
+        }
+      })
     })
-
-    it('returns error message if file name contains "/"', () => {
-      const { checkNewFolderName } = useFileActionsCreateNewFolder()
-
-      const result = checkNewFolderName('file/name')
-
-      expect(result).toBe('File name cannot contain "/"')
+  })
+  describe('addNewFolder', () => {
+    it('create new folder', async () => {
+      const space = mock<SpaceResource>({ id: '1' })
+      getWrapper({
+        space,
+        setup: async ({ addNewFolder }, { storeOptions }) => {
+          await addNewFolder('myfolder')
+          await nextTick()
+          expect(storeOptions.modules.Files.mutations.UPSERT_RESOURCE).toHaveBeenCalled()
+          expect(storeOptions.actions.hideModal).toHaveBeenCalled()
+          expect(storeOptions.actions.showMessage).toHaveBeenCalledWith(
+            expect.anything(),
+            expect.objectContaining({
+              title: '"myfolder" was created successfully'
+            })
+          )
+        }
+      })
     })
-
-    it('returns error message if file name is equal to "."', () => {
-      const { checkNewFolderName } = useFileActionsCreateNewFolder()
-
-      const result = checkNewFolderName('.')
-
-      expect(result).toBe('File name cannot be equal to "."')
+    it('show error message if createFolder fails', async () => {
+      const consoleErrorMock = jest.spyOn(console, 'error').mockImplementation()
+      const space = mock<SpaceResource>({ id: '1' })
+      getWrapper({
+        resolveCreateFolder: false,
+        space,
+        setup: async ({ addNewFolder }, { storeOptions }) => {
+          await addNewFolder('myfolder')
+          await nextTick()
+          expect(storeOptions.actions.showMessage).toHaveBeenCalledWith(
+            expect.anything(),
+            expect.objectContaining({
+              title: 'Failed to create folder',
+              status: 'danger'
+            })
+          )
+          consoleErrorMock.mockRestore()
+        }
+      })
     })
-
-    it('returns error message if file name is equal to ".."', () => {
-      const { checkNewFolderName } = useFileActionsCreateNewFolder()
-
-      const result = checkNewFolderName('..')
-
-      expect(result).toBe('File name cannot be equal to ".."')
-    })
-
-    it('returns error message if file name ends with whitespace', () => {
-      const { checkNewFolderName } = useFileActionsCreateNewFolder()
-
-      const result = checkNewFolderName('file name ')
-
-      expect(result).toBe('File name cannot end with whitespace')
+  })
+  describe('createNewFolderModal', () => {
+    it('should show modal', async () => {
+      const space = mock<SpaceResource>({ id: '1' })
+      getWrapper({
+        space,
+        setup: async ({ actions }, { storeOptions }) => {
+          unref(actions)[0].handler()
+          await nextTick()
+          expect(storeOptions.actions.createModal).toHaveBeenCalled()
+        }
+      })
     })
   })
 })
+
+function getWrapper({
+  resolveCreateFolder = true,
+  space = undefined,
+  setup
+}: {
+  resolveCreateFolder?: boolean
+  space?: SpaceResource
+  setup: (
+    instance: ReturnType<typeof useFileActionsCreateNewFolder>,
+    options: { storeOptions: typeof defaultStoreMockOptions }
+  ) => void
+}) {
+  const mocks = {
+    ...defaultComponentMocks({
+      currentRoute: mock<RouteLocation>({ name: 'files-spaces-generic' })
+    }),
+    space
+  }
+  mocks.$clientService.webdav.createFolder.mockImplementation(() => {
+    if (resolveCreateFolder) {
+      return Promise.resolve({ id: 1, type: 'folder' } as FolderResource)
+    }
+    return Promise.reject('error')
+  })
+
+  const storeOptions = {
+    ...defaultStoreMockOptions
+  }
+  const currentFolder = {
+    id: 1,
+    path: '/'
+  }
+  storeOptions.modules.Files.getters.currentFolder.mockReturnValue(currentFolder)
+  const store = createStore(storeOptions)
+  return {
+    wrapper: getComposableWrapper(
+      () => {
+        const instance = useFileActionsCreateNewFolder({ store })
+        setup(instance, { storeOptions })
+      },
+      {
+        store,
+        mocks
+      }
+    )
+  }
+}
