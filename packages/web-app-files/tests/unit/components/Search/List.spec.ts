ce1d27055c1c8387daaf93109fbba0c026f08a62
Jannik Stehle <50302941+JammingBen@users.noreply.github.com>
2023-05-23 14:43:23 +0200

    Implement fulltext filter on search result page (#9059)

--- packages/web-app-files/tests/unit/components/Search/List.spec.ts
+++ packages/web-app-files/tests/unit/components/Search/List.spec.ts
@@ -21,7 +21,8 @@ jest.mock('web-pkg/src/composables/appDefaults')
 const selectors = {
   noContentMessageStub: 'no-content-message-stub',
   resourceTableStub: 'resource-table-stub',
-  tagFilter: '.files-search-filter-tags'
+  tagFilter: '.files-search-filter-tags',
+  fullTextFilter: '.files-search-filter-full-text'
 }
 
 describe('List component', () => {
@@ -50,19 +51,41 @@ describe('List component', () => {
         ])
       })
       it('should set initial filter when tags are given via query param', async () => {
+        const searchTerm = 'term'
         const tagFilterQuery = 'tag1'
         const { wrapper } = getWrapper({
           availableTags: ['tag1'],
+          searchTerm,
           tagFilterQuery
         })
         await wrapper.vm.loadAvailableTagsTask.last
-        expect(wrapper.emitted('search')[0][0]).toEqual(tagFilterQuery)
+        expect(wrapper.emitted('search')[0][0]).toEqual(`${searchTerm} Tags:"${tagFilterQuery}"`)
+      })
+    })
+    describe('fullText', () => {
+      it('should render filter', () => {
+        const { wrapper } = getWrapper()
+        expect(wrapper.find(selectors.fullTextFilter).exists()).toBeTruthy()
+      })
+      it('should set initial filter when fullText is set active via query param', async () => {
+        const searchTerm = 'term'
+        const { wrapper } = getWrapper({ searchTerm, fullTextFilterQuery: 'true' })
+        await wrapper.vm.loadAvailableTagsTask.last
+        expect(wrapper.emitted('search')[0][0]).toEqual(`Content:"${searchTerm}"`)
       })
     })
   })
 })
 
-function getWrapper({ availableTags = [], resources = [], tagFilterQuery = null } = {}) {
+function getWrapper({
+  availableTags = [],
+  resources = [],
+  searchTerm = '',
+  tagFilterQuery = null,
+  fullTextFilterQuery = null
+} = {}) {
+  jest.mocked(queryItemAsString).mockImplementationOnce(() => searchTerm)
+  jest.mocked(queryItemAsString).mockImplementationOnce(() => fullTextFilterQuery)
   jest.mocked(queryItemAsString).mockImplementationOnce(() => tagFilterQuery)
 
   const resourcesViewDetailsMock = useResourcesViewDefaultsMock({
---
+++ packages/web-pkg/tests/unit/components/ItemFilterToggle.spec.ts
@@ -0,0 +1,56 @@
+import ItemFilterToggle from 'web-pkg/src/components/ItemFilterToggle.vue'
+import { defaultComponentMocks, defaultPlugins, mount } from 'web-test-helpers'
+import { queryItemAsString } from 'web-pkg/src/composables/appDefaults'
+
+jest.mock('web-pkg/src/composables/appDefaults')
+
+const selectors = {
+  labelSpan: '.oc-filter-chip-label',
+  filterBtn: '.oc-filter-chip-button'
+}
+
+describe('ItemFilterToggle', () => {
+  it('renders the toggle filter including its label', () => {
+    const filterLabel = 'Toggle'
+    const { wrapper } = getWrapper({ props: { filterLabel } })
+    expect(wrapper.find(selectors.labelSpan).text()).toEqual(filterLabel)
+  })
+  it('emits the "toggleFilter"-event on click', async () => {
+    const { wrapper } = getWrapper()
+    await wrapper.find(selectors.filterBtn).trigger('click')
+    expect(wrapper.emitted('toggleFilter').length).toBeGreaterThan(0)
+  })
+  describe('route query', () => {
+    it('sets the active state as query param', async () => {
+      const { wrapper, mocks } = getWrapper()
+      const currentRouteQuery = (mocks.$router.currentRoute as any).query
+      expect(mocks.$router.push).not.toHaveBeenCalled()
+      await wrapper.find(selectors.filterBtn).trigger('click')
+      expect(currentRouteQuery[wrapper.vm.queryParam]).toBeDefined()
+      expect(mocks.$router.push).toHaveBeenCalled()
+    })
+    it('sets the active state initially when given via query param', () => {
+      const { wrapper } = getWrapper({ initialQuery: 'true' })
+      expect(wrapper.vm.filterActive).toEqual(true)
+    })
+  })
+})
+
+function getWrapper({ props = {}, initialQuery = '' } = {}) {
+  jest.mocked(queryItemAsString).mockImplementation(() => initialQuery)
+  const mocks = defaultComponentMocks()
+  return {
+    mocks,
+    wrapper: mount(ItemFilterToggle, {
+      props: {
+        filterLabel: 'Toggle',
+        filterName: 'toggle',
+        ...props
+      },
+      global: {
+        plugins: [...defaultPlugins()],
+        mocks
+      }
+    })
+  }
+}
