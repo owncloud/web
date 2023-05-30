6652f21bd88aff065c157a43031e805b4765bd21
Shu12388y <spl.sp999@gmail.com>
2023-05-30 23:05:45 +0530

    solve the issue Set meta prop theme-color #5847

--- packages/web-runtime/src/composables/head/useHead.ts
+++ packages/web-runtime/src/composables/head/useHead.ts
@@ -7,13 +7,21 @@ export const useHead = ({ store }: { store: Store<any> }) => {
   _useHead(
     computed(() => {
       const favicon = store.getters['configuration']?.currentTheme?.logo?.favicon
+      const themeColor = store.getters['configuration']?.currentTheme?.themeColor
       return {
         meta: [
           {
             name: 'generator',
             content: [getWebVersion(), getBackendVersion({ store })].filter(Boolean).join(', ')
+          },
+          themeColor && {
+            name: 'theme-color',
+            content: '#1b223d'
           }
-        ],
+
+
+        ].filter(Boolean),
+
         ...(favicon && { link: [{ rel: 'icon', href: favicon }] })
       }
     })
