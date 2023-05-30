df7c34862fdc1e1c8932194233b053237e0d43f8
Paul Neubauer <paulneubauer@live.de>
2023-05-30 16:31:25 +0200

    Set overflow visible

--- packages/design-system/src/components/OcBreadcrumb/OcBreadcrumb.vue
+++ packages/design-system/src/components/OcBreadcrumb/OcBreadcrumb.vue
@@ -345,7 +345,7 @@ export default defineComponent({
 
 <style lang="scss">
 .oc-breadcrumb {
-  overflow: hidden;
+  overflow: visible;
   &-item-dragover {
     transition: background 0.06s, border 0s 0.08s, border-color 0s, border-width 0.06s;
     background-color: var(--oc-color-background-highlight);
