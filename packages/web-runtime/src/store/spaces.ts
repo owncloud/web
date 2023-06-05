c04014545103df1bd40dca511b59e3929a74f5b0
Benedikt Kulmann <benedikt@kulmann.biz>
2023-06-05 14:51:32 +0200

    fix: initial space preloading

--- packages/web-runtime/src/store/spaces.ts
+++ packages/web-runtime/src/store/spaces.ts
@@ -11,6 +11,7 @@ import { AxiosResponse } from 'axios'
 const state = {
   spaces: [],
   spacesInitialized: false,
+  mountPointsInitialized: false,
   spacesLoading: false,
   spaceMembers: []
 }
@@ -18,6 +19,7 @@ const state = {
 const getters = {
   spaces: (state) => state.spaces,
   spacesInitialized: (state) => state.spacesInitialized,
+  mountPointsInitialized: (state) => state.mountPointsInitialized,
   spacesLoading: (state) => state.spacesLoading,
   spaceMembers: (state) => state.spaceMembers
 }
@@ -29,6 +31,9 @@ const mutations = {
   },
   SET_SPACES_INITIALIZED(state, initialized) {
     state.spacesInitialized = initialized
+  },
+  SET_MOUNT_POINTS_INITIALIZED(state, initialized) {
+    state.mountPointsInitialized = initialized
   },
   SET_SPACES_LOADING(state, loading) {
     state.spacesLoading = loading
@@ -113,29 +118,40 @@ const actions = {
   async loadSpaces(context, { graphClient }: { graphClient: Graph }) {
     context.commit('SET_SPACES_LOADING', true)
     try {
-      const graphResponse = await graphClient.drives.listMyDrives()
-      if (!graphResponse.data) {
-        return
-      }
-      const spaces = graphResponse.data.value.map((space) =>
-        buildSpace({ ...space, serverUrl: configurationManager.serverUrl })
-      )
-      context.commit('ADD_SPACES', spaces)
+      /**
+       * FIXME: this is bad for two reasons:
+       * 1. fetching by specific drive type is bad because if more drive types are being added it needs additional code.
+       *    as soon as the backend allows to filter by `driveType neq virtual` we want to use that here.
+       * 2. fetching the mountpoint drives only on first access is kind of error prone, because mount points are
+       *    trying to be accessed in multiple code locations. all of them need to check now if mountpoints need to be
+       *    fetched first. but at the moment fetching mountpoints is kind of expensive, so we need to accept that for now.
+       */
+      const [personalSpaces, projectSpaces] = await Promise.all([
+        loadSpacesByType({ graphClient, driveType: 'personal' }),
+        loadSpacesByType({ graphClient, driveType: 'project' })
+      ])
+      context.commit('ADD_SPACES', [...personalSpaces, ...projectSpaces])
       context.commit('SET_SPACES_INITIALIZED', true)
     } finally {
       context.commit('SET_SPACES_LOADING', false)
     }
   },
-  async reloadProjectSpaces(context, { graphClient }) {
-    const graphResponse = await graphClient.drives.listMyDrives('name asc', 'driveType eq project')
-    if (!graphResponse.data) {
+  async loadMountPoints(context, { graphClient }: { graphClient: Graph }) {
+    // fetching mount points is particularly expensive, so we do that only on first access.
+    if (context.getters.mountPointsInitialized) {
       return
     }
-    const spaces = graphResponse.data.value.map((space) =>
-      buildSpace({ ...space, serverUrl: configurationManager.serverUrl })
-    )
+    try {
+      const mountPointSpaces = await loadSpacesByType({ graphClient, driveType: 'mountpoint' })
+      context.commit('ADD_SPACES', mountPointSpaces)
+    } finally {
+      context.commit('SET_MOUNT_POINTS_INITIALIZED', true)
+    }
+  },
+  async reloadProjectSpaces(context, { graphClient }) {
+    const projectSpaces = await loadSpacesByType({ graphClient, driveType: 'project' })
     context.commit('CLEAR_PROJECT_SPACES')
-    context.commit('ADD_SPACES', spaces)
+    context.commit('ADD_SPACES', projectSpaces)
   },
   async loadSpaceMembers(
     context,
@@ -248,6 +264,25 @@ const actions = {
 
     context.commit('REMOVE_SPACE_MEMBER', share)
   }
+}
+
+const loadSpacesByType = async ({
+  graphClient,
+  driveType
+}: {
+  graphClient: Graph
+  driveType: string
+}): Promise<SpaceResource[]> => {
+  const graphResponse = await graphClient.drives.listMyDrives(
+    'name asc',
+    `driveType eq ${driveType}`
+  )
+  if (!graphResponse.data) {
+    return []
+  }
+  return graphResponse.data.value.map((space) =>
+    buildSpace({ ...space, serverUrl: configurationManager.serverUrl })
+  )
 }
 
 export default {
