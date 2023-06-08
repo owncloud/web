8e9e29b327ff411214410cb44641742398b53974
Dominik Schmidt <dev@dominik-schmidt.de>
2023-06-08 14:32:57 +0200

    Test ocResourceIconMapping injection is working

--- packages/design-system/src/components/OcResourceIcon/OcResourceIcon.spec.ts
+++ packages/design-system/src/components/OcResourceIcon/OcResourceIcon.spec.ts
@@ -1,26 +1,61 @@
 import { shallowMount } from 'web-test-helpers'
 import { AVAILABLE_SIZES } from '../../helpers/constants'
 import { OcResourceIcon } from '..'
+import { OcResourceIconMapping, ocResourceIconMappingInjectionKey } from './types'
+import { Resource } from 'web-client'
 
-const resources = ['file', 'folder', 'space']
+const resources: Resource[] = [
+  {
+    type: 'file'
+  },
+  {
+    type: 'file',
+    extension: 'not-a-real-extension'
+  },
+  {
+    type: 'folder'
+  },
+  {
+    type: 'space'
+  }
+].map((r) => {
+  return { ...r, id: null, path: null }
+})
+
+const resourceIconMapping: OcResourceIconMapping = {
+  extension: {
+    'not-a-real-extension': {
+      name: 'not-real',
+      color: 'red'
+    }
+  },
+  mimeType: {}
+}
 
 describe('OcResourceIcon', () => {
-  resources.forEach((resourceType) => {
+  resources.forEach((resource) => {
     AVAILABLE_SIZES.forEach((size) => {
-      it(`renders OcIcon for resource type ${resourceType} in size ${size}`, () => {
-        const { wrapper } = getWrapper({ resourceType, size })
+      it(`renders OcIcon for resource type ${resource.type}${
+        resource.extension ? ` (${resource.extension})` : ''
+      } in size ${size}`, () => {
+        const { wrapper } = getWrapper({ resource, size })
         expect(wrapper.html()).toMatchSnapshot()
       })
     })
   })
 })
 
-function getWrapper(props) {
+function getWrapper({ resource, size }: { resource: Resource; size: string }) {
   return {
     wrapper: shallowMount(OcResourceIcon as any, {
+      global: {
+        provide: {
+          [ocResourceIconMappingInjectionKey]: resourceIconMapping
+        }
+      },
       props: {
-        resource: { type: props.resourceType },
-        size: props.size
+        resource,
+        size
       }
     })
   }
