7f8bdac33ecad156a7c4d6c3336b99205b2c616f
Jan <j.ackermann91@gmail.com>
2023-06-16 15:17:42 +0200

    Bad quick fix

--- packages/web-pkg/src/composables/spaces/useGetMatchingSpace.ts
+++ packages/web-pkg/src/composables/spaces/useGetMatchingSpace.ts
@@ -1,17 +1,35 @@
 import { useConfigurationManager, useStore } from 'web-pkg/src/composables'
 import { Resource, SpaceResource } from 'web-client'
-import { buildShareSpaceResource } from 'web-client/src/helpers'
+import {
+  buildPublicSpaceResource,
+  buildShareSpaceResource,
+  isPublicSpaceResource
+} from 'web-client/src/helpers'
+import { configurationManager } from 'web-pkg'
+import { getCurrentInstance } from 'vue'
+import { useGettext } from 'vue3-gettext'
 
 export const useGetMatchingSpace = () => {
   const store = useStore()
   const configurationManager = useConfigurationManager()
   const spaces = store.getters['runtime/spaces/spaces']
-
+  const { $gettext } = useGettext()
   const getInternalSpace = (storageId) => {
     return spaces.find((space) => space.id === storageId)
   }
 
   const getMatchingSpace = (resource: Resource): SpaceResource => {
+    if (resource.downloadURL.split('/')?.[5] === 'public-files') {
+      return buildPublicSpaceResource({
+        id: store.getters['runtime/auth/publicLinkToken'],
+        name: $gettext('Public files'),
+        ...(store.getters['runtime/auth/publicLinkPassword'] && {
+          publicLinkPassword: store.getters['runtime/auth/publicLinkPassword']
+        }),
+        serverUrl: configurationManager.serverUrl
+      })
+    }
+
     return (
       getInternalSpace(resource.storageId) ||
       buildShareSpaceResource({
