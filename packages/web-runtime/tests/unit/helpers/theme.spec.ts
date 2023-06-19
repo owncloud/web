4d2e775179cce346abfd3bc638b02769b03a39f9
Paul Neubauer <paulneubauer@live.de>
2023-06-19 11:19:10 +0200

    Update/add unittests

--- packages/web-runtime/tests/unit/helpers/theme.spec.ts
+++ packages/web-runtime/tests/unit/helpers/theme.spec.ts
@@ -7,32 +7,37 @@ jest.spyOn(console, 'error').mockImplementation(() => undefined)
 
 describe('theme loading and error reporting', () => {
   it('should load the default theme if location is empty', async () => {
-    const { theme } = await loadTheme()
-    expect(theme).toMatchObject(defaultTheme.web)
+    const { web, common } = await loadTheme()
+    expect(web).toMatchObject(defaultTheme.web)
+    expect(common).toMatchObject(defaultTheme.common)
   })
 
   it('should load the default theme if location is not a json file extension', async () => {
-    const { theme } = await loadTheme('some_location_without_json_file_ending.xml')
-    expect(theme).toMatchObject(defaultTheme.web)
+    const { web, common } = await loadTheme('some_location_without_json_file_ending.xml')
+    expect(web).toMatchObject(defaultTheme.web)
+    expect(common).toMatchObject(defaultTheme.common)
   })
 
   it('should load the default theme if location is not found', async () => {
     fetchMock.mockResponse(new Error() as any, { status: 404 })
-    const { theme } = await loadTheme('http://www.owncloud.com/unknown.json')
-    expect(theme).toMatchObject(defaultTheme.web)
+    const { web, common } = await loadTheme('http://www.owncloud.com/unknown.json')
+    expect(web).toMatchObject(defaultTheme.web)
+    expect(common).toMatchObject(defaultTheme.common)
   })
 
   it('should load the default theme if location is not a valid json file', async () => {
     const customTheme = merge({}, defaultTheme, { default: { logo: { login: 'custom.svg' } } })
     fetchMock.mockResponse(JSON.stringify(customTheme) + '-invalid')
-    const { theme } = await loadTheme('http://www.owncloud.com/invalid.json')
-    expect(theme).toMatchObject(defaultTheme.web)
+    const { web, common } = await loadTheme('http://www.owncloud.com/invalid.json')
+    expect(web).toMatchObject(defaultTheme.web)
+    expect(common).toMatchObject(defaultTheme.common)
   })
 
   it('should load the default theme if server errors', async () => {
     fetchMock.mockReject(new Error())
-    const { theme } = await loadTheme('http://www.owncloud.com')
-    expect(theme).toMatchObject(defaultTheme.web)
+    const { web, common } = await loadTheme('http://www.owncloud.com')
+    expect(web).toMatchObject(defaultTheme.web)
+    expect(common).toMatchObject(defaultTheme.common)
   })
 
   it('should load the custom theme if a custom location is given', async () => {
@@ -40,8 +45,8 @@ describe('theme loading and error reporting', () => {
 
     fetchMock.mockResponse(JSON.stringify(customTheme))
 
-    const { theme: theme1 } = await loadTheme('http://www.owncloud.com/custom.json')
-    const { theme: theme2 } = await loadTheme('/custom.json')
+    const { web: theme1 } = await loadTheme('http://www.owncloud.com/custom.json')
+    const { web: theme2 } = await loadTheme('/custom.json')
 
     expect(theme1).toMatchObject(customTheme)
     expect(theme2).toMatchObject(customTheme)
