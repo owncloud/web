6cff5f6945ffa6c576fac99a9a6ffd6210b638ac
Jan <j.ackermann91@gmail.com>
2023-07-06 07:52:20 +0200

    Add hover state

--- packages/web-pkg/src/components/BatchActions.vue
+++ packages/web-pkg/src/components/BatchActions.vue
@@ -51,6 +51,11 @@ export default defineComponent({
     padding-right: var(--oc-space-small) !important;
     gap: var(--oc-space-xsmall) !important;
   }
+  .action-menu-item:hover:not([disabled]),
+  .action-menu-item:focus:not([disabled]) {
+    background-color: var(--oc-color-background-highlight);
+    border-color: var(--oc-color-background-highlight);
+  }
 
   display: block;
   li {
