1b1af6373220ea0979ea24c6e1b14485d568354a
Jacob Noah <jacob.noah.jnn@gmail.com>
2023-07-13 16:57:25 +0200

    "Shared on" set the latest time

--- packages/web-app-files/src/helpers/resources.ts
+++ packages/web-app-files/src/helpers/resources.ts
@@ -89,6 +89,9 @@ function addSharedWithToShares(shares) {
       previousShare?.file_source === share.file_source
     ) {
       if (ShareTypes.containsAnyValue(ShareTypes.authenticated, [parseInt(share.share_type)])) {
+        if (share.stime > previousShare.stime) {
+          previousShare.stime = share.stime
+        }
         previousShare.sharedWith.push({
           username: share.share_with,
           name: share.share_with_displayname,
@@ -128,6 +131,7 @@ function addSharedWithToShares(shares) {
     }
 
     previousShare = share
+    console.log(share.stime)
     resources.push(share)
   }
   return resources
