ad28e9b86edcd284f8fa92eece89029702c1fab7
Kathrin Koebel <kathrin@koebel.ch>
2023-07-20 16:44:43 +0545

    add nested view for Dicom viewer

--- packages/web-app-dicom-viewer/src/App.vue
+++ packages/web-app-dicom-viewer/src/App.vue
@@ -1,26 +1,32 @@
-/* from PDF viewer */
 <template>
   <main>
+    <!--
+    <h1 class="oc-invisible-sr" v-text="pageTitle" />
+    -->
     <app-top-bar :resource="resource" @close="closeApp" />
+
     <!-- note: preview app also lets user download file directly from this view -> see button nested in
-    app-top-bar. is this feature also desired for dicom viewer? -->
+    app-top-bar. is this feature also desired for dicom viewer?
 
     <loading-screen v-if="loading" />
+    -->
 
     <!-- alternative implementaion of loading screen from preview app, integrated in this vue
     <div v-if="true" class="oc-position-center">
       <oc-spinner :aria-label="$gettext('Loading media file')" size="xlarge" />
     </div>
-    -->
+
 
     <error-screen v-if="loadingError" />
+    -->
 
     <!--
-    <simple-dicom-viewer-screen />
-    <SimpleDicomViewerScreen />
+      v-else
     -->
-    <div v-else class="oc-height-1-1 dicom-viewer">
-      <h1>DICOM file placeholder</h1>
+    <div
+      class="oc-height-1-1 oc-width-1-1 oc-flex oc-flex-center oc-flex-middle oc-p-s oc-box-shadow-medium dicom-viewer"
+    >
+      <SimpleDicomViewerScreen />
       <!--
       <object class="dicom-viewer oc-width-1-1" :data="url" type="application/dicom" />
       -->
@@ -32,7 +38,7 @@
 import AppTopBar from 'web-pkg/src/components/AppTopBar.vue'
 import ErrorScreen from './components/ErrorScreen.vue'
 import LoadingScreen from './components/LoadingScreen.vue'
-//import SimpleDicomViewerScreen from './components/SimpleDicomViewerScreen.vue'
+import SimpleDicomViewerScreen from './components/SimpleDicomViewerScreen.vue'
 import { useAppDefaults } from 'web-pkg/src/composables'
 import { defineComponent } from 'vue'
 
@@ -41,7 +47,8 @@ export default defineComponent({
   components: {
     ErrorScreen,
     LoadingScreen,
-    AppTopBar
+    AppTopBar,
+    SimpleDicomViewerScreen
   },
   setup() {
     /* no full screen handing so far, see preview app for reference of implementation */
@@ -57,8 +64,9 @@ export default defineComponent({
     loadingError: false,
     url: '',
     resource: null
-  })
+  }),
   /*
+  ,
   watch: {
     currentFileContext: {
       handler: function () {
@@ -68,6 +76,16 @@ export default defineComponent({
       immediate: true
     }
   },
+*/
+  computed: {
+    pageTitle() {
+      return this.$gettext('Preview for %{resourceName}', {
+        resourceName: this.resource.name
+      })
+    }
+  }
+  /*
+  ,
   beforeUnmount() {
     this.unloadDicom()
   },
@@ -77,7 +95,13 @@ export default defineComponent({
         this.loading = true
         const resource = await this.getFileInfo(fileContext)
 
-        if (resource.mimeType !== ('application/dicom' || 'application/octet-stream' || 'application/dicom+xml' || 'application/json')) {
+        if (
+          resource.mimeType !==
+          ('application/dicom' ||
+            'application/octet-stream' ||
+            'application/dicom+xml' ||
+            'application/json')
+        ) {
           return
         }
 
@@ -96,13 +120,14 @@ export default defineComponent({
     unloadDicom() {
       this.revokeUrl(this.url)
     }
+
   }
   */
 })
 </script>
 <style lang="scss" scoped>
 .dicom-viewer {
-  border: 10px solid green; // just for testing
+  border: none;
   margin: 0;
   padding: 0;
   overflow: hidden;
---
+++ packages/web-app-dicom-viewer/src/components/SimpleDicomViewerScreen.vue
@@ -0,0 +1,121 @@
+<template>
+  <!--
+    // additional classes from preview app, check if they are needed oc-flex oc-flex-center
+  oc-flex-middle oc-p-s oc-box-shadow-medium dicom-viewer" // more css classes from preview app
+  :class="{ lightbox: isFullScreenModeActivated }
+
+  <div class="oc-height-1-1 oc-position-center oc-text-center">
+    <oc-spinner size="xlarge" />
+    <p v-translate class="oc-invisible">Loading DICOM file</p>
+  </div>
+  -->
+  <div
+    class="oc-width-1-1 oc-flex oc-flex-center oc-flex-middle oc-p-s oc-box-shadow-medium dicom-viewer"
+  >
+    <h1>DICOM will be displayed here</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SimpleDicomViewerScreen'
+})
+</script>
+
+<style lang="scss" scoped>
+.dicom-viewer {
+  border: 10px solid blue;
+  height: calc(100% - 52px);
+}
+</style>
+
+<!--
+<script lang="ts"></script>
+
+// Packages
+import Hammer from 'hammerjs'
+import dicomParser from 'dicom-parser'
+import * as cornerstone from 'cornerstone-core'
+import * as cornerstoneMath from 'cornerstone-math'
+import * as cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader'
+import * as cornerstoneTools from '@cornerstonejs/tools'
+
+// Externals
+cornerstoneWADOImageLoader.external.cornerstone = cornerstone
+cornerstoneWADOImageLoader.external.dicomParser = dicomParser
+cornerstoneTools.external.cornerstoneMath = cornerstoneMath
+cornerstoneTools.external.cornerstone = cornerstone
+cornerstoneTools.external.Hammer = Hammer
+
+// CodeSandbox live updates components in an odd way.
+// We do this to protect ourselves from duplicate initializations
+if (!cornerstoneWADOImageLoader.initialized) {
+  // WadoImageLoader Registration/Config
+  const config = {
+    webWorkerPath: '/codecs/cornerstoneWADOImageLoaderWebWorker.js',
+    taskConfiguration: {
+      decodeTask: {
+        codecsPath: '/codecs/cornerstoneWADOImageLoaderCodecs.js'
+      }
+    }
+  }
+  cornerstoneWADOImageLoader.webWorkerManager.initialize(config)
+  cornerstoneWADOImageLoader.initialized = true
+}
+*/
+
+export default {
+  name: 'SimpleDicomViewerScreen'
+  /*
+  ,
+  created() {
+    cornerstoneTools.init({
+      globalToolSyncEnabled: true
+    })
+
+    // Grab Tool Classes
+    const WwwcTool = cornerstoneTools.WwwcTool
+    const PanTool = cornerstoneTools.PanTool
+    const PanMultiTouchTool = cornerstoneTools.PanMultiTouchTool
+    const ZoomTool = cornerstoneTools.ZoomTool
+    const ZoomTouchPinchTool = cornerstoneTools.ZoomTouchPinchTool
+    const ZoomMouseWheelTool = cornerstoneTools.ZoomMouseWheelTool
+
+    // Add them
+    cornerstoneTools.addTool(PanTool)
+    cornerstoneTools.addTool(ZoomTool)
+    cornerstoneTools.addTool(WwwcTool)
+    cornerstoneTools.addTool(PanMultiTouchTool)
+    cornerstoneTools.addTool(ZoomTouchPinchTool)
+    cornerstoneTools.addTool(ZoomMouseWheelTool)
+
+    // Set tool modes
+    cornerstoneTools.setToolActive('Pan', { mouseButtonMask: 4 }) // Middle
+    cornerstoneTools.setToolActive('Zoom', { mouseButtonMask: 2 }) // Right
+    cornerstoneTools.setToolActive('Wwwc', { mouseButtonMask: 1 }) // Left & Touch
+    cornerstoneTools.setToolActive('PanMultiTouch', {})
+    cornerstoneTools.setToolActive('ZoomMouseWheel', {})
+    cornerstoneTools.setToolActive('ZoomTouchPinch', {})
+  },
+  mounted() {
+    // Enable Canvas
+    /*
+    this.canvas = this.$refs.canvas
+    cornerstone.enable(this.canvas, {
+      renderer: 'webgl'
+    })
+    */
+  // Load Image
+  /*
+    const codeSandboxProjectUrl = 'https://9z04x8ykjr.codesandbox.io'
+    const imageId = `wadouri:${codeSandboxProjectUrl}/000000.dcm`
+    cornerstone.loadImage(imageId).then((image) => {
+      cornerstone.displayImage(this.canvas, image)
+    })
+
+  }
+}
+</script>
+-->
