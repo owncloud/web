59240fe55175189ce61277bcc5029022e6cd80ed
Jannik Stehle <jannik.stehle@gmail.com>
2023-08-03 07:02:50 +0200

    chore: remove oC10 integration app

--- packages/web-integration-oc10/js/app.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $('#apps')
-    .find('li[data-id=web]')
-    .click(function () {
-      $.post(OC.generateUrl('/apps/web/settings/default'), { isDefault: true })
-    })
-})
