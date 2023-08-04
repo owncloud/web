eff2f2b530e691a0df78f4f3bba4e42ffa735839
Dominik Schmidt <dev@dominik-schmidt.de>
2023-08-04 02:54:40 +0200

    get rid of boilerplate

---
+++ packages/web-pkg/src/components/AppTemplates/AppWrapperRoute.ts
@@ -0,0 +1,25 @@
+import { defineComponent, h } from 'vue'
+import AppWrapper, { AppWrapperSlotArgs } from './AppWrapper.vue'
+import { UrlForResourceOptions } from 'web-pkg/src/composables'
+
+export function AppWrapperRoute(
+  fileEditor: ReturnType<typeof defineComponent>,
+  options: { applicationId: string; urlForResourceOptions?: UrlForResourceOptions }
+) {
+  return defineComponent({
+    render() {
+      return h(AppWrapper, options, {
+        default: (slotArgs: AppWrapperSlotArgs) => {
+          const { currentContent, ...restArgs } = slotArgs
+          return h(fileEditor, {
+            currentContent: currentContent.value,
+            'onUpdate:currentContent': (value) => {
+              currentContent.value = value
+            },
+            ...restArgs
+          })
+        }
+      })
+    }
+  })
+}
