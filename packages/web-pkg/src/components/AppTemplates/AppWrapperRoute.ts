2fa5e843343951664bdbc958fb96401cace92069
Dominik Schmidt <dev@dominik-schmidt.de>
2023-08-07 13:10:09 +0200

    Port web-app-draw-io to AppWrapper

--- packages/web-pkg/src/components/AppTemplates/AppWrapperRoute.ts
+++ packages/web-pkg/src/components/AppTemplates/AppWrapperRoute.ts
@@ -1,10 +1,15 @@
 import { defineComponent, h } from 'vue'
 import AppWrapper, { AppWrapperSlotArgs } from './AppWrapper.vue'
 import { UrlForResourceOptions } from 'web-pkg/src/composables'
+import { Resource } from 'web-client/src'
 
 export function AppWrapperRoute(
   fileEditor: ReturnType<typeof defineComponent>,
-  options: { applicationId: string; urlForResourceOptions?: UrlForResourceOptions }
+  options: {
+    applicationId: string
+    urlForResourceOptions?: UrlForResourceOptions
+    importResourceWithExtension?: (resource: Resource) => string
+  }
 ) {
   return defineComponent({
     render() {
