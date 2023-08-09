ef8f18e91154c35e525b8bcc0daaf98f574329b9
Jacob Noah <jacob.noah.jnn@gmail.com>
2023-08-09 19:08:46 +0200

    Revert "Create new folder, navigate to page - useScrollTo"
    This reverts commit cc3fb0a177900dd52ed4092e5114b1d03aa1842b.

--- packages/web-app-files/src/composables/scrollTo/useScrollTo.ts
+++ packages/web-app-files/src/composables/scrollTo/useScrollTo.ts
@@ -5,18 +5,6 @@ import { useStore } from 'web-pkg/src/composables/store/useStore'
 import { eventBus } from 'web-pkg/src/services'
 import { useRouteQuery } from 'web-pkg/src/composables'
 import { SideBarEventTopics } from 'web-pkg/src/composables/sideBar'
-import {
-  SortField,
-  useRouteName,
-  useRouter,
-  useSort,
-  useViewMode,
-  ViewModeConstants
-} from 'web-pkg/src/composables'
-import { determineSortFields as determineResourceTilesSortFields } from 'web-app-files/src/helpers/ui/resourceTiles'
-import { determineSortFields as determineResourceTableSortFields } from 'web-app-files/src/helpers/ui/resourceTable'
-import { useRoute } from 'vue-router'
-import { findIndex } from 'lodash-es'
 
 export interface ScrollToResult {
   scrollToResource(resourceId: Resource['id'], options?: { forceScroll?: boolean }): void
@@ -34,38 +22,7 @@ export const useScrollTo = (): ScrollToResult => {
     return queryItemAsString(unref(detailsQuery))
   })
 
-  const router = useRouter()
-  const route = useRoute()
-  const files = computed((): Array<Resource> => store.getters['Files/files'])
-  const currentRoute = useRouteName()
-  const itemsPerPageQuery = useRouteQuery('items-per-page', '1')
-  const pageQuery = useRouteQuery('page', '1')
-  const currentViewModeQuery = useRouteQuery(
-    `${unref(currentRoute)}-view-mode`,
-    ViewModeConstants.defaultModeName
-  )
-
-  const currentViewMode = computed((): string => queryItemAsString(currentViewModeQuery.value))
-  const viewMode = useViewMode(currentViewMode)
-  const sortFields = computed((): SortField[] => {
-    if (unref(viewMode) === ViewModeConstants.tilesView.name) {
-      return determineResourceTilesSortFields(unref(files)[0])
-    }
-    return determineResourceTableSortFields(unref(files)[0])
-  })
-  const { items: sortedItems } = useSort({ items: files, fields: sortFields })
-
-  const scrollToResource = async (resourceId: Resource['id'], options = { forceScroll: false }) => {
-    if (unref(sortedItems)) {
-      const index = findIndex(unref(sortedItems), (item: Resource) => item.id === resourceId)
-      if (index >= 0) {
-        const page = Math.ceil((index + 1) / Number(unref(itemsPerPageQuery)))
-        if (page !== Number(unref(pageQuery))) {
-          await router.push({ ...unref(route), query: { ...unref(route).query, page } })
-        }
-      }
-    }
-
+  const scrollToResource = (resourceId: Resource['id'], options = { forceScroll: false }) => {
     const resourceElement = document.querySelectorAll(
       `[data-item-id='${resourceId}']`
     )[0] as HTMLElement
