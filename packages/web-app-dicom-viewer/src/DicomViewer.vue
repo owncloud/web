c693e565d0e1ac398fcff36a1dda26a6533a1b6a
Kathrin Koebel <kathrin@koebel.ch>
2023-08-10 16:31:55 +0545

    add some cornerstone config

--- packages/web-app-dicom-viewer/src/DicomViewer.vue
+++ packages/web-app-dicom-viewer/src/DicomViewer.vue
@@ -17,8 +17,6 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-
 // import cornerstone packages
 import Hammer from 'hammerjs'
 import dicomParser from 'dicom-parser'
@@ -26,6 +24,44 @@ import * as cornerstoneMath from 'cornerstone-math'
 import * as cornerstone from '@cornerstonejs/core'
 import * as cornerstoneTools from '@cornerstonejs/tools'
 import * as cornerstoneDICOMImageLoader from '@cornerstonejs/dicom-image-loader'
+
+// other imports
+import { defineComponent } from 'vue'
+import { RenderingEngine, Types, Enums } from '@cornerstonejs/core'
+
+// declaring some const & references
+const { ViewportType } = Enums
+
+// specify external dependencies
+cornerstoneTools.external.Hammer = Hammer
+cornerstoneTools.external.cornerstone = cornerstone
+cornerstoneTools.external.cornerstoneMath = cornerstoneMath
+cornerstoneDICOMImageLoader.external.cornerstone = cornerstone
+cornerstoneDICOMImageLoader.external.dicomParser = dicomParser
+
+// configure cornerstone dicom image loader
+const { preferSizeOverAccuracy, useNorm16Texture } = cornerstone.getConfiguration().rendering
+cornerstoneDICOMImageLoader.configure({
+  useWebWorkers: true,
+  decodeConfig: {
+    convertFloatPixelDataToInt: false,
+    use16BitDataType: preferSizeOverAccuracy || useNorm16Texture
+  }
+})
+
+// configure web worker framework
+var config = {
+  maxWebWorkers: navigator.hardwareConcurrency || 1,
+  startWebWorkersOnDemand: true,
+  // the following config items are added from a sample - TODO: further look into the specifics of the configuration
+  webWorkerTaskPaths: [],
+  taskConfiguration: {
+    decodeTask: {
+      initializeCodecsOnStartup: true,
+      strict: true
+    }
+  }
+}
 
 export default defineComponent({
   props: {
