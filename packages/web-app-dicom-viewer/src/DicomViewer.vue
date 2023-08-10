abc2d4b6e6c99735e6c42a9edab42290e76482d6
Kathrin Koebel <kathrin@koebel.ch>
2023-08-10 16:20:52 +0545

    adjust HTML structure, add element for viewpoint

--- packages/web-app-dicom-viewer/src/DicomViewer.vue
+++ packages/web-app-dicom-viewer/src/DicomViewer.vue
@@ -1,11 +1,20 @@
 <template>
+  <!-- check ouf if "application/dcm" is also needed in the type definition below -->
   <object
     class="dicom-viewer oc-width-1-1 oc-height-1-1"
     :data="url"
     type="application/octet-stream"
-  />
+  >
+    <!-- check ouf if the classes of the div below are still accurate/needed -->
+    <div
+      id="dicom-viewer"
+      class="oc-height-1-1 oc-width-1-1 oc-flex oc-flex-center oc-flex-middle oc-p-s oc-box-shadow-medium"
+    >
+      <!-- div element for dicom viewport -->
+      <div id="dicom-canvas" class="dicom-canvas"></div>
+    </div>
+  </object>
 </template>
-<!-- check ouf if "application/dcm" is also needed here -->
 
 <script lang="ts">
 import { defineComponent } from 'vue'
@@ -21,24 +30,21 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.pdf-viewer {
+.dicom-viewer {
   border: none;
   margin: 0;
   padding: 0;
   overflow: hidden;
 }
 
-.dicom-viewer {
+#dicom-viewer {
   border: 10px solid blue; // none
-  height: calc(100% - 52px);
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
+  height: 100%; //calc(100% - 52px);
 }
 
 .dicom-canvas {
   border: 10px solid yellow; //none
-  width: 2000px; // 100%;
+  width: 500px; // 100%;
   height: 500px; // 100%;
 }
 </style>
