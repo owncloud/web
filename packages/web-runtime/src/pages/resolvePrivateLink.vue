5fa7e7054d76a634f9c9ca7899cf071a70d5ef09
Dominik Schmidt <dev@dominik-schmidt.de>
2023-08-15 15:14:39 +0200

    Remove debugger statement

--- packages/web-runtime/src/pages/resolvePrivateLink.vue
+++ packages/web-runtime/src/pages/resolvePrivateLink.vue
@@ -99,7 +99,6 @@ export default defineComponent({
 
     const { loadFileInfoByIdTask } = useLoadFileInfoById({ clientService })
     const resolvePrivateLinkTask = useTask(function* (signal, id) {
-      // debugger
       let path
       let matchingSpace = getMatchingSpace(id)
       let resourceIsNestedInShare = false
