e5459b03b664ce5f4cb28a8f6f1527081b7c59c6
Kathrin Koebel <kathrin@koebel.ch>
2023-08-31 12:19:32 +0545

    clean up code

--- packages/web-app-dicom-viewer/tests/unit/DicomViewer.spec.ts
+++ packages/web-app-dicom-viewer/tests/unit/DicomViewer.spec.ts
@@ -1,4 +1,5 @@
 import DicomViewer from '../../src/DicomViewer.vue'
+/*
 import { nextTick, ref } from 'vue'
 import {
   createStore,
@@ -9,16 +10,8 @@ import {
 } from 'web-test-helpers'
 import { useAppDefaultsMock } from 'web-test-helpers/src/mocks/useAppDefaultsMock'
 import { FileContext, useAppDefaults } from 'web-pkg/src/composables/appDefaults'
+*/
 import { mock } from 'jest-mock-extended'
-
-jest.mock('web-pkg/src/composables/appDefaults', () => {
-  const { queryItemAsString } = jest.requireActual('web-pkg/src/composables/appDefaults')
-  return {
-    useAppDefaults: jest.fn(),
-    useAppFileHandling: jest.fn(),
-    queryItemAsString
-  }
-})
 
 // -------------------------------------------------
 // suggested test cases
@@ -44,8 +37,6 @@ jest.mock('web-pkg/src/composables/appDefaults', () => {
 // -------------------------------------------------
 
 // defining data
-// example from preview app, so far not used in any test case
-
 const dicomFiles = [
   {
     id: '1',
@@ -53,21 +44,22 @@ const dicomFiles = [
     mimeType: 'application/dicom',
     path: 'personal/admin/MRBRAIN.dcm'
   }
-]
+] // so far not used in any test case
 
 const dicomTestFilePath = './testfiles/MRBRAIN.dcm' // check if this needs to be an import or a const
 const dicomURL = 'https://dav/spaces/path/to/file.dcm?OC-Credential=xyz'
 const wadouriDicomURL = 'wadouri:https://dav/spaces/path/to/file.dcm?OC-Credential=xyz'
 
 // test cases
-describe('Dicom viewer app for testing only', () => {
+describe('Dicom viewer app', () => {
   describe('dummy test', () => {
-    it('do nothing', () => {
+    it('do nothing :)', () => {
       console.log(dicomTestFilePath)
       expect(dicomTestFilePath).toBe(dicomTestFilePath)
     })
   })
 })
+
 /*
 describe('Dicom viewer app', () => {
   describe('Method "addWadouriPrefix"', () => {
@@ -77,66 +69,4 @@ describe('Dicom viewer app', () => {
     })
   })
 })
-*/
-
-// examples for test functions from preview app
-/*
-describe('Preview app', () => {
-  describe('Method "preloadImages"', () => {
-    it('should preload images if active file changes', async () => {
-      const { wrapper } = createShallowMountWrapper()
-      await nextTick()
-
-      wrapper.vm.toPreloadImageIds = []
-      wrapper.vm.setActiveFile('personal/admin/sleeping_dog.gif')
-
-      await nextTick()
-
-      expect(wrapper.vm.toPreloadImageIds).toEqual(['8', '9', '1', '6', '4'])
-    })
-  })
-})
-
-
-
-const storeOptions = defaultStoreMockOptions
-storeOptions.modules.Files.getters.activeFiles.mockImplementation(() => ['3'])
-const store = createStore(storeOptions)
-
-
-function createShallowMountWrapper() {
-  jest.mocked(useAppDefaults).mockImplementation(() =>
-    useAppDefaultsMock({
-      currentFileContext: ref(
-        mock<FileContext>({
-          path: 'personal/admin/MRBRAIN.dcm',
-          space: {
-            getDriveAliasAndItem: jest.fn().mockImplementation((file) => {
-              return dicomFiles.find((filteredFile) => filteredFile.id == file.id)?.path
-            })
-          }
-        })
-      ),
-      activeFiles: ref(dicomFiles)
-    })
-  )
-
-  const mocks = defaultComponentMocks()
-  mocks.$previewService.loadPreview.mockResolvedValue('')
-  return {
-    wrapper: shallowMount(DicomViewer, {
-      data: function () {
-        return {}
-      },
-      global: {
-        plugins: [
-          ...defaultPlugins()
-          //store
-        ],
-        mocks,
-        provide: mocks
-      }
-    })
-  }
-}
 */
