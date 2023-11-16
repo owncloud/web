c1ba61e78a04ed5d63550db495f11efcb50c71d1
Jan Ackermann <jan.alexander.ackermann@outlook.com>
2023-11-16 11:03:02 +0100

    Add keyup keydown

--- packages/web-pkg/src/components/CreateShortcutModal.vue
+++ packages/web-pkg/src/components/CreateShortcutModal.vue
@@ -14,6 +14,8 @@
           id="create-shortcut-modal-url-input"
           v-model="inputUrl"
           :label="$gettext('Shortcut to a webpage or file')"
+          @keyup.up="onKeyUpDrop"
+          @keyup.down="onKeyDownDrop"
         />
         <oc-drop
           v-if="showDrop"
@@ -23,36 +25,37 @@
           drop-id="create-shortcut-modal-contextmenu"
           toggle="#create-shortcut-modal-url-input"
           :close-on-click="true"
+
         >
           <oc-list>
-            <li class="oc-p-xs">
+            <li class="oc-p-xs selectable-item">
               <oc-button
                 class="oc-width-1-1"
                 appearance="raw"
                 justify-content="left"
                 @click="dropItemUrlClicked"
               >
-                <oc-icon name="external-link" />
-                <span v-text="dropItemUrl" />
+                <oc-icon name="external-link"/>
+                <span v-text="dropItemUrl"/>
               </oc-button>
             </li>
             <li v-if="searchTask.isRunning" class="oc-p-xs oc-flex oc-flex-center">
-              <oc-spinner />
+              <oc-spinner/>
             </li>
             <template v-if="searchResult?.values?.length">
               <li
                 class="create-shortcut-modal-search-separator oc-text-muted oc-text-small oc-pl-xs"
               >
-                <span v-text="$gettext('Link to a file')" />
+                <span v-text="$gettext('Link to a file')"/>
               </li>
-              <li v-for="(value, index) in searchResult.values" :key="index" class="oc-p-xs">
+              <li v-for="(value, index) in searchResult.values" :key="index" class="oc-p-xs selectable-item">
                 <oc-button
                   class="oc-width-1-1"
                   appearance="raw"
                   justify-content="left"
                   @click="dropItemResourceClicked(value)"
                 >
-                  <resource-preview :search-result="value" :is-clickable="false" />
+                  <resource-preview :search-result="value" :is-clickable="false"/>
                 </oc-button>
               </li>
             </template>
@@ -67,7 +70,7 @@
             :fix-message-line="true"
           />
           <span class="oc-ml-s oc-flex oc-flex-bottom create-shortcut-modal-url-extension"
-            >.url</span
+          >.url</span
           >
         </div>
       </template>
@@ -76,26 +79,26 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, ref, unref, computed, watch, nextTick, Ref } from 'vue'
-import { Resource, SpaceResource } from '@ownclouders/web-client'
-import { useClientService, useFolderLink, useRouter, useSearch, useStore } from '../composables'
-import { urlJoin } from '@ownclouders/web-client/src/utils'
-import { useGettext } from 'vue3-gettext'
+import {defineComponent, PropType, ref, unref, computed, watch, nextTick, Ref} from 'vue'
+import {Resource, SpaceResource} from '@ownclouders/web-client'
+import {useClientService, useFolderLink, useRouter, useSearch, useStore} from '../composables'
+import {urlJoin} from '@ownclouders/web-client/src/utils'
+import {useGettext} from 'vue3-gettext'
 import DOMPurify from 'dompurify'
-import { OcDrop } from '@ownclouders/design-system/src/components'
-import { resolveFileNameDuplicate } from '../helpers'
-import { useTask } from 'vue-concurrency'
-import { debounce } from 'lodash-es'
+import {OcDrop} from '@ownclouders/design-system/src/components'
+import {resolveFileNameDuplicate} from '../helpers'
+import {useTask} from 'vue-concurrency'
+import {debounce} from 'lodash-es'
 import ResourcePreview from './Search/ResourcePreview.vue'
-import { SearchResult, SearchResultValue } from './Search'
-import { isLocationPublicActive } from '../router'
+import {SearchResult, SearchResultValue} from './Search'
+import {isLocationPublicActive} from '../router'
 
 const SEARCH_LIMIT = 7
 const SEARCH_DEBOUNCE_TIME = 200
 
 export default defineComponent({
   name: 'CreateShortcutModal',
-  components: { ResourcePreview },
+  components: {ResourcePreview},
   props: {
     space: {
       type: Object as PropType<SpaceResource>,
@@ -108,10 +111,10 @@ export default defineComponent({
   },
   setup(props) {
     const clientService = useClientService()
-    const { $gettext } = useGettext()
+    const {$gettext} = useGettext()
     const store = useStore()
     const router = useRouter()
-    const { search } = useSearch()
+    const {search} = useSearch()
     const {
       getPathPrefix,
       getParentFolderName,
@@ -124,6 +127,7 @@ export default defineComponent({
     const inputUrl = ref('')
     const inputFilename = ref('')
     const searchResult: Ref<SearchResult> = ref(null)
+    const activeDropItemIndex = ref(null)
 
     const dropItemUrl = computed(() => {
       let url = unref(inputUrl).trim()
@@ -150,7 +154,7 @@ export default defineComponent({
 
     const inputFileNameErrorMessage = computed(() => {
       if (unref(fileAlreadyExists)) {
-        return $gettext('%{name} already exists', { name: `${unref(inputFilename)}.url` })
+        return $gettext('%{name} already exists', {name: `${unref(inputFilename)}.url`})
       }
 
       return ''
@@ -184,7 +188,8 @@ export default defineComponent({
           filename = resolveFileNameDuplicate(`${filename}.url`, 'url', unref(files)).slice(0, -4)
         }
         inputFilename.value = filename
-      } catch (_) {}
+      } catch (_) {
+      }
     }
 
     const dropItemResourceClicked = (item: SearchResultValue) => {
@@ -206,13 +211,64 @@ export default defineComponent({
       }
     }
 
+    const findNextDropItemIndex = (previous = false) => {
+      const elements = Array.from(document.querySelectorAll('li.selectable-item'))
+      let index =
+        unref(activeDropItemIndex) !== null ? unref(activeDropItemIndex) : previous ? elements.length : -1
+      const increment = previous ? -1 : 1
+
+      console.log(elements)
+
+      do {
+        index += increment
+        if (index < 0 || index > elements.length - 1) {
+          return null
+        }
+      } while (elements[index].classList.contains('disabled'))
+
+
+      return index
+    }
+
+    const scrollToActiveDropItemIndex = () => {
+      if (typeof unref(dropRef).$el.scrollTo !== 'function') {
+        return
+      }
+
+      console.log("SCROLL TO")
+
+      const elements = unref(dropRef).$el.querySelectorAll('.selectable-item')
+
+      console.log(elements[unref(activeDropItemIndex)].getBoundingClientRect().y)
+
+
+      unref(dropRef).$el.scrollTo(
+        0,
+        unref(activeDropItemIndex) === null
+          ? 0
+          : elements[unref(activeDropItemIndex)].getBoundingClientRect().y -
+          elements[unref(activeDropItemIndex)].getBoundingClientRect().height
+      )
+    }
+    const onKeyUpDrop = () => {
+
+      activeDropItemIndex.value = findNextDropItemIndex(true)
+      scrollToActiveDropItemIndex()
+    }
+
+    const onKeyDownDrop = () => {
+      activeDropItemIndex.value = findNextDropItemIndex(false)
+      scrollToActiveDropItemIndex()
+    }
+
+
     const createShortcut = async (url: string, filename: string) => {
       // Closes the modal
       props.cancel()
 
       try {
         // Omit possible xss code
-        const sanitizedUrl = DOMPurify.sanitize(url, { USE_PROFILES: { html: true } })
+        const sanitizedUrl = DOMPurify.sanitize(url, {USE_PROFILES: {html: true}})
 
         const content = `[InternetShortcut]\nURL=${sanitizedUrl}`
         const path = urlJoin(unref(currentFolder).path, `${filename}.url`)
@@ -239,6 +295,7 @@ export default defineComponent({
         ;(unref(dropRef) as InstanceType<typeof OcDrop>).show()
 
         if (!isLocationPublicActive(router, 'files-public-link')) {
+          activeDropItemIndex.value = null
           debouncedSearch()
         }
       }
@@ -262,7 +319,9 @@ export default defineComponent({
       getFolderLink,
       getParentFolderLink,
       getParentFolderName,
-      getParentFolderLinkIconAdditionalAttributes
+      getParentFolderLinkIconAdditionalAttributes,
+      onKeyDownDrop,
+      onKeyUpDrop,
     }
   }
 })
@@ -285,7 +344,8 @@ export default defineComponent({
     text-decoration: none !important;
   }
 
-  li:hover {
+  li:hover,
+  li.active {
     background-color: var(--oc-color-background-highlight);
   }
 }
