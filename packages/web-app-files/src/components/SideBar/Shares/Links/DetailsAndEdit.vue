4fd1da21da0ac5cb41e9431105874b3cb2e512cb
Paul Neubauer <paulneubauer@live.de>
2023-11-29 11:56:37 +0100

    Bugfix: Disable expiration date alias link followup (#10085)
    * Bugfix: Disable expiration date alias link followup
    
    * Add to changelog
    
    * Fix missing padding

--- packages/web-app-files/src/components/SideBar/Shares/Links/DetailsAndEdit.vue
+++ packages/web-app-files/src/components/SideBar/Shares/Links/DetailsAndEdit.vue
@@ -72,7 +72,10 @@
           padding-size="small"
           mode="click"
         >
-          <oc-list class="edit-public-link-dropdown-menu oc-files-context-actions-border oc-pb-s">
+          <oc-list
+            v-if="editOptions.length > 0"
+            class="edit-public-link-dropdown-menu oc-pb-s oc-files-context-actions-border"
+          >
             <li
               v-for="(option, i) in editOptions"
               :key="`public-link-edit-option-${i}`"
@@ -121,7 +124,10 @@
               </oc-button>
             </li>
           </oc-list>
-          <oc-list class="edit-public-link-dropdown-menu oc-pt-s">
+          <oc-list
+            class="edit-public-link-dropdown-menu"
+            :class="{ 'oc-pt-s': editOptions.length > 0 }"
+          >
             <li class="oc-rounded oc-menu-item-hover">
               <oc-button
                 appearance="raw"
