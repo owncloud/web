ca5843334f78951813680beacb11f2403e3c71ec
Paul Neubauer <paulneubauer@live.de>
2023-12-05 16:05:20 +0100

    Update unittests

--- packages/web-pkg/tests/unit/components/ViewOptions.spec.ts
+++ packages/web-pkg/tests/unit/components/ViewOptions.spec.ts
@@ -117,7 +117,7 @@ describe('ViewOptions component', () => {
       })
       const rootStyle = (document.querySelector(':root') as HTMLElement).style
       expect(rootStyle.getPropertyValue('--oc-size-tiles-resize-step')).toEqual(
-        `${tileSize * 12}rem`
+        `${tileSize * 10}rem`
       )
     })
   })
