44854b67871c05d785baf9d9df88f295ea8b65ad
Pascal Wengerter <pascal@wengerter.info>
2023-12-05 23:36:23 +0100

    Restructure theme.json and loading to reintroduce top-level common section and dedicated client key

--- packages/web-runtime/src/helpers/theme.ts
+++ packages/web-runtime/src/helpers/theme.ts
@@ -3,25 +3,31 @@ import defaultTheme from '../../themes/owncloud/theme.json'
 import { v4 as uuidV4 } from 'uuid'
 
 export const loadTheme = async (location = '') => {
-  const defaults = defaultTheme.web
+  const defaultOwnCloudTheme = {
+    defaults: {
+      ...defaultTheme.clients.web.defaults,
+      common: defaultTheme.common
+    },
+    themes: defaultTheme.clients.web.themes
+  }
 
   if (location.split('.').pop() !== 'json') {
     if (isEqual(process.env.NODE_ENV, 'development')) {
       console.info(`Theme '${location}' does not specify a json file, using default theme.`)
     }
-    return defaults
+    return defaultOwnCloudTheme
   }
 
   try {
     const response = await fetch(location, { headers: { 'X-Request-ID': uuidV4() } })
     if (!response.ok) {
-      return defaults
+      return defaultOwnCloudTheme
     }
     const theme = await response.json()
     // Check if web key (with default and themes) is in the loaded theme, else log error and use default theme?
-    return theme.web || {}
+    return theme.clients.web || {}
   } catch (e) {
     console.error(`Failed to load theme '${location}', using default theme.`)
-    return defaults
+    return defaultOwnCloudTheme
   }
 }
