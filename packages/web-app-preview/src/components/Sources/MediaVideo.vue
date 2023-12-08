861adc21e0ebb7744414302f912fe8dadae2f267
Jacob Noah <138569835+jacob-nv@users.noreply.github.com>
2023-12-08 23:33:42 +0100

    Updated video dimensions to fit view horizontal/vertical (#10101)

--- packages/web-app-preview/src/components/Sources/MediaVideo.vue
+++ packages/web-app-preview/src/components/Sources/MediaVideo.vue
@@ -1,10 +1,16 @@
 <template>
-  <video :key="`media-video-${file.id}`" controls preload="preload" :autoplay="isAutoPlayEnabled">
+  <video
+    ref="video"
+    :key="`media-video-${file.id}`"
+    controls
+    preload="preload"
+    :autoplay="isAutoPlayEnabled"
+  >
     <source :src="file.url" :type="file.mimeType" />
   </video>
 </template>
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { defineComponent, onBeforeUnmount, onMounted, PropType, ref } from 'vue'
 import { CachedFile } from '../../helpers/types'
 
 export default defineComponent({
@@ -17,6 +23,27 @@ export default defineComponent({
     isAutoPlayEnabled: {
       type: Boolean,
       default: true
+    }
+  },
+  setup() {
+    const video = ref(null)
+    const resizeVideoDimensions = () => {
+      const stageMedia: HTMLElement = document.querySelector('.stage_media')
+      video.value.style.maxHeight = `${stageMedia.offsetHeight - 10}px`
+      video.value.style.maxWidth = `${stageMedia.offsetWidth - 10}px`
+    }
+
+    onMounted(() => {
+      resizeVideoDimensions()
+      window.addEventListener('resize', resizeVideoDimensions)
+    })
+
+    onBeforeUnmount(() => {
+      window.removeEventListener('resize', resizeVideoDimensions)
+    })
+
+    return {
+      video
     }
   }
 })
