eb8f9fb19c01b0870e0cda87b1b1924bc0f3b631
Jan Ackermann <jan.alexander.ackermann@outlook.com>
2024-01-15 19:31:48 +0100

    Cleanup

--- packages/web-runtime/src/components/Topbar/ThemeSwitcher.vue
+++ packages/web-runtime/src/components/Topbar/ThemeSwitcher.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <oc-select
-      :placeholder="$gettext('Please choose...')"
       :model-value="currentTheme"
       :clearable="false"
       :options="availableThemes"
@@ -11,35 +10,30 @@
   </div>
 </template>
 <script lang="ts">
-import { computed, defineComponent, unref } from 'vue'
-import { useGettext } from 'vue3-gettext'
-import { useThemeStore } from '@ownclouders/web-pkg'
+import { defineComponent, unref } from 'vue'
+import { useThemeStore, WebThemeType } from '@ownclouders/web-pkg'
 import { storeToRefs } from 'pinia'
 
 export default defineComponent({
   emits: ['update'],
   setup(props, { emit }) {
-    const { $gettext } = useGettext()
     const themeStore = useThemeStore()
 
     const { setAndApplyTheme } = themeStore
 
-    const updateTheme = (newTheme) => {
+    const updateTheme = (newTheme: WebThemeType) => {
       setAndApplyTheme(newTheme)
       emit('update')
     }
 
     const { availableThemes, currentTheme } = storeToRefs(themeStore)
 
-    const buttonLabel = computed(() => $gettext('Click to switch theme'))
-
-    const isThemeCurrentlyActive = (theme) => {
+    const isThemeCurrentlyActive = (theme: WebThemeType) => {
       return unref(currentTheme) === theme
     }
 
     return {
       availableThemes,
-      buttonLabel,
       currentTheme,
       isThemeCurrentlyActive,
       updateTheme
