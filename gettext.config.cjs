17c1cba456c366f214b70fc5ed777308d2538a5f
Lukas Hirt <lukashirt@mail.com>
2024-01-22 15:32:30 +0100

    chore: replace easygettext with scripts from vue3 gettext

---
+++ gettext.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  input: {
+    path: './src',
+    include: ['**/*.js', '**/*.ts', '**/*.vue']
+  },
+  output: {
+    locales: ['bg', 'cs', 'de', 'en', 'es', 'fr', 'it', 'nl', 'ko', 'sq', 'sv', 'tr', 'zh'],
+    path: './l10n/locale',
+    potPath: '../template.pot',
+    flat: false,
+    linguas: false
+  }
+}
