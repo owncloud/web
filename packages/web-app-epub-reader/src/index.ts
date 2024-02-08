035a5c140a3d70b42616e36f2e9dd9aaa236bde2
Jan Ackermann <jan.alexander.ackermann@outlook.com>
2024-02-08 16:18:08 +0100

    Clean index.ts

--- packages/web-app-epub-reader/src/index.ts
+++ packages/web-app-epub-reader/src/index.ts
@@ -1,48 +1,13 @@
 import { useGettext } from 'vue3-gettext'
 import translations from '../l10n/translations.json'
 import EpubReader from './App.vue'
-import {
-  AppWrapperRoute,
-  ApplicationFileExtension,
-  defineWebApplication
-} from '@ownclouders/web-pkg'
+import { AppWrapperRoute, defineWebApplication } from '@ownclouders/web-pkg'
 
 export default defineWebApplication({
-  setup({ applicationConfig }) {
+  setup() {
     const { $gettext } = useGettext()
 
     const appId = 'epub-reader'
-
-    const fileExtensions = () => {
-      const extensions: ApplicationFileExtension[] = [
-        {
-          extension: 'epub',
-          label: $gettext('EPUB file')
-        }
-      ]
-
-      const config = applicationConfig || {}
-      extensions.push(...(config.extraExtensions || []).map((ext: string) => ({ extension: ext })))
-
-      let primaryExtensions: string[] = config.primaryExtensions || ['epub']
-
-      if (typeof primaryExtensions === 'string') {
-        primaryExtensions = [primaryExtensions]
-      }
-
-      return extensions.reduce<ApplicationFileExtension[]>((acc, extensionItem) => {
-        const isPrimary = primaryExtensions.includes(extensionItem.extension)
-        if (isPrimary) {
-          extensionItem.newFileMenu = {
-            menuTitle() {
-              return $gettext(extensionItem.label)
-            }
-          }
-        }
-        acc.push(extensionItem)
-        return acc
-      }, [])
-    }
 
     const routes = [
       {
@@ -67,16 +32,12 @@ export default defineWebApplication({
         name: $gettext('Epub Reader'),
         id: appId,
         icon: 'book-read',
-        isFileEditor: true,
-        defaultExtension: 'epub',
-        extensions: fileExtensions().map((extensionItem) => {
-          return {
-            extension: extensionItem.extension,
-            ...(Object.hasOwn(extensionItem, 'newFileMenu') && {
-              newFileMenu: extensionItem.newFileMenu
-            })
+        extensions: [
+          {
+            extension: 'epub',
+            routeName: 'epub-reader'
           }
-        })
+        ]
       },
       routes,
       translations
