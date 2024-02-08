bd66d31065b3e745be4554ceaa12ec9c00b1dce0
Pascal Wengerter <pascal@wengerter.info>
2024-02-08 22:48:02 +0100

    Add draft using notebookjs to render jupyter notebook as HTML preview

---
+++ packages/web-app-jupyter-viewer/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="oc-jupyter-viewer oc-width-1-1 oc-height-1-1">
+    <div id="" class="notebook-container" v-html="content"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, onMounted } from 'vue'
+import Prism from 'prismjs'
+import nb from 'notebookjs'
+import 'prismjs/themes/prism.css'
+
+export default defineComponent({
+  name: 'JuypterViewer',
+  props: {
+    currentContent: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props) {
+    const highlighter = (code, lang) => {
+      if (typeof lang === 'undefined') lang = 'markup'
+
+      console.log(code, lang, Prism.languages[lang])
+      return Prism.languages[lang] ? Prism.highlight(code, Prism.languages[lang]) : code
+    }
+
+    nb.highlighter = function (text, pre, code, lang) {
+      var language = lang || 'text'
+      pre.className = 'language-' + language
+      if (typeof code != 'undefined') {
+        code.className = 'language-' + language
+      }
+      return highlighter(text, language)
+    }
+
+    const content = computed(() => nb.parse(JSON.parse(props.currentContent)).render().outerHTML)
+
+    onMounted(() => {
+      Prism.highlightAll()
+    })
+
+    return {
+      content
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.oc-jupyter-viewer {
+  overflow-y: scroll;
+
+  .notebook-container {
+    padding: 3rem 2rem;
+  }
+}
+</style>
---
+++ packages/web-app-jupyter-viewer/src/index.ts
@@ -0,0 +1,78 @@
+import { useGettext } from 'vue3-gettext'
+import translations from '../l10n/translations.json'
+import TextEditor from './App.vue'
+import {
+  AppWrapperRoute,
+  ApplicationFileExtension,
+  defineWebApplication,
+  useUserStore
+} from '@ownclouders/web-pkg'
+
+export default defineWebApplication({
+  setup({ applicationConfig }) {
+    const { $gettext } = useGettext()
+    const userStore = useUserStore()
+
+    const appId = 'jupyter-viewer'
+
+    const fileExtensions = () => {
+      const extensions: ApplicationFileExtension[] = [
+        {
+          extension: 'ipynb',
+          label: $gettext('Jupyter notebook')
+        }
+      ]
+
+      const config = applicationConfig || {}
+      extensions.push(...(config.extraExtensions || []).map((ext: string) => ({ extension: ext })))
+
+      let primaryExtensions: string[] = config.primaryExtensions || ['ipynb']
+
+      if (typeof primaryExtensions === 'string') {
+        primaryExtensions = [primaryExtensions]
+      }
+
+      return extensions
+    }
+
+    const routes = [
+      {
+        path: '/:driveAliasAndItem(.*)?',
+        component: AppWrapperRoute(TextEditor, {
+          applicationId: appId
+        }),
+        name: 'jupyter-viewer',
+        meta: {
+          authContext: 'hybrid',
+          title: $gettext('Jupyter Viewer'),
+          patchCleanPath: true
+        }
+      }
+    ]
+
+    return {
+      appInfo: {
+        name: $gettext('Jupyter Viewer'),
+        id: appId,
+        icon: 'jupyter',
+        color: '#0D856F',
+        isFileEditor: false,
+        applicationMenu: {
+          enabled: () => {
+            return !!userStore.user
+          },
+          priority: 20,
+          openAsEditor: true
+        },
+        defaultExtension: 'ipynb',
+        extensions: fileExtensions().map((extensionItem) => {
+          return {
+            extension: extensionItem.extension
+          }
+        })
+      },
+      routes,
+      translations
+    }
+  }
+})
