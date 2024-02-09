1e6820667a9e274ce6503eb87235cc4634deaade
Jan Ackermann <jan.alexander.ackermann@outlook.com>
2024-02-09 12:33:48 +0100

    Fix scrolling readonly markdown not possible

--- packages/web-app-text-editor/src/App.vue
+++ packages/web-app-text-editor/src/App.vue
@@ -38,6 +38,10 @@ export default defineComponent({
 .oc-text-editor-readonly {
   //Toastui Editor doesn't have margins in view mode, adjusted for uniformity
   padding: 18px 25px;
+
+  // Fixes readonly mode scrolling not available
+  height: calc(100vh - 52px);
+  overflow: auto;
 }
 
 .toastui-editor-defaultUI {
