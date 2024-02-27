b570da51e4aed6b3a96e5ceea2eb4d05687dd96a
Jan Ackermann <jan.alexander.ackermann@outlook.com>
2024-02-27 19:59:30 +0100

    Fix unit tests

--- packages/web-runtime/tests/unit/components/UploadInfo.spec.ts
+++ packages/web-runtime/tests/unit/components/UploadInfo.spec.ts
@@ -1,6 +1,7 @@
 import UploadInfo from '../../../src/components/UploadInfo.vue'
 import { defaultPlugins, shallowMount, defaultComponentMocks } from 'web-test-helpers'
 import { ResourceListItem } from '@ownclouders/web-pkg'
+import { nextTick } from 'vue'
 
 const selectors = {
   overlay: '#upload-info',
@@ -21,150 +22,145 @@ describe('UploadInfo component', () => {
     const overlay = wrapper.find(selectors.overlay)
     expect(overlay.exists()).toBeFalsy()
   })
-  it('should show the component', () => {
-    const { wrapper } = getShallowWrapper({ showInfo: true })
+  it('should show the component', async () => {
+    const { wrapper } = getShallowWrapper()
+    wrapper.vm.showInfo = true
+    await nextTick()
     const overlay = wrapper.find(selectors.overlay)
     expect(overlay.exists()).toBeTruthy()
   })
   describe('title', () => {
-    it('should show that an upload is in progress', () => {
-      const { wrapper } = getShallowWrapper({
-        showInfo: true,
-        filesInProgressCount: 1,
-        runningUploads: 1
-      })
+    it('should show that an upload is in progress', async () => {
+      const { wrapper } = getShallowWrapper()
+      wrapper.vm.showInfo = true
+      wrapper.vm.filesInProgressCount = 1
+      wrapper.vm.runningUploads = 1
+      await nextTick()
+
       const uploadTitle = wrapper.find(selectors.title).text()
       expect(uploadTitle).toBe('1 item uploading...')
     })
-    it('should show that an upload was successful', () => {
-      const { wrapper } = getShallowWrapper({
-        showInfo: true,
-        filesInProgressCount: 0,
-        runningUploads: 0,
-        successful: ['1'],
-        errors: {}
-      })
+    it('should show that an upload was successful', async () => {
+      const { wrapper } = getShallowWrapper()
+      wrapper.vm.showInfo = true
+      wrapper.vm.successful = ['1']
+      await nextTick()
+
       const uploadTitle = wrapper.find(selectors.title).text()
       expect(uploadTitle).toBe('Upload complete')
     })
-    it('should show that an upload failed', () => {
-      const { wrapper } = getShallowWrapper({
-        showInfo: true,
-        filesInProgressCount: 0,
-        runningUploads: 0,
-        errors: [{ name: 'file', type: 'file' }],
-        successful: []
-      })
+    it('should show that an upload failed', async () => {
+      const { wrapper } = getShallowWrapper()
+      wrapper.vm.showInfo = true
+      ;(wrapper.vm.errors = [{ name: 'file', type: 'file' }]), await nextTick()
+
       const uploadTitle = wrapper.find(selectors.title).text()
       expect(uploadTitle).toBe('Upload failed')
     })
-    it('should show that an upload was cancelled', () => {
-      const { wrapper } = getShallowWrapper({
-        showInfo: true,
-        filesInProgressCount: 0,
-        runningUploads: 0,
-        errors: {},
-        successful: [],
-        uploadsCancelled: true
-      })
+    it('should show that an upload was cancelled', async () => {
+      const { wrapper } = getShallowWrapper()
+      wrapper.vm.showInfo = true
+      wrapper.vm.uploadsCancelled = true
+      await nextTick()
+
       const uploadTitle = wrapper.find(selectors.title).text()
       expect(uploadTitle).toBe('Upload cancelled')
     })
-    it('should show that an upload is preparing', () => {
-      const { wrapper } = getShallowWrapper({
-        showInfo: true,
-        filesInProgressCount: 0,
-        runningUploads: 1,
-        inPreparation: true
-      })
+    it('should show that an upload is preparing', async () => {
+      const { wrapper } = getShallowWrapper()
+      wrapper.vm.showInfo = true
+      wrapper.vm.runningUploads = 1
+      wrapper.vm.inPreparation = true
+      await nextTick()
+
       const uploadTitle = wrapper.find(selectors.title).text()
       expect(uploadTitle).toBe('Preparing upload...')
     })
-    it('should show that an upload is being finalized', () => {
-      const { wrapper } = getShallowWrapper({
-        showInfo: true,
-        filesInProgressCount: 1,
-        runningUploads: 1,
-        inFinalization: true,
-        errors: [],
-        successful: []
-      })
+    it('should show that an upload is being finalized', async () => {
+      const { wrapper } = getShallowWrapper()
+      wrapper.vm.showInfo = true
+      wrapper.vm.filesInProgressCount = 1
+      wrapper.vm.runningUploads = 1
+      wrapper.vm.inFinalization = true
+      await nextTick()
+
       const uploadTitle = wrapper.find(selectors.title).text()
       expect(uploadTitle).toBe('Finalizing upload...')
     })
   })
   describe('progress bar', () => {
-    it('should show the progress bar when an upload is in progress', () => {
-      const { wrapper } = getShallowWrapper({
-        showInfo: true,
-        filesInProgressCount: 1,
-        runningUploads: 1
-      })
+    it('should show the progress bar when an upload is in progress', async () => {
+      const { wrapper } = getShallowWrapper()
+      wrapper.vm.showInfo = true
+      wrapper.vm.filesInProgressCount = 1
+      wrapper.vm.runningUploads = 1
+      await nextTick()
+
       const progressBar = wrapper.find(selectors.progress)
       expect(progressBar.exists()).toBeTruthy()
     })
   })
   describe('info', () => {
-    it('should show the number of successful items', () => {
-      const { wrapper } = getShallowWrapper({
-        showInfo: true,
-        filesInProgressCount: 0,
-        runningUploads: 0,
-        errors: {},
-        successful: ['1', '2']
-      })
+    it('should show the number of successful items', async () => {
+      const { wrapper } = getShallowWrapper()
+      wrapper.vm.showInfo = true
+      wrapper.vm.successful = ['1', '2']
+      await nextTick()
+
       const info = wrapper.find(selectors.success).text()
       expect(info).toBe('2 items uploaded')
     })
-    it('should show the number of failed items', () => {
-      const { wrapper } = getShallowWrapper({
-        showInfo: true,
-        filesInProgressCount: 0,
-        runningUploads: 0,
-        errors: [{ name: 'file', type: 'file' }],
-        successful: ['1']
-      })
+    it('should show the number of failed items', async () => {
+      const { wrapper } = getShallowWrapper()
+      wrapper.vm.showInfo = true
+      wrapper.vm.errors = [{ name: 'file', type: 'file' }]
+      wrapper.vm.successful = ['1']
+      await nextTick()
+
       const info = wrapper.find(selectors.error).text()
       expect(info).toBe('1 of 2 items failed')
     })
   })
   describe('details', () => {
-    it('should hide the info by default', () => {
-      const { wrapper } = getShallowWrapper({
-        showInfo: true
-      })
+    it('should hide the info by default', async () => {
+      const { wrapper } = getShallowWrapper()
+      wrapper.vm.showInfo = true
+      await nextTick()
+
       const info = wrapper.find(selectors.info.items)
       expect(info.exists()).toBeFalsy()
     })
-    it('should list all the uploaded files when the info is displayed', () => {
-      const { wrapper } = getShallowWrapper({
-        showInfo: true,
-        infoExpanded: true,
-        uploads: [
-          { name: 'file', type: 'file', meta: { uploadId: '1' } },
-          { name: 'file2', type: 'file', meta: { uploadId: '2' } }
-        ]
-      })
+    it('should list all the uploaded files when the info is displayed', async () => {
+      const { wrapper } = getShallowWrapper()
+      wrapper.vm.showInfo = true
+      wrapper.vm.infoExpanded = true
+      wrapper.vm.uploads = [
+        { name: 'file', type: 'file', meta: { uploadId: '1' } },
+        { name: 'file2', type: 'file', meta: { uploadId: '2' } }
+      ]
+      await nextTick()
+
       const info = wrapper.find(selectors.info.items)
       expect(info.exists()).toBeTruthy()
 
       const uploadedItems = wrapper.findAll(selectors.info.item)
       expect(uploadedItems.length).toBe(2)
     })
-    it('should show a message on the failed uploaded files', () => {
-      const { wrapper } = getShallowWrapper({
-        showInfo: true,
-        infoExpanded: true,
-        uploads: [
-          { name: 'file', type: 'file', meta: { uploadId: '1' } },
-          { name: 'file2', type: 'file', meta: { uploadId: '2' } },
-          { name: 'file3', type: 'file', meta: { uploadId: '3' } }
-        ],
-        errors: {
+    it('should show a message on the failed uploaded files', async () => {
+      const { wrapper } = getShallowWrapper()
+      wrapper.vm.showInfo = true
+      wrapper.vm.infoExpanded = true
+      ;(wrapper.vm.uploads = [
+        { name: 'file', type: 'file', meta: { uploadId: '1' } },
+        { name: 'file2', type: 'file', meta: { uploadId: '2' } },
+        { name: 'file3', type: 'file', meta: { uploadId: '3' } }
+      ]),
+        (wrapper.vm.errors = {
           1: new Error(),
           2: new Error()
-        }
-      })
+        })
+      await nextTick()
+
       const info = wrapper.find(selectors.info.items)
       expect(info.exists()).toBeTruthy()
 
@@ -173,21 +169,21 @@ describe('UploadInfo component', () => {
       expect(infoMessages.at(0).text()).toBe('Unknown error')
       expect(infoMessages.at(0).text()).toBe('Unknown error')
     })
-    it('folder is clickable', () => {
-      const { wrapper } = getShallowWrapper({
-        showInfo: true,
-        infoExpanded: true,
-        uploads: [
-          {
-            name: 'file',
-            type: 'folder',
-            isFolder: true,
-            targetRoute: { params: { driveAliasAndItem: 'some/drive/alias' } },
-            path: '',
-            meta: { uploadId: '1' }
-          }
-        ]
-      })
+    it('folder is clickable', async () => {
+      const { wrapper } = getShallowWrapper()
+      wrapper.vm.showInfo = true
+      wrapper.vm.infoExpanded = true
+      wrapper.vm.uploads = [
+        {
+          name: 'file',
+          type: 'folder',
+          isFolder: true,
+          targetRoute: { params: { driveAliasAndItem: 'some/drive/alias' } },
+          path: '',
+          meta: { uploadId: '1' }
+        }
+      ]
+      await nextTick()
 
       const info = wrapper.find(selectors.info.items)
       expect(info.exists()).toBeTruthy()
@@ -211,18 +207,7 @@ describe('UploadInfo component', () => {
   })
 })
 
-function getShallowWrapper({
-  showInfo = false,
-  infoExpanded = false,
-  uploads = [],
-  filesInProgressCount = 0,
-  runningUploads = 0,
-  successful = [],
-  errors = {},
-  uploadsCancelled = false,
-  inPreparation = false,
-  inFinalization = false
-} = {}) {
+function getShallowWrapper() {
   const mocks = {
     ...defaultComponentMocks()
   }
@@ -230,20 +215,6 @@ function getShallowWrapper({
   return {
     mocks,
     wrapper: shallowMount(UploadInfo, {
-      data() {
-        return {
-          showInfo,
-          infoExpanded,
-          uploads,
-          filesInProgressCount,
-          runningUploads,
-          successful,
-          errors,
-          uploadsCancelled,
-          inPreparation,
-          inFinalization
-        }
-      },
       global: {
         plugins: [...defaultPlugins()],
         mocks
