3c9b170fb1ade8e5a9f24e9e66aa522af50b11e9
Saw-jan <saw.jan.grg3e@gmail.com>
2024-03-06 17:37:58 +0545

    test(e2e): options to run suites or feature file

--- tests/e2e/run-e2e.sh
+++ tests/e2e/run-e2e.sh
@@ -3,24 +3,36 @@
 SCRIPT_PATH=$(dirname "$0")
 SCRIPT_PATH=$(cd "${SCRIPT_PATH}" && pwd) # normalized and made absolute
 FEATURES_DIR="${SCRIPT_PATH}/cucumber/features"
-PROJECT_ROOT="$SCRIPT_PATH/../../"
+PROJECT_ROOT=$(cd "$SCRIPT_PATH/../../" && pwd)
+E2E_COMMAND="pnpm test:e2e:cucumber"
 
-ALL_SUITES=$(find "${FEATURES_DIR}"/ -type d | sort | rev | cut -d"/" -f1,2 | rev | grep -v '^features/$')
+ALL_SUITES=""
+FEATURE_FILE=""
 
+SKIP_RUN_PARTS=true
 EXCLUDE_SUITES=""
 RUN_PART=""
 TOTAL_PARTS=""
 
-# cli options:
-# --suites      - suites to run. Comma separated values
-#                 e.g.: --suites smoke,shares
-# --exclude     - exclude suites from running. Comma separated values
-#                 e.g.: --exclude spaces,search
-# --run-part    - part to run: <part>/<total-parts>
-#                 e.g.: --run-part 2/4 (runs part 2 out of 4)
+HELP_COMMAND="
+Available options:
+    --feature   - feature file to run
+                  e.g.: --feature tests/e2e/cucumber/features/journeys/kindergarten.feature
+    --suites    - suites to run. Comma separated values (folder names)
+                  e.g.: --suites smoke,shares
+    --exclude   - exclude suites from running. Comma separated values
+                  e.g.: --exclude spaces,search
+    --run-part  - part to run: <part>/<total-parts>
+                  e.g.: --run-part 2/4 (runs part 2 out of 4)
+"
+
 while [[ $# -gt 0 ]]; do
     key="$1"
     case ${key} in
+    --feature)
+        FEATURE_FILE=$2
+        shift 2
+        ;;
     --exclude)
         EXCLUDE_SUITES=${2//,/ }
         shift 2
@@ -30,6 +42,7 @@ while [[ $# -gt 0 ]]; do
         shift 2
         ;;
     --run-part)
+        SKIP_RUN_PARTS=false
         parts=${2/\// }
         # shellcheck disable=SC2206
         parts=($parts)
@@ -38,45 +51,59 @@ while [[ $# -gt 0 ]]; do
 
         shift 2
         ;;
+    --help | -h)
+        echo "$HELP_COMMAND"
+        exit 0
+        ;;
+    *)
+        echo "ERR: Unknown option '$1'"
+        echo "$HELP_COMMAND"
+        exit 1
+        ;;
     esac
 done
 
-if [[ -z "$ALL_SUITES" ]]; then
-    echo "ERR: No suites provided with '--suites' option"
-    echo "USAGE: --suites suite1,suite2"
-    exit 1
-fi
+if [[ -n "$FEATURE_FILE" ]]; then
+    E2E_COMMAND+=" $FEATURE_FILE"
+elif [[ "$ALL_SUITES" == "" ]]; then
+    ALL_SUITES=$(find "${FEATURES_DIR}"/ -type d | sort | rev | cut -d"/" -f1 | rev | xargs)
 
-if [[ -n "$EXCLUDE_SUITES" ]]; then
-    for suite in $ALL_SUITES; do
+    if [[ -n "$EXCLUDE_SUITES" ]]; then
         for exclude_suite in $EXCLUDE_SUITES; do
-            if [[ "$suite" == "$exclude_suite" ]]; then
-                ALL_SUITES="${ALL_SUITES/$suite/}"
-            fi
+            ALL_SUITES=${ALL_SUITES/$exclude_suite/}
         done
-    done
-fi
-
-if [[ -n $RUN_PART && -n $TOTAL_PARTS ]]; then
-    ALL_SUITES_COUNT=$(echo "${ALL_SUITES}" | wc -l)
-    SUITES_PER_RUN=$((ALL_SUITES_COUNT / TOTAL_PARTS))
-    REMAINING_SUITES=$((ALL_SUITES_COUNT - (TOTAL_PARTS * SUITES_PER_RUN)))
-
-    if [[ ${RUN_PART} -le ${REMAINING_SUITES} ]]; then
-        SUITES_PER_RUN=$((SUITES_PER_RUN + 1))
-        PREVIOUS_INDEX=$(((RUN_PART - 1) * SUITES_PER_RUN))
-    else
-        MAX=$((REMAINING_SUITES * (SUITES_PER_RUN + 1)))
-        REM=$((((RUN_PART - 1) - REMAINING_SUITES) * SUITES_PER_RUN))
-        PREVIOUS_INDEX=$((MAX + REM))
     fi
 
-    SUITES_UPTO=$((SUITES_PER_RUN + PREVIOUS_INDEX))
+    # build suites for running in parts
+    if [[ "$SKIP_RUN_PARTS" != true && -n $RUN_PART && -n $TOTAL_PARTS ]]; then
+        ALL_SUITES_COUNT=$(echo "${ALL_SUITES}" | wc -l)
+        SUITES_PER_RUN=$((ALL_SUITES_COUNT / TOTAL_PARTS))
+        REMAINING_SUITES=$((ALL_SUITES_COUNT - (TOTAL_PARTS * SUITES_PER_RUN)))
 
-    # shellcheck disable=SC2207
-    SUITES_TO_RUN+=($(echo "${ALL_SUITES}" | head -n "$SUITES_UPTO" | tail -n "$SUITES_PER_RUN"))
-    echo "${SUITES_TO_RUN[@]}"
+        if [[ ${RUN_PART} -le ${REMAINING_SUITES} ]]; then
+            SUITES_PER_RUN=$((SUITES_PER_RUN + 1))
+            PREVIOUS_INDEX=$(((RUN_PART - 1) * SUITES_PER_RUN))
+        else
+            MAX=$((REMAINING_SUITES * (SUITES_PER_RUN + 1)))
+            REM=$((((RUN_PART - 1) - REMAINING_SUITES) * SUITES_PER_RUN))
+            PREVIOUS_INDEX=$((MAX + REM))
+        fi
+
+        SUITES_UPTO=$((SUITES_PER_RUN + PREVIOUS_INDEX))
+
+        # shellcheck disable=SC2207
+        SUITES_TO_RUN+=($(echo "${ALL_SUITES}" | head -n "$SUITES_UPTO" | tail -n "$SUITES_PER_RUN"))
+        echo "${SUITES_TO_RUN[@]}"
+    fi
+
+    # build suites command
+    suites=$(echo "$ALL_SUITES" | xargs | sed -E "s/( )+/,/g")
+    E2E_COMMAND+=" $FEATURES_DIR/{$suites}/*.feature"
 fi
 
 # SUITES_GLOB="$FEATURES_DIR/{${SUITES_TO_RUN[@]}}/*.feature"
-echo $ALL_SUITES_COUNT
+# echo $ALL_SUITES_COUNT
+
+# run e2e test
+cd "$PROJECT_ROOT" || exit 1
+$E2E_COMMAND
