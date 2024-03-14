37afeb45651bb6f325942799c62d0fd619f6bb92
Jan Ackermann <jan.alexander.ackermann@outlook.com>
2024-03-14 18:52:45 +0100

    Lint

--- packages/web-app-admin-settings/src/components/General/SideBar/DetailsPanel.vue
+++ packages/web-app-admin-settings/src/components/General/SideBar/DetailsPanel.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="oc-trash-no-selection" class="oc-text-center oc-mt-xl">
     <oc-icon size="xxlarge" name="settings-4" fill-type="fill" />
-    <p v-text="$gettext('Select a resource from the left sidebar to manage it')"/>
+    <p v-text="$gettext('Select a resource from the left sidebar to manage it')" />
   </div>
 </template>
 <script lang="ts">
