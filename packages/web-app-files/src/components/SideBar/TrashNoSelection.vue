de8620b8b75fac0973339f9ac4bda423cf8d50ab
Jan Ackermann <jan.alexander.ackermann@outlook.com>
2024-03-14 16:33:35 +0100

    Add details panel for trash overview

---
+++ packages/web-app-files/src/components/SideBar/TrashNoSelection.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="oc-trash-no-selection" class="oc-text-center oc-mt-xl">
+    <oc-icon size="xxlarge" name="delete-bin" fill-type="line" />
+    <p data-testid="selectedTrashText" v-text="selectTrashString" />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'NoSelection',
+  computed: {
+    selectTrashString() {
+      return this.$gettext('Select a trash to view details.')
+    }
+  }
+})
+</script>
