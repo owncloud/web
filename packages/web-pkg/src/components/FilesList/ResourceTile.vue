4bcb737d4fb40724e29f168ccbc8626cee6dc10f
Jan Ackermann <jan.alexander.ackermann@outlook.com>
2024-04-05 12:44:53 +0200

    Add changelog item

--- packages/web-pkg/src/components/FilesList/ResourceTile.vue
+++ packages/web-pkg/src/components/FilesList/ResourceTile.vue
@@ -164,7 +164,7 @@ export default defineComponent({
     })
 
     const shouldDisplayThumbnails = (resource: Resource) => {
-      return !isResourceTxtFileAlmostEmpty(resource)
+      return resource.thumbnail && !isResourceTxtFileAlmostEmpty(resource)
     }
 
     return {
