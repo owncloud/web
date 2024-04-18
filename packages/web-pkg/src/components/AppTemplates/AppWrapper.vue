e98d2d5f29f78402a672c9fb84538d7fc0331723
Pascal Wengerter <pascal@wengerter.info>
2024-04-18 10:06:14 +0200

    Fix linting

--- packages/web-pkg/src/components/AppTemplates/AppWrapper.vue
+++ packages/web-pkg/src/components/AppTemplates/AppWrapper.vue
@@ -383,12 +383,15 @@ export default defineComponent({
       }
       const editorOptions = configStore.options.editor
       if (editorOptions.autosaveEnabled) {
-        autosaveIntervalId = setInterval(async () => {
-          if (isDirty.value) {
-            await save()
-            autosavePopup()
-          }
-        }, (editorOptions.autosaveInterval || 120) * 1000)
+        autosaveIntervalId = setInterval(
+          async () => {
+            if (isDirty.value) {
+              await save()
+              autosavePopup()
+            }
+          },
+          (editorOptions.autosaveInterval || 120) * 1000
+        )
       }
     })
     onBeforeUnmount(() => {
