c85e3c6541c8479cff677a8349ed1c1cb74d63cd
Prajwol Amatya <prajwolamatya11@gmail.com>
2024-05-15 15:47:35 +0545

    wait for element to be visible on navigating to space

--- tests/e2e/support/objects/app-files/spaces/actions.ts
+++ tests/e2e/support/objects/app-files/spaces/actions.ts
@@ -88,6 +88,7 @@ export interface openSpaceArgs {
 export const openSpace = async (args: openSpaceArgs): Promise<void> => {
   const { page, id } = args
   await page.locator(util.format(spaceIdSelector, id)).click()
+  await page.locator('#files-space-table').waitFor()
 }
 /**/
 
