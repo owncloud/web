4274b1b2e4d48af4edb3bba00a5277daa81190f9
Viktor Scharf <scharf.vi@gmail.com>
2024-05-16 14:19:43 +0200

    remove old acceptance tests

--- tests/acceptance/customCommands/clickElementAt.js
+++
@@ -1,3 +0,0 @@
-exports.command = function clickElementAt(selector, x, y, mouseButton = 0) {
-  return this.moveToElement(selector, x, y).mouseButtonClick(mouseButton)
-}
--- tests/acceptance/stepDefinitions/debugContext.js
+++
@@ -1,5 +0,0 @@
-const { When } = require('@cucumber/cucumber')
-
-When('pause for {int}', async function (duration) {
-  await new Promise((resolve) => setTimeout(resolve, duration))
-})
