97796058ba854f4c8a6d683bb764047679790f6e
Benedikt Kulmann <benedikt@kulmann.biz>
2024-06-10 11:39:11 +0200

    fix: use gettext config for l10n-write

--- gettext.config.cjs
+++ gettext.config.cjs
@@ -4,9 +4,10 @@ module.exports = {
     include: ['**/*.js', '**/*.ts', '**/*.vue']
   },
   output: {
-    locales: ['bg', 'cs', 'de', 'en', 'es', 'fr', 'it', 'nl', 'ko', 'ru', 'sq', 'sv', 'tr', 'zh'],
+    locales: ['bg', 'cs', 'de', 'es', 'fr', 'it', 'nl', 'ko', 'ru', 'sq', 'sv', 'tr', 'zh'],
     path: './l10n/locale',
     potPath: '../template.pot',
+    jsonPath: "../translations.json",
     flat: false,
     linguas: false
   }
