4cd0a150b2baa1edfac3f69b08c92c1a2fbaa311
Alex <jan.alexander.ackermann@outlook.com>
2024-06-13 10:38:18 +0200

    Show artist and title in audio preview app (#11038)
    * Show artist and title in audio preview app
    
    * Simplify
    
    * Beautify stuff

--- packages/web-app-preview/src/components/Sources/MediaAudio.vue
+++ packages/web-app-preview/src/components/Sources/MediaAudio.vue
@@ -1,11 +1,15 @@
 <template>
-  <audio :key="`media-audio-${file.id}`" controls preload="preload" :autoplay="isAutoPlayEnabled">
-    <source :src="file.url" :type="file.mimeType" />
-  </audio>
+  <div class="audio-container oc-flex oc-flex-column">
+    <audio :key="`media-audio-${file.id}`" controls preload="preload" :autoplay="isAutoPlayEnabled">
+      <source :src="file.url" :type="file.mimeType" />
+    </audio>
+    <p class="oc-text-muted oc-text-small" v-if="audioText" v-text="audioText"></p>
+  </div>
 </template>
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { computed, defineComponent, PropType } from 'vue'
 import { CachedFile } from '../../helpers/types'
+import { Resource } from '@ownclouders/web-client'
 
 export default defineComponent({
   name: 'MediaAudio',
@@ -14,10 +18,29 @@ export default defineComponent({
       type: Object as PropType<CachedFile>,
       required: true
     },
+    resource: {
+      type: Object as PropType<Resource>,
+      required: true
+    },
     isAutoPlayEnabled: {
       type: Boolean,
       default: true
     }
+  },
+  setup(props) {
+    const audioText = computed(() => {
+      if (props.resource.audio?.artist && props.resource.audio?.title) {
+        return `${props.resource.audio.artist} - ${props.resource.audio.title}`
+      }
+      return ''
+    })
+
+    return { audioText }
   }
 })
 </script>
+<style lang="scss" scoped>
+.audio-container {
+  width: 300px;
+}
+</style>
