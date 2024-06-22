a1a2199eba40561ad266b6c03085bc9473aa6ac5
Jan Ackermann <jan.alexander.ackermann@outlook.com>
2024-06-22 23:15:45 +0200

    fix iframe title

--- packages/web-pkg/src/components/Modals/FilePickerModal.vue
+++ packages/web-pkg/src/components/Modals/FilePickerModal.vue
@@ -44,7 +44,7 @@ export default defineComponent({
       e.extension ? e.extension : e.mimeType
     )
 
-    const iframeTitle = themeStore.currentTheme.name
+    const iframeTitle = themeStore.currentTheme.common?.name
     const iframeUrl = new URL(parentFolderRoute.href, window.location.origin)
     iframeUrl.searchParams.append('embed', 'true')
     iframeUrl.searchParams.append('embed-target', 'file')
