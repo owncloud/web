2c11012245bf6d44c3c262b75de6c9b1a9e73858
Jan Ackermann <jan.alexander.ackermann@outlook.com>
2024-08-07 20:39:00 +0200

    fix e2e

--- tests/e2e/support/objects/app-files/resource/actions.ts
+++ tests/e2e/support/objects/app-files/resource/actions.ts
@@ -62,7 +62,7 @@ const actionSkipButton = '.oc-modal-body-actions-cancel'
 const actionSecondaryConfirmationButton = '.oc-modal-body-actions-secondary'
 const versionRevertButton = '//*[@data-testid="file-versions-revert-button"]'
 const sideBarActionButton =
-  '//div[contains(@class, "files-side-bar")]//*[contains(@data-testid, "action-handler")]/span[text()="%s"]'
+  '//div[contains(@class, "files-side-bar")]//*[contains(@data-testid, "action-handler")]//span[text()="%s"]'
 const notificationMessageDialog = '.oc-notification-message-title'
 const notificationMessage = '.oc-notification-message'
 const permanentDeleteButton = '.oc-files-actions-delete-permanent-trigger'
