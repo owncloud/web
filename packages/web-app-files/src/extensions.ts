6dd15ac576297aecddb0cf83b291cc0a155a1d89
Benedikt Kulmann <benedikt@kulmann.biz>
2024-09-04 22:22:50 +0200

    Add default actions extension point (#11515)
    * feat: introduce default actions extension point

--- packages/web-app-files/src/extensions.ts
+++ packages/web-app-files/src/extensions.ts
@@ -3,17 +3,15 @@ import {
   Extension,
   useCapabilityStore,
   useConfigStore,
-  useFileActionsCopyQuickLink,
-  useFileActionsShowShares,
   useRouter,
   useSearch,
   useUserStore
 } from '@ownclouders/web-pkg'
-import { computed, unref } from 'vue'
+import { computed } from 'vue'
 import { SDKSearch } from './search'
 import { useSideBarPanels } from './composables/extensions/useFileSideBars'
 import { useFolderViews } from './composables/extensions/useFolderViews'
-import { quickActionsExtensionPoint } from './extensionPoints'
+import { useFileActions } from './composables/extensions/useFileActions'
 import { urlJoin } from '@ownclouders/web-client'
 
 export const extensions = (appInfo: ApplicationInformation) => {
@@ -23,13 +21,12 @@ export const extensions = (appInfo: ApplicationInformation) => {
   const router = useRouter()
   const { search: searchFunction } = useSearch()
 
-  const { actions: showSharesActions } = useFileActionsShowShares()
-  const { actions: quickLinkActions } = useFileActionsCopyQuickLink()
-
+  const fileActionExtensions = useFileActions()
   const folderViewExtensions = useFolderViews()
   const sideBarPanelExtensions = useSideBarPanels()
 
   return computed<Extension[]>(() => [
+    ...fileActionExtensions,
     ...folderViewExtensions,
     ...sideBarPanelExtensions,
     {
@@ -37,18 +34,6 @@ export const extensions = (appInfo: ApplicationInformation) => {
       extensionPointIds: ['app.search.provider'],
       type: 'search',
       searchProvider: new SDKSearch(capabilityStore, router, searchFunction, configStore)
-    },
-    {
-      id: 'com.github.owncloud.web.files.quick-action.collaborator',
-      extensionPointIds: [quickActionsExtensionPoint.id],
-      type: 'action',
-      action: unref(showSharesActions)[0]
-    },
-    {
-      id: 'com.github.owncloud.web.files.quick-action.quicklink',
-      extensionPointIds: [quickActionsExtensionPoint.id],
-      type: 'action',
-      action: unref(quickLinkActions)[0]
     },
     ...((userStore.user && [
       {
