21ac91069b1cce62d447fa399ceaffef6a30eeac
Benedikt Kulmann <benedikt@kulmann.biz>
2024-10-07 14:08:29 +0200

    test: unit tests for AppAuthors in app store

--- packages/web-app-app-store/src/components/AppAuthors.vue
+++ packages/web-app-app-store/src/components/AppAuthors.vue
@@ -1,15 +1,18 @@
 <template>
   <ul class="oc-mb-rm oc-p-rm">
-    <li v-for="author in app.authors" :key="author.name" class="app-author-item">
-      <a v-if="author.url" :href="author.url" target="_blank">{{ author.name }}</a>
-      <span v-else>{{ author.name }}</span>
+    <li v-for="author in authors" :key="author.name" class="app-author-item">
+      <a v-if="author.url" :href="author.url" data-testid="author-link" target="_blank">
+        {{ author.name }}
+      </a>
+      <span v-else data-testid="author-label">{{ author.name }}</span>
     </li>
   </ul>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { computed, defineComponent, PropType } from 'vue'
 import { App } from '../types'
+import { isEmpty } from 'lodash-es'
 
 export default defineComponent({
   props: {
@@ -17,6 +20,26 @@ export default defineComponent({
       type: Object as PropType<App>,
       required: true,
       default: (): App => undefined
+    }
+  },
+  setup(props) {
+    const authors = computed(() => {
+      return (props.app.authors || []).filter((author) => {
+        if (isEmpty(author.name)) {
+          return false
+        }
+        if (!isEmpty(author.url)) {
+          try {
+            new URL(author.url)
+          } catch {
+            return false
+          }
+        }
+        return true
+      })
+    })
+    return {
+      authors
     }
   }
 })
