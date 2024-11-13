d34444d4d1cd5b8c952be26b42a8104e2d1e06c1
Jannik Stehle <jannik.stehle@kiteworks.com>
2024-11-13 13:09:07 +0100

    fix: file picker when embed delegate authentication is enabled
    Fixes the "Open" and "Save as" actions when the server has enabled `embed.delegateAuthentication`. This flag needs to be ignored for these 2 actions since Web is basically embedding itself and always needs authentication.

--- packages/web-pkg/src/components/Modals/SaveAsModal.vue
+++ packages/web-pkg/src/components/Modals/SaveAsModal.vue
@@ -63,6 +63,7 @@ export default defineComponent({
     iframeUrl.searchParams.append('embed', 'true')
     iframeUrl.searchParams.append('embed-target', 'location')
     iframeUrl.searchParams.append('embed-choose-file-name', 'true')
+    iframeUrl.searchParams.append('embed-delegate-authentication', 'false')
     iframeUrl.searchParams.append('embed-choose-file-name-suggestion', props.originalResource.name)
 
     const onLoad = () => {
