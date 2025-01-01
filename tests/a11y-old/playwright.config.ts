f0dcf418e9171befdb0dc5d796c9383c883fc890
Kathrin Koebel <kathrin@koebel.ch>
2025-01-01 15:51:38 +0545

    move tests into e2e folder

--- tests/a11y-old/playwright.config.ts
+++ tests/a11y-old/playwright.config.ts
@@ -4,7 +4,11 @@ export default defineConfig({
   testDir: '.',
   testMatch: 'a11y/**/*.spec.ts',
   use: {
-    baseURL: 'https://host.docker.internal:9200'
+    baseURL: 'https://host.docker.internal:9200',
+    personal: '/files/spaces/personal',
+    shares: '/files/shares/with-me',
+    spaces: '/files/spaces/projects',
+    deleted: '/files/trash/personal'
   },
   projects: [
     {
