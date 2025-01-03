bab9d4f3517f6276bf01ab55e6310903a02d20d7
Kathrin Koebel <kathrin@koebel.ch>
2025-01-03 17:41:56 +0545

    adding tests

--- tests/e2e/a11y/a11y.spec.ts
+++ tests/e2e/a11y/a11y.spec.ts
@@ -1,40 +1,132 @@
 import { test, expect } from './axeBuilder'
 
 test.describe('Accessibility', () => {
-  test('login page should not have any automatically detectable accessibility issues', async ({ page, baseURL, makeAxeBuilder }, testInfo ) => {
-    await page.goto(`${baseURL}`)
-    await page.fill('#oc-login-username', 'admin')
-    await page.fill('#oc-login-password', 'admin')
-    await page.click("button[type='submit']")
-    await page.locator('#files').waitFor()
+  test.describe('login page', () => {
+    test('should not have any automatically detectable accessibility issues', async ({ page, baseURL, makeAxeBuilder } ) => {
+      await page.goto(`${baseURL}`)
+      await page.locator('.oc-login-bg').waitFor()
 
-    // users settings
-    await page.goto(`${baseURL}/admin-settings/users`)
-    await page.locator('#user-list').waitFor()
+      const a11yResult = await makeAxeBuilder()
+        .include('.oc-login-form')
+        .exclude('#oc-login-username') // autocomplete attribute is incorrectly formatted
+        .exclude('#oc-login-password') // autocomplete attribute is incorrectly formatted
+        .analyze()
 
-    // https://host.docker.internal:9200/admin-settings/users
-    const a11yResult = await makeAxeBuilder()
-      .include('#user-list')
-      //excluding known issues --> they still need to be reported
-      .exclude('#users-filter-confirm')
-      .analyze()
+      expect(a11yResult.violations).toEqual([])
+    })
+  }),
+  test.describe('app header', () => {
+    test.skip('application switcher should not have any automatically detectable accessibility issues', async ({ page, baseURL, makeAxeBuilder } ) => {
+      // maybe add tests later for the follwing items in the application switcher:
+      // text editor (creating new file), app store, admin settings for users, groups, spaces management
+    }),
+    test.skip('search bar should not have any automatically detectable accessibility issues', async ({ page, baseURL, makeAxeBuilder } ) => {
+      // todo: test search preview andlist filter type
+    }),
+    test.skip('notifications should not have any automatically detectable accessibility issues', async ({ page, baseURL, makeAxeBuilder } ) => {
+    }),
+    test.skip('my account popup should not have any automatically detectable accessibility issues', async ({ page, baseURL, makeAxeBuilder } ) => {
+    }),
+    test.skip('account preferences page should not have any automatically detectable accessibility issues', async ({ page, baseURL, makeAxeBuilder } ) => {
+    })
+  }),
+  test.describe('sidebar', () => {
+    test('full sidebar should not have any automatically detectable accessibility issues', async ({ page, baseURL, makeAxeBuilder } ) => {
+      await page.goto(`${baseURL}`)
+      await page.fill('#oc-login-username', 'admin')
+      await page.fill('#oc-login-password', 'admin')
+      await page.click("button[type='submit']")
+      await page.locator('#web-nav-sidebar').waitFor()
 
-    // print violations to console
-    console.log('violation details:')
-    var count = 0
-    for (var o of a11yResult.violations) {
-      console.log(o)
-      count ++
-    }
-    console.log('total number of violations: ' + count)
+      const a11yResult = await makeAxeBuilder()
+        .include('#web-nav-sidebar')
+        .analyze()
 
-    // check output of the reports below
-    await testInfo.attach('accessibility-scan-results', {
-      body: JSON.stringify(a11yResult, null, 2),
-      contentType: 'application/json'
+      expect(a11yResult.violations).toEqual([])
+    }),
+    test('collapsed sidebar should not have any automatically detectable accessibility issues', async ({ page, baseURL, makeAxeBuilder } ) => {
+      await page.goto(`${baseURL}`)
+      await page.fill('#oc-login-username', 'admin')
+      await page.fill('#oc-login-password', 'admin')
+      await page.click("button[type='submit']")
+      await page.locator('#web-nav-sidebar').waitFor()
+      // collapse sidebar
+      await page.locator('.toggle-sidebar-button').click()
+      await page.locator('.oc-app-navigation-collapsed').waitFor()
+
+      const a11yResult = await makeAxeBuilder()
+        .include('#web-nav-sidebar')
+        .analyze()
+
+      expect(a11yResult.violations).toEqual([])
+    })
+  }),
+  test.describe('personal space page', () => {
+    // precondition for the following tests: there should be some files uploaded
+
+    test('files section in default table view should not have any automatically detectable accessibility issues', async ({ page, baseURL, makeAxeBuilder } ) => {  
+      await page.goto(`${baseURL}`)
+      await page.fill('#oc-login-username', 'admin')
+      await page.fill('#oc-login-password', 'admin')
+      await page.click("button[type='submit']")
+      await page.locator('#files').waitFor()
+
+      const a11yResult = await makeAxeBuilder()
+        .include('#files') //
+        .exclude('.resource-table-edit-name') // buttons must have discernible text
+        .exclude('.oc-resource-icon-link') // buttons/links must have discernible text 
+        .analyze()
+
+      expect(a11yResult.violations).toEqual([])
+    }),
+    test('files section in condensed table view should not have any automatically detectable accessibility issues', async ({ page, baseURL, makeAxeBuilder } ) => {
+      await page.goto(`${baseURL}`)
+      await page.fill('#oc-login-username', 'admin')
+      await page.fill('#oc-login-password', 'admin')
+      await page.click("button[type='submit']")
+      await page.locator('#web-content-main').waitFor()
+      // switch view
+      await page.locator('.resource-table-condensed').click()
+      await page.locator('#files-space-table.condensed').waitFor()
+
+      const a11yResult = await makeAxeBuilder()
+        .include('#files-space-table') //
+        .exclude('.resource-table-edit-name') // buttons must have discernible text
+        .exclude('.oc-resource-icon-link') // buttons/links must have discernible text 
+        .analyze()
+
+      expect(a11yResult.violations).toEqual([])
+    }),
+    test('files section in tiles view should not have any automatically detectable accessibility issues', async ({ page, baseURL, makeAxeBuilder } ) => {
+      await page.goto(`${baseURL}`)
+      await page.fill('#oc-login-username', 'admin')
+      await page.fill('#oc-login-password', 'admin')
+      await page.click("button[type='submit']")
+      await page.locator('#web-content-main').waitFor()
+      // switch view
+      await page.locator('.resource-tiles').click() 
+      await page.locator('.oc-tiles-controls').waitFor()
+
+      const a11yResult = await makeAxeBuilder()
+        .include('#tiles-view') //
+        //.exclude('id or class') // issue?
+        .analyze()
+
+      // print violations to console
+      console.log('violation details:')
+      var count = 0
+      for (var o of a11yResult.violations) {
+        console.log(o)
+        count ++
+      }
+      console.log('total number of violations: ' + count)
+
+      expect(a11yResult.violations).not.toEqual([])
     })
 
-    expect(a11yResult.violations).toEqual([])
+    // test uploading files .files-app-bar-actions #new-file-menu-btn (for new file), #upload-menu-btn (for uploading file)
+    // tests for settings  
+
   })
 })
 
