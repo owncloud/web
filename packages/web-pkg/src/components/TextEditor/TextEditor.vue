93e0b2f14cf67324e8658e9c28f69159f4f9b9fe
Lukas Hirt <info@hirt.cz>
2025-02-04 12:04:06 +0100

    feat: replace toastui editor with md editor
    The original lib used in the text editor component is having CVE but is no longer maintained. Since we do not wish to maintain a fork of the lib, we decided to replace it. This brings in https://github.com/imzbf/md-editor-v3
    
    Co-authored-by: mzner <mzner@users.noreply.github.com>

--- packages/web-pkg/src/components/TextEditor/TextEditor.vue
+++ packages/web-pkg/src/components/TextEditor/TextEditor.vue
@@ -1,25 +1,44 @@
 <template>
-  <app-loading-spinner v-if="loading" />
-  <div v-else id="text-editor-container" ref="toastUiEditorRef" :data-markdown-mode="isMarkdown" />
+  <div id="text-editor-container" class="oc-height-1-1">
+    <md-preview
+      v-if="isReadOnly"
+      id="space-description-preview"
+      :model-value="currentContent"
+      :language="languages[language.current] || 'en-US'"
+      :theme="theme"
+      read-only
+      :toolbars="[]"
+    />
+    <md-editor
+      v-else
+      id="text-editor-component"
+      :model-value="currentContent"
+      :language="languages[language.current] || 'en-US'"
+      :theme="theme"
+      :preview="isMarkdown"
+      :toolbars="isMarkdown ? undefined : []"
+      :read-only="isReadOnly"
+      @on-change="(value) => $emit('update:currentContent', value)"
+    />
+  </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, unref, PropType, ref, onMounted, nextTick } from 'vue'
+import { computed, defineComponent, unref, PropType } from 'vue'
 import { Resource } from '@ownclouders/web-client'
 
-import '@toast-ui/editor/dist/toastui-editor.css'
-import '@toast-ui/editor/dist/theme/toastui-editor-dark.css'
+import { config, MdEditor, MdPreview } from 'md-editor-v3'
+import 'md-editor-v3/lib/style.css'
 
-// @ts-ignore
-import { Editor, EditorCore, EditorOptions } from '@toast-ui/editor'
+import { languageUserDefined, languages } from './l18n'
+
 import { useGettext } from 'vue3-gettext'
 import { useThemeStore } from '../../composables'
 import { AppConfigObject } from '../../apps'
-import AppLoadingSpinner from './../AppLoadingSpinner.vue'
 
 export default defineComponent({
   name: 'TextEditor',
-  components: { AppLoadingSpinner },
+  components: { MdEditor, MdPreview },
   props: {
     applicationConfig: { type: Object as PropType<AppConfigObject>, required: false },
     currentContent: {
@@ -31,15 +50,12 @@ export default defineComponent({
     resource: { type: Object as PropType<Resource>, required: false }
   },
   emits: ['update:currentContent'],
-  setup(props, { emit }) {
+  setup(props) {
     const language = useGettext()
-    const themeStore = useThemeStore()
-    const toastUiEditorRef = ref<HTMLElement>()
-    const loading = ref(true)
+    const { currentTheme } = useThemeStore()
 
     // Should not be a ref, otherwise functions like setMarkdown won't work
-    let toastUiEditor: EditorCore = null
-    const config = computed(() => {
+    const editorConfig = computed(() => {
       // TODO: Remove typecasting once vue-tsc has figured it out
       const { showPreviewOnlyMd = true } = props.applicationConfig as AppConfigObject
       return { showPreviewOnlyMd }
@@ -49,102 +65,52 @@ export default defineComponent({
       return (
         props.markdownMode ||
         ['md', 'markdown'].includes(props.resource?.extension) ||
-        !unref(config).showPreviewOnlyMd
+        !unref(editorConfig).showPreviewOnlyMd
       )
     })
 
-    const keyDownHandler = (event: KeyboardEvent, editor: EditorCore) => {
-      const ctrl = event.ctrlKey || event.metaKey
-      if (!ctrl) {
-        return
+    const theme = computed(() => (unref(currentTheme).isDark ? 'dark' : 'light'))
+
+    config({
+      editorConfig: {
+        languageUserDefined
       }
-
-      switch (event.key) {
-        case 'y':
-          editor.exec('redo')
-          break
-        case 'z':
-          editor.exec('undo')
-          break
-      }
-    }
-
-    const loadSyntaxHighlighting = async () => {
-      const [plugin] = await Promise.all([
-        import(
-          `@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all.js`
-        ),
-        import(
-          // @ts-ignore
-          `@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css`
-        ),
-        // @ts-ignore
-        import('prismjs/themes/prism.css')
-      ])
-
-      return plugin.default
-    }
-
-    onMounted(async () => {
-      let codeSyntaxHighlight: unknown
-      if (unref(isMarkdown)) {
-        codeSyntaxHighlight = await loadSyntaxHighlighting()
-
-        if (!props.isReadOnly) {
-          await import('./l18n')
-        }
-      }
-
-      loading.value = false
-      await nextTick()
-
-      let config: EditorOptions = {
-        el: unref(toastUiEditorRef),
-        usageStatistics: false, // sends hostname to google analytics DISABLE
-        initialValue: props.currentContent,
-        useCommandShortcut: false,
-        hideModeSwitch: true,
-        language: language.current,
-        height: '100%',
-        plugins: [...((codeSyntaxHighlight && [codeSyntaxHighlight]) || [])],
-        viewer: props.isReadOnly,
-        events: {
-          change: () => {
-            emit('update:currentContent', toastUiEditor.getMarkdown())
-          },
-          keydown: (_mode: string, event: KeyboardEvent) => {
-            keyDownHandler(event, toastUiEditor)
-          }
-        },
-        ...(themeStore.currentTheme.isDark && { theme: 'dark' })
-      }
-
-      if (!unref(isMarkdown)) {
-        config = {
-          ...config,
-          toolbarItems: [],
-          initialEditType: 'wysiwyg'
-        }
-      }
-
-      toastUiEditor = Editor.factory(config) as EditorCore
     })
 
     return {
-      toastUiEditorRef,
       isMarkdown,
-      loading
+      theme,
+      language,
+      languages
     }
   }
 })
 </script>
 <style lang="scss">
+#text-editor-component {
+  height: 100%;
+}
+
 .toastui-editor-tabs {
   // Fix tab with for long i18n text
   .tab-item {
     width: auto;
     padding-left: var(--oc-space-small);
     padding-right: var(--oc-space-small);
+  }
+}
+
+#space-description-preview {
+  background-color: transparent;
+  width: max-content;
+
+  .md-editor-preview-wrapper {
+    padding: 0;
+  }
+
+  .md-editor-preview {
+    color: var(--oc-color-text-default);
+    font-size: var(--oc-text-default);
   }
 }
 </style>
