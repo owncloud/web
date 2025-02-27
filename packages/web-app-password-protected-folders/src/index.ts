3de9d1814dcaef0e835a51575e5133e06c8bda07
Lukas Hirt <info@hirt.cz>
2025-02-27 10:08:15 +0100

    fix: disable psec creation in public links
    To prevent all kinds of issues with nested password protected folders, disable their creation inside of folders shared via public link.

--- packages/web-app-password-protected-folders/src/index.ts
+++ packages/web-app-password-protected-folders/src/index.ts
@@ -1,29 +1,20 @@
 import { useGettext } from 'vue3-gettext'
 import translations from '../l10n/translations.json'
-import {
-  defineWebApplication,
-  PASSWORD_PROTECTED_FOLDER_FILE_EXTENSION
-} from '@ownclouders/web-pkg'
+import { defineWebApplication } from '@ownclouders/web-pkg'
 import { useExtensions } from './composables/useExtensions'
-import { useCustomHandler } from './composables/useCustomHandler'
+import { useApplicationFileExtensions } from './composables/useApplicationFileExtensions'
 
 export default defineWebApplication({
   setup() {
     const { $gettext } = useGettext()
     const extensions = useExtensions()
-    const { customHandler } = useCustomHandler()
+    const applicationFileExtensions = useApplicationFileExtensions()
 
     return {
       appInfo: {
         name: $gettext('Password Protected Folders'),
         id: 'password-protected-folders',
-        extensions: [
-          {
-            newFileMenu: { menuTitle: () => $gettext('Password Protected Folder') },
-            extension: PASSWORD_PROTECTED_FOLDER_FILE_EXTENSION,
-            customHandler
-          }
-        ]
+        extensions: applicationFileExtensions
       },
       translations,
       extensions
