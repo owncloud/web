539a69664f08f11ec284fdd075ca6e8f6634a711
Lukas Hirt <info@hirt.cz>
2025-03-10 11:17:21 +0100

    chore(design-system): drop docs tokens import

--- packages/design-system/build/build-tokens.js
+++ packages/design-system/build/build-tokens.js
@@ -10,22 +10,7 @@ StyleDictionary.extend({
   parsers: [
     {
       pattern: /\.yaml$/,
-      parse: ({ contents, filePath }) => {
-        // This is a bit of a hack to prevent name collisions which would drop the tokens then
-        if (filePath.split('/').some((n) => n === 'docs')) {
-          const parsed = yaml.parse(contents)
-
-          Object.keys(parsed).forEach((k) => {
-            parsed['docs-' + k] = parsed[k]
-
-            delete parsed[k]
-          })
-
-          return parsed
-        }
-
-        return yaml.parse(contents)
-      }
+      parse: ({ contents }) => yaml.parse(contents)
     }
   ],
   source: [path.join(__dirname, '../src/tokens/**/*.yaml')],
@@ -43,11 +28,6 @@ StyleDictionary.extend({
           destination: 'ods.json',
           format: 'format/ods/json',
           filter: ({ filePath }) => filePath.includes('/ods/')
-        },
-        {
-          destination: 'docs.scss',
-          format: 'format/ods/scss',
-          filter: ({ filePath }) => filePath.includes('/docs/')
         }
       ]
     }
