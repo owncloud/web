9a8784859abbce1415a8412b3fbd4450a9055581
Lukas Hirt <info@hirt.cz>
2025-03-18 10:30:09 +0100

    chore: drop deprecated sentry option
    The logErrors option from init function of Sentry has been deprecated. The Vue integration now sets this internally.

--- packages/web-runtime/src/container/bootstrap.ts
+++ packages/web-runtime/src/container/bootstrap.ts
@@ -679,7 +679,6 @@ export const startSentry = (configStore: ConfigStore, app: App): void => {
       dsn,
       environment,
       attachProps: true,
-      logErrors: true,
       transportOptions: transportOptions as SentryConfig['transportOptions'],
       ...moreSentryOptions
     })
