455b3a7859834b741fcb4fdd9802af30ebb4d31e
Matteo <matteo.sonnenholzner@kiteworks.com>
2025-04-08 10:10:12 +0200

    chore: refactor oc-notifications component using script setup

--- packages/design-system/src/components/OcNotifications/OcNotifications.vue
+++ packages/design-system/src/components/OcNotifications/OcNotifications.vue
@@ -4,43 +4,53 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
+<script lang="ts" setup>
+import { computed } from 'vue'
 /**
- * Notifications are used to inform users about errors, warnings and as confirmations for their actions. They will automatically disappear after a certain amount of time.
+ * OcNotifications Component
  *
- * The default slot can be filled with [OcNotificationMessage](#/oC%20Components/OcNotificationMessage) elements.
+ * This component is used to display a container for notification messages. It supports different positions
+ * for displaying notifications on the screen.
  *
- * ## Accessibility
- *
- * ### Notifications for screen reader users
+ * Notifications for screen reader users
  * This component uses so called live regions in order to announce its content to screen readers once the notification appeared (this is not the normal modus operandi for screen readers, since their reading order is usually the DOM order – when the user does not take shortcuts). There are two types of live regions: `aria-live="polite"` (equivalent to `role="status"`) and `aria-live="assertive"` (equivalent to `role="alert"`). The latter directly interrupts the current output of the screen reader, the former waits until the current output is finished and reads the announcement afterwards. Since 'assertive' should be used sparingly, only `<oc-notfication-message>`'s "danger" status prop value uses `aria-live="assertive"` (and `role="alert"`). Using `aria-live` and `role="assertive|status"` simultaneously is for compatibility reasons regarding different browser and assistive technology pairings.
+ *
+ * @component
+ * @name OcNotifications
+ * @status ready
+ * @release 1.0.0
+ *
+ * @props {string} [position='default'] - The position of the notification container.
+ *                                        Possible values: 'default', 'top-left', 'top-center', 'top-right'.
+ *
+ * @slots default - Slot to include notification messages, such as OcNotificationMessage components.
+ *
+ * @computed
+ * @computed {string} classes - Dynamically computed CSS class based on the `position` prop.
+ *
+ * @example
+ * <OcNotifications position="top-right">
+ *   <OcNotificationMessage
+ *     status="success"
+ *     title="Success"
+ *     message="Your operation was successful."
+ *   />
+ * </OcNotifications>
+ *
  */
-export default defineComponent({
+
+interface Props {
+  position?: 'default' | 'top-left' | 'top-center' | 'top-right'
+}
+
+defineOptions({
   name: 'OcNotifications',
   status: 'ready',
-  release: '1.0.0',
-  props: {
-    /**
-     * Position of the notifications.
-     * Valid values are: `top-left, top-center and top-right`
-     */
-    position: {
-      type: String,
-      required: false,
-      default: 'default',
-      validator: (value: string) => {
-        return ['default', 'top-left', 'top-center', 'top-right'].includes(value)
-      }
-    }
-  },
-  computed: {
-    classes() {
-      return `oc-notification-${this.position}`
-    }
-  }
+  release: '1.0.0'
 })
+const { position = 'default' } = defineProps<Props>()
+
+const classes = computed(() => `oc-notification-${position}`)
 </script>
 
 <style lang="scss">
@@ -70,102 +80,3 @@ export default defineComponent({
   }
 }
 </style>
-
-<docs>
-```js
-  <template>
-    <section>
-      <h3 class="oc-heading-divider">
-        Notifications examples
-      </h3>
-
-      <oc-button @click="show('left')">Show left notifications</oc-button>
-      <oc-button @click="show('center')">Show center notifications</oc-button>
-      <oc-button @click="show('right')">Show right notifications</oc-button>
-
-      <oc-notifications position="top-left">
-        <oc-notification-message
-            v-for="item in messages.left"
-            :key="item.title"
-            :status="item.status"
-            :title="item.title"
-            :message="item.message"
-            :timeout="item.timeout"
-            @close="removeNotification('left', item)"
-        />
-      </oc-notifications>
-
-      <oc-notifications position="top-center">
-        <oc-notification-message
-            v-for="item in messages.center"
-            :key="item.title"
-            :status="item.status"
-            :title="item.title"
-            :message="item.message"
-            :timeout="item.timeout"
-            @close="removeNotification('center', item)"
-        />
-      </oc-notifications>
-
-      <oc-notifications position="top-right">
-        <oc-notification-message
-            v-for="(item, index) in messages.right"
-            :key="item.title"
-            :status="item.status"
-            :title="item.title"
-            :message="item.message"
-            :timeout="item.timeout"
-            @close="removeNotification('right', item)"
-        />
-      </oc-notifications>
-
-    </section>
-  </template>
-  <script>
-    export default {
-      data: () => {
-        return {
-          messages: {
-            left: [],
-            center: [],
-            right: []
-          }
-        }
-      },
-      methods: {
-        removeNotification(position, item) {
-          this.messages[position] = this.messages[position].filter(el => el !== item);
-        },
-        show(position) {
-          this.messages[position] = [
-            {
-              title: 'Default without a message',
-              status: 'passive'
-            },
-            {
-              title: 'This is a primary notification with a long title that spans more than just one line',
-              message: 'And I have a message, too.',
-              status: 'primary'
-            },
-            {
-              title: 'This action could have consequences',
-              message: 'Are you sure?',
-              status: 'warning'
-            },
-            {
-              title: 'Folder created',
-              message: 'Just another day in paradise',
-              status: 'success'
-            },
-            {
-              title: 'Upload failed',
-              message: 'Holy Shoot. Something broke',
-              status: 'danger'
-            },
-          ]
-        }
-      }
-    }
-  </script>
-```
-</docs>
