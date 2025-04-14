5ac61862fb39b90ff507fe85a3341dfd0fcea59e
Matteo <matteo.sonnenholzner@kiteworks.com>
2025-04-14 13:51:52 +0200

    bugfix: set correct html lang attribute

---
+++ changelog/unreleased/bugfix-wrong-html-lang-attribute.md
@@ -0,0 +1,6 @@
+Bugfix: Fix wrong HTML lang attribute
+
+We've added a fix to ensure that the HTML lang attribute is set correctly in the web application. This change improves accessibility and ensures that screen readers and other assistive technologies can properly interpret the language of the content.
+
+https://github.com/owncloud/web/pull/12478
+https://github.com/owncloud/web/issues/12409
--- packages/web-runtime/src/App.vue
+++ packages/web-runtime/src/App.vue
@@ -13,11 +13,11 @@
     <modal-wrapper />
   </div>
 </template>
-<script lang="ts">
+<script lang="ts" setup>
+import { setCurrentLanguage } from './helpers/language'
 import SkipTo from './components/SkipTo.vue'
 import ModalWrapper from './components/ModalWrapper.vue'
 import { useLayout } from './composables/layout'
-import { computed, defineComponent, onWatcherCleanup, unref, watch } from 'vue'
 import { additionalTranslations } from './helpers/additionalTranslations' // eslint-disable-line
 import {
   eventBus,
@@ -27,156 +27,142 @@ import {
   useThemeStore
 } from '@ownclouders/web-pkg'
 import { useHead } from './composables/head'
-import { RouteLocation } from 'vue-router'
+import { RouteLocation, useRoute } from 'vue-router'
 import { storeToRefs } from 'pinia'
 import { isEqual } from 'lodash-es'
 import { MaintenanceBanner } from './components/MaintenanceBanner'
-import { useTemplateRef } from 'vue'
+import { useTemplateRef, ref, onMounted, unref, computed, onWatcherCleanup, watch } from 'vue'
 import { MaybeElement, useElementSize } from '@vueuse/core'
 import { useMaintenanceMode } from './composables/maintenanceMode'
+import { useGettext } from 'vue3-gettext'
 
-export default defineComponent({
-  components: {
-    SkipTo,
-    ModalWrapper,
-    MaintenanceBanner
-  },
-  setup() {
-    const resourcesStore = useResourcesStore()
-    const themeStore = useThemeStore()
-    const { currentTheme } = storeToRefs(themeStore)
+const resourcesStore = useResourcesStore()
+const themeStore = useThemeStore()
+const { currentTheme } = storeToRefs(themeStore)
 
-    const configStore = useConfigStore()
-    const { maintenanceMode } = storeToRefs(configStore)
+const configStore = useConfigStore()
+const { maintenanceMode } = storeToRefs(configStore)
 
-    const { startCheckingMaintenanceMode, stopCheckingMaintenanceMode } = useMaintenanceMode()
+const { startCheckingMaintenanceMode, stopCheckingMaintenanceMode } = useMaintenanceMode()
 
-    const router = useRouter()
-    useHead()
+const router = useRouter()
+const route = useRoute()
+useHead()
 
-    const activeRoute = computed(() => router.resolve(unref(router.currentRoute)))
+const activeRoute = computed(() => router.resolve(unref(router.currentRoute)))
 
-    const { layout, layoutType } = useLayout({ router })
+const { layout, layoutType } = useLayout({ router })
 
-    const maintenanceBannerElement = useTemplateRef('maintenance-banner')
-    const { height: maintenanceBannerHeight } = useElementSize(
-      maintenanceBannerElement as unknown as MaybeElement
-    )
+const maintenanceBannerElement = useTemplateRef('maintenance-banner')
+const { height: maintenanceBannerHeight } = useElementSize(
+  maintenanceBannerElement as unknown as MaybeElement
+)
+const announcement = ref('')
 
-    watch(
-      () => unref(activeRoute),
-      (newRoute, oldRoute) => {
-        /**
-         * Hide global loading spinner. It usually gets hidden after all apps
-         * have been loaded, but in some scenarios (plain layouts) we never load them.
-         */
-        if (unref(layoutType) !== 'application') {
-          const loader = document.getElementById('splash-loading')
-          if (!loader?.classList.contains('splash-hide')) {
-            loader.classList.add('splash-hide')
-          }
-        }
+const getText = useGettext()
+const { $gettext, current: currentLanguage } = getText
 
-        const getAppContextFromRoute = (route: RouteLocation): string[] => {
-          return route?.path?.split('/').slice(1, 4)
-        }
+function announceRouteChange(pageTitle: string) {
+  announcement.value = $gettext('Navigated to %{ pageTitle }', { pageTitle })
+}
 
-        const oldAppContext = getAppContextFromRoute(oldRoute)
-        const newAppContext = getAppContextFromRoute(newRoute)
-
-        if (isEqual(oldAppContext, newAppContext)) {
-          return
-        }
-
-        if ('driveAliasAndItem' in newRoute.params) {
-          return
-        }
-
-        /*
-         * If app context has been changed and no file context is set, we will reset current folder.
-         */
-        resourcesStore.setCurrentFolder(null)
-      }
-    )
-
-    watch(
-      maintenanceMode,
-      (maintenanceMode) => {
-        if (maintenanceMode) {
-          startCheckingMaintenanceMode()
-        } else {
-          stopCheckingMaintenanceMode()
-        }
-
-        onWatcherCleanup(() => {
-          stopCheckingMaintenanceMode()
-        })
-      },
-      { immediate: true }
-    )
-
-    return {
-      layout,
-      currentTheme,
-      maintenanceMode,
-      maintenanceBannerElement,
-      maintenanceBannerHeight
-    }
-  },
-  data() {
-    return {
-      windowWidth: 0,
-      announcement: '',
-      activeBlobStyle: {}
-    }
-  },
-  watch: {
-    $route: {
-      immediate: true,
-      handler: function (to) {
-        const extracted = this.extractPageTitleFromRoute(to)
-        if (!extracted) {
-          return
-        }
-        const { shortDocumentTitle, fullDocumentTitle } = extracted
-        this.announceRouteChange(shortDocumentTitle)
-        document.title = fullDocumentTitle
-      }
-    }
-  },
-  mounted() {
-    eventBus.subscribe(
-      'runtime.documentTitle.changed',
-      ({
-        shortDocumentTitle,
-        fullDocumentTitle
-      }: {
-        shortDocumentTitle: string
-        fullDocumentTitle: string
-      }) => {
-        document.title = fullDocumentTitle
-        this.announceRouteChange(shortDocumentTitle)
-      }
-    )
-  },
-  methods: {
-    announceRouteChange(pageTitle: string) {
-      this.announcement = this.$gettext('Navigated to %{ pageTitle }', { pageTitle })
-    },
-
-    extractPageTitleFromRoute(route: RouteLocation) {
-      const routeTitle = route.meta.title ? this.$gettext(route.meta.title.toString()) : undefined
-      if (!routeTitle) {
-        return
-      }
-      const glue = ' - '
-      const titleSegments = [routeTitle]
-      return {
-        shortDocumentTitle: titleSegments.join(glue),
-        fullDocumentTitle: [...titleSegments, this.currentTheme.common.name].join(glue)
-      }
-    }
+function extractPageTitleFromRoute(route: RouteLocation) {
+  const routeTitle = route.meta.title ? $gettext(route.meta.title.toString()) : undefined
+  if (!routeTitle) {
+    return
   }
+  const glue = ' - '
+  const titleSegments = [routeTitle]
+  return {
+    shortDocumentTitle: titleSegments.join(glue),
+    fullDocumentTitle: [...titleSegments, unref(currentTheme).common.name].join(glue)
+  }
+}
+watch(
+  () => unref(activeRoute),
+  (newRoute, oldRoute) => {
+    /**
+     * Hide global loading spinner. It usually gets hidden after all apps
+     * have been loaded, but in some scenarios (plain layouts) we never load them.
+     */
+    if (unref(layoutType) !== 'application') {
+      const loader = document.getElementById('splash-loading')
+      if (!loader?.classList.contains('splash-hide')) {
+        loader.classList.add('splash-hide')
+      }
+    }
+
+    const getAppContextFromRoute = (route: RouteLocation): string[] => {
+      return route?.path?.split('/').slice(1, 4)
+    }
+
+    const oldAppContext = getAppContextFromRoute(oldRoute)
+    const newAppContext = getAppContextFromRoute(newRoute)
+
+    if (isEqual(oldAppContext, newAppContext)) {
+      return
+    }
+
+    if ('driveAliasAndItem' in newRoute.params) {
+      return
+    }
+
+    /*
+     * If app context has been changed and no file context is set, we will reset current folder.
+     */
+    resourcesStore.setCurrentFolder(null)
+  }
+)
+
+watch(
+  () => route,
+  (to) => {
+    const extracted = extractPageTitleFromRoute(to)
+    if (!extracted) {
+      return
+    }
+    const { shortDocumentTitle, fullDocumentTitle } = extracted
+    announceRouteChange(shortDocumentTitle)
+    document.title = fullDocumentTitle
+  },
+  { immediate: true }
+)
+
+watch(
+  maintenanceMode,
+  (maintenanceMode) => {
+    if (maintenanceMode) {
+      startCheckingMaintenanceMode()
+    } else {
+      stopCheckingMaintenanceMode()
+    }
+
+    onWatcherCleanup(() => {
+      stopCheckingMaintenanceMode()
+    })
+  },
+  { immediate: true }
+)
+
+onMounted(() => {
+  eventBus.subscribe(
+    'runtime.documentTitle.changed',
+    ({
+      shortDocumentTitle,
+      fullDocumentTitle
+    }: {
+      shortDocumentTitle: string
+      fullDocumentTitle: string
+    }) => {
+      document.title = fullDocumentTitle
+      announceRouteChange(shortDocumentTitle)
+    }
+  )
+
+  setCurrentLanguage({
+    language: getText,
+    languageSetting: currentLanguage
+  })
 })
 </script>
 <style lang="scss">
