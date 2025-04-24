00acc538087c5028ddbf6592aa42f7009703a82e
Matteo <matteo.sonnenholzner@kiteworks.com>
2025-04-24 17:44:56 +0200

    feat: add loading spinner to submit button for password-protected public links

--- packages/web-runtime/src/pages/resolvePublicLink.vue
+++ packages/web-runtime/src/pages/resolvePublicLink.vue
@@ -33,7 +33,8 @@
               variation="primary"
               appearance="filled"
               class="oc-login-authorize-button"
-              :disabled="!password"
+              :disabled="!password || resolvePublicLinkTask.isRunning"
+              :show-spinner="resolvePublicLinkTask.isRunning"
               submit="submit"
             >
               <span v-text="$gettext('Continue')" />
@@ -58,7 +59,7 @@
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { DavHttpError, SharePermissionBit } from '@ownclouders/web-client'
 import { authService } from '../services/auth'
 
@@ -75,7 +76,7 @@ import {
   useThemeStore
 } from '@ownclouders/web-pkg'
 import { useTask } from 'vue-concurrency'
-import { ref, unref, computed, defineComponent, onMounted } from 'vue'
+import { ref, unref, computed, onMounted } from 'vue'
 import {
   buildPublicSpaceResource,
   isPublicSpaceResource,
@@ -87,233 +88,210 @@ import { RouteLocationNamedRaw } from 'vue-router'
 import { dirname } from 'path'
 import { storeToRefs } from 'pinia'
 
-export default defineComponent({
-  name: 'ResolvePublicLink',
-  setup() {
-    const configStore = useConfigStore()
-    const clientService = useClientService()
-    const router = useRouter()
-    const route = useRoute()
-    const authStore = useAuthStore()
-    const { $gettext } = useGettext()
-    const token = useRouteParam('token')
-    const redirectUrl = useRouteQuery('redirectUrl')
-    const themeStore = useThemeStore()
-    const spacesStore = useSpacesStore()
+const configStore = useConfigStore()
+const clientService = useClientService()
+const router = useRouter()
+const route = useRoute()
+const authStore = useAuthStore()
+const { $gettext } = useGettext()
+const token = useRouteParam('token')
+const redirectUrl = useRouteQuery('redirectUrl')
+const themeStore = useThemeStore()
+const spacesStore = useSpacesStore()
 
-    const { currentTheme } = storeToRefs(themeStore)
-    const password = ref('')
+const { currentTheme } = storeToRefs(themeStore)
+const password = ref('')
 
-    const isOcmLink = computed(() => {
-      const split = unref(route).path.split('/')?.[1]
-      return split === 'o'
-    })
+const isOcmLink = computed(() => {
+  const split = unref(route).path.split('/')?.[1]
+  return split === 'o'
+})
 
-    const publicLinkType = computed(() => {
-      return unref(isOcmLink) ? 'ocm' : 'public-link'
-    })
+const publicLinkType = computed(() => (unref(isOcmLink) ? 'ocm' : 'public-link'))
 
-    const publicLinkSpace = computed(() =>
-      buildPublicSpaceResource({
-        id: unref(token),
-        driveType: 'public',
-        publicLinkType: unref(publicLinkType),
-        ...(unref(password) && { publicLinkPassword: unref(password) })
-      })
+const publicLinkSpace = computed(() =>
+  buildPublicSpaceResource({
+    id: unref(token),
+    driveType: 'public',
+    publicLinkType: unref(publicLinkType),
+    ...(unref(password) && { publicLinkPassword: unref(password) })
+  })
+)
+
+const item = computed(() => queryItemAsString(unref(route).params.driveAliasAndItem))
+
+const detailsQuery = useRouteQuery('details')
+const details = computed(() => queryItemAsString(unref(detailsQuery)))
+
+const loadedSpace = ref<PublicSpaceResource>()
+const isPasswordRequired = ref(false)
+const isInternalLink = ref(false)
+
+const loadPublicSpaceTask = useTask(function* (signal) {
+  try {
+    loadedSpace.value = yield clientService.webdav.getFileInfo(
+      unref(publicLinkSpace),
+      {},
+      { signal }
     )
+  } catch (error) {
+    const err = error as DavHttpError
 
-    const item = computed(() => {
-      return queryItemAsString(unref(route).params.driveAliasAndItem)
-    })
-
-    const detailsQuery = useRouteQuery('details')
-    const details = computed(() => {
-      return queryItemAsString(unref(detailsQuery))
-    })
-
-    const loadedSpace = ref<PublicSpaceResource>()
-    const isPasswordRequired = ref(false)
-    const isInternalLink = ref(false)
-
-    const loadPublicSpaceTask = useTask(function* (signal) {
-      try {
-        loadedSpace.value = yield clientService.webdav.getFileInfo(
-          unref(publicLinkSpace),
-          {},
-          { signal }
-        )
-      } catch (error) {
-        const err = error as DavHttpError
-
-        if (err.statusCode === 401) {
-          if (err.errorCode === 'ERR_MISSING_BASIC_AUTH') {
-            isPasswordRequired.value = true
-          }
-
-          if (err.errorCode === 'ERR_MISSING_BEARER_AUTH') {
-            isInternalLink.value = true
-          }
-
-          return
-        }
-        if (err.statusCode === 404) {
-          throw new Error($gettext('The resource could not be located, it may not exist anymore.'))
-        }
-        throw err
-      }
-    })
-
-    const verifyPasswordTask = useTask(function* (signal) {
-      try {
-        loadedSpace.value = yield clientService.webdav.getFileInfo(
-          unref(publicLinkSpace),
-          {},
-          { signal }
-        )
-        if (!isPublicSpaceResource(unref(loadedSpace))) {
-          const e: any = new Error($gettext('The resource is not a public link.'))
-          e.resource = unref(loadedSpace)
-          throw e
-        }
-      } catch (e) {
-        if (e.statusCode === 401) {
-          throw e
-        }
-        throw new Error($gettext('The resource could not be located, it may not exist anymore.'))
-      }
-    })
-    const wrongPassword = computed(() => {
-      if (verifyPasswordTask.isError) {
-        return verifyPasswordTask.last.error.statusCode === 401
-      }
-      return false
-    })
-
-    const resolvePublicLinkTask = useTask(function* (signal, passwordRequired: boolean) {
-      if (unref(isOcmLink) && !configStore.options.ocm.openRemotely) {
-        throw new Error($gettext('Opening files from remote is disabled'))
+    if (err.statusCode === 401) {
+      if (err.errorCode === 'ERR_MISSING_BASIC_AUTH') {
+        isPasswordRequired.value = true
       }
 
-      if (unref(isInternalLink)) {
-        router.push({ name: 'login', query: { redirectUrl: `/i/${unref(token)}` } })
-        return
+      if (err.errorCode === 'ERR_MISSING_BEARER_AUTH') {
+        isInternalLink.value = true
       }
 
-      yield authService.resolvePublicLink(
-        unref(token),
-        passwordRequired,
-        passwordRequired ? unref(password) : '',
-        unref(publicLinkType)
-      )
+      return
+    }
+    if (err.statusCode === 404) {
+      throw new Error($gettext('The resource could not be located, it may not exist anymore.'))
+    }
+    throw err
+  }
+})
 
-      if (passwordRequired) {
-        try {
-          yield verifyPasswordTask.perform()
-        } catch (e) {
-          authStore.clearPublicLinkContext()
-          console.error(e, e.resource)
-          throw e
-        }
-      }
+const verifyPasswordTask = useTask(function* (signal) {
+  try {
+    loadedSpace.value = yield clientService.webdav.getFileInfo(
+      unref(publicLinkSpace),
+      {},
+      { signal }
+    )
+    if (!isPublicSpaceResource(unref(loadedSpace))) {
+      const e: any = new Error($gettext('The resource is not a public link.'))
+      e.resource = unref(loadedSpace)
+      throw e
+    }
+  } catch (e) {
+    if (e.statusCode === 401) {
+      throw e
+    }
+    throw new Error($gettext('The resource could not be located, it may not exist anymore.'))
+  }
+})
+const wrongPassword = computed(() => {
+  if (verifyPasswordTask.isError) {
+    return verifyPasswordTask.last.error.statusCode === 401
+  }
+  return false
+})
 
-      const url = queryItemAsString(unref(redirectUrl))
-      if (url) {
-        router.push({ path: url })
-        return
-      }
+const resolvePublicLinkTask = useTask(function* (signal, passwordRequired: boolean) {
+  if (unref(isOcmLink) && !configStore.options.ocm.openRemotely) {
+    throw new Error($gettext('Opening files from remote is disabled'))
+  }
 
-      if (unref(loadedSpace).publicLinkPermission === SharePermissionBit.Create) {
-        router.push({
-          name: 'files-public-upload',
-          params: { token: unref(token) },
-          query: { fileId: unref(publicLinkSpace).fileId }
-        })
-        return
-      }
+  if (unref(isInternalLink)) {
+    router.push({ name: 'login', query: { redirectUrl: `/i/${unref(token)}` } })
+    return
+  }
 
-      let scrollTo: string
-      let fileId: string
-      let path: string
+  yield authService.resolvePublicLink(
+    unref(token),
+    passwordRequired,
+    passwordRequired ? unref(password) : '',
+    unref(publicLinkType)
+  )
 
-      if (['folder', 'space'].includes(unref(loadedSpace).type)) {
-        fileId = unref(loadedSpace).fileId
-        path = unref(item)
-      } else {
-        fileId = unref(loadedSpace).parentFolderId
-        scrollTo = unref(loadedSpace).fileId
-        path = dirname(unref(item))
-      }
-
-      spacesStore.upsertSpace(unref(loadedSpace))
-
-      const driveAliasAndItem = urlJoin(unref(isOcmLink) ? `ocm/` : `public/`, unref(token), path)
-      const targetLocation: RouteLocationNamedRaw = {
-        name: 'files-public-link',
-        query: {
-          openWithDefaultApp: 'true',
-          ...(!!fileId && { fileId }),
-          ...(!!scrollTo && { scrollTo }),
-          ...(unref(details) && { details: unref(details) })
-        },
-        params: {
-          driveAliasAndItem
-        }
-      }
-
-      router.push(targetLocation)
-    })
-
-    const errorMessage = computed<string>(() => {
-      if (resolvePublicLinkTask.isError && resolvePublicLinkTask.last.error.statusCode !== 401) {
-        return resolvePublicLinkTask.last.error.message
-      }
-
-      if (loadPublicSpaceTask.isError) {
-        return loadPublicSpaceTask.last.error.message
-      }
-      return null
-    })
-
-    onMounted(async () => {
-      try {
-        if (unref(isOcmLink)) {
-          await resolvePublicLinkTask.perform(false)
-          return
-        }
-
-        await loadPublicSpaceTask.perform()
-
-        if (!unref(isPasswordRequired)) {
-          await resolvePublicLinkTask.perform(false)
-        }
-      } catch (e) {
-        console.error(e)
-      }
-    })
-
-    const footerSlogan = computed(() => currentTheme.value.common.slogan)
-    const passwordFieldLabel = computed(() => {
-      return $gettext('Enter password for public link')
-    })
-    const wrongPasswordMessage = computed(() => {
-      if (unref(wrongPassword)) {
-        return $gettext('Incorrect password')
-      }
-      return null
-    })
-
-    return {
-      isPasswordRequired,
-      password,
-      wrongPassword,
-      passwordFieldLabel,
-      wrongPasswordMessage,
-      errorMessage,
-      footerSlogan,
-      resolvePublicLinkTask,
-      loadPublicSpaceTask
+  if (passwordRequired) {
+    try {
+      yield verifyPasswordTask.perform()
+    } catch (e) {
+      authStore.clearPublicLinkContext()
+      console.error(e, e.resource)
+      throw e
     }
   }
+
+  const url = queryItemAsString(unref(redirectUrl))
+  if (url) {
+    router.push({ path: url })
+    return
+  }
+
+  if (unref(loadedSpace).publicLinkPermission === SharePermissionBit.Create) {
+    router.push({
+      name: 'files-public-upload',
+      params: { token: unref(token) },
+      query: { fileId: unref(publicLinkSpace).fileId }
+    })
+    return
+  }
+
+  let scrollTo: string
+  let fileId: string
+  let path: string
+
+  if (['folder', 'space'].includes(unref(loadedSpace).type)) {
+    fileId = unref(loadedSpace).fileId
+    path = unref(item)
+  } else {
+    fileId = unref(loadedSpace).parentFolderId
+    scrollTo = unref(loadedSpace).fileId
+    path = dirname(unref(item))
+  }
+
+  spacesStore.upsertSpace(unref(loadedSpace))
+
+  const driveAliasAndItem = urlJoin(unref(isOcmLink) ? `ocm/` : `public/`, unref(token), path)
+  const targetLocation: RouteLocationNamedRaw = {
+    name: 'files-public-link',
+    query: {
+      openWithDefaultApp: 'true',
+      ...(!!fileId && { fileId }),
+      ...(!!scrollTo && { scrollTo }),
+      ...(unref(details) && { details: unref(details) })
+    },
+    params: {
+      driveAliasAndItem
+    }
+  }
+
+  router.push(targetLocation)
+})
+
+const errorMessage = computed<string>(() => {
+  if (resolvePublicLinkTask.isError && resolvePublicLinkTask.last.error.statusCode !== 401) {
+    return resolvePublicLinkTask.last.error.message
+  }
+
+  if (loadPublicSpaceTask.isError) {
+    return loadPublicSpaceTask.last.error.message
+  }
+  return null
+})
+
+onMounted(async () => {
+  try {
+    if (unref(isOcmLink)) {
+      await resolvePublicLinkTask.perform(false)
+      return
+    }
+
+    await loadPublicSpaceTask.perform()
+
+    if (!unref(isPasswordRequired)) {
+      await resolvePublicLinkTask.perform(false)
+    }
+  } catch (e) {
+    console.error(e)
+  }
+})
+
+const footerSlogan = computed(() => currentTheme.value.common.slogan)
+const passwordFieldLabel = computed(() => {
+  return $gettext('Enter password for public link')
+})
+const wrongPasswordMessage = computed(() => {
+  if (unref(wrongPassword)) {
+    return $gettext('Incorrect password')
+  }
+  return null
 })
 </script>
 
