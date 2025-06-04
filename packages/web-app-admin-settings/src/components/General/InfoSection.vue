4559866f3d991f76865bf11502ffc105e1fca605
Matteo <matteo.sonnenholzner@kiteworks.com>
2025-06-04 12:49:42 +0200

    chore: refactor info-section component to use script setup

--- packages/web-app-admin-settings/src/components/General/InfoSection.vue
+++ packages/web-app-admin-settings/src/components/General/InfoSection.vue
@@ -5,45 +5,31 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script lang="ts" setup>
 import { useCapabilityStore } from '@ownclouders/web-pkg'
 import { useGettext } from 'vue3-gettext'
 
-export default defineComponent({
-  name: 'InfoSection',
-  setup() {
-    const capabilityStore = useCapabilityStore()
-    const { $gettext } = useGettext()
+const capabilityStore = useCapabilityStore()
+const { $gettext } = useGettext()
 
-    let backendProductName = ''
-    let backendVersion = ''
-    let backendEdition = ''
-    let webClientVersion = ''
+let backendProductName = ''
+let backendVersion = ''
+let backendEdition = ''
+let webClientVersion = ''
 
-    const backendStatus = capabilityStore.status
+const backendStatus = capabilityStore.status
 
-    if (backendStatus && backendStatus.versionstring) {
-      backendProductName = backendStatus.product || 'ownCloud'
-      backendVersion = backendStatus.productversion || backendStatus.versionstring
-      backendEdition = backendStatus.edition
-      webClientVersion = process.env.PACKAGE_VERSION
-    }
+if (backendStatus && backendStatus.versionstring) {
+  backendProductName = backendStatus.product || 'ownCloud'
+  backendVersion = backendStatus.productversion || backendStatus.versionstring
+  backendEdition = backendStatus.edition
+  webClientVersion = process.env.PACKAGE_VERSION
+}
 
-    const infoItems = [
-      { term: $gettext('ownCloud'), definition: backendProductName },
-      { term: $gettext('Edition'), definition: backendEdition },
-      { term: $gettext('Version'), definition: backendVersion },
-      { term: $gettext('Web client version'), definition: webClientVersion }
-    ]
-
-    return {
-      backendProductName,
-      backendVersion,
-      backendEdition,
-      infoItems,
-      webClientVersion
-    }
-  }
-})
+const infoItems = [
+  { term: $gettext('ownCloud'), definition: backendProductName },
+  { term: $gettext('Edition'), definition: backendEdition },
+  { term: $gettext('Version'), definition: backendVersion },
+  { term: $gettext('Web client version'), definition: webClientVersion }
+]
 </script>
