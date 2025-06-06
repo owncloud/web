63e13a110d5683a55b2d0f34c230f7f373692f2d
Matteo <matteo.sonnenholzner@kiteworks.com>
2025-06-06 07:57:25 +0200

    chore: refactor group-info-box component to use script setup

--- packages/web-app-admin-settings/src/components/Groups/SideBar/GroupInfoBox.vue
+++ packages/web-app-admin-settings/src/components/Groups/SideBar/GroupInfoBox.vue
@@ -1,41 +1,26 @@
 <template>
   <div class="oc-flex group-info oc-mb-l">
-    <avatar-image class="oc-mb-m" :width="80" :userid="_group.id" :user-name="_group.displayName" />
-    <span class="oc-text-muted group-info-display-name" v-text="_group.displayName"></span>
+    <avatar-image class="oc-mb-m" :width="80" :userid="group.id" :user-name="group.displayName" />
+    <span class="oc-text-muted group-info-display-name" v-text="group.displayName"></span>
     <span class="oc-text-muted" v-text="groupMembersText"></span>
   </div>
 </template>
-<script lang="ts">
-import { computed, defineComponent, PropType, unref } from 'vue'
+<script lang="ts" setup>
+import { computed, unref } from 'vue'
 import { Group } from '@ownclouders/web-client/graph/generated'
 import { useGettext } from 'vue3-gettext'
-export default defineComponent({
-  name: 'GroupInfoBox',
-  props: {
-    group: {
-      type: Object as PropType<Group>,
-      required: true
-    }
-  },
-  setup(props) {
-    const _group = computed<Group>(() => props.group)
-    const { $ngettext } = useGettext()
-    const groupMembersText = computed(() => {
-      return $ngettext(
-        '%{groupCount} member',
-        '%{groupCount} members',
-        unref(_group).members.length,
-        {
-          groupCount: unref(_group).members.length.toString()
-        }
-      )
-    })
 
-    return {
-      groupMembersText,
-      _group
-    }
-  }
+interface Props {
+  group: Group
+}
+
+const props = defineProps<Props>()
+const group = computed<Group>(() => props.group)
+const { $ngettext } = useGettext()
+const groupMembersText = computed(() => {
+  return $ngettext('%{groupCount} member', '%{groupCount} members', unref(group).members.length, {
+    groupCount: unref(group).members.length.toString()
+  })
 })
 </script>
 <style lang="scss">
