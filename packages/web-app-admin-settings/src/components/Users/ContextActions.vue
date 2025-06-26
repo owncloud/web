8048a329093ba05a9e1f1b31aa8a7f789bf3dc0b
Matteo <matteo.sonnenholzner@kiteworks.com>
2025-06-26 09:32:47 +0200

    chore: refactor users-context-actions to use script setup

--- packages/web-app-admin-settings/src/components/Users/ContextActions.vue
+++ packages/web-app-admin-settings/src/components/Users/ContextActions.vue
@@ -4,74 +4,63 @@
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import {
   useUserActionsEdit,
   useUserActionsDelete,
   useUserActionsEditQuota
 } from '../../composables/actions/users'
-import { computed, defineComponent, PropType, unref } from 'vue'
+import { computed, unref } from 'vue'
 import { ContextActionMenu } from '@ownclouders/web-pkg'
 import { User } from '@ownclouders/web-client/graph/generated'
 import { useActionsShowDetails } from '@ownclouders/web-pkg'
 
-export default defineComponent({
-  name: 'ContextActions',
-  components: { ContextActionMenu },
-  props: {
-    items: {
-      type: Array as PropType<User[]>,
-      required: true
-    }
-  },
-  setup(props) {
-    const filterParams = computed(() => ({ resources: props.items }))
+interface Props {
+  items: User[]
+}
+const props = defineProps<Props>()
 
-    const { actions: showDetailsActions } = useActionsShowDetails()
-    const { actions: editQuotaActions } = useUserActionsEditQuota()
-    const { actions: userEditActions } = useUserActionsEdit()
-    const { actions: userDeleteActions } = useUserActionsDelete()
+const filterParams = computed(() => ({ resources: props.items }))
 
-    const menuItemsPrimaryActions = computed(() =>
-      [...unref(userEditActions), ...unref(userDeleteActions)].filter((item) =>
-        item.isVisible(unref(filterParams))
-      )
-    )
-    const menuItemsSecondaryActions = computed(() =>
-      [...unref(editQuotaActions)].filter((item) => item.isVisible(unref(filterParams)))
-    )
+const { actions: showDetailsActions } = useActionsShowDetails()
+const { actions: editQuotaActions } = useUserActionsEditQuota()
+const { actions: userEditActions } = useUserActionsEdit()
+const { actions: userDeleteActions } = useUserActionsDelete()
 
-    const menuItemsSidebar = computed(() =>
-      [...unref(showDetailsActions)].filter((item) => item.isVisible(unref(filterParams)))
-    )
+const menuItemsPrimaryActions = computed(() =>
+  [...unref(userEditActions), ...unref(userDeleteActions)].filter((item) =>
+    item.isVisible(unref(filterParams))
+  )
+)
+const menuItemsSecondaryActions = computed(() =>
+  [...unref(editQuotaActions)].filter((item) => item.isVisible(unref(filterParams)))
+)
 
-    const menuSections = computed(() => {
-      const sections = []
+const menuItemsSidebar = computed(() =>
+  [...unref(showDetailsActions)].filter((item) => item.isVisible(unref(filterParams)))
+)
 
-      if (unref(menuItemsPrimaryActions).length) {
-        sections.push({
-          name: 'primaryActions',
-          items: unref(menuItemsPrimaryActions)
-        })
-      }
-      if (unref(menuItemsSecondaryActions).length) {
-        sections.push({
-          name: 'secondaryActions',
-          items: unref(menuItemsSecondaryActions)
-        })
-      }
-      if (unref(menuItemsSidebar).length) {
-        sections.push({
-          name: 'sidebar',
-          items: unref(menuItemsSidebar)
-        })
-      }
-      return sections
+const menuSections = computed(() => {
+  const sections = []
+
+  if (unref(menuItemsPrimaryActions).length) {
+    sections.push({
+      name: 'primaryActions',
+      items: unref(menuItemsPrimaryActions)
     })
-
-    return {
-      menuSections
-    }
   }
+  if (unref(menuItemsSecondaryActions).length) {
+    sections.push({
+      name: 'secondaryActions',
+      items: unref(menuItemsSecondaryActions)
+    })
+  }
+  if (unref(menuItemsSidebar).length) {
+    sections.push({
+      name: 'sidebar',
+      items: unref(menuItemsSidebar)
+    })
+  }
+  return sections
 })
 </script>
