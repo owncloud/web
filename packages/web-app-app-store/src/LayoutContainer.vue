49f0f4835c1293d21abb75bee00c76a8de3a3404
Matteo <matteo.sonnenholzner@kiteworks.com>
2025-07-08 11:39:25 +0200

    chore(web-app-app-store): [OCISDEV-173] refactor app-store-layout-container component to use script setup

--- packages/web-app-app-store/src/LayoutContainer.vue
+++ packages/web-app-app-store/src/LayoutContainer.vue
@@ -7,32 +7,20 @@
   </main>
 </template>
 
-<script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue'
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
 import { useAppsStore } from './piniaStores'
 import { AppLoadingSpinner } from '@ownclouders/web-pkg'
-
-export default defineComponent({
-  name: 'LayoutContainer',
-  components: { AppLoadingSpinner },
-  setup() {
-    const appsStore = useAppsStore()
-
-    const areAppsLoading = ref(true)
-    const appsLoadingPromise = appsStore.loadApps()
-    onMounted(async () => {
-      try {
-        await appsLoadingPromise
-      } catch (e) {
-        console.error(e)
-      } finally {
-        areAppsLoading.value = false
-      }
-    })
-
-    return {
-      areAppsLoading
-    }
+const appsStore = useAppsStore()
+const areAppsLoading = ref(true)
+const appsLoadingPromise = appsStore.loadApps()
+onMounted(async () => {
+  try {
+    await appsLoadingPromise
+  } catch (e) {
+    console.error(e)
+  } finally {
+    areAppsLoading.value = false
   }
 })
 </script>
