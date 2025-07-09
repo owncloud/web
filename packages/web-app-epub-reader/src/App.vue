c96e297d98ce165b26089f12e87a726fa81f7afd
Matteo <matteo.sonnenholzner@kiteworks.com>
2025-07-09 09:11:10 +0200

    chore(web-app-epub-reader): [OCISDEV-177] refactor web-app-epub-reader app component to use script setup

--- packages/web-app-epub-reader/src/App.vue
+++ packages/web-app-epub-reader/src/App.vue
@@ -90,17 +90,16 @@
   </div>
 </template>
 
-<script lang="ts">
-import { computed, defineComponent, nextTick, PropType, ref, unref, watch } from 'vue'
+<script lang="ts" setup>
+import { computed, nextTick, ref, unref, watch } from 'vue'
 import { Resource } from '@ownclouders/web-client'
-import {
-  AppConfigObject,
-  Key,
-  useKeyboardActions,
-  useLocalStorage,
-  useThemeStore
-} from '@ownclouders/web-pkg'
+import { Key, useKeyboardActions, useLocalStorage, useThemeStore } from '@ownclouders/web-pkg'
 import ePub, { Book, NavItem, Rendition, Location } from 'epubjs'
+
+interface Props {
+  currentContent: string
+  resource: Resource
+}
 
 const DARK_THEME_CONFIG = {
   html: {
@@ -118,162 +117,128 @@ const LIGHT_THEME_CONFIG = {
 const MAX_FONT_SIZE_PERCENTAGE = 150
 const MIN_FONT_SIZE_PERCENTAGE = 50
 const FONT_SIZE_PERCENTAGE_STEP = 10
+const { currentContent, resource } = defineProps<Props>()
 
-export default defineComponent({
-  name: 'EpubReader',
-  props: {
-    applicationConfig: { type: Object as PropType<AppConfigObject>, required: true },
-    currentContent: {
-      type: String,
-      required: true
-    },
-    isReadOnly: { type: Boolean, required: false },
-    resource: { type: Object as PropType<Resource>, required: true }
-  },
-  emits: ['close'],
-  setup(props) {
-    const keyboardActions = useKeyboardActions()
-    const bookContainer = ref<Element>()
-    const chapters = ref<NavItem[]>([])
-    const currentChapter = ref<NavItem>()
-    const navigateLeftDisabled = ref(false)
-    const navigateRightDisabled = ref(false)
-    const localStorageData = useLocalStorage<{ fontSizePercentage?: number }>(`oc_epubReader`, {})
-    const currentFontSizePercentage = ref(unref(localStorageData).fontSizePercentage || 100)
-    const themeStore = useThemeStore()
-    const book = ref<Book>()
-    const rendition = ref<Rendition>()
+const keyboardActions = useKeyboardActions()
+const bookContainer = ref<Element>()
+const chapters = ref<NavItem[]>([])
+const currentChapter = ref<NavItem>()
+const navigateLeftDisabled = ref(false)
+const navigateRightDisabled = ref(false)
+const localStorageData = useLocalStorage<{ fontSizePercentage?: number }>(`oc_epubReader`, {})
+const currentFontSizePercentage = ref(unref(localStorageData).fontSizePercentage || 100)
+const themeStore = useThemeStore()
+const book = ref<Book>()
+const rendition = ref<Rendition>()
 
-    const navigateLeft = () => {
-      unref(rendition).prev()
+const navigateLeft = () => {
+  unref(rendition).prev()
+}
+
+const navigateRight = () => {
+  unref(rendition).next()
+}
+
+const showChapter = (chapter: NavItem) => {
+  unref(rendition).display(chapter.href)
+}
+
+const increaseFontSize = () => {
+  currentFontSizePercentage.value = Math.min(
+    unref(currentFontSizePercentage) + FONT_SIZE_PERCENTAGE_STEP,
+    MAX_FONT_SIZE_PERCENTAGE
+  )
+}
+
+const resetFontSize = () => {
+  currentFontSizePercentage.value = 100
+}
+
+const decreaseFontSize = () => {
+  currentFontSizePercentage.value = Math.max(
+    unref(currentFontSizePercentage) - FONT_SIZE_PERCENTAGE_STEP,
+    MIN_FONT_SIZE_PERCENTAGE
+  )
+}
+
+const increaseFontSizeDisabled = computed(() => {
+  return unref(currentFontSizePercentage) >= MAX_FONT_SIZE_PERCENTAGE
+})
+
+const decreaseFontSizeDisabled = computed(() => {
+  return unref(currentFontSizePercentage) <= MIN_FONT_SIZE_PERCENTAGE
+})
+
+keyboardActions.bindKeyAction({ primary: Key.ArrowLeft }, () => navigateLeft())
+keyboardActions.bindKeyAction({ primary: Key.ArrowRight }, () => navigateRight())
+
+watch(
+  () => currentContent,
+  async () => {
+    await nextTick()
+
+    if (unref(book)) {
+      unref(book).destroy()
     }
 
-    const navigateRight = () => {
-      unref(rendition).next()
-    }
-
-    const showChapter = (chapter: NavItem) => {
-      unref(rendition).display(chapter.href)
-    }
-
-    const increaseFontSize = () => {
-      currentFontSizePercentage.value = Math.min(
-        unref(currentFontSizePercentage) + FONT_SIZE_PERCENTAGE_STEP,
-        MAX_FONT_SIZE_PERCENTAGE
-      )
-    }
-
-    const resetFontSize = () => {
-      currentFontSizePercentage.value = 100
-    }
-
-    const decreaseFontSize = () => {
-      currentFontSizePercentage.value = Math.max(
-        unref(currentFontSizePercentage) - FONT_SIZE_PERCENTAGE_STEP,
-        MIN_FONT_SIZE_PERCENTAGE
-      )
-    }
-
-    const increaseFontSizeDisabled = computed(() => {
-      return unref(currentFontSizePercentage) >= MAX_FONT_SIZE_PERCENTAGE
-    })
-
-    const decreaseFontSizeDisabled = computed(() => {
-      return unref(currentFontSizePercentage) <= MIN_FONT_SIZE_PERCENTAGE
-    })
-
-    keyboardActions.bindKeyAction({ primary: Key.ArrowLeft }, () => navigateLeft())
-    keyboardActions.bindKeyAction({ primary: Key.ArrowRight }, () => navigateRight())
-
-    watch(
-      () => props.currentContent,
-      async () => {
-        await nextTick()
-
-        if (unref(book)) {
-          unref(book).destroy()
-        }
-
-        const localStorageResourceData = useLocalStorage<{ currentLocation?: Location }>(
-          `oc_epubReader_resource_${props.resource.id}`,
-          {}
-        )
-
-        book.value = ePub(props.currentContent)
-
-        unref(book).loaded.navigation.then(({ toc }) => {
-          chapters.value = toc
-          currentChapter.value = toc?.[0]
-        })
-
-        rendition.value = unref(book).renderTo(unref(bookContainer), {
-          flow: 'paginated',
-          width: 650,
-          height: '90%' // Don't use full height to avoid cut-off text
-        })
-
-        unref(rendition).themes.register('dark', DARK_THEME_CONFIG)
-        unref(rendition).themes.register('light', LIGHT_THEME_CONFIG)
-        unref(rendition).themes.select(themeStore.currentTheme.isDark ? 'dark' : 'light')
-        unref(rendition).themes.fontSize(`${unref(currentFontSizePercentage)}%`)
-        unref(rendition).display(unref(localStorageResourceData)?.currentLocation?.start?.cfi)
-
-        unref(rendition).on('keydown', (event: KeyboardEvent) => {
-          if (event.key === Key.ArrowLeft) {
-            navigateLeft()
-          }
-          if (event.key === Key.ArrowRight) {
-            navigateRight()
-          }
-        })
-
-        unref(rendition).on('relocated', () => {
-          const currentLocation = unref(rendition).currentLocation() as any & Location
-          localStorageResourceData.value = { currentLocation }
-          navigateLeftDisabled.value = currentLocation.atStart === true
-          navigateRightDisabled.value = currentLocation.atEnd === true
-
-          const locationCfi = currentLocation.start.cfi
-          const spineItem = unref(book).spine.get(locationCfi)
-          const navItem = unref(book).navigation.get(spineItem.href)
-          // Might be sub nav item and therefore undefined
-          if (navItem) {
-            currentChapter.value = navItem
-          }
-        })
-      },
-      {
-        immediate: true
-      }
+    const localStorageResourceData = useLocalStorage<{ currentLocation?: Location }>(
+      `oc_epubReader_resource_${resource.id}`,
+      {}
     )
 
-    watch(currentFontSizePercentage, () => {
-      unref(rendition).themes.fontSize(`${unref(currentFontSizePercentage)}%`)
-      localStorageData.value = {
-        ...unref(localStorageData),
-        fontSizePercentage: unref(currentFontSizePercentage)
+    book.value = ePub(currentContent)
+
+    unref(book).loaded.navigation.then(({ toc }) => {
+      chapters.value = toc
+      currentChapter.value = toc?.[0]
+    })
+
+    rendition.value = unref(book).renderTo(unref(bookContainer), {
+      flow: 'paginated',
+      width: 650,
+      height: '90%' // Don't use full height to avoid cut-off text
+    })
+
+    unref(rendition).themes.register('dark', DARK_THEME_CONFIG)
+    unref(rendition).themes.register('light', LIGHT_THEME_CONFIG)
+    unref(rendition).themes.select(themeStore.currentTheme.isDark ? 'dark' : 'light')
+    unref(rendition).themes.fontSize(`${unref(currentFontSizePercentage)}%`)
+    unref(rendition).display(unref(localStorageResourceData)?.currentLocation?.start?.cfi)
+
+    unref(rendition).on('keydown', (event: KeyboardEvent) => {
+      if (event.key === Key.ArrowLeft) {
+        navigateLeft()
+      }
+      if (event.key === Key.ArrowRight) {
+        navigateRight()
       }
     })
 
-    return {
-      bookContainer,
-      navigateLeft,
-      navigateLeftDisabled,
-      navigateRight,
-      navigateRightDisabled,
-      currentChapter,
-      chapters,
-      showChapter,
-      resetFontSize,
-      increaseFontSize,
-      decreaseFontSize,
-      increaseFontSizeDisabled,
-      decreaseFontSizeDisabled,
-      currentFontSizePercentage,
-      FONT_SIZE_PERCENTAGE_STEP,
-      rendition,
-      book
-    }
+    unref(rendition).on('relocated', () => {
+      const currentLocation = unref(rendition).currentLocation() as any & Location
+      localStorageResourceData.value = { currentLocation }
+      navigateLeftDisabled.value = currentLocation.atStart === true
+      navigateRightDisabled.value = currentLocation.atEnd === true
+
+      const locationCfi = currentLocation.start.cfi
+      const spineItem = unref(book).spine.get(locationCfi)
+      const navItem = unref(book).navigation.get(spineItem.href)
+      // Might be sub nav item and therefore undefined
+      if (navItem) {
+        currentChapter.value = navItem
+      }
+    })
+  },
+  {
+    immediate: true
+  }
+)
+
+watch(currentFontSizePercentage, () => {
+  unref(rendition).themes.fontSize(`${unref(currentFontSizePercentage)}%`)
+  localStorageData.value = {
+    ...unref(localStorageData),
+    fontSizePercentage: unref(currentFontSizePercentage)
   }
 })
 </script>
