c47d59924ce115ff6cdce2cdd256a50a91c45d5b
Matteo <matteo.sonnenholzner@kiteworks.com>
2025-07-30 13:17:44 +0200

    chore(web-app-files): [OCISDEV-179] refactor expiration-date-indicator component to use script setup

--- packages/web-app-files/src/components/SideBar/Shares/ExpirationDateIndicator.vue
+++ packages/web-app-files/src/components/SideBar/Shares/ExpirationDateIndicator.vue
@@ -10,47 +10,38 @@
   </div>
 </template>
 
-<script lang="ts">
-import { computed, defineComponent, PropType, unref } from 'vue'
+<script lang="ts" setup>
+import { computed, unref } from 'vue'
 import { DateTime } from 'luxon'
 import { formatDateFromDateTime, formatRelativeDateFromDateTime } from '@ownclouders/web-pkg'
 import { useGettext } from 'vue3-gettext'
 
-export default defineComponent({
-  name: 'ExpirationDateIndicator',
-  props: {
-    expirationDate: { type: Object as PropType<DateTime>, required: false, default: null }
-  },
-  setup(props) {
-    const { $gettext, current: currentLanguage } = useGettext()
+interface Props {
+  expirationDate?: DateTime
+}
+const { expirationDate = null } = defineProps<Props>()
+const { $gettext, current: currentLanguage } = useGettext()
 
-    const expirationDateRelative = computed(() => {
-      return formatRelativeDateFromDateTime(props.expirationDate, currentLanguage)
-    })
+const expirationDateRelative = computed(() => {
+  return formatRelativeDateFromDateTime(expirationDate, currentLanguage)
+})
 
-    const dateExpire = computed(() => {
-      return formatDateFromDateTime(props.expirationDate, currentLanguage)
-    })
+const dateExpire = computed(() => {
+  return formatDateFromDateTime(expirationDate, currentLanguage)
+})
 
-    const expirationDateTooltip = computed(() => {
-      return $gettext(
-        'Expires %{timeToExpiry} (%{expiryDate})',
-        { timeToExpiry: unref(expirationDateRelative), expiryDate: unref(dateExpire) },
-        true
-      )
-    })
+const expirationDateTooltip = computed(() => {
+  return $gettext(
+    'Expires %{timeToExpiry} (%{expiryDate})',
+    { timeToExpiry: unref(expirationDateRelative), expiryDate: unref(dateExpire) },
+    true
+  )
+})
 
-    const screenreaderShareExpiration = computed(() => {
-      return $gettext('Share expires %{ expiryDateRelative } (%{ expiryDate })', {
-        expiryDateRelative: unref(expirationDateRelative),
-        expiryDate: unref(dateExpire)
-      })
-    })
-
-    return {
-      expirationDateTooltip,
-      screenreaderShareExpiration
-    }
-  }
+const screenreaderShareExpiration = computed(() => {
+  return $gettext('Share expires %{ expiryDateRelative } (%{ expiryDate })', {
+    expiryDateRelative: unref(expirationDateRelative),
+    expiryDate: unref(dateExpire)
+  })
 })
 </script>
