ebeba66ca219aabb0f6708d9b0b022d6e7f14cd8
Matteo <matteo.sonnenholzner@kiteworks.com>
2025-08-18 09:45:42 +0200

    chore(web-app-pkg): [OCISDEV-256] refactor action-menu-item component to use script setup

--- packages/web-pkg/src/components/ContextActions/ActionMenuItem.vue
+++ packages/web-pkg/src/components/ContextActions/ActionMenuItem.vue
@@ -53,136 +53,106 @@
   </li>
 </template>
 
-<script lang="ts">
-import { computed, defineComponent, PropType, unref } from 'vue'
+<script lang="ts" setup>
+import { computed, unref } from 'vue'
 import { Action, ActionOptions, useConfigStore } from '../../composables'
 import { useGettext } from 'vue3-gettext'
 import { storeToRefs } from 'pinia'
 
-export default defineComponent({
-  name: 'ActionMenuItem',
-  props: {
-    action: {
-      type: Object as PropType<Action>,
-      required: true
-    },
-    actionOptions: {
-      type: Object as PropType<ActionOptions>,
-      required: true
-    },
-    size: {
-      type: String,
-      required: false,
-      default: 'medium'
-    },
-    appearance: {
-      type: String,
-      default: 'raw'
-    },
-    variation: {
-      type: String,
-      default: 'passive'
-    },
-    shortcutHint: {
-      type: Boolean,
-      default: true,
-      required: false
-    },
-    showTooltip: {
-      type: Boolean,
-      default: false,
-      required: false
-    }
-  },
-  setup(props) {
-    const { $gettext } = useGettext()
-    const configStore = useConfigStore()
-    const { options } = storeToRefs(configStore)
+interface Props {
+  action: Action
+  actionOptions: ActionOptions
+  size?: string
+  appearance?: string
+  variation?: string
+  shortcutHint?: boolean
+  showTooltip?: boolean
+}
+const {
+  action,
+  actionOptions,
+  size = 'medium',
+  appearance = 'raw',
+  variation = 'passive',
+  shortcutHint = true,
+  showTooltip = false
+} = defineProps<Props>()
+const { $gettext } = useGettext()
+const configStore = useConfigStore()
+const { options } = storeToRefs(configStore)
 
-    const componentType = computed<string>(() => {
-      if (Object.hasOwn(props.action, 'route')) {
-        return 'router-link'
-      }
-      if (Object.hasOwn(props.action, 'href')) {
-        return 'a'
-      }
-      if (Object.hasOwn(props.action, 'handler')) {
-        return 'button'
-      }
-      console.warn(
-        'ActionMenuItem: No handler, route or href callback found in action',
-        props.action
-      )
-      return 'button'
+const componentType = computed<string>(() => {
+  if (Object.hasOwn(action, 'route')) {
+    return 'router-link'
+  }
+  if (Object.hasOwn(action, 'href')) {
+    return 'a'
+  }
+  if (Object.hasOwn(action, 'handler')) {
+    return 'button'
+  }
+  console.warn('ActionMenuItem: No handler, route or href callback found in action', action)
+  return 'button'
+})
+
+const componentProps = computed(() => {
+  const properties = {
+    appearance: action.appearance || appearance,
+    variation: action.variation || variation,
+    ...(action.isDisabled && {
+      disabled: action.isDisabled(actionOptions)
+    }),
+    ...(action.id && { id: action.id })
+  }
+
+  return {
+    ...properties,
+    ...(unref(componentType) === 'router-link' && {
+      to: action.route(actionOptions)
+    }),
+    ...(unref(componentType) === 'a' && {
+      href: action.href(actionOptions)
+    }),
+    ...(['router-link', 'a'].includes(unref(componentType)) && {
+      target: options.value.cernFeatures ? '_blank' : '_self'
     })
+  }
+})
 
-    const componentProps = computed(() => {
-      const properties = {
-        appearance: props.action.appearance || props.appearance,
-        variation: props.action.variation || props.variation,
-        ...(props.action.isDisabled && {
-          disabled: props.action.isDisabled(props.actionOptions)
-        }),
-        ...(props.action.id && { id: props.action.id })
-      }
+const isMacOs = computed(() => {
+  return window.navigator.userAgent.includes('Mac')
+})
 
-      return {
-        ...properties,
-        ...(unref(componentType) === 'router-link' && {
-          to: props.action.route(props.actionOptions)
-        }),
-        ...(unref(componentType) === 'a' && {
-          href: props.action.href(props.actionOptions)
-        }),
-        ...(['router-link', 'a'].includes(unref(componentType)) && {
-          target: options.value.cernFeatures ? '_blank' : '_self'
-        })
-      }
+const openInNewTabHint = computed(() => {
+  return $gettext(
+    'Hold %{key} and click to open in new tab',
+    { key: unref(isMacOs) ? '⌘' : $gettext('ctrl') },
+    true
+  )
+})
+
+const hasExternalImageIcon = computed(() => {
+  return action.icon && /^https?:\/\//i.test(action.icon)
+})
+const componentListeners = computed(() => {
+  if (typeof action.handler !== 'function') {
+    return {}
+  }
+
+  const callback = () =>
+    action.handler({
+      ...actionOptions
     })
-
-    const isMacOs = computed(() => {
-      return window.navigator.platform.match('Mac')
-    })
-
-    const openInNewTabHint = computed(() => {
-      return $gettext(
-        'Hold %{key} and click to open in new tab',
-        { key: unref(isMacOs) ? '⌘' : $gettext('ctrl') },
-        true
-      )
-    })
-
+  if (action.keepOpen) {
     return {
-      componentType,
-      componentProps,
-      openInNewTabHint
-    }
-  },
-  computed: {
-    hasExternalImageIcon() {
-      return this.action.icon && /^https?:\/\//i.test(this.action.icon)
-    },
-    componentListeners() {
-      if (typeof this.action.handler !== 'function') {
-        return {}
-      }
-
-      const callback = () =>
-        this.action.handler({
-          ...this.actionOptions
-        })
-      if (this.action.keepOpen) {
-        return {
-          click: (event: Event) => {
-            event.stopPropagation()
-            callback()
-          }
-        }
-      }
-      return {
-        click: callback
+      click: (event: Event) => {
+        event.stopPropagation()
+        callback()
       }
     }
+  }
+  return {
+    click: callback
   }
 })
 </script>
