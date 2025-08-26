094f660e1e655b85f365c6c7ab07110158bff568
Lukas Hirt <info@hirt.cz>
2025-08-26 11:54:34 +0200

    chore: fix @noble/hashes imports

--- polyfills/crypto.js
+++ polyfills/crypto.js
@@ -1,5 +1,5 @@
-import { pbkdf2 } from '@noble/hashes/pbkdf2'
-import { sha512 } from '@noble/hashes/sha512'
+import { pbkdf2 } from '@noble/hashes/pbkdf2.js'
+import { sha512 } from '@noble/hashes/sha2.js'
 
 export const pbkdf2Sync = (password, salt, c, dkLen) => {
   return Buffer.from(pbkdf2(sha512, password, salt, { c, dkLen }))
