80e3e5981eccaf55101d3f894c7180ca47301775
Matteo <matteo.sonnenholzner@kiteworks.com>
2025-09-08 11:02:26 +0200

    chore(web-pkg): [OCISDEV-256] refactor pagination component to use script setup

--- packages/web-pkg/src/components/Pagination.vue
+++ packages/web-pkg/src/components/Pagination.vue
@@ -2,33 +2,26 @@
   <oc-pagination
     v-if="pages > 1"
     :pages="pages"
-    :current-page="currentPage"
+    :current-page="props.currentPage"
     :max-displayed="3"
     :current-route="$route"
     class="files-pagination oc-flex oc-flex-center oc-my-s"
   />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script lang="ts" setup>
+import { watch } from 'vue'
 
-export default defineComponent({
-  props: {
-    pages: {
-      type: Number,
-      required: true
-    },
-    currentPage: {
-      type: Number,
-      required: true
-    }
-  },
-  watch: {
-    currentPage: {
-      handler: function () {
-        document.getElementsByClassName('files-view-wrapper')[0]?.scrollTo(0, 0)
-      }
-    }
+interface Props {
+  pages: number
+  currentPage: number
+}
+
+const props = defineProps<Props>()
+watch(
+  () => props.currentPage,
+  () => {
+    document.getElementsByClassName('files-view-wrapper')[0]?.scrollTo(0, 0)
   }
-})
+)
 </script>
