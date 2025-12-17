8fea0e784742ed7a4751516f2c3059db37e7ac66
Niraj Acharya <nirajacharyacode@gmail.com>
2025-12-17 12:40:37 +0545

    test: convert tiles.feature test to playwright

--- tests/e2e-playwright/steps/ui/resources.ts
+++ tests/e2e-playwright/steps/ui/resources.ts
@@ -1,5 +1,6 @@
 import { objects } from '../../../e2e/support'
 import { ActorsEnvironment, FilesEnvironment } from '../../../e2e/support/environment'
+import { createResourceTypes } from '../../../e2e/support/objects/app-files/resource/actions'
 
 export async function uploadResource({
   actorsEnvironment,
@@ -57,5 +58,66 @@ export async function createDir({
   await resourceObject.create({
     name: directoryName,
     type: 'folder'
+  })
+}
+
+export async function switchToTilesViewMode({
+  actorsEnvironment,
+  stepUser
+}: {
+  actorsEnvironment: ActorsEnvironment
+  stepUser: string
+}): Promise<void> {
+  const { page } = actorsEnvironment.getActor({ key: stepUser })
+  const resourceObject = new objects.applicationFiles.Resource({ page })
+  await resourceObject.switchToTilesViewMode()
+}
+
+export async function expectThatResourcesAreTiles({
+  actorsEnvironment,
+  stepUser
+}: {
+  actorsEnvironment: ActorsEnvironment
+  stepUser: string
+}): Promise<void> {
+  const { page } = actorsEnvironment.getActor({ key: stepUser })
+  const resourceObject = new objects.applicationFiles.Resource({ page })
+  await resourceObject.expectThatResourcesAreTiles()
+}
+
+export async function openFolder({
+  actorsEnvironment,
+  stepUser,
+  resource
+}: {
+  actorsEnvironment: ActorsEnvironment
+  stepUser: string
+  resource: string
+}): Promise<void> {
+  const { page } = actorsEnvironment.getActor({ key: stepUser })
+  const resourceObject = new objects.applicationFiles.Resource({ page })
+  await resourceObject.openFolder(resource)
+}
+
+export async function createFolder({
+  actorsEnvironment,
+  stepUser,
+  resource,
+  content,
+  password
+}: {
+  actorsEnvironment: ActorsEnvironment
+  stepUser: string
+  resource: string
+  content?: string
+  password?: string
+}): Promise<void> {
+  const { page } = actorsEnvironment.getActor({ key: stepUser })
+  const resourceObject = new objects.applicationFiles.Resource({ page })
+  await resourceObject.create({
+    name: resource,
+    type: 'folder' as createResourceTypes,
+    content: content,
+    password: password
   })
 }
