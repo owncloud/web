e96bbe2d91150fdb7474643c8fb6c1a2f434db78
pradip <pradipsubedi360@gmail.com>
2026-01-06 16:58:44 +0545

    test: migrate general.feature to playwright

---
+++ tests/e2e-playwright/specs/admin-settings/general.spec.ts
@@ -0,0 +1,47 @@
+import { test } from '@playwright/test'
+import { config } from '../../../e2e/config.js'
+import {
+  ActorsEnvironment,
+  FilesEnvironment,
+  UsersEnvironment
+} from '../../../e2e/support/environment'
+import { setAccessAndRefreshToken } from '../../helpers/setAccessAndRefreshToken'
+import * as ui from '../../steps/ui/index'
+
+test.describe('general management', () => {
+  let actorsEnvironment
+  const usersEnvironment = new UsersEnvironment()
+  const filesEnvironment = new FilesEnvironment()
+
+  test.beforeEach(async ({ browser }) => {
+    actorsEnvironment = new ActorsEnvironment({
+      context: {
+        acceptDownloads: config.acceptDownloads,
+        reportDir: config.reportDir,
+        tracingReportDir: config.tracingReportDir,
+        reportHar: config.reportHar,
+        reportTracing: config.reportTracing,
+        reportVideo: config.reportVideo,
+        failOnUncaughtConsoleError: config.failOnUncaughtConsoleError
+      },
+      browser: browser
+    })
+
+    await setAccessAndRefreshToken(usersEnvironment)
+    await ui.logInUser({ usersEnvironment, actorsEnvironment, stepUser: 'Admin' })
+  })
+
+  test('logo can be changed in the admin settings', async () => {
+    await ui.openApplication({ actorsEnvironment, stepUser: 'Admin', name: 'admin-settings' })
+    await ui.navigateToGeneralManagementPage({ actorsEnvironment, stepUser: 'Admin' })
+    await ui.uploadLogoFromLocalPath({
+      actorsEnvironment,
+      stepUser: 'Admin',
+      localFile: 'filesForUpload/testavatar.png',
+      filesEnvironment
+    })
+    await ui.navigateToGeneralManagementPage({ actorsEnvironment, stepUser: 'Admin' })
+    await ui.resetLogo({ actorsEnvironment, stepUser: 'Admin' })
+    await ui.logOutUser({ actorsEnvironment, stepUser: 'Admin' })
+  })
+})
---
+++ tests/e2e-playwright/steps/ui/adminSettings.ts
@@ -0,0 +1,52 @@
+import { expect } from '@playwright/test'
+import { objects } from '../../../e2e/support'
+import { ActorsEnvironment, FilesEnvironment } from '../../../e2e/support/environment'
+
+export async function navigateToGeneralManagementPage({
+  actorsEnvironment,
+  stepUser
+}: {
+  actorsEnvironment: ActorsEnvironment
+  stepUser: string
+}): Promise<void> {
+  const { page } = actorsEnvironment.getActor({ key: stepUser })
+  const pageObject = new objects.applicationAdminSettings.page.General({ page })
+  await pageObject.navigate()
+  const a11yObject = new objects.a11y.Accessibility({ page })
+  const violations = await a11yObject.getSevereAccessibilityViolations(
+    a11yObject.getSelectors().body
+  )
+  expect(
+    violations,
+    `Found ${violations.length} severe accessibility violations on General Management page`
+  ).toHaveLength(0)
+}
+
+export async function uploadLogoFromLocalPath({
+  actorsEnvironment,
+  stepUser,
+  localFile,
+  filesEnvironment
+}: {
+  actorsEnvironment: ActorsEnvironment
+  stepUser: string
+  localFile: string
+  filesEnvironment: FilesEnvironment
+}): Promise<void> {
+  const { page } = actorsEnvironment.getActor({ key: stepUser })
+  const generalObject = new objects.applicationAdminSettings.General({ page })
+  const logoPath = filesEnvironment.getFile({ name: localFile.split('/').pop() }).path
+  await generalObject.uploadLogo({ path: logoPath })
+}
+
+export async function resetLogo({
+  actorsEnvironment,
+  stepUser
+}: {
+  actorsEnvironment: ActorsEnvironment
+  stepUser: string
+}): Promise<void> {
+  const { page } = actorsEnvironment.getActor({ key: stepUser })
+  const generalObject = new objects.applicationAdminSettings.General({ page })
+  await generalObject.resetLogo()
+}
