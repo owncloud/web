86fdc3b9ad6291e5c62c3bf7bfff6c87f1d28952
Salipa-Gurung <salipagurung@gmail.com>
2026-01-06 17:25:18 +0545

    test: migrate copyMove.feature to playwright test
    add a11y test

--- tests/e2e/support/objects/a11y/actions.ts
+++ tests/e2e/support/objects/a11y/actions.ts
@@ -50,7 +50,9 @@ export const selectors = {
   actionConfirmButton: '.oc-modal-body-actions-confirm',
   contextMenuContainer: '#oc-files-context-menu',
   groupList: '.group-list',
-  editPanel: '.sidebar-panel__body-EditPanel:visible'
+  editPanel: '.sidebar-panel__body-EditPanel:visible',
+  filesTable: '.files-table',
+  filesAppBarActions: '.files-app-bar-actions'
 }
 
 const a11yRuleTags = ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'best-practice']
