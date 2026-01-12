cdc27b4f2b44c6b19dbfc0f7fa2d4a1c649ab5f3
pradip <pradipsubedi360@gmail.com>
2026-01-12 16:33:05 +0545

    test: migrate groups.feature to playwright

---
+++ tests/e2e-playwright/specs/admin-settings/groups.spec.ts
@@ -0,0 +1,97 @@
+import { test, expect } from '@playwright/test'
+import { config } from '../../../e2e/config.js'
+import { ActorsEnvironment, UsersEnvironment } from '../../../e2e/support/environment'
+import { setAccessAndRefreshToken } from '../../helpers/setAccessAndRefreshToken'
+import * as ui from '../../steps/ui/index'
+import * as api from '../../steps/api/api'
+
+test.describe('groups management', () => {
+  let actorsEnvironment
+  const usersEnvironment = new UsersEnvironment()
+
+  test.beforeEach(async ({ browser }) => {
+    actorsEnvironment = new ActorsEnvironment({
+      context: {
+        acceptDownloads: config.acceptDownloads,
+        reportDir: config.reportDir,
+        tracingReportDir: config.tracingReportDir,
+        reportHar: config.reportHar,
+        reportTracing: config.reportTracing,
+        reportVideo: config.reportVideo,
+        failOnUncaughtConsoleError: config.failOnUncaughtConsoleError
+      },
+      browser: browser
+    })
+
+    await setAccessAndRefreshToken(usersEnvironment)
+    await ui.logInUser({ usersEnvironment, actorsEnvironment, stepUser: 'Admin' })
+  })
+
+  test.afterEach(async () => {
+    const admin = usersEnvironment.getUser({ key: 'Admin' })
+    await api.cleanUpGroup(admin)
+  })
+
+  test('admin creates group', async () => {
+    await ui.openApplication({ actorsEnvironment, stepUser: 'Admin', name: 'admin-settings' })
+    await ui.navigateToGroupsManagementPage({ actorsEnvironment, stepUser: 'Admin' })
+    await ui.createGroups({
+      actorsEnvironment,
+      stepUser: 'Admin',
+      groupIds: ['sales', 'security']
+    })
+    expect(
+      await ui.groupsExist({
+        actorsEnvironment,
+        stepUser: 'Admin',
+        groupIds: ['sales', 'security']
+      })
+    ).toBeTruthy()
+
+    await ui.logOutUser({ actorsEnvironment, stepUser: 'Admin' })
+  })
+
+  test('admin deletes group', async () => {
+    await api.createGroups({
+      groupIds: ['sales', 'security', 'finance'],
+      admin: usersEnvironment.getUser({ key: 'Admin' })
+    })
+    await ui.openApplication({ actorsEnvironment, stepUser: 'Admin', name: 'admin-settings' })
+    await ui.navigateToGroupsManagementPage({ actorsEnvironment, stepUser: 'Admin' })
+    await ui.deleteGroups({
+      actorsEnvironment,
+      stepUser: 'Admin',
+      actionType: 'context menu',
+      groupsToBeDeleted: ['sales']
+    })
+
+    expect(
+      await ui.groupsExist({
+        actorsEnvironment,
+        stepUser: 'Admin',
+        groupIds: ['sales']
+      })
+    ).toBeFalsy()
+
+    await ui.logOutUser({ actorsEnvironment, stepUser: 'Admin' })
+  })
+
+  test('edit groups', async () => {
+    await api.createGroups({
+      groupIds: ['sales'],
+      admin: usersEnvironment.getUser({ key: 'Admin' })
+    })
+    await ui.openApplication({ actorsEnvironment, stepUser: 'Admin', name: 'admin-settings' })
+    await ui.navigateToGroupsManagementPage({ actorsEnvironment, stepUser: 'Admin' })
+    await ui.changeGroup({
+      actorsEnvironment,
+      stepUser: 'Admin',
+      key: 'sales',
+      attribute: 'displayName',
+      value: 'a renamed group',
+      action: 'context-menu'
+    })
+
+    await ui.logOutUser({ actorsEnvironment, stepUser: 'Admin' })
+  })
+})
