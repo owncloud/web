33d4ab53d0ffa6c4c7e3491548a9e09c78a301f1
Niraj Acharya <nirajacharyacode@gmail.com>
2026-01-12 14:19:34 +0545

    test: convert participantManagement.feature test to playwright

--- tests/e2e-playwright/steps/ui/spaces.ts
+++ tests/e2e-playwright/steps/ui/spaces.ts
@@ -1,6 +1,8 @@
-import { ActorsEnvironment } from '../../../e2e/support/environment'
+import { ActorsEnvironment, UsersEnvironment } from '../../../e2e/support/environment'
 import { objects } from '../../../e2e/support'
 import { Space } from '../../../e2e/support/types'
+import { Locator } from '@playwright/test'
+import { getDynamicRoleIdByName, ResourceType } from '../../../e2e/support/api/share/share'
 
 export async function navigateToPersonalSpacePage({
   actorsEnvironment,
@@ -56,4 +58,82 @@ export async function createProjectSpace({
   const { page } = actorsEnvironment.getActor({ key: stepUser })
   const spacesObject = new objects.applicationFiles.Spaces({ page })
   await spacesObject.create({ key: id || name, space: { name: name, id: id } as unknown as Space })
+}
+
+export async function navigateToTrashbin({
+  actorsEnvironment,
+  stepUser,
+  space
+}: {
+  actorsEnvironment: ActorsEnvironment
+  stepUser: string
+  space?: string
+}): Promise<void> {
+  const { page } = actorsEnvironment.getActor({ key: stepUser })
+  const pageObject = new objects.applicationFiles.page.trashbin.Overview({ page })
+  await pageObject.navigate()
+  if (space) {
+    const trashbinObject = new objects.applicationFiles.Trashbin({ page })
+    await trashbinObject.open(space)
+  }
+}
+
+export async function getSpaceLocator({
+  actorsEnvironment,
+  stepUser,
+  space
+}: {
+  actorsEnvironment: ActorsEnvironment
+  stepUser: string
+  actionType: string
+  space?: string
+}): Promise<Locator> {
+  const { page } = actorsEnvironment.getActor({ key: stepUser })
+  const spacesObject = new objects.applicationFiles.Spaces({ page })
+  return await spacesObject.getSpaceLocator(space)
+}
+
+export async function changeRoles({
+  actorsEnvironment,
+  usersEnvironment,
+  stepUser,
+  role,
+  sharee
+}: {
+  actorsEnvironment: ActorsEnvironment
+  usersEnvironment: UsersEnvironment
+  stepUser: string
+  role: string
+  sharee: string
+}): Promise<void> {
+  const { page } = actorsEnvironment.getActor({ key: stepUser })
+  const spacesObject = new objects.applicationFiles.Spaces({ page })
+  const sharer = usersEnvironment.getUser({ key: stepUser })
+
+  const roleId = await getDynamicRoleIdByName(sharer, role, 'space' as ResourceType)
+  const member = {
+    collaborator: usersEnvironment.getUser({ key: sharee }),
+    role: roleId
+  }
+  await spacesObject.changeRoles({ users: [member] })
+}
+export async function removeAccessToMember({
+  actorsEnvironment,
+  usersEnvironment,
+  stepUser
+}: {
+  actorsEnvironment: ActorsEnvironment
+  usersEnvironment: UsersEnvironment
+  stepUser: string
+}): Promise<void> {
+  const { page } = actorsEnvironment.getActor({ key: stepUser })
+  const spacesObject = new objects.applicationFiles.Spaces({ page })
+  await spacesObject.removeAccessToMember({
+    users: [
+      {
+        collaborator: usersEnvironment.getUser({ key: stepUser })
+      }
+    ],
+    removeOwnSpaceAccess: true
+  })
 }
