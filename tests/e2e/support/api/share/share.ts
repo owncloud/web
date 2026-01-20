d3b0b0c216f26d6c40a02469481f807e1f4fd63e
Julian Koberg <julian.koberg@kiteworks.com>
2026-01-20 16:10:34 +0100

    fix: adjust sharing-roles e2e test
    Signed-off-by: Julian Koberg <julian.koberg@kiteworks.com>

--- tests/e2e/support/api/share/share.ts
+++ tests/e2e/support/api/share/share.ts
@@ -36,6 +36,7 @@ export const shareRoles: Readonly<{
   'Can upload': string
   'Can manage': string
   'Can edit': string
+  'Can edit with versions and trashbin': string
   'Can edit without versions': string
   'Can view': string
   'Secret File Drop': string
@@ -46,6 +47,7 @@ export const shareRoles: Readonly<{
   'Can upload': 'contributor',
   'Can manage': 'manager',
   'Can edit': 'editor',
+  'Can edit with versions and trashbin': 'editor',
   'Can edit without versions': 'editor',
   'Can view': 'viewer',
   'Secret File Drop': 'uploader',
@@ -72,12 +74,12 @@ export const linkShareRoles: Readonly<{
 const defaultRoles = {
   viewer: { id: 'b1e2218d-eef8-4d4c-b82d-0f1a1b48f3b5', displayName: 'Can view' },
   'space viewer': { id: 'a8d5fe5e-96e3-418d-825b-534dbdf22b99', displayName: 'Can view (space)' },
-  editor: { id: 'fb6c3e19-e378-47e5-b277-9732f9de6e21', displayName: 'Can edit without versions' },
+  editor: { id: 'fb6c3e19-e378-47e5-b277-9732f9de6e21', displayName: 'Can edit with trashbin' },
   'file editor': {
     id: '2d00ce52-1fc2-4dbc-8b95-a73b73395f5a',
-    displayName: 'Can edit without versions (file)'
+    displayName: 'Can edit (file)'
   },
-  'space editor': { id: '58c63c02-1d89-4572-916a-870abc5a1b7d', displayName: 'Can edit (space)' },
+  'space editor': { id: '58c63c02-1d89-4572-916a-870abc5a1b7d', displayName: 'Can edit with versions and trashbin' },
   manager: { id: '312c0871-5ef7-4b3a-85b6-0e4074c64049', displayName: 'Can Manage' },
   uploader: { id: '1c996275-f1c9-4e71-abdf-a42f6495e960', displayName: 'Can upload' },
   'secure viewer': { id: 'aa97fe03-7980-45ac-9e50-b325749fd7e6', displayName: 'Can view (secure)' },
