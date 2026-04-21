f31892a603247449214beb0eb5e2e34ed01ecb71
Niraj Acharya <nirajacharyacode@google.com>
2026-04-21 14:21:38 +0545

    test: remove internal link test

--- tests/e2e/support/store/link.ts
+++ tests/e2e/support/store/link.ts
@@ -3,7 +3,6 @@ import { Link } from '../types'
 export const createdLinkStore = new Map<string, Link>()
 
 export const roleDisplayText: Record<string, string> = {
-  'Invited people': 'Only for invited people',
   'Can view': 'Anyone with the link can view',
   'Can upload': 'Anyone with the link can upload',
   'Can edit': 'Anyone with the link can edit',
