877412e220da0a3940b3620c1b6e552b8fd28943
Matteo <matteo.sonnenholzner@kiteworks.com>
2026-05-22 14:41:37 +0200

    feat: add filter to type

--- packages/web-app-admin-settings/src/views/Spaces.vue
+++ packages/web-app-admin-settings/src/views/Spaces.vue
@@ -126,7 +126,7 @@ const loadResourcesTask = useTask(function* (signal) {
       sharesStore.graphRoles,
       {
         orderBy: 'name asc',
-        filter: 'driveType'
+        filter: 'driveType eq project'
       },
       { signal }
     )
